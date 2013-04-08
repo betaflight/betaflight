d61df6fc8241944cb9cd332913ce7b60413d67b4
cTn <ctndesigner@gmail.com>
2013-04-09 01:49:49 +0200

    pid tuning improvements

--- tabs/pid_tuning.js
+++ tabs/pid_tuning.js
@@ -1,4 +1,5 @@
 function tab_initialize_pid_tuning() {
+    // Fill in the data from PIDs array
     var needle = 0;
 
     var i = 0;
@@ -53,6 +54,50 @@ function tab_initialize_pid_tuning() {
     $('.pid_tuning .MAG input').each(function() {
         $(this).val(PIDs[needle][i++].toFixed(2));       
     });
-    needle++;    
+    needle++;  
+
+    // UI Hooks
+    
+    $('.pid_tuning input').change(function() {
+        // if any of the fields changed, unlock update button
+        $('a.update').addClass('active');
+    });
+    
+    $('a.update').click(function() {
+        if ($(this).hasClass('active')) {
+            // Catch all the changes and stuff the inside PIDs array
+            var needle_main = 0;
+            var needle_secondary = 0;
+            
+            $('.pid_tuning input').each(function() {
+                PIDs[needle_main][needle_secondary] = parseFloat($(this).val());
+                needle_secondary++;
+                
+                // exceptions (required for the "shorter" PID arrays, 2 fields, 1 field, etc)
+                if (needle_main == 4) {
+                    if (needle_secondary >= 2) {
+                        needle_main++;
+                        needle_secondary = 0;
+                    }
+                } else if (needle_main == 8) {
+                    if (needle_secondary >= 1) {
+                        needle_main++;
+                        needle_secondary = 0;
+                    }
+                } else {
+                    if (needle_secondary >= 3) {
+                        needle_main++;
+                        needle_secondary = 0;
+                    }
+                }
+            });
+            
+            // Send over the changes
+            
+            
+            // remove the active status
+            $(this).removeClass('active');
+        }
+    });
     
 }
