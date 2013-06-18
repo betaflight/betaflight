2b4e5642b336baed9c2da28ed21a130cb7bc6b8d
cTn <ctndesigner@gmail.com>
2013-06-18 20:13:26 +0200

    experimental storage of last used port in GUI

--- js/serial_backend.js
+++ js/serial_backend.js
@@ -145,6 +145,18 @@ $(document).ready(function() {
                         text: port
                     }));        
                 });
+                
+                chrome.storage.local.get('last_used_port', function(result) {
+                    // if last_used_port was set, we try to select it
+                    if (typeof result.last_used_port != 'undefined') {
+                        // check if same port exists, if it does, select it
+                        ports.forEach(function(port) {
+                            if (port == result.last_used_port) {
+                                $(port_picker).val(result.last_used_port);
+                            }
+                        });
+                    }
+                });
             } else {
                 $(port_picker).append($("<option/>", {
                     value: 0,
@@ -218,6 +230,19 @@ function onOpen(openInfo) {
     if (connectionId != -1) {
         console.log('Connection was opened with ID: ' + connectionId);
         
+        // save selected port with chrome.storage if the port differs
+        chrome.storage.local.get('last_used_port', function(result) {
+            if (typeof result.last_used_port != 'undefined') {
+                if (result.last_used_port != selected_port) {
+                    // last used port doesn't match the one found in local db, we will store the new one
+                    chrome.storage.local.set({'last_used_port': selected_port}, function() {
+                        // Debug message is currently disabled (we dont need to spam the console log with that)
+                        // console.log('Last selected port was saved in chrome.storage.');
+                    });
+                }
+            }
+        });
+
         connection_delay = setTimeout(function() {
             // start polling
             serial_poll = setInterval(readPoll, 10);
