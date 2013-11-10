b21ff9ce1b4b1c35b9b0f301db86ed86845196e9
cTn <ctndesigner@gmail.com>
2013-11-10 22:53:54 +0100

    polished

--- tabs/servos.js
+++ tabs/servos.js
@@ -18,7 +18,7 @@ function tab_initialize_servos() {
                     
                     process_directions('YAW', 5, 0);
                     
-                    process_servos('Yaw Servo', '', SERVO_CONFIG[5], false);
+                    process_servos('Yaw Servo', '', 5, false);
                     
                     servos = [5];
                     break;
@@ -31,8 +31,8 @@ function tab_initialize_servos() {
                     process_directions('L NICK', 4, 0);
                     process_directions('R NICK', 5, 0);
                     
-                    process_servos('Left Servo', '', SERVO_CONFIG[4], false);
-                    process_servos('Right Servo', '', SERVO_CONFIG[5], false);
+                    process_servos('Left Servo', '', 4, false);
+                    process_servos('Right Servo', '', 5, false);
                     
                     servos = [4, 5];
                     break;
@@ -58,8 +58,8 @@ function tab_initialize_servos() {
                     process_directions('L NICK', 3, 0);
                     process_directions('R NICK', 4, 0);
                     
-                    process_servos('Left Wing', '', SERVO_CONFIG[3], false);
-                    process_servos('Right Wing', '', SERVO_CONFIG[4], false);
+                    process_servos('Left Wing', '', 3, false);
+                    process_servos('Right Wing', '', 4, false);
                     
                     servos = [3, 4];
                     break;
@@ -85,8 +85,8 @@ function tab_initialize_servos() {
                     process_directions('PITCH', 4, 0);
                     process_directions('ROLL', 5, 0);
                     
-                    process_servos('Roll', '', SERVO_CONFIG[5], false);
-                    process_servos('Nick', '', SERVO_CONFIG[4], false);
+                    process_servos('Roll', '', 5, false);
+                    process_servos('Nick', '', 4, false);
                     
                     servos = [4, 5];
                     break;
@@ -94,10 +94,10 @@ function tab_initialize_servos() {
                     // looking ok so far
                     model.html('Singlecopter');
                     
-                    process_servos('Right', 'R YAW', SERVO_CONFIG[3], true);
-                    process_servos('Left', 'L YAW', SERVO_CONFIG[4], true);
-                    process_servos('Front', 'F YAW', SERVO_CONFIG[5], true);
-                    process_servos('Rear', 'YAW', SERVO_CONFIG[6], true);
+                    process_servos('Right', 'R YAW', 3, true);
+                    process_servos('Left', 'L YAW', 4, true);
+                    process_servos('Front', 'F YAW', 5, true);
+                    process_servos('Rear', 'YAW', 6, true);
                     
                     servos = [3, 4, 5, 6];
                     break;
@@ -111,8 +111,7 @@ function tab_initialize_servos() {
     $('a.update').click(function() {
         // update bitfields
         $('div.tab-servos table.directions tr:not(".main")').each(function() {
-            var info = $('select', this).data();
-            info = info.info; // move info one level higher
+            var info = $('select', this).data('info');
             var val = parseInt($('select', this).val());
             
             // in this stage we need to know which bitfield and which bitposition needs to be flipped
@@ -121,29 +120,28 @@ function tab_initialize_servos() {
         });
         
         // update the rest
-        var itter = 0;
         $('div.tab-servos table.fields tr:not(".main")').each(function() {
+            var info = $(this).data('info');
+            
             if ($('.middle input', this).is(':disabled')) {
                 var val = $('.channel input:checked', this).index();
                 
-                SERVO_CONFIG[servos[0] + itter].middle = parseInt(val);
+                SERVO_CONFIG[info.obj].middle = parseInt(val);
             } else {
-                SERVO_CONFIG[servos[0] + itter].middle = parseInt($('.middle input', this).val());
+                SERVO_CONFIG[info.obj].middle = parseInt($('.middle input', this).val());
             }
             
-            SERVO_CONFIG[servos[0] + itter].min = parseInt($('.min input', this).val());
-            SERVO_CONFIG[servos[0] + itter].max = parseInt($('.max input', this).val());
+            SERVO_CONFIG[info.obj].min = parseInt($('.min input', this).val());
+            SERVO_CONFIG[info.obj].max = parseInt($('.max input', this).val());
 
             // update rate if direction fields exist
             if ($('.direction input', this).length) {
-                if ($('.direction input:first', this).is(':checked')) SERVO_CONFIG[servos[0] + itter].rate = bit_set(SERVO_CONFIG[servos[0] + itter].rate, 0);
-                else SERVO_CONFIG[servos[0] + itter].rate = bit_clear(SERVO_CONFIG[servos[0] + itter].rate, 0);
+                if ($('.direction input:first', this).is(':checked')) SERVO_CONFIG[info.obj].rate = bit_set(SERVO_CONFIG[info.obj].rate, 0);
+                else SERVO_CONFIG[info.obj].rate = bit_clear(SERVO_CONFIG[info.obj].rate, 0);
                 
-                if ($('.direction input:last', this).is(':checked')) SERVO_CONFIG[servos[0] + itter].rate = bit_set(SERVO_CONFIG[servos[0] + itter].rate, 1);
-                else SERVO_CONFIG[servos[0] + itter].rate = bit_clear(SERVO_CONFIG[servos[0] + itter].rate, 1);
+                if ($('.direction input:last', this).is(':checked')) SERVO_CONFIG[info.obj].rate = bit_set(SERVO_CONFIG[info.obj].rate, 1);
+                else SERVO_CONFIG[info.obj].rate = bit_clear(SERVO_CONFIG[info.obj].rate, 1);
             }
-            
-            itter++;
         });
         
         // send settings over to mcu
@@ -197,9 +195,9 @@ function process_servos(name, alternate, obj, directions) {
     $('div.tab-servos table.fields').append('\
         <tr> \
             <td style="text-align: center">' + name + '</td>\
-            <td class="middle"><input type="number" min="1000" max="2000" value="' + obj.middle +'" /></td>\
-            <td class="min"><input type="number" min="1000" max="2000" value="' + obj.min +'" /></td>\
-            <td class="max"><input type="number" min="1000" max="2000" value="' + obj.max +'" /></td>\
+            <td class="middle"><input type="number" min="1000" max="2000" value="' + SERVO_CONFIG[obj].middle +'" /></td>\
+            <td class="min"><input type="number" min="1000" max="2000" value="' + SERVO_CONFIG[obj].min +'" /></td>\
+            <td class="max"><input type="number" min="1000" max="2000" value="' + SERVO_CONFIG[obj].max +'" /></td>\
             <td class="channel">\
                 <input type="checkbox"/>\
                 <input type="checkbox"/>\
@@ -217,22 +215,21 @@ function process_servos(name, alternate, obj, directions) {
         </tr> \
     ');    
     
-    if (obj.middle <= 7) {
+    if (SERVO_CONFIG[obj].middle <= 7) {
         $('div.tab-servos table.fields tr:last td.middle input').prop('disabled', true);
-        $('div.tab-servos table.fields tr:last td.channel').find('input').eq(obj.middle).prop('checked', true);
+        $('div.tab-servos table.fields tr:last td.channel').find('input').eq(SERVO_CONFIG[obj].middle).prop('checked', true);
     }
     
     if (directions == true) {
-        $('div.tab-servos table.fields tr:last td.direction input:first').prop('checked', bit_check(obj.rate, 0));
-        $('div.tab-servos table.fields tr:last td.direction input:last').prop('checked', bit_check(obj.rate, 1));
-        
-        // store additional data
-        //$('div.tab-servos table.fields tr:last td.direction').data('info', {});
+        $('div.tab-servos table.fields tr:last td.direction input:first').prop('checked', bit_check(SERVO_CONFIG[obj].rate, 0));
+        $('div.tab-servos table.fields tr:last td.direction input:last').prop('checked', bit_check(SERVO_CONFIG[obj].rate, 1));
     } else if (directions == 2) {
         // reserved for rate
     } else {
         $('div.tab-servos table.fields tr:last td.direction').html('');
     }
+    
+    $('div.tab-servos table.fields tr:last').data('info', {'obj': obj});
     
     // UI hooks
     $('div.tab-servos table.fields tr:last td.channel').find('input').click(function() {
