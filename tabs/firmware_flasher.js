f15f749ed3b025401b458b2820c576859f5d44c4
cTn <ctndesigner@gmail.com>
2013-11-13 09:09:02 +0100

    initial code for reading local hex file

--- tabs/firmware_flasher.js
+++ tabs/firmware_flasher.js
@@ -2,7 +2,38 @@ function tab_initialize_firmware_flasher() {
     ga_tracker.sendAppView('Firmware Flasher');
     GUI.active_tab = 'firmware_flasher';
     
+    var intel_hex = false;
+    
     $('#content').load("./tabs/firmware_flasher.html", function() {
-        // empty for now
+        // UI Hooks
+        $('a.load_file').click(function() {
+            chrome.fileSystem.chooseEntry({type: 'openFile', accepts: [{extensions: ['hex']}]}, function(fileEntry) {
+                if (!fileEntry) {
+                    // no "valid" file selected/created, aborting
+                    console.log('No valid file selected, aborting');
+                    return;
+                }
+                
+                chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
+                    console.log('Loading file from: ' + path);
+                    
+                    fileEntry.file(function(file) {
+                        var reader = new FileReader();
+
+                        reader.onerror = function (e) {
+                            console.error(e);
+                        };
+                        
+                        reader.onloadend = function(e) {
+                            console.log('File loaded');
+                            
+                            intel_hex = e.target.result;
+                        };
+
+                        reader.readAsText(file);
+                    });
+                });
+            });
+        });
     });
 }
