da1ecbbbde6bb9e12b1adf8efe195037dbf7b131
sbaron <s.baron@server-5.net>
2013-11-28 12:50:26 +0100

    Restored previous version, but remove inline declaration in drv_serial.c to avoid linker errors

--- src/drv_serial.c
+++ src/drv_serial.c
@@ -8,4 +8,33 @@ void serialPrint(serialPort_t *instance, const char *str)
     }
 }
 
+uint32_t serialGetBaudRate(serialPort_t *instance)
+{
+    return instance->baudRate;
+}
+
+void serialWrite(serialPort_t *instance, uint8_t ch)
+{
+    instance->vTable->serialWrite(instance, ch);
+}
+
+uint8_t serialTotalBytesWaiting(serialPort_t *instance)
+{
+    return instance->vTable->serialTotalBytesWaiting(instance);
+}
+
+uint8_t serialRead(serialPort_t *instance)
+{
+    return instance->vTable->serialRead(instance);
+}
+
+void serialSetBaudRate(serialPort_t *instance, uint32_t baudRate)
+{
+    instance->vTable->serialSetBaudRate(instance, baudRate);
+}
+
+bool isSerialTransmitBufferEmpty(serialPort_t *instance)
+{
+    return instance->vTable->isSerialTransmitBufferEmpty(instance);
+}
 
