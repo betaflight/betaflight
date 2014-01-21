b7458ee0e020fb251e1c5edf435903da7b390cb6
cTn <ctndesigner@gmail.com>
2014-01-21 22:46:54 +0100

    bugfix

--- tabs/motor_outputs.js
+++ tabs/motor_outputs.js
@@ -42,29 +42,28 @@ function tab_initialize_motor_outputs() {
     
     // enable Motor data pulling
     GUI.interval_add('motor_poll', function() {
-        // Request New Motor data
-        send_message(MSP_codes.MSP_STATUS, MSP_codes.MSP_STATUS);
-        send_message(MSP_codes.MSP_MOTOR, MSP_codes.MSP_MOTOR);
-        
-        // Update UI
-        for (var i = 0; i < MOTOR_DATA.length; i++) {
-            MOTOR_DATA[i] -= 1000; 
-            var margin_top = 220.0 - (MOTOR_DATA[i] * 0.22);
-            var height = (MOTOR_DATA[i] * 0.22);
-            var color = parseInt(MOTOR_DATA[i] * 0.256);
-            $('.motor-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
-        } 
-
-        // Request New Servo data
-        send_message(MSP_codes.MSP_SERVO, MSP_codes.MSP_SERVO);
-        
-        // Update UI
-        for (var i = 0; i < SERVO_DATA.length; i++) {
-            SERVO_DATA[i] -= 1000; 
-            var margin_top = 220.0 - (SERVO_DATA[i] * 0.22);
-            var height = (SERVO_DATA[i] * 0.22);
-            var color = parseInt(SERVO_DATA[i] * 0.256);
-            $('.servo-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
-        } 
+        // Request New data
+        send_message(MSP_codes.MSP_STATUS, MSP_codes.MSP_STATUS, false, function() {
+            send_message(MSP_codes.MSP_MOTOR, MSP_codes.MSP_MOTOR, false, function() {
+                send_message(MSP_codes.MSP_SERVO, MSP_codes.MSP_SERVO, false, function() {
+                    // Update UI
+                    for (var i = 0; i < MOTOR_DATA.length; i++) {
+                        MOTOR_DATA[i] -= 1000; 
+                        var margin_top = 220.0 - (MOTOR_DATA[i] * 0.22);
+                        var height = (MOTOR_DATA[i] * 0.22);
+                        var color = parseInt(MOTOR_DATA[i] * 0.256);
+                        $('.motor-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
+                    }
+                    
+                    for (var i = 0; i < SERVO_DATA.length; i++) {
+                        SERVO_DATA[i] -= 1000; 
+                        var margin_top = 220.0 - (SERVO_DATA[i] * 0.22);
+                        var height = (SERVO_DATA[i] * 0.22);
+                        var color = parseInt(SERVO_DATA[i] * 0.256);
+                        $('.servo-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
+                    } 
+                });
+            });
+        });
     }, 50);
 }
