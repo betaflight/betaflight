7bde2a159e258f9440ab688250731584eb8ddf5c
cTn <ctndesigner@gmail.com>
2014-02-07 21:55:06 +0100

    new ihex parser

--- js/workers/hex_parser.js
+++ js/workers/hex_parser.js
@@ -12,60 +12,51 @@ function read_hex_file(data) {
     var hexfile_valid = true; // if any of the crc checks failed, this variable flips to false
     
     var result = {
-        data:                       [],
-        end_of_file:                false,
-        extended_linear_address:    [],
-        start_linear_address:       0,
-        bytes:                      0
+        data:                   [],
+        end_of_file:            false,
+        bytes_total:            0,
+        start_linear_address:   0
     };
     
-    var next_address_pos = 0;
+    var extended_linear_address = 0;
+    var next_address = 0;
     
-    for (var i = 0; i < data.length; i++) {
-        var byte_count = parseInt(data[i].substr(1, 2), 16); // each byte is represnted by two chars
-        var address = data[i].substr(3, 4);
-        var record_type = parseInt(data[i].substr(7, 2), 16); // also converting from hex to decimal
-        var content = data[i].substr(9, byte_count * 2);
-        var checksum = parseInt(data[i].substr(9 + byte_count * 2, 2), 16); // also converting from hex to decimal (this is a 2's complement value)
-       
+    for (var i = 0; i < data.length && hexfile_valid; i++) {
+        // each byte is represnted by two chars
+        var byte_count = parseInt(data[i].substr(1, 2), 16);
+        var address = parseInt(data[i].substr(3, 4), 16);
+        var record_type = parseInt(data[i].substr(7, 2), 16);
+        var content = data[i].substr(9, byte_count * 2); // still in string format
+        var checksum = parseInt(data[i].substr(9 + byte_count * 2, 2), 16); // (this is a 2's complement value)
+        
         switch (record_type) {
-            case 0x00: // data record
-                // fix "holes" if there are any
-                if (parseInt(address, 16) != next_address_pos) {
-                    var difference = parseInt(address, 16) - (next_address_pos);
-                    
-                    // fill in the difference
-                    for (var x = 0; x < difference; x++) {
-                        result.data.push(0xFF);
-                        result.bytes++;
-                    }
-                    
-                    console.log('HEX_PARSER - Address hole detected, expected: ' + next_address_pos + ', received: ' + parseInt(address, 16) + ', filling: ' + difference + ' bytes');
+            case 0x00: // data record            
+                if (address != next_address || next_address == 0) {
+                    result.data.push({'address': extended_linear_address + address, 'bytes': 0, 'data': []});
                 }
                 
-                // update for next comparison
-                next_address_pos = parseInt(address, 16) + byte_count;
+                // store address for next comparison
+                next_address = address + byte_count;
                 
                 // process data
-                var crc = byte_count + parseInt(address.substr(0, 2), 16) + parseInt(address.substr(2, 2), 16) + record_type;
-                for (var needle = 0; needle < byte_count * 2; needle += 2) {
+                var crc = byte_count + parseInt(data[i].substr(3, 2), 16) + parseInt(data[i].substr(5, 2), 16) + record_type;
+                for (var needle = 0; needle < byte_count * 2; needle += 2) { // * 2 because of 2 hex chars per 1 byte
                     var num = parseInt(content.substr(needle, 2), 16); // get one byte in hex and convert it to decimal
-                    result.data.push(num);
+                    var data_block = result.data.length - 1;
+                    
+                    result.data[data_block].data.push(num);
+                    result.data[data_block].bytes++; 
                     
                     crc += num;
-                    result.bytes++;
+                    result.bytes_total++;
                 }
                 
-                // change crc to 2's complement (same as checksum)
-                crc = ~crc + 1;
-                crc &= 0xFF;
+                // change crc to 2's complement
+                crc = (~crc + 1) & 0xFF;
                 
                 // verify 
                 if (crc != checksum) {
                     hexfile_valid = false;
-                    
-                    // break out of the for loop as crc is wrong anyway, we dont need to process any more data
-                    i = data.length;
                 }
                 break;
             case 0x01: // end of file record
@@ -79,30 +70,15 @@ function read_hex_file(data) {
                 break;
             case 0x03: // start segment address record
                 // not implemented
-                console.log('start segment address record found - NOT IMPLEMENTED !!!');
-                break;
-            case 0x04: // extended linear address record                
-                var extended_linear_address = (parseInt(content.substr(0, 2), 16) << 24) | parseInt(content.substr(2, 2), 16) << 16;
-                result.extended_linear_address.push(extended_linear_address);
-                
-                if (next_address_pos != 0) { // dont execute the first time
-                    extended_linear_address -= 0x08000000;
-                    var difference = extended_linear_address - next_address_pos;
-                    
-                    // fill in the difference
-                    for (var x = 0; x < difference; x++) {
-                        result.data.push(0xFF);
-                        result.bytes++;
-                    }
-                    
-                    if (difference > 0) console.log('HEX_PARSER - Address hole detected (changing linear address), expected: ' + next_address_pos + ', received: ' + extended_linear_address + ', filling: ' + difference + ' bytes');
-                    
-                    // reset some variables
-                    next_address_pos = 0;
+                if (parseInt(content, 16) != 0) { // ignore if segment is 0
+                    console.log('start segment address record found - NOT IMPLEMENTED !!!');
                 }
                 break;
+            case 0x04: // extended linear address record
+                extended_linear_address = (parseInt(content.substr(0, 2), 16) << 24) | parseInt(content.substr(2, 2), 16) << 16;
+                break;
             case 0x05: // start linear address record
-                result.start_linear_address = (parseInt(content.substr(0, 2), 16) << 24) | (parseInt(content.substr(2, 2), 16) << 16) | (parseInt(content.substr(4, 2), 16) << 8) | parseInt(content.substr(6, 2), 16);
+                result.start_linear_address = parseInt(content, 16)
                 break;
         }
     }
