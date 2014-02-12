8e97a68cb1145bf52cdd442af09b7ae454623afc
cTn <ctndesigner@gmail.com>
2014-02-12 10:43:21 +0100

    bumping up bitrate for testing

--- js/stm32.js
+++ js/stm32.js
@@ -61,7 +61,7 @@ STM32_protocol.prototype.connect = function(hex) {
                 flashing_bitrate = 921600;
                 break;
             case 'MacOS':
-                flashing_bitrate = 38400; /* locked to max supported until new serial api is fixed */
+                flashing_bitrate = 921600; /* max of 38400 was fixed in dev branch, 33 still broken */
                 break;
             case 'ChromeOS':
             case 'Linux':
