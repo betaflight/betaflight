219a0dfdfa8caa0267949ce10a9333c458cbbb13
cTn <ctndesigner@gmail.com>
2014-03-02 23:29:19 +0100

    motor UI bugfixes, bug reported by mastag25

--- tabs/motor_outputs.js
+++ tabs/motor_outputs.js
@@ -3,94 +3,102 @@ function tab_initialize_motor_outputs() {
     GUI.active_tab = 'motor_outputs';
     
     send_message(MSP_codes.MSP_MISC, MSP_codes.MSP_MISC, false, function() {
-        $('#content').load("./tabs/motor_outputs.html", function() {
-            // if CAP_DYNBALANCE is true
-            if (bit_check(CONFIG.capability, 2)) {
-                $('div.motor_testing').show();
-            }
-            
-            $('input.min').val(MISC.mincommand);
-            $('input.max').val(MISC.maxthrottle);
-            
-            
-            $('div.sliders input').prop('min', MISC.mincommand);
-            $('div.sliders input').prop('max', MISC.maxthrottle);
-            $('div.sliders input').val(MISC.mincommand);
-            $('div.values li:not(:last)').html(MISC.mincommand);
-            
-            // UI hooks
-            $('div.sliders input:not(.master)').change(function() {
-                var index = $(this).index();
-                
-                $('div.values li').eq(index).html($(this).val());
-                
-                // send data to mcu
-                var buffer_out = [];
-                
-                for (var i = 0; i < 8; i++) {
-                    var val = parseInt($('div.sliders input').eq(i).val());
-                    
-                    buffer_out.push(lowByte(val));
-                    buffer_out.push(highByte(val));
+        send_message(MSP_codes.MSP_MOTOR, MSP_codes.MSP_MOTOR, false, function() {
+            $('#content').load("./tabs/motor_outputs.html", function() {
+                // if CAP_DYNBALANCE is true
+                if (bit_check(CONFIG.capability, 2)) {
+                    $('div.motor_testing').show();
                 }
                 
-                send_message(MSP_codes.MSP_SET_MOTOR, buffer_out);
-            });
-            
-            $('div.sliders input.master').change(function() {
-                var val = $(this).val();
+                var number_of_valid_outputs = (MOTOR_DATA.indexOf(0) > -1) ? MOTOR_DATA.indexOf(0) : 8;
                 
-                $('div.sliders input').val(val);
-                $('div.sliders input:not(:last):first').change();
-            });
-            
-            $('div.notice input[type="checkbox"]').change(function() {
-                if ($(this).is(':checked')) {
-                    $('div.sliders input, .notice input[type="number"]').prop('disabled', false);
-                } else {
-                    // disable sliders / min max
-                    $('div.sliders input, .notice input[type="number"]').prop('disabled', true);
+                $('input.min').val(MISC.mincommand);
+                $('input.max').val(MISC.maxthrottle);
+                
+                
+                $('div.sliders input').prop('min', MISC.mincommand);
+                $('div.sliders input').prop('max', MISC.maxthrottle);
+                $('div.sliders input').val(MISC.mincommand);
+                $('div.values li:not(:last)').html(MISC.mincommand);
+                
+                // UI hooks
+                $('div.sliders input:not(.master)').change(function() {
+                    var index = $(this).index();
                     
-                    // change all values to default
-                    $('div.sliders input').val(1000);
-                    $('div.values li:not(:last)').html(1000);
+                    $('div.values li').eq(index).html($(this).val());
                     
-                    // trigger change event so values are sent to mcu
-                    $('div.sliders input').change();
-                }
-            });
-            
-            // enable Motor data pulling
-            GUI.interval_add('motor_poll', function() {
-                // Request New data
-                send_message(MSP_codes.MSP_STATUS, MSP_codes.MSP_STATUS, false, function() {
-                    send_message(MSP_codes.MSP_MOTOR, MSP_codes.MSP_MOTOR, false, function() {
-                        send_message(MSP_codes.MSP_SERVO, MSP_codes.MSP_SERVO, false, function() {
-                            // Update UI
-                            
-                            var block_height = $('div.m-block:first').height();
-                            
-                            for (var i = 0; i < MOTOR_DATA.length; i++) {
-                                MOTOR_DATA[i] -= 1000; 
-                                var margin_top = block_height - (MOTOR_DATA[i] * (block_height / 1000));
-                                var height = (MOTOR_DATA[i] * (block_height / 1000));
-                                var color = parseInt(MOTOR_DATA[i] * 0.256);
+                    // send data to mcu
+                    var buffer_out = [];
+                    
+                    for (var i = 0; i < 8; i++) {
+                        var val = parseInt($('div.sliders input').eq(i).val());
+                        
+                        buffer_out.push(lowByte(val));
+                        buffer_out.push(highByte(val));
+                    }
+                    
+                    send_message(MSP_codes.MSP_SET_MOTOR, buffer_out);
+                });
+                
+                $('div.sliders input.master').change(function() {
+                    var val = $(this).val();
+                    
+                    $('div.sliders input:not(:disabled, :last)').val(val);
+                    $('div.values li:not(:last)').slice(0, number_of_valid_outputs).html(val);
+                    $('div.sliders input:not(:last):first').change();
+                });
+                
+                $('div.notice input[type="checkbox"]').change(function() {
+                    if ($(this).is(':checked')) {
+                        $('div.sliders input').slice(0, number_of_valid_outputs).prop('disabled', false);
+                        
+                        // unlock master slider
+                        $('div.sliders input:last').prop('disabled', false);
+                    } else {
+                        // disable sliders / min max
+                        $('div.sliders input').prop('disabled', true);
+                        
+                        // change all values to default
+                        $('div.sliders input').val(1000);
+                        $('div.values li:not(:last)').html(1000);
+                        
+                        // trigger change event so values are sent to mcu
+                        $('div.sliders input').change();
+                    }
+                });
+                
+                // enable Motor data pulling
+                GUI.interval_add('motor_poll', function() {
+                    // Request New data
+                    send_message(MSP_codes.MSP_STATUS, MSP_codes.MSP_STATUS, false, function() {
+                        send_message(MSP_codes.MSP_MOTOR, MSP_codes.MSP_MOTOR, false, function() {
+                            send_message(MSP_codes.MSP_SERVO, MSP_codes.MSP_SERVO, false, function() {
+                                // Update UI
                                 
-                                $('.motor-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
-                            }
-                            
-                            for (var i = 0; i < SERVO_DATA.length; i++) {
-                                SERVO_DATA[i] -= 1000; 
-                                var margin_top = block_height - (SERVO_DATA[i] * (block_height / 1000));
-                                var height = (SERVO_DATA[i] * (block_height / 1000));
-                                var color = parseInt(SERVO_DATA[i] * 0.256);
+                                var block_height = $('div.m-block:first').height();
                                 
-                                $('.servo-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
-                            } 
+                                for (var i = 0; i < MOTOR_DATA.length; i++) {
+                                    var data = MOTOR_DATA[i] - 1000;
+                                    var margin_top = block_height - (data * (block_height / 1000));
+                                    var height = (data * (block_height / 1000));
+                                    var color = parseInt(data * 0.256);
+                                    
+                                    $('.motor-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
+                                }
+                                
+                                for (var i = 0; i < SERVO_DATA.length; i++) {
+                                    var data = SERVO_DATA[i] - 1000;
+                                    var margin_top = block_height - (data * (block_height / 1000));
+                                    var height = (data * (block_height / 1000));
+                                    var color = parseInt(data * 0.256);
+                                    
+                                    $('.servo-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
+                                } 
+                            });
                         });
                     });
-                });
-            }, 50, true);
+                }, 50, true);
+            });
         });
     });
 }
