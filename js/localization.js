c9d22a9002586ff41078e199a9de043f780c7424
cTn <ctndesigner@gmail.com>
2014-03-20 22:24:49 +0100

    initial changes required for i18n system

---
+++ js/localization.js
@@ -0,0 +1,32 @@
+function localize() {
+    var localized = 0;
+
+    var translate = function(messageID) {
+        localized++;
+
+        return chrome.i18n.getMessage(messageID);
+    };
+
+    $('[i18n]:not(.i18n-replaced').each(function() {
+        var element = $(this);
+
+        element.html(translate(element.attr('i18n')));
+        element.addClass('i18n-replaced');
+    });
+
+    $('[i18n_title]:not(.i18n_title-replaced').each(function() {
+        var element = $(this);
+
+        element.attr('title', translate(element.attr('i18n_title')));
+        element.addClass('i18n_title-replaced');
+    });
+
+    $('[i18n_value]:not(.i18n_value-replaced').each(function() {
+        var element = $(this);
+
+        element.val(translate(element.attr('i18n_value')));
+        element.addClass('i18n_value-replaced');
+    });
+
+    return localized;
+}
