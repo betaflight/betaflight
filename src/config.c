99ff409783cc4b22221a9ef009d72c57dbfc7c01
Johannes Kasberger <schugabe@gmx.at>
2014-04-07 23:07:51 +0200

    increment EEPROM_CONF_VERSION

--- src/config.c
+++ src/config.c
@@ -13,7 +13,7 @@ master_t mcfg;  // master config struct with data independent from profiles
 config_t cfg;   // profile config struct
 const char rcChannelLetters[] = "AERT1234";
 
-static const uint8_t EEPROM_CONF_VERSION = 60;
+static const uint8_t EEPROM_CONF_VERSION = 61;
 static uint32_t enabledSensors = 0;
 static void resetConf(void);
 
