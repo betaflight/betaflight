30d5f13323d733989e8505c503ddb144f6bd2955
Dominic Clifton <me@dominicclifton.name>
2014-04-08 01:17:22 +0100

    Fix LED support for OLIMEXINO and FY90Q targets.

--- src/board.h
+++ src/board.h
@@ -186,6 +186,8 @@ typedef struct baro_t
 
 #define GYRO
 #define ACC
+#define LED0
+#define LED1
 
 #define SENSORS_SET (SENSOR_ACC)
 
@@ -194,11 +196,18 @@ typedef struct baro_t
 #ifdef OLIMEXINO
 // OLIMEXINO
 
+#ifdef OLIMEXINO_UNCUT_LED2_E_JUMPER
 // LED2 is using one of the pwm pins (PWM2), so we must not use PWM2.  @See pwmInit()
 #define LED0_GPIO   GPIOA
-#define LED0_PIN    Pin_1 // D3, PA1/USART2_RTS/ADC1/TIM2_CH3 - "LED2" on silkscreen, Yellow
+#define LED0_PIN    GPIO_Pin_1 // D3, PA1/USART2_RTS/ADC1/TIM2_CH3 - "LED2" on silkscreen, Yellow
+#define LED0
+#endif
+
+#ifdef OLIMEXINO_UNCUT_LED1_E_JUMPER
 #define LED1_GPIO   GPIOA
-#define LED1_PIN    Pin_5 // D13, PA5/SPI1_SCK/ADC5 - "LED1" on silkscreen, Green
+#define LED1_PIN    GPIO_Pin_5 // D13, PA5/SPI1_SCK/ADC5 - "LED1" on silkscreen, Green
+#define LED1
+#endif
 
 #define GYRO
 #define ACC
@@ -207,6 +216,7 @@ typedef struct baro_t
 
 #else
 // Afroflight32
+
 #define LED0_GPIO   GPIOB
 #define LED0_PIN    Pin_3 // PB3 (LED)
 #define LED1_GPIO   GPIOB
@@ -223,6 +233,8 @@ typedef struct baro_t
 #define LEDRING
 #define SONAR
 #define BUZZER
+#define LED0
+#define LED1
 
 #define SENSORS_SET (SENSOR_ACC | SENSOR_BARO | SENSOR_MAG)
 
@@ -230,13 +242,25 @@ typedef struct baro_t
 #endif
 
 // Helpful macros
+#ifdef LED0
 #define LED0_TOGGLE              digitalToggle(LED0_GPIO, LED0_PIN);
 #define LED0_OFF                 digitalHi(LED0_GPIO, LED0_PIN);
 #define LED0_ON                  digitalLo(LED0_GPIO, LED0_PIN);
+#else
+#define LED0_TOGGLE
+#define LED0_OFF
+#define LED0_ON
+#endif
 
+#ifdef LED1
 #define LED1_TOGGLE              digitalToggle(LED1_GPIO, LED1_PIN);
 #define LED1_OFF                 digitalHi(LED1_GPIO, LED1_PIN);
 #define LED1_ON                  digitalLo(LED1_GPIO, LED1_PIN);
+#else
+#define LED1_TOGGLE
+#define LED1_OFF
+#define LED1_ON
+#endif
 
 #ifdef BEEP_GPIO
 #define BEEP_TOGGLE              digitalToggle(BEEP_GPIO, BEEP_PIN);
