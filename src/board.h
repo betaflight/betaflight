45dc25def10b440f07c58d9767a2b90ca411c289
Dominic Clifton <me@dominicclifton.name>
2014-04-08 01:25:23 +0100

    Use the appropriate GPIO pin defines.

--- src/board.h
+++ src/board.h
@@ -199,13 +199,13 @@ typedef struct baro_t
 #ifdef OLIMEXINO_UNCUT_LED2_E_JUMPER
 // LED2 is using one of the pwm pins (PWM2), so we must not use PWM2.  @See pwmInit()
 #define LED0_GPIO   GPIOA
-#define LED0_PIN    GPIO_Pin_1 // D3, PA1/USART2_RTS/ADC1/TIM2_CH3 - "LED2" on silkscreen, Yellow
+#define LED0_PIN    Pin_1 // D3, PA1/USART2_RTS/ADC1/TIM2_CH3 - "LED2" on silkscreen, Yellow
 #define LED0
 #endif
 
 #ifdef OLIMEXINO_UNCUT_LED1_E_JUMPER
 #define LED1_GPIO   GPIOA
-#define LED1_PIN    GPIO_Pin_5 // D13, PA5/SPI1_SCK/ADC5 - "LED1" on silkscreen, Green
+#define LED1_PIN    Pin_5 // D13, PA5/SPI1_SCK/ADC5 - "LED1" on silkscreen, Green
 #define LED1
 #endif
 
