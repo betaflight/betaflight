09b7268ec1f1dba8b0440e18517b2f384bd565d2
dongie <timecop@gmail.com>
2014-05-26 20:24:14 +0900

    hardware divider supports up to 8S, make it so in the code as well.

--- src/sensors.c
+++ src/sensors.c
@@ -155,8 +155,8 @@ void batteryInit(void)
 
     voltage = batteryAdcToVoltage((uint16_t)(voltage / 32));
 
-    // autodetect cell count, going from 2S..6S
-    for (i = 1; i < 6; i++) {
+    // autodetect cell count, going from 2S..8S
+    for (i = 1; i < 8; i++) {
         if (voltage < i * mcfg.vbatmaxcellvoltage)
             break;
     }
