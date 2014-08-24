betaflight/betaflight

commit 3f0754d295ff6acedeb32729029a33ebceef916a

Replace global flags with stateFlags, flightModeFlags and armingFlags.
Each flag was previously a whole byte, now all of the flags only take up
4 bytes as they are represented by bit masks.

This is cleaner because the different kind of flags are now separated.

Additionally this changes the behaviour of arming slightly.  When using
a switch to arm the aircraft will not arm unless the switch has been in
the off state once.  This prevents arming if you power the aircraft with
a low throttle and the switch in the on position.

src/main/io/serial_msp.c
@@ -466,17 +466,17 @@ static bool processOutCommand(uint8_t cmdMSP)
         // the bits in order, instead of setting the enabled bits based on BOXINDEX. WHERE IS THE FUCKING LOGIC IN THIS, FUCKWADS.
         // Serialize the boxes in the order we delivered them, until multiwii retards fix their shit
         junk = 0;
-        tmp = f.ANGLE_MODE << BOXANGLE |
-            f.HORIZON_MODE << BOXHORIZON |
-            f.BARO_MODE << BOXBARO |
-            f.MAG_MODE << BOXMAG |
-            f.HEADFREE_MODE << BOXHEADFREE |
+        tmp = FLIGHT_MODE(ANGLE_MODE) << BOXANGLE |
+            FLIGHT_MODE(HORIZON_MODE) << BOXHORIZON |
+            FLIGHT_MODE(BARO_MODE) << BOXBARO |
+            FLIGHT_MODE(MAG_MODE) << BOXMAG |
+            FLIGHT_MODE(HEADFREE_MODE) << BOXHEADFREE |
             rcOptions[BOXHEADADJ] << BOXHEADADJ |
             rcOptions[BOXCAMSTAB] << BOXCAMSTAB |
             rcOptions[BOXCAMTRIG] << BOXCAMTRIG |
-            f.GPS_HOME_MODE << BOXGPSHOME |
-            f.GPS_HOLD_MODE << BOXGPSHOLD |
-            f.PASSTHRU_MODE << BOXPASSTHRU |
+            FLIGHT_MODE(GPS_HOME_MODE) << BOXGPSHOME |
+            FLIGHT_MODE(GPS_HOLD_MODE) << BOXGPSHOLD |
+            FLIGHT_MODE(PASSTHRU_MODE) << BOXPASSTHRU |
             rcOptions[BOXBEEPERON] << BOXBEEPERON |
             rcOptions[BOXLEDMAX] << BOXLEDMAX |
             rcOptions[BOXLLIGHTS] << BOXLLIGHTS |
@@ -485,7 +485,7 @@ static bool processOutCommand(uint8_t cmdMSP)
             rcOptions[BOXOSD] << BOXOSD |
             rcOptions[BOXTELEMETRY] << BOXTELEMETRY |
             rcOptions[BOXAUTOTUNE] << BOXAUTOTUNE |
-            f.ARMED << BOXARM;
+            ARMING_FLAG(ARMED) << BOXARM;
         for (i = 0; i < numberBoxItems; i++) {
             int flag = (tmp & (1 << availableBoxes[i]));
             if (flag)
@@ -636,7 +636,7 @@ static bool processOutCommand(uint8_t cmdMSP)
 #ifdef GPS
     case MSP_RAW_GPS:
         headSerialReply(16);
-        serialize8(f.GPS_FIX);
+        serialize8(STATE(GPS_FIX));
         serialize8(GPS_numSat);
         serialize32(GPS_coord[LAT]);
         serialize32(GPS_coord[LON]);
@@ -715,7 +715,7 @@ static bool processInCommand(void)
 
     switch (currentPort->cmdMSP) {
     case MSP_SELECT_SETTING:
-        if (!f.ARMED) {
+        if (!ARMING_FLAG(ARMED)) {
             masterConfig.current_profile_index = read8();
             if (masterConfig.current_profile_index > 2) {
                 masterConfig.current_profile_index = 0;
@@ -817,21 +817,21 @@ static bool processInCommand(void)
         }
         break;
     case MSP_RESET_CONF:
-        if (!f.ARMED) {
+        if (!ARMING_FLAG(ARMED)) {
             resetEEPROM();
             readEEPROM();
         }
         break;
     case MSP_ACC_CALIBRATION:
-        if (!f.ARMED)
+        if (!ARMING_FLAG(ARMED))
             accSetCalibrationCycles(CALIBRATING_ACC_CYCLES);
         break;
     case MSP_MAG_CALIBRATION:
-        if (!f.ARMED)
-            f.CALIBRATE_MAG = 1;
+        if (!ARMING_FLAG(ARMED))
+            ENABLE_STATE(CALIBRATE_MAG);
         break;
     case MSP_EEPROM_WRITE:
-        if (f.ARMED) {
+        if (ARMING_FLAG(ARMED)) {
             headSerialError(0);
             return true;
         }
@@ -840,13 +840,17 @@ static bool processInCommand(void)
         break;
 #ifdef GPS
     case MSP_SET_RAW_GPS:
-        f.GPS_FIX = read8();
+        if (read8()) {
+            ENABLE_STATE(GPS_FIX);
+        } else {
+            DISABLE_STATE(GPS_FIX);
+        }
         GPS_numSat = read8();
         GPS_coord[LAT] = read32();
         GPS_coord[LON] = read32();
         GPS_altitude = read16();
         GPS_speed = read16();
-        GPS_update |= 2;        // New data signalisation to GPS functions
+        GPS_update |= 2;        // New data signalisation to GPS functions // FIXME Magic Numbers
         break;
     case MSP_SET_WP:
         wp_no = read8();    //get the wp number
@@ -859,8 +863,8 @@ static bool processInCommand(void)
         if (wp_no == 0) {
             GPS_home[LAT] = lat;
             GPS_home[LON] = lon;
-            f.GPS_HOME_MODE = 0;        // with this flag, GPS_set_next_wp will be called in the next loop -- OK with SERIAL GPS / OK with I2C GPS
-            f.GPS_FIX_HOME = 1;
+            DISABLE_FLIGHT_MODE(GPS_HOME_MODE);        // with this flag, GPS_set_next_wp will be called in the next loop -- OK with SERIAL GPS / OK with I2C GPS
+            ENABLE_STATE(GPS_FIX_HOME);
             if (alt != 0)
                 AltHold = alt;          // temporary implementation to test feature with apps
         } else if (wp_no == 16) {       // OK with SERIAL GPS  --  NOK for I2C GPS / needs more code dev in order to inject GPS coord inside I2C GPS
@@ -890,7 +894,7 @@ static void mspProcessPort(void)
 
         if (currentPort->c_state == IDLE) {
             currentPort->c_state = (c == '$') ? HEADER_START : IDLE;
-            if (currentPort->c_state == IDLE && !f.ARMED)
+            if (currentPort->c_state == IDLE && !ARMING_FLAG(ARMED))
                 evaluateOtherData(c); // if not armed evaluate all other incoming serial data
         } else if (currentPort->c_state == HEADER_START) {
             currentPort->c_state = (c == 'M') ? HEADER_M : IDLE;

src/main/config/runtime_config.c
@@ -20,7 +20,9 @@
 
 #include "config/runtime_config.h"
 
-flags_t f;
+uint8_t armingFlags = 0;
+uint8_t stateFlags = 0;
+uint16_t flightModeFlags = 0;
 
 // each entry in the array is a bitmask, 3 bits per aux channel (only aux 1 to 4), aux1 is first, each bit corresponds to an rc channel reading
 // bit 1 - stick LOW

src/main/config/runtime_config.h
@@ -45,27 +45,51 @@ enum {
 extern uint8_t rcOptions[CHECKBOX_ITEM_COUNT];
 
 // FIXME some of these are flight modes, some of these are general status indicators
-typedef struct flags_t {
-    uint8_t OK_TO_ARM;
-    uint8_t PREVENT_ARMING;
-    uint8_t ARMED;
-    uint8_t ANGLE_MODE;
-    uint8_t HORIZON_MODE;
-    uint8_t MAG_MODE;
-    uint8_t BARO_MODE;
-    uint8_t GPS_HOME_MODE;
-    uint8_t GPS_HOLD_MODE;
-    uint8_t HEADFREE_MODE;
-    uint8_t PASSTHRU_MODE;
-    uint8_t GPS_FIX;
-    uint8_t GPS_FIX_HOME;
-    uint8_t SMALL_ANGLE;
-    uint8_t CALIBRATE_MAG;
-    uint8_t FIXED_WING;                     // set when in flying_wing or airplane mode. currently used by althold selection code
-    uint8_t AUTOTUNE_MODE;
-} flags_t;
+typedef enum {
+    OK_TO_ARM       = (1 << 0),
+    PREVENT_ARMING  = (1 << 1),
+    SEEN_BOXARM_OFF = (1 << 2),
+    ARMED           = (1 << 3)
+} armingFlag_e;
 
-extern flags_t f;
+extern uint8_t armingFlags;
+
+#define DISABLE_ARMING_FLAG(mask) (armingFlags &= ~(mask))
+#define ENABLE_ARMING_FLAG(mask) (armingFlags |= mask)
+#define ARMING_FLAG(mask) (armingFlags & mask)
+
+typedef enum {
+    ANGLE_MODE      = (1 << 0),
+    HORIZON_MODE    = (1 << 1),
+    MAG_MODE        = (1 << 2),
+    BARO_MODE       = (1 << 3),
+    GPS_HOME_MODE   = (1 << 4),
+    GPS_HOLD_MODE   = (1 << 5),
+    HEADFREE_MODE   = (1 << 6),
+    AUTOTUNE_MODE   = (1 << 7),
+    PASSTHRU_MODE   = (1 << 8),
+} flightModeFlags_e;
+
+extern uint16_t flightModeFlags;
+
+#define DISABLE_FLIGHT_MODE(mask) (flightModeFlags &= ~(mask))
+#define ENABLE_FLIGHT_MODE(mask) (flightModeFlags |= mask)
+#define FLIGHT_MODE(mask) (flightModeFlags & mask)
+
+typedef enum {
+    GPS_FIX_HOME   = (1 << 0),
+    GPS_FIX        = (1 << 1),
+    CALIBRATE_MAG  = (1 << 2),
+    SMALL_ANGLE    = (1 << 3),
+    FIXED_WING     = (1 << 4),                   // set when in flying_wing or airplane mode. currently used by althold selection code
+} stateFlags_t;
+
+#define DISABLE_STATE(mask) (stateFlags &= ~(mask))
+#define ENABLE_STATE(mask) (stateFlags |= mask)
+#define STATE(mask) (stateFlags & mask)
+
+extern uint8_t stateFlags;
+
 
 bool sensors(uint32_t mask);
 void sensorsSet(uint32_t mask);

src/main/flight/altitudehold.c
@@ -105,7 +105,7 @@ static void fixedWingAltHold()
 
 void updateAltHold(void)
 {
-    if (f.FIXED_WING) {
+    if (STATE(FIXED_WING)) {
         fixedWingAltHold();
     } else {
         multirotorAltHold();
@@ -116,15 +116,15 @@ void updateAltHoldState(void)
 {
     // Baro alt hold activate
     if (rcOptions[BOXBARO]) {
-        if (!f.BARO_MODE) {
-            f.BARO_MODE = 1;
+        if (!FLIGHT_MODE(BARO_MODE)) {
+            ENABLE_FLIGHT_MODE(BARO_MODE);
             AltHold = EstAlt;
             initialThrottleHold = rcCommand[THROTTLE];
             errorVelocityI = 0;
             BaroPID = 0;
         }
     } else {
-        f.BARO_MODE = 0;
+        DISABLE_FLIGHT_MODE(BARO_MODE);
     }
 }
 

src/main/flight/failsafe.c
@@ -105,7 +105,7 @@ void failsafeAvoidRearm(void)
 {
     // This will prevent the automatic rearm if failsafe shuts it down and prevents
     // to restart accidently by just reconnect to the tx - you will have to switch off first to rearm
-    f.PREVENT_ARMING = 1;
+    ENABLE_ARMING_FLAG(PREVENT_ARMING);
 }
 
 void onValidDataReceived(void)
@@ -129,7 +129,7 @@ void updateState(void)
         return;
     }
 
-    if (shouldForceLanding(f.ARMED)) { // Stabilize, and set Throttle to specified level
+    if (shouldForceLanding(ARMING_FLAG(ARMED))) { // Stabilize, and set Throttle to specified level
         failsafeAvoidRearm();
 
         for (i = 0; i < 3; i++) {
@@ -139,7 +139,7 @@ void updateState(void)
         failsafe.events++;
     }
 
-    if (shouldHaveCausedLandingByNow() || !f.ARMED) {
+    if (shouldHaveCausedLandingByNow() || !ARMING_FLAG(ARMED)) {
         mwDisarm();
     }
 }

src/main/flight/flight.c
@@ -120,13 +120,13 @@ static void pidBaseflight(pidProfile_t *pidProfile, controlRateConfig_t *control
             }
 #endif
 
-            if (f.ANGLE_MODE) {
+            if (FLIGHT_MODE(ANGLE_MODE)) {
                 // it's the ANGLE mode - control is angle based, so control loop is needed
                 AngleRate = errorAngle * pidProfile->A_level;
             } else {
                 //control is GYRO based (ACRO and HORIZON - direct sticks control is applied to rate PID
                 AngleRate = (float)((controlRateConfig->rollPitchRate + 20) * rcCommand[axis]) / 50.0f; // 200dps to 1200dps max yaw rate
-                if (f.HORIZON_MODE) {
+                if (FLIGHT_MODE(HORIZON_MODE)) {
                     // mix up angle error to desired AngleRate to add a little auto-level feel
                     AngleRate += errorAngle * pidProfile->H_level;
                 }
@@ -185,7 +185,7 @@ static void pidMultiWii(pidProfile_t *pidProfile, controlRateConfig_t *controlRa
     // **** PITCH & ROLL & YAW PID ****
     prop = max(abs(rcCommand[PITCH]), abs(rcCommand[ROLL])); // range [0;500]
     for (axis = 0; axis < 3; axis++) {
-        if ((f.ANGLE_MODE || f.HORIZON_MODE) && (axis == FD_ROLL || axis == FD_PITCH)) { // MODE relying on ACC
+        if ((FLIGHT_MODE(ANGLE_MODE) || FLIGHT_MODE(HORIZON_MODE)) && (axis == FD_ROLL || axis == FD_PITCH)) { // MODE relying on ACC
             // observe max inclination
 #ifdef GPS
             errorAngle = constrain(2 * rcCommand[axis] + GPS_angle[axis], -((int) max_angle_inclination),
@@ -207,7 +207,7 @@ static void pidMultiWii(pidProfile_t *pidProfile, controlRateConfig_t *controlRa
             errorAngleI[axis] = constrain(errorAngleI[axis] + errorAngle, -10000, +10000); // WindUp
             ITermACC = (errorAngleI[axis] * pidProfile->I8[PIDLEVEL]) >> 12;
         }
-        if (!f.ANGLE_MODE || f.HORIZON_MODE || axis == FD_YAW) { // MODE relying on GYRO or YAW axis
+        if (!FLIGHT_MODE(ANGLE_MODE) || FLIGHT_MODE(HORIZON_MODE) || axis == FD_YAW) { // MODE relying on GYRO or YAW axis
             error = (int32_t) rcCommand[axis] * 10 * 8 / pidProfile->P8[axis];
             error -= gyroData[axis] / 4;
 
@@ -219,11 +219,11 @@ static void pidMultiWii(pidProfile_t *pidProfile, controlRateConfig_t *controlRa
 
             ITermGYRO = (errorGyroI[axis] / 125 * pidProfile->I8[axis]) / 64;
         }
-        if (f.HORIZON_MODE && (axis == FD_ROLL || axis == FD_PITCH)) {
+        if (FLIGHT_MODE(HORIZON_MODE) && (axis == FD_ROLL || axis == FD_PITCH)) {
             PTerm = (PTermACC * (500 - prop) + PTermGYRO * prop) / 500;
             ITerm = (ITermACC * (500 - prop) + ITermGYRO * prop) / 500;
         } else {
-            if (f.ANGLE_MODE && (axis == FD_ROLL || axis == FD_PITCH)) {
+            if (FLIGHT_MODE(ANGLE_MODE) && (axis == FD_ROLL || axis == FD_PITCH)) {
                 PTerm = PTermACC;
                 ITerm = ITermACC;
             } else {
@@ -277,9 +277,9 @@ static void pidRewrite(pidProfile_t *pidProfile, controlRateConfig_t *controlRat
             }
 #endif
 
-            if (!f.ANGLE_MODE) { //control is GYRO based (ACRO and HORIZON - direct sticks control is applied to rate PID
+            if (!FLIGHT_MODE(ANGLE_MODE)) { //control is GYRO based (ACRO and HORIZON - direct sticks control is applied to rate PID
                 AngleRateTmp = ((int32_t)(controlRateConfig->rollPitchRate + 27) * rcCommand[axis]) >> 4;
-                if (f.HORIZON_MODE) {
+                if (FLIGHT_MODE(HORIZON_MODE)) {
                     // mix up angle error to desired AngleRateTmp to add a little auto-level feel
                     AngleRateTmp += (errorAngle * pidProfile->I8[PIDLEVEL]) >> 8;
                 }

src/main/flight/imu.c
@@ -239,7 +239,7 @@ void acc_calc(uint32_t deltaT)
     rotateV(&accel_ned.V, &rpy);
 
     if (imuRuntimeConfig->acc_unarmedcal == 1) {
-        if (!f.ARMED) {
+        if (!ARMING_FLAG(ARMED)) {
             accZoffset -= accZoffset / 64;
             accZoffset += accel_ned.V.Z;
         }
@@ -329,7 +329,11 @@ static void getEstimatedAttitude(void)
             EstG.A[axis] = (EstG.A[axis] * imuRuntimeConfig->gyro_cmpf_factor + accSmooth[axis]) * invGyroComplimentaryFilterFactor;
     }
 
-    f.SMALL_ANGLE = (EstG.A[Z] > smallAngle);
+    if (EstG.A[Z] > smallAngle) {
+        ENABLE_STATE(SMALL_ANGLE);
+    } else {
+        DISABLE_STATE(SMALL_ANGLE);
+    }
 
     // Attitude of the estimated vector
     anglerad[AI_ROLL] = atan2f(EstG.V.Y, EstG.V.Z);

src/main/flight/mixer.c
@@ -296,9 +296,9 @@ void mixerUsePWMOutputConfiguration(pwmOutputConfiguration_t *pwmOutputConfigura
     // set flag that we're on something with wings
     if (currentMixerConfiguration == MULTITYPE_FLYING_WING ||
             currentMixerConfiguration == MULTITYPE_AIRPLANE)
-        f.FIXED_WING = 1;
+        ENABLE_STATE(FIXED_WING);
     else
-        f.FIXED_WING = 0;
+        DISABLE_STATE(FIXED_WING);
 
     mixerResetMotors();
 }
@@ -351,7 +351,7 @@ void writeServos(void)
                 pwmWriteServo(0, servo[5]);
             } else {
                 // otherwise, only move servo when copter is armed
-                if (f.ARMED)
+                if (ARMING_FLAG(ARMED))
                     pwmWriteServo(0, servo[5]);
                 else
                     pwmWriteServo(0, 0); // kill servo signal completely.
@@ -412,7 +412,7 @@ static void airplaneMixer(void)
     int16_t flapperons[2] = { 0, 0 };
     int i;
 
-    if (!f.ARMED)
+    if (!ARMING_FLAG(ARMED))
         servo[7] = escAndServoConfig->mincommand; // Kill throttle when disarmed
     else
         servo[7] = constrain(rcCommand[THROTTLE], escAndServoConfig->minthrottle, escAndServoConfig->maxthrottle);
@@ -435,7 +435,7 @@ static void airplaneMixer(void)
         servo[2] += rxConfig->midrc;
     }
 
-    if (f.PASSTHRU_MODE) {   // Direct passthru from RX
+    if (FLIGHT_MODE(PASSTHRU_MODE)) {   // Direct passthru from RX
         servo[3] = rcCommand[ROLL] + flapperons[0];     // Wing 1
         servo[4] = rcCommand[ROLL] + flapperons[1];     // Wing 2
         servo[5] = rcCommand[YAW];                      // Rudder
@@ -489,12 +489,12 @@ void mixTable(void)
             break;
 
         case MULTITYPE_FLYING_WING:
-            if (!f.ARMED)
+            if (!ARMING_FLAG(ARMED))
                 servo[7] = escAndServoConfig->mincommand;
             else
                 servo[7] = constrain(rcCommand[THROTTLE], escAndServoConfig->minthrottle, escAndServoConfig->maxthrottle);
             motor[0] = servo[7];
-            if (f.PASSTHRU_MODE) {
+            if (FLIGHT_MODE(PASSTHRU_MODE)) {
                 // do not use sensors for correction, simple 2 channel mixing
                 servo[3] = (servoDirection(3, 1) * rcCommand[PITCH]) + (servoDirection(3, 2) * rcCommand[ROLL]);
                 servo[4] = (servoDirection(4, 1) * rcCommand[PITCH]) + (servoDirection(4, 2) * rcCommand[ROLL]);
@@ -587,7 +587,7 @@ void mixTable(void)
                     motor[i] = escAndServoConfig->mincommand;
             }
         }
-        if (!f.ARMED) {
+        if (!ARMING_FLAG(ARMED)) {
             motor[i] = motor_disarmed[i];
         }
     }

commit b6a8e20d8c6a8a8efd456b5919a44c0871a960d1

Fix reporting of flight modes broken in 3f0754d
The conversion of the flags to bitmasks meant the mask was being bitshifted instead of 1 or 0.

src/main/io/serial_msp.c
@@ -441,6 +441,8 @@ void mspInit(serialConfig_t *serialConfig)
     openAllMSPSerialPorts(serialConfig);
 }
 
+#define IS_ENABLED(mask) (mask == 0 ? 0 : 1)
+
 static bool processOutCommand(uint8_t cmdMSP)
 {
     uint32_t i, tmp, junk;
@@ -466,17 +468,17 @@ static bool processOutCommand(uint8_t cmdMSP)
         // the bits in order, instead of setting the enabled bits based on BOXINDEX. WHERE IS THE FUCKING LOGIC IN THIS, FUCKWADS.
         // Serialize the boxes in the order we delivered them, until multiwii retards fix their shit
         junk = 0;
-        tmp = FLIGHT_MODE(ANGLE_MODE) << BOXANGLE |
-            FLIGHT_MODE(HORIZON_MODE) << BOXHORIZON |
-            FLIGHT_MODE(BARO_MODE) << BOXBARO |
-            FLIGHT_MODE(MAG_MODE) << BOXMAG |
-            FLIGHT_MODE(HEADFREE_MODE) << BOXHEADFREE |
+        tmp = IS_ENABLED(FLIGHT_MODE(ANGLE_MODE)) << BOXANGLE |
+            IS_ENABLED(FLIGHT_MODE(HORIZON_MODE)) << BOXHORIZON |
+            IS_ENABLED(FLIGHT_MODE(BARO_MODE)) << BOXBARO |
+            IS_ENABLED(FLIGHT_MODE(MAG_MODE)) << BOXMAG |
+            IS_ENABLED(FLIGHT_MODE(HEADFREE_MODE)) << BOXHEADFREE |
             rcOptions[BOXHEADADJ] << BOXHEADADJ |
             rcOptions[BOXCAMSTAB] << BOXCAMSTAB |
             rcOptions[BOXCAMTRIG] << BOXCAMTRIG |
-            FLIGHT_MODE(GPS_HOME_MODE) << BOXGPSHOME |
-            FLIGHT_MODE(GPS_HOLD_MODE) << BOXGPSHOLD |
-            FLIGHT_MODE(PASSTHRU_MODE) << BOXPASSTHRU |
+            IS_ENABLED(FLIGHT_MODE(GPS_HOME_MODE)) << BOXGPSHOME |
+            IS_ENABLED(FLIGHT_MODE(GPS_HOLD_MODE)) << BOXGPSHOLD |
+            IS_ENABLED(FLIGHT_MODE(PASSTHRU_MODE)) << BOXPASSTHRU |
             rcOptions[BOXBEEPERON] << BOXBEEPERON |
             rcOptions[BOXLEDMAX] << BOXLEDMAX |
             rcOptions[BOXLLIGHTS] << BOXLLIGHTS |