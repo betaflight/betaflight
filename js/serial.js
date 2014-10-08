c34938b9d6776a7829b4abfb3a2c5a0025e7b9c3
cTn <ctndesigner@gmail.com>
2014-10-08 11:15:02 +0200

    serial.send won't depend on callback anymore

--- js/serial.js
+++ js/serial.js
@@ -148,7 +148,7 @@ var serial = {
                     self.bytes_sent += sendInfo.bytesSent;
 
                     // fire callback
-                    callback(sendInfo);
+                    if (callback) callback(sendInfo);
 
                     // remove data for current transmission form the buffer
                     self.output_buffer.shift();
