ccc0b839e6ec9bf2eef4fd67d903d88076c2897d
Dominic Clifton <me@dominicclifton.name>
2014-12-19 19:48:19 +0000

    Minor tweaks to the firmwre flasher.

--- tabs/firmware_flasher.js
+++ tabs/firmware_flasher.js
@@ -58,12 +58,13 @@ TABS.firmware_flasher.initialize = function (callback) {
                             );
                             
                             var summary = {
-                                "name"    : release.name,
-                                "url"     : asset.browser_download_url,
-                                "file"    : asset.name,
-                                "target"  : target,
-                                "date"    : formattedDate,
-                                "notes"   : release.body
+                                "releaseUrl": release.html_url,
+                                "name"      : release.name,
+                                "url"       : asset.browser_download_url,
+                                "file"      : asset.name,
+                                "target"    : target,
+                                "date"      : formattedDate,
+                                "notes"     : release.body
                             };
 
                             var select_e = 
@@ -242,8 +243,9 @@ TABS.firmware_flasher.initialize = function (callback) {
                         }
                         
                         $('div.release_info .target').text(summary.target);
+                        $('div.release_info .name').text(summary.name).prop('href', summary.url);
                         $('div.release_info .date').text(summary.date);
-                        $('div.release_info .file').text(summary.file).prop('href', summary.url);
+                        $('div.release_info .file').text(summary.file).prop('href', summary.releaseUrl);
                         $('div.release_info .notes').text(summary.notes);
 
                         $('div.release_info').slideDown();
