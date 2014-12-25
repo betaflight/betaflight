f71eba57608d5fe2751ac8d984814cfb1ce83ba7
Roeland Lutters <Roeland.lutters@telenet.be>
2014-12-25 22:10:00 +0100

    Fixed the displayed firmware-date in the firmware flasher.

--- tabs/firmware_flasher.js
+++ tabs/firmware_flasher.js
@@ -53,8 +53,8 @@ TABS.firmware_flasher.initialize = function (callback) {
                             var date = new Date(release.published_at);
                             var formattedDate = "{0}-{1}-{2} {3}:{4}".format(
                                 date.getFullYear(),
-                                date.getMonth(),
-                                date.getDay(),
+                                date.getMonth() + 1,
+                                date.getDate(),
                                 date.getHours(),
                                 date.getMinutes()
                             );
