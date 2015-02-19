f89aaa101136d91344ca42bce5f168253e3d184f
Richard Birkby <rbirkby@gmail.com>
2015-02-19 20:39:57 +0000

    Add flight instruments to setup tab

--- tabs/setup.js
+++ tabs/setup.js
@@ -41,7 +41,7 @@ TABS.setup.initialize = function (callback) {
         if (CONFIG.apiVersion < CONFIGURATOR.backupRestoreMinApiVersionAccepted) {
             $('#content .backup').addClass('disabled');
             $('#content .restore').addClass('disabled');
-            
+
             GUI.log(chrome.i18n.getMessage('initialSetupBackupAndRestoreApiVersion', [CONFIG.apiVersion, CONFIGURATOR.backupRestoreMinApiVersionAccepted]));
         }
         // initialize 3D
@@ -54,6 +54,8 @@ TABS.setup.initialize = function (callback) {
         if (!bit_check(CONFIG.activeSensors, 2)) {
             $('a.calibrateMag').addClass('disabled');
         }
+
+        self.initializeInstruments();
 
         // UI Hooks
         $('a.calibrateAccel').click(function () {
@@ -153,7 +155,7 @@ TABS.setup.initialize = function (callback) {
 
         function get_slow_data() {
             MSP.send_message(MSP_codes.MSP_STATUS);
-            
+
             MSP.send_message(MSP_codes.MSP_ANALOG, false, false, function () {
                 bat_voltage_e.text(chrome.i18n.getMessage('initialSetupBatteryValue', [ANALOG.voltage]));
                 bat_mah_drawn_e.text(chrome.i18n.getMessage('initialSetupBatteryMahValue', [ANALOG.mAhdrawn]));
@@ -174,6 +176,10 @@ TABS.setup.initialize = function (callback) {
                 heading_e.text(chrome.i18n.getMessage('initialSetupheading', [SENSOR_DATA.kinematics[2]]));
                 self.render3D();
             });
+
+            MSP.send_message(MSP_codes.MSP_RAW_IMU, false, false, function () {
+              self.updateInstruments();
+            });
         }
 
         GUI.interval_add('setup_data_pull_fast', get_fast_data, 33, true); // 30 fps
@@ -181,6 +187,18 @@ TABS.setup.initialize = function (callback) {
 
         if (callback) callback();
     }
+};
+
+TABS.setup.initializeInstruments = function() {
+    var options = {size:90, showBox : false, img_directory: 'images/flightindicators/'};
+    var attitude = $.flightIndicator('#attitude', 'attitude', options);
+    var heading = $.flightIndicator('#heading', 'heading', options);
+
+    this.updateInstruments = function() {
+        attitude.setRoll(SENSOR_DATA.kinematics[0]);
+        attitude.setPitch(SENSOR_DATA.kinematics[1]);
+        heading.setHeading(SENSOR_DATA.magnetometer[0] * 360);
+    };
 };
 
 TABS.setup.initialize3D = function (compatibility) {
@@ -199,7 +217,7 @@ TABS.setup.initialize3D = function (compatibility) {
         renderer = new THREE.WebGLRenderer({canvas: canvas.get(0), alpha: true, antialias: true});
         useWebGlRenderer = true;
     } else {
-    
+
         renderer = new THREE.CanvasRenderer({canvas: canvas.get(0), alpha: true});
     }
 
@@ -212,7 +230,7 @@ TABS.setup.initialize3D = function (compatibility) {
     } else {
         model_file = 'fallback'
     }
-    
+
     // Temporary workaround for 'custom' model until akfreak's custom model is merged.
     var useLegacyCustomModel = false;
     if (model_file == 'custom') {
