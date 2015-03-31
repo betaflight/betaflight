af7097688f5ced99dc174b296756dd05f2dd61da
Paul Rogalinski <paul@paul.vc>
2015-03-31 18:49:07 +0200

    support for build artifact upload to a remote server

--- .travis.sh
+++ .travis.sh
@@ -3,5 +3,26 @@
 if [ $RUNTESTS ] ; then
 	cd ./src/test && make test
 else
-	make -j2
+	if [ $PUBLISH_URL ] ; then
+
+		make -j2
+
+		TARGET_FILE=obj/cleanflight_${TARGET}
+
+		if [ -f ${TARGET_FILE}.bin ];
+                then
+                        TARGET_FILE=${TARGET_FILE}.bin
+		elif [ -f ${TARGET_FILE}.hex ];
+                then
+                        TARGET_FILE=${TARGET_FILE}.hex
+		else
+			echo "build artifact (hex or bin) for ${TARGET_FILE} not found, aborting";
+			exit 1
+                fi
+	
+		curl -F file=@${TARGET_FILE} ${PUBLISH_URL}
+	else
+		make -j2
+	fi
+
 fi
