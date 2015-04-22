betaflight/betaflight

commit 08efc2e66de819f56b9002b46e95d8acedb9b92d

Fix tri_unarmed_servo (was broken by pid_at_min_throttle changes)

src/main/mw.c
@@ -702,7 +702,9 @@ void loop(void)
         // If we're armed, at minimum throttle, and we do arming via the
         // sticks, do not process yaw input from the rx.  We do this so the
         // motors do not spin up while we are trying to arm or disarm.
-        if (isUsingSticksForArming() && rcData[THROTTLE] <= masterConfig.rxConfig.mincheck) {
+        // Allow yaw control for tricopters if the user wants the servo to move even when unarmed.
+        if (isUsingSticksForArming() && rcData[THROTTLE] <= masterConfig.rxConfig.mincheck
+                && !(masterConfig.mixerMode == MIXER_TRI && masterConfig.mixerConfig.tri_unarmed_servo)) {
             rcCommand[YAW] = 0;
         }
 

commit c085b2c61d5102776c7d4e0501cd59a81a4bdb0c

Fix CJMCU build (broken in 08efc2e)

src/main/mw.c
@@ -704,7 +704,10 @@ void loop(void)
         // motors do not spin up while we are trying to arm or disarm.
         // Allow yaw control for tricopters if the user wants the servo to move even when unarmed.
         if (isUsingSticksForArming() && rcData[THROTTLE] <= masterConfig.rxConfig.mincheck
-                && !(masterConfig.mixerMode == MIXER_TRI && masterConfig.mixerConfig.tri_unarmed_servo)) {
+#ifndef USE_QUAD_MIXER_ONLY
+                && !(masterConfig.mixerMode == MIXER_TRI && masterConfig.mixerConfig.tri_unarmed_servo)
+#endif
+        ) {
             rcCommand[YAW] = 0;
         }
 