14a95a3f18f633b65a0465ae0a0db53c57739977
Steveis <steveis@stevis.com>
2015-05-04 10:19:58 +0100

    include delay(20)

--- src/main/drivers/barometer_bmp085.c
+++ src/main/drivers/barometer_bmp085.c
@@ -154,6 +154,7 @@ bool bmp085Detect(const bmp085Config_t *config, baro_t *baro)
     if (bmp085InitDone)
         return true;
 
+    delay(20); // datasheet says 10ms, we'll be careful and do 20.
     ack = i2cRead(BMP085_I2C_ADDR, BMP085_CHIP_ID__REG, 1, &data); /* read Chip Id */
     if(!ack)
         return false;
