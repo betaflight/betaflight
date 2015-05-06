betaflight/betaflight

commit c0aec95e1426a110bff7235332073891935f1f48

STM32F303 - Skip UART3 pins in PWM mapping when UART3 is used.

src/main/drivers/pwm_mapping.c
@@ -375,6 +375,12 @@ pwmOutputConfiguration_t *pwmInit(drv_pwm_config_t *init)
             continue;
 #endif
 
+#ifdef STM32F303xC
+        // skip UART3 ports (PB10/PB11)
+        if (init->useUART3 && timerHardwarePtr->gpio == UART3_GPIO && (timerHardwarePtr->pin == UART3_TX_PIN || timerHardwarePtr->pin == UART3_RX_PIN))
+            continue;
+#endif
+
 #ifdef SOFTSERIAL_1_TIMER
         if (init->useSoftSerial && timerHardwarePtr->tim == SOFTSERIAL_1_TIMER)
             continue;

src/main/drivers/pwm_mapping.h
@@ -45,6 +45,9 @@ typedef struct drv_pwm_config_t {
 #ifdef STM32F10X
     bool useUART2;
 #endif
+#ifdef STM32F303xC
+    bool useUART3;
+#endif
     bool useVbat;
     bool useOneshot;
     bool useSoftSerial;

src/main/main.c
@@ -201,6 +201,9 @@ void init(void)
 #if defined(USE_USART2) && defined(STM32F10X)
     pwm_params.useUART2 = doesConfigurationUsePort(SERIAL_PORT_USART2);
 #endif
+#ifdef STM32F303xC
+    pwm_params.useUART3 = doesConfigurationUsePort(SERIAL_PORT_USART3);
+#endif
     pwm_params.useVbat = feature(FEATURE_VBAT);
     pwm_params.useSoftSerial = feature(FEATURE_SOFTSERIAL);
     pwm_params.useParallelPWM = feature(FEATURE_RX_PARALLEL_PWM);

commit 5bf687a1501d5cd34fc8768bce49d2d7a1c1c77d

Fix CheBuzzF3 target, broken in c0aec95e1426a110bff7235332073891935f1f48

src/main/drivers/pwm_mapping.c
@@ -375,7 +375,7 @@ pwmOutputConfiguration_t *pwmInit(drv_pwm_config_t *init)
             continue;
 #endif
 
-#ifdef STM32F303xC
+#if defined(STM32F303xC) && defined(USE_USART3)
         // skip UART3 ports (PB10/PB11)
         if (init->useUART3 && timerHardwarePtr->gpio == UART3_GPIO && (timerHardwarePtr->pin == UART3_TX_PIN || timerHardwarePtr->pin == UART3_RX_PIN))
             continue;