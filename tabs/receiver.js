502a70233a6dfa5dd30a6571f02f2979be5fec00
borisbstyle <dreambb1982@gmail.com>
2015-05-08 01:30:45 +0200

    update

--- tabs/receiver.js
+++ tabs/receiver.js
@@ -37,10 +37,10 @@ TABS.receiver.initialize = function (callback) {
 
         $('.tunings .rate input[name="rate"]').val(RC_tuning.RC_RATE.toFixed(2));
         $('.tunings .rate input[name="expo"]').val(RC_tuning.RC_EXPO.toFixed(2));
-		$('.tunings .rate input[name="yaw_expo"]').val(RC_tuning.RC_YAW_EXPO.toFixed(2));
+		$('.tunings .yaw_rate input[name="yaw_expo"]').val(RC_tuning.RC_YAW_EXPO.toFixed(2));
         
 		if (semver.lt(CONFIG.apiVersion, "1.10.0")) {
-            $('.tunings .rate input[name="yaw_expo"]').hide();
+            $('.tunings .yaw_rate input[name="yaw_expo"]').hide();
         }
 		
         chrome.storage.local.get('rx_refresh_rate', function (result) {
@@ -273,7 +273,7 @@ TABS.receiver.initialize = function (callback) {
 
             RC_tuning.RC_RATE = parseFloat($('.tunings .rate input[name="rate"]').val());
             RC_tuning.RC_EXPO = parseFloat($('.tunings .rate input[name="expo"]').val());
-			RC_tuning.RC_YAW_EXPO = parseFloat($('.tunings .rate input[name="yaw_expo"]').val());
+			RC_tuning.RC_YAW_EXPO = parseFloat($('.tunings .yaw_rate input[name="yaw_expo"]').val());
 
             // catch rc map
             var RC_MAP_Letters = ['A', 'E', 'R', 'T', '1', '2', '3', '4'];
