713e138c4296b32c5f497b45467589fe7573e0ac
atomiclama <andy@purplelama.co.uk>
2015-05-18 12:10:59 +0100

    Changed idle pulse width from fixed 1ms to that configured. Stops motors, that have none standard pulse ranges, from running up after a save or exit from cli.

--- src/main/main.c
+++ src/main/main.c
@@ -226,7 +226,7 @@ void init(void)
 
     pwm_params.useOneshot = feature(FEATURE_ONESHOT125);
     pwm_params.motorPwmRate = masterConfig.motor_pwm_rate;
-    pwm_params.idlePulse = PULSE_1MS; // standard PWM for brushless ESC (default, overridden below)
+    pwm_params.idlePulse = masterConfig.escAndServoConfig.mincommand;
     if (feature(FEATURE_3D))
         pwm_params.idlePulse = masterConfig.flight3DConfig.neutral3d;
     if (pwm_params.motorPwmRate > 500)
