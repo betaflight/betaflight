99e94a818c2379755ed73662b3ff30c08dc178f8
henn1001 <hendrik-schlee@web.de>
2015-09-18 13:48:45 +0200

    Added MPU6000 interrupt, work in progress

--- src/main/target/CC3D/target.h
+++ src/main/target/CC3D/target.h
@@ -54,6 +54,10 @@
 
 #define ACC_SPI_MPU6000_ALIGN CW270_DEG
 
+// MPU6000 interrupts
+#define EXTI15_10_CALLBACK_HANDLER_COUNT 1 // MPU data ready
+#define USE_MPU_DATA_READY_SIGNAL
+
 // External I2C BARO
 #define BARO
 #define USE_BARO_MS5611
