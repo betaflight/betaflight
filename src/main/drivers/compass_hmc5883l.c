betaflight/betaflight

commit c6f5b98a7916a369bcf9445d04feef4036dc3ce5

Improve failure LED status flashing.  Now users can identify and report hardware failures by counting the number of long flashes.
Fix up sensor read API so that code that uses sensors can detect
malfunctions.

If a failure mode occurs in a debug mode the code reboots the system
rather than rebooting to the bootloader.

src/main/drivers/compass_hmc5883l.c
@@ -305,14 +305,19 @@ void hmc5883lInit(void)
     hmc5883lConfigureDataReadyInterruptHandling();
 }
 
-void hmc5883lRead(int16_t *magData)
+bool hmc5883lRead(int16_t *magData)
 {
     uint8_t buf[6];
 
-    i2cRead(MAG_ADDRESS, MAG_DATA_REGISTER, 6, buf);
+    bool ack = i2cRead(MAG_ADDRESS, MAG_DATA_REGISTER, 6, buf);
+    if (ack) {
+        return false;
+    }
     // During calibration, magGain is 1.0, so the read returns normal non-calibrated values.
     // After calibration is done, magGain is set to calculated gain values.
     magData[X] = (int16_t)(buf[0] << 8 | buf[1]) * magGain[X];
     magData[Z] = (int16_t)(buf[2] << 8 | buf[3]) * magGain[Z];
     magData[Y] = (int16_t)(buf[4] << 8 | buf[5]) * magGain[Y];
+
+    return true;
 }

Makefile
@@ -575,7 +575,7 @@ OPTIMIZE	 = -Os
 LTO_FLAGS	 = -flto -fuse-linker-plugin $(OPTIMIZE)
 endif
 
-DEBUG_FLAGS	 = -ggdb3
+DEBUG_FLAGS	 = -ggdb3 -DDEBUG
 
 CFLAGS		 = $(ARCH_FLAGS) \
 		   $(LTO_FLAGS) \

src/main/config/config.c
@@ -840,7 +840,7 @@ void readEEPROM(void)
 {
     // Sanity check
     if (!isEEPROMContentValid())
-        failureMode(10);
+        failureMode(FAILURE_INVALID_EEPROM_CONTENTS);
 
     suspendRxSignal();
 
@@ -920,7 +920,7 @@ void writeEEPROM(void)
 
     // Flash write failed - just die now
     if (status != FLASH_COMPLETE || !isEEPROMContentValid()) {
-        failureMode(10);
+        failureMode(FAILURE_FLASH_WRITE_FAILED);
     }
 
     resumeRxSignal();

src/main/drivers/accgyro_adxl345.c
@@ -57,7 +57,7 @@
 #define ADXL345_FIFO_STREAM 0x80
 
 static void adxl345Init(void);
-static void adxl345Read(int16_t *accelData);
+static bool adxl345Read(int16_t *accelData);
 
 static bool useFifo = false;
 
@@ -96,7 +96,7 @@ static void adxl345Init(void)
 
 uint8_t acc_samples = 0;
 
-static void adxl345Read(int16_t *accelData)
+static bool adxl345Read(int16_t *accelData)
 {
     uint8_t buf[8];
 
@@ -109,7 +109,11 @@ static void adxl345Read(int16_t *accelData)
 
         do {
             i++;
-            i2cRead(ADXL345_ADDRESS, ADXL345_DATA_OUT, 8, buf);
+
+            if (!i2cRead(ADXL345_ADDRESS, ADXL345_DATA_OUT, 8, buf)) {
+                return false;;
+            }
+
             x += (int16_t)(buf[0] + (buf[1] << 8));
             y += (int16_t)(buf[2] + (buf[3] << 8));
             z += (int16_t)(buf[4] + (buf[5] << 8));
@@ -120,9 +124,15 @@ static void adxl345Read(int16_t *accelData)
         accelData[2] = z / i;
         acc_samples = i;
     } else {
-        i2cRead(ADXL345_ADDRESS, ADXL345_DATA_OUT, 6, buf);
+
+        if (!i2cRead(ADXL345_ADDRESS, ADXL345_DATA_OUT, 6, buf)) {
+            return false;
+        }
+
         accelData[0] = buf[0] + (buf[1] << 8);
         accelData[1] = buf[2] + (buf[3] << 8);
         accelData[2] = buf[4] + (buf[5] << 8);
     }
+
+    return true;
 }

src/main/drivers/accgyro_bma280.c
@@ -33,7 +33,7 @@
 #define BMA280_PMU_RANGE   0x0F
 
 static void bma280Init(void);
-static void bma280Read(int16_t *accelData);
+static bool bma280Read(int16_t *accelData);
 
 bool bma280Detect(acc_t *acc)
 {
@@ -57,14 +57,18 @@ static void bma280Init(void)
     acc_1G = 512 * 8;
 }
 
-static void bma280Read(int16_t *accelData)
+static bool bma280Read(int16_t *accelData)
 {
     uint8_t buf[6];
 
-    i2cRead(BMA280_ADDRESS, BMA280_ACC_X_LSB, 6, buf);
+    if (!i2cRead(BMA280_ADDRESS, BMA280_ACC_X_LSB, 6, buf)) {
+        return false;
+    }
 
     // Data format is lsb<5:0><crap><new_data_bit> | msb<13:6>
     accelData[0] = (int16_t)((buf[0] >> 2) + (buf[1] << 8));
     accelData[1] = (int16_t)((buf[2] >> 2) + (buf[3] << 8));
     accelData[2] = (int16_t)((buf[4] >> 2) + (buf[5] << 8));
+
+    return true;
 }

src/main/drivers/accgyro_l3g4200d.c
@@ -57,7 +57,7 @@
 static uint8_t mpuLowPassFilter = L3G4200D_DLPF_32HZ;
 
 static void l3g4200dInit(void);
-static void l3g4200dRead(int16_t *gyroADC);
+static bool l3g4200dRead(int16_t *gyroADC);
 
 bool l3g4200dDetect(gyro_t *gyro, uint16_t lpf)
 {
@@ -103,20 +103,24 @@ static void l3g4200dInit(void)
 
     ack = i2cWrite(L3G4200D_ADDRESS, L3G4200D_CTRL_REG4, L3G4200D_FS_SEL_2000DPS);
     if (!ack)
-        failureMode(3);
+        failureMode(FAILURE_ACC_INIT);
 
     delay(5);
     i2cWrite(L3G4200D_ADDRESS, L3G4200D_CTRL_REG1, L3G4200D_POWER_ON | mpuLowPassFilter);
 }
 
 // Read 3 gyro values into user-provided buffer. No overrun checking is done.
-static void l3g4200dRead(int16_t *gyroADC)
+static bool l3g4200dRead(int16_t *gyroADC)
 {
     uint8_t buf[6];
 
-    i2cRead(L3G4200D_ADDRESS, L3G4200D_AUTOINCR | L3G4200D_GYRO_OUT, 6, buf);
+    if (!i2cRead(L3G4200D_ADDRESS, L3G4200D_AUTOINCR | L3G4200D_GYRO_OUT, 6, buf)) {
+        return false;
+    }
 
     gyroADC[X] = (int16_t)((buf[0] << 8) | buf[1]);
     gyroADC[Y] = (int16_t)((buf[2] << 8) | buf[3]);
     gyroADC[Z] = (int16_t)((buf[4] << 8) | buf[5]);
+
+    return true;
 }

src/main/drivers/accgyro_l3gd20.c
@@ -68,8 +68,8 @@
 
 static void l3gd20SpiInit(SPI_TypeDef *SPIx)
 {
+    UNUSED(SPIx); // FIXME
     GPIO_InitTypeDef GPIO_InitStructure;
-    SPI_InitTypeDef SPI_InitStructure;
 
     RCC_AHBPeriphClockCmd(L3GD20_CS_GPIO_CLK_PERIPHERAL, ENABLE);
 
@@ -120,7 +120,7 @@ void l3gd20GyroInit(void)
     delay(100);
 }
 
-static void l3gd20GyroRead(int16_t *gyroADC)
+static bool l3gd20GyroRead(int16_t *gyroADC)
 {
     uint8_t buf[6];
 
@@ -143,6 +143,8 @@ static void l3gd20GyroRead(int16_t *gyroADC)
     debug[1] = (int16_t)((buf[3] << 8) | buf[2]);
     debug[2] = (int16_t)((buf[5] << 8) | buf[4]);
 #endif
+
+    return true;
 }
 
 // Page 9 in datasheet, So - Sensitivity, Full Scale = 2000, 70 mdps/digit

src/main/drivers/accgyro_lsm303dlhc.c
@@ -131,14 +131,15 @@ void lsm303dlhcAccInit(void)
 }
 
 // Read 3 gyro values into user-provided buffer. No overrun checking is done.
-static void lsm303dlhcAccRead(int16_t *gyroADC)
+static bool lsm303dlhcAccRead(int16_t *gyroADC)
 {
     uint8_t buf[6];
 
-    bool ok = i2cRead(LSM303DLHC_ACCEL_ADDRESS, AUTO_INCREMENT_ENABLE | OUT_X_L_A, 6, buf);
+    bool ack = i2cRead(LSM303DLHC_ACCEL_ADDRESS, AUTO_INCREMENT_ENABLE | OUT_X_L_A, 6, buf);
 
-    if (!ok)
-        return;
+    if (!ack) {
+        return false;
+    }
 
     // the values range from -8192 to +8191
     gyroADC[X] = (int16_t)((buf[1] << 8) | buf[0]) / 2;
@@ -150,6 +151,8 @@ static void lsm303dlhcAccRead(int16_t *gyroADC)
     debug[1] = (int16_t)((buf[3] << 8) | buf[2]);
     debug[2] = (int16_t)((buf[5] << 8) | buf[4]);
 #endif
+
+    return true;
 }
 
 bool lsm303dlhcAccDetect(acc_t *acc)

commit 0d6d83925f6fa5e5f7c2ef1bc5afad1721ef079e

Fix hmc5883 detection broken in c6f5b98a7916a369bcf9445d04feef4036dc3ce5

src/main/drivers/compass_hmc5883l.c
@@ -310,7 +310,7 @@ bool hmc5883lRead(int16_t *magData)
     uint8_t buf[6];
 
     bool ack = i2cRead(MAG_ADDRESS, MAG_DATA_REGISTER, 6, buf);
-    if (ack) {
+    if (!ack) {
         return false;
     }
     // During calibration, magGain is 1.0, so the read returns normal non-calibrated values.
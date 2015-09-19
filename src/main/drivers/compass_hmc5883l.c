0d6d83925f6fa5e5f7c2ef1bc5afad1721ef079e
Dominic Clifton <me@dominicclifton.name>
2015-09-19 14:30:27 +0100

    Fix hmc5883 detection broken in c6f5b98a7916a369bcf9445d04feef4036dc3ce5

--- src/main/drivers/compass_hmc5883l.c
+++ src/main/drivers/compass_hmc5883l.c
@@ -310,7 +310,7 @@ bool hmc5883lRead(int16_t *magData)
     uint8_t buf[6];
 
     bool ack = i2cRead(MAG_ADDRESS, MAG_DATA_REGISTER, 6, buf);
-    if (ack) {
+    if (!ack) {
         return false;
     }
     // During calibration, magGain is 1.0, so the read returns normal non-calibrated values.
