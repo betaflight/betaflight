b723f94c1f53fa2d607037daf3c251d7a40422b2
Petr Ledvina <ledvinap@gmail.com>
2015-10-09 13:12:27 +0200

    improve code readability
    it is possible to use traling comma, it does improve git diffs etc.

--- src/main/io/display.h
+++ src/main/io/display.h
@@ -23,14 +23,12 @@ typedef enum {
     PAGE_BATTERY,
     PAGE_SENSORS,
     PAGE_RX,
-    PAGE_PROFILE
+    PAGE_PROFILE,
 #ifdef GPS
-    ,
-    PAGE_GPS
+    PAGE_GPS,
 #endif
 #ifdef ENABLE_DEBUG_OLED_PAGE
-    ,
-    PAGE_DEBUG
+    PAGE_DEBUG,
 #endif
 } pageId_e;
 
