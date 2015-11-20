392ec7412a23b230f702551e24e878d62ccf5f86
Nicholas Sherlock <n.sherlock@gmail.com>
2015-11-20 20:26:53 +1300

    Reset the card when a timeout/fault is detected during init/read/write

--- src/main/drivers/sdcard.h
+++ src/main/drivers/sdcard.h
@@ -249,12 +249,19 @@ typedef enum {
     SDCARD_BLOCK_OPERATION_ERASE,
 } sdcardBlockOperation_e;
 
+typedef enum {
+    SDCARD_OPERATION_IN_PROGRESS,
+    SDCARD_OPERATION_BUSY,
+    SDCARD_OPERATION_SUCCESS,
+    SDCARD_OPERATION_FAILURE
+} sdcardOperationStatus_e;
+
 typedef void(*sdcard_operationCompleteCallback_c)(sdcardBlockOperation_e operation, uint32_t blockIndex, uint8_t *buffer, uint32_t callbackData);
 
 bool sdcard_init();
 
 bool sdcard_readBlock(uint32_t blockIndex, uint8_t *buffer, sdcard_operationCompleteCallback_c callback, uint32_t callbackData);
-bool sdcard_writeBlock(uint32_t blockIndex, uint8_t *buffer, sdcard_operationCompleteCallback_c callback, uint32_t callbackData);
+sdcardOperationStatus_e sdcard_writeBlock(uint32_t blockIndex, uint8_t *buffer, sdcard_operationCompleteCallback_c callback, uint32_t callbackData);
 
 void sdcard_poll();
 bool sdcard_isReady();
