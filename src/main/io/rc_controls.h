72e9a4dc174b13324088e04b0e0a5a4518284365
borisbstyle <dreambb1982@gmail.com>
2015-12-07 11:28:58 +0100

    Fix motor_stop instability

--- src/main/io/rc_controls.h
+++ src/main/io/rc_controls.h
@@ -126,8 +126,7 @@ typedef struct modeActivationCondition_s {
 
 #define IS_RANGE_USABLE(range) ((range)->startStep < (range)->endStep)
 
-#define SHOULD_RESET_ERRORS ((throttleStatus == THROTTLE_LOW && !(IS_RC_MODE_ACTIVE(BOXAIRMODE))) || !(ARMING_FLAG(ARMED)) || failsafeIsActive()|| feature(FEATURE_MOTOR_STOP))
-
+#define SHOULD_RESET_ERRORS ((throttleStatus == THROTTLE_LOW && !(IS_RC_MODE_ACTIVE(BOXAIRMODE))) || !(ARMING_FLAG(ARMED)) || ((throttleStatus == THROTTLE_LOW && feature(FEATURE_MOTOR_STOP))))
 typedef struct controlRateConfig_s {
     uint8_t rcRate8;
     uint8_t rcExpo8;
