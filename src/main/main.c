0b5b327a71e8a1d983f9fd39b8ee135a37307f29
Martin Budden <mjbudden@gmail.com>
2015-12-13 12:38:09 +0000

    Added comment that pwmInit() needs to be called asap.

--- src/main/main.c
+++ src/main/main.c
@@ -265,6 +265,7 @@ void init(void)
 
     pwmRxInit(masterConfig.inputFilteringMode);
 
+    // pwmInit() needs to be called as soon as possible for ESC compatibility reasons
     pwmOutputConfiguration_t *pwmOutputConfiguration = pwmInit(&pwm_params);
 
     mixerUsePWMOutputConfiguration(pwmOutputConfiguration);
