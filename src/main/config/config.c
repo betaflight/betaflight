56102f52c4152a69722b1e365ff0bfce4561dac5
Dominic Clifton <me@dominicclifton.name>
2015-12-17 18:19:58 +0100

    Bump config version for changes in 1e6dfc925f7cc663b89f790bab97c9c239f699fd

--- src/main/config/config.c
+++ src/main/config/config.c
@@ -134,7 +134,7 @@ static uint32_t activeFeaturesLatch = 0;
 static uint8_t currentControlRateProfileIndex = 0;
 controlRateConfig_t *currentControlRateProfile;
 
-static const uint8_t EEPROM_CONF_VERSION = 107;
+static const uint8_t EEPROM_CONF_VERSION = 108;
 
 static void resetAccelerometerTrims(flightDynamicsTrims_t *accelerometerTrims)
 {
