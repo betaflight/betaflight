ef7631ea7e5722a809ddc0c45855d73a7be0755a
borisbstyle <dreambb1982@gmail.com>
2016-01-13 02:20:26 +0100

    Add more AUX channels to F3 targets in high performance mode

--- src/main/rx/rx.c
+++ src/main/rx/rx.c
@@ -92,7 +92,11 @@ uint32_t rcInvalidPulsPeriod[MAX_SUPPORTED_RC_CHANNEL_COUNT];
 #define DELAY_5_HZ (1000000 / 5)
 #define SKIP_RC_ON_SUSPEND_PERIOD 1500000           // 1.5 second period in usec (call frequency independent)
 #define SKIP_RC_SAMPLES_ON_RESUME  2                // flush 2 samples to drop wrong measurements (timing independent)
+#ifdef STM32F303xC
+#define MAX_RC_CHANNELS_HIGH_PERFORMANCE 10         // Maximum channels allowed during fast refresh rates for more performance
+#else
 #define MAX_RC_CHANNELS_HIGH_PERFORMANCE 8          // Maximum channels allowed during fast refresh rates for more performance
+#endif
 
 rxRuntimeConfig_t rxRuntimeConfig;
 static rxConfig_t *rxConfig;
@@ -446,7 +450,11 @@ static uint8_t getRxChannelCount(void) {
 
     if (!maxChannelsAllowed) {
         if (targetLooptime < 1000) {
-            maxChannelsAllowed = MAX_RC_CHANNELS_HIGH_PERFORMANCE;
+            if (MAX_RC_CHANNELS_HIGH_PERFORMANCE > rxRuntimeConfig.channelCount) {
+                maxChannelsAllowed = rxRuntimeConfig.channelCount;
+            } else {
+                maxChannelsAllowed = MAX_RC_CHANNELS_HIGH_PERFORMANCE;
+            }
         } else {
             maxChannelsAllowed = rxRuntimeConfig.channelCount;
         }
