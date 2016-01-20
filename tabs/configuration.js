2d4964f9a23bbb281e25f8e9bbb377eac72cf7d5
dan557 <dan@scottsmail.x10.mx>
2016-01-20 17:51:41 +0000

    Update Current and voltage readouts every 250ms

--- tabs/configuration.js
+++ tabs/configuration.js
@@ -371,6 +371,14 @@ TABS.configuration.initialize = function (callback, scrollPosition) {
         $('input[name="midthrottle"]').val(MISC.midrc);
         $('input[name="maxthrottle"]').val(MISC.maxthrottle);
         $('input[name="mincommand"]').val(MISC.mincommand);
+        
+        //Update Battery Data
+        function update_bat_data() {
+            MSP.send_message(MSP_codes.MSP_ANALOG, false, false, function () {
+				$('input[name="batteryvoltage"]').val([ANALOG.voltage]);
+				$('input[name="batterycurrent"]').val([ANALOG.amperage.toFixed(2)]);
+            });
+        }
 
         // fill battery
         $('input[name="mincellvoltage"]').val(MISC.vbatmincellvoltage);
@@ -571,7 +579,7 @@ TABS.configuration.initialize = function (callback, scrollPosition) {
         GUI.interval_add('status_pull', function status_pull() {
             MSP.send_message(MSP_codes.MSP_STATUS);
         }, 250, true);
-
+        GUI.interval_add('bat_update_data', update_bat_data, 250, true); // 4 fps
         GUI.content_ready(callback);
     }
 };
