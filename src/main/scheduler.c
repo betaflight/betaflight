b406b00b3d941f38a2627cd84d0ae290bedd9144
Martin Budden <mjbudden@gmail.com>
2016-01-21 08:46:37 +0000

    Fixed static inline declaration of queue iterators. Minor tidy of case when selectedTask == NULL

--- src/main/scheduler.c
+++ src/main/scheduler.c
@@ -116,7 +116,7 @@ STATIC_UNIT_TESTED void queueRemove(cfTask_t *task)
 /*
  * Returns first item queue or NULL if queue empty
  */
-INLINE_UNIT_TESTED cfTask_t *queueFirst(void)
+STATIC_INLINE_UNIT_TESTED cfTask_t *queueFirst(void)
 {
     taskQueuePos = 0;
     return taskQueueArray[0]; // guaranteed to be NULL if queue is empty
@@ -125,7 +125,7 @@ INLINE_UNIT_TESTED cfTask_t *queueFirst(void)
 /*
  * Returns next item in queue or NULL if at end of queue
  */
-INLINE_UNIT_TESTED cfTask_t *queueNext(void)
+STATIC_INLINE_UNIT_TESTED cfTask_t *queueNext(void)
 {
     return taskQueueArray[++taskQueuePos]; // guaranteed to be NULL at end of queue
 }
@@ -261,7 +261,7 @@ void scheduler(void)
 
         if (task->dynamicPriority > selectedTaskDynPrio) {
             const bool outsideRealtimeGuardInterval = (timeToNextRealtimeTask > realtimeGuardInterval);
-            bool taskCanBeChosenForScheduling =
+            const bool taskCanBeChosenForScheduling =
                 (outsideRealtimeGuardInterval) ||
                 (task->taskAgeCycles > 1) ||
                 (task->staticPriority == TASK_PRIORITY_REALTIME);
@@ -275,19 +275,17 @@ void scheduler(void)
     totalWaitingTasksSamples++;
     totalWaitingTasks += waitingTasks;
 
+    currentTask = selectedTask;
+
     /* Found a task that should be run */
     if (selectedTask != NULL) {
         selectedTask->taskLatestDeltaTime = currentTime - selectedTask->lastExecutedAt;
         selectedTask->lastExecutedAt = currentTime;
         selectedTask->dynamicPriority = 0;
 
-        currentTask = selectedTask;
-
-        const uint32_t currentTimeBeforeTaskCall = micros();
-
         /* Execute task */
+        const uint32_t currentTimeBeforeTaskCall = micros();
         selectedTask->taskFunc();
-
         const uint32_t taskExecutionTime = micros() - currentTimeBeforeTaskCall;
 
         selectedTask->averageExecutionTime = ((uint32_t)selectedTask->averageExecutionTime * 31 + taskExecutionTime) / 32;
@@ -297,11 +295,7 @@ void scheduler(void)
 #endif
 #if defined SCHEDULER_DEBUG
         debug[3] = (micros() - currentTime) - taskExecutionTime;
-#endif
-    }
-    else {
-        currentTask = NULL;
-#if defined SCHEDULER_DEBUG
+    } else {
         debug[3] = (micros() - currentTime);
 #endif
     }
