fd134d25bb797040ba14ade9ddd32425576ad52d
U-DESKTOP-6RKQHB7\Alex <alexandre.salem@gmail.com>
2016-01-23 22:42:47 +0100

    version initiale

--- main/common/colorconversion.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-rgbColor24bpp_t* hsvToRgb24(const hsvColor_t *c);
--- main/drivers/timer_stm32f10x.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-
-#include "stm32f10x.h"
-
-void TIM_SelectOCxM_NoDisable(TIM_TypeDef* TIMx, uint16_t TIM_Channel, uint16_t TIM_OCMode);
