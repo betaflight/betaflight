a31a0cb20be56f25e17cd9fbba361be4f0181a76
U-DESKTOP-6RKQHB7\Alex <alexandre.salem@gmail.com>
2016-01-26 13:25:50 +0100

    3D Airmode enhancement

--- src/main/config/config.c
+++ src/main/config/config.c
@@ -134,7 +134,7 @@ static uint32_t activeFeaturesLatch = 0;
 static uint8_t currentControlRateProfileIndex = 0;
 controlRateConfig_t *currentControlRateProfile;
 
-static const uint8_t EEPROM_CONF_VERSION = 118;
+static const uint8_t EEPROM_CONF_VERSION = 119;
 
 static void resetAccelerometerTrims(flightDynamicsTrims_t *accelerometerTrims)
 {
@@ -178,6 +178,7 @@ static void resetPidProfile(pidProfile_t *pidProfile)
 
     pidProfile->gyro_lpf_hz = 60;    // filtering ON by default
     pidProfile->dterm_lpf_hz = 0;    // filtering ON by default
+    pidProfile->deltaFromGyro = 1;
     pidProfile->airModeInsaneAcrobilityFactor = 0;
 
     pidProfile->P_f[ROLL] = 1.1f;
@@ -440,7 +441,7 @@ static void resetConf(void)
     masterConfig.rxConfig.rssi_channel = 0;
     masterConfig.rxConfig.rssi_scale = RSSI_SCALE_DEFAULT;
     masterConfig.rxConfig.rssi_ppm_invert = 0;
-    masterConfig.rxConfig.rcSmoothing = 1;
+    masterConfig.rxConfig.rcSmoothing = 0;
     masterConfig.rxConfig.fpvCamAngleDegrees = 0;
 
     resetAllRxChannelRangeConfigurations(masterConfig.rxConfig.channelRanges);
