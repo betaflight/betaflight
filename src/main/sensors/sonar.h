3e53256d433424b2398783b8ba9b1f76a567a596
borisbstyle <dreambb1982@gmail.com>
2016-02-01 22:32:44 +0100

    Sonar merge

--- src/main/sensors/sonar.h
+++ src/main/sensors/sonar.h
@@ -16,11 +16,14 @@
  */
 
 #pragma once
-#include "sensors/battery.h"
+
+#define SONAR_OUT_OF_RANGE (-1)
+
+extern int16_t sonarMaxRangeCm;
+extern int16_t sonarCfAltCm;
+extern int16_t sonarMaxAltWithTiltCm;
 
 void sonarUpdate(void);
-
 int32_t sonarRead(void);
-int32_t sonarCalculateAltitude(int32_t sonarAlt, int16_t tiltAngle);
+int32_t sonarCalculateAltitude(int32_t sonarDistance, float cosTiltAngle);
 int32_t sonarGetLatestAltitude(void);
-
