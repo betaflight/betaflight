efda3f86c1704062c1afc75dfe4144251d7e8588
borisbstyle <dreambb1982@gmail.com>
2016-02-02 00:04:45 +0100

    fix merge

--- src/main/mw.c
+++ src/main/mw.c
@@ -63,6 +63,7 @@
 #include "io/serial_cli.h"
 #include "io/serial_msp.h"
 #include "io/statusindicator.h"
+#include "io/asyncfatfs/asyncfatfs.h"
 
 #include "rx/rx.h"
 #include "rx/msp.h"
@@ -743,6 +744,10 @@ void taskMainPidLoop(void)
     if (motorControlEnable) {
         writeMotors();
     }
+
+#ifdef USE_SDCARD
+    afatfs_poll();
+#endif
 
 #ifdef BLACKBOX
     if (!cliMode && feature(FEATURE_BLACKBOX)) {
