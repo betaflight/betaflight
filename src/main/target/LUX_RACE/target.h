c89535ad261fba76636cc40109d9c5006c61eda1
borisbstyle <dreambb1982@gmail.com>
2016-02-03 02:22:34 +0100

    Disable Ledstrip for LUX_RACE target

--- src/main/target/LUX_RACE/target.h
+++ src/main/target/LUX_RACE/target.h
@@ -158,6 +158,9 @@
 #define BLACKBOX
 #define GPS
 #define GTUNE
+
+
+/* FIXME - Ledstrip cannot compile
 #define LED_STRIP
 
 #define LED_STRIP_TIMER TIM16
@@ -172,6 +175,7 @@
 #define WS2811_TIMER_APB2_PERIPHERAL    RCC_APB2Periph_TIM16
 #define WS2811_DMA_CHANNEL              DMA1_Channel3
 #define WS2811_IRQ                      DMA1_Channel3_IRQn
+*/
 
 #define TELEMETRY
 #define SERIAL_RX
