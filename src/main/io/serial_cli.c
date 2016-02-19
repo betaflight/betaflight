fa391507b600737de906b89aa3dfe562c969b544
borisbstyle <dreambb1982@gmail.com>
2016-02-19 01:45:12 +0100

    Fix typo in cli for acroPlus offset

--- src/main/io/serial_cli.c
+++ src/main/io/serial_cli.c
@@ -718,7 +718,7 @@ const clivalue_t valueTable[] = {
     { "d_vel",                      VAR_UINT8  | PROFILE_VALUE, &masterConfig.profile[0].pidProfile.D8[PIDVEL], .config.minmax = { 0,  200 } },
 
     { "acro_plus_factor",           VAR_UINT8  | PROFILE_VALUE, &masterConfig.profile[0].pidProfile.acroPlusFactor, .config.minmax = {1, 100 } },
-    { "acro_plus_offset",           VAR_UINT8  | PROFILE_VALUE, &masterConfig.profile[0].pidProfile.acroPlusFactor, .config.minmax = {1, 90 } },
+    { "acro_plus_offset",           VAR_UINT8  | PROFILE_VALUE, &masterConfig.profile[0].pidProfile.acroPlusOffset, .config.minmax = {1, 90 } },
 
 #ifdef BLACKBOX
     { "blackbox_rate_num",          VAR_UINT8  | MASTER_VALUE,  &masterConfig.blackbox_rate_num, .config.minmax = { 1,  32 } },
