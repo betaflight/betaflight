ae700e6daf44f5e0cab9ffc8c9022fd5237deb32
Dominic Clifton <me@dominicclifton.name>
2016-02-22 19:12:43 +0100

    Prepare for release.

--- js/data_storage.js
+++ js/data_storage.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var CONFIGURATOR = {
-    'releaseDate': 1454638198085, // new Date().getTime() - Fri Feb 05 2016 03:09:53 GMT+0100
+    'releaseDate': 1456164608903, // new Date().getTime() - Mon Feb 22 2016 19:10:05 GMT+0100
     
      // all versions are specified and compared using semantic versioning http://semver.org/
-    'apiVersionAccepted': '1.2.0',
+    'apiVersionAccepted': '1.2.1',
     'backupRestoreMinApiVersionAccepted': '1.5.0',
     'pidControllerChangeMinApiVersion': '1.5.0',
     'backupFileMinVersionAccepted': '0.55.0', // chrome.runtime.getManifest().version is stored as string, so does this one
