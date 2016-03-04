c0fa59aaf17dfd2e239e456c85b12d0d68336bc0
Vlad Adamenko <elf128@gmail.com>
2016-03-03 23:10:31 -0500

    Flash FS investigation

--- src/main/drivers/flash_m25p16.c
+++ src/main/drivers/flash_m25p16.c
@@ -196,7 +196,7 @@ static bool m25p16_readIdentification()
 bool m25p16_init()
 {
     //Maximum speed for standard READ command is 20mHz, other commands tolerate 25mHz
-    spiSetDivisor(M25P16_SPI_INSTANCE, SPI_18MHZ_CLOCK_DIVIDER);
+    spiSetDivisor(M25P16_SPI_INSTANCE, SPI_4_5MHZ_CLOCK_DIVIDER);
 
     return m25p16_readIdentification();
 }
