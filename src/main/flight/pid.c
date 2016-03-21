28124b09815bf35c3a0156e381bd7057aa3d4fe9
Pawel <pspychalski@gmail.com>
2016-03-21 16:12:45 +0100

    Iterm scaling for FP-PID lowered by 4

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -94,7 +94,7 @@ float pidRcCommandToRate(int16_t stick, uint8_t rate)
 }
 
 #define FP_PID_RATE_P_MULTIPLIER    40.0f
-#define FP_PID_RATE_I_MULTIPLIER    40.0f
+#define FP_PID_RATE_I_MULTIPLIER    10.0f
 #define FP_PID_RATE_D_MULTIPLIER    4000.0f
 #define FP_PID_LEVEL_P_MULTIPLIER   40.0f
 
