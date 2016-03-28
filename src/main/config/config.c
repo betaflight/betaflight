betaflight/betaflight

commit 87f160a8d1d30f7faa732b3ccfd5129a397fda1c

extract batteryConfig_t configuration settings from master_t

src/main/config/config.c
@@ -364,7 +364,7 @@ STATIC_UNIT_TESTED void resetConf(void)
     masterConfig.yaw_control_direction = 1;
     gyroConfig.gyroMovementCalibrationThreshold = 32;
 
-    resetBatteryConfig(&masterConfig.batteryConfig);
+    resetBatteryConfig(&batteryConfig);
 
 #ifdef TELEMETRY
     masterConfig.telemetryConfig.hottAlarmSoundInterval = 5;

src/main/blackbox/blackbox.c
@@ -434,7 +434,7 @@ static bool testBlackboxConditionUncached(FlightLogFieldCondition condition)
             return feature(FEATURE_VBAT);
 
         case FLIGHT_LOG_FIELD_CONDITION_AMPERAGE_ADC:
-            return feature(FEATURE_CURRENT_METER) && masterConfig.batteryConfig.currentMeterType == CURRENT_SENSOR_ADC;
+            return feature(FEATURE_CURRENT_METER) && batteryConfig.currentMeterType == CURRENT_SENSOR_ADC;
 
         case FLIGHT_LOG_FIELD_CONDITION_SONAR:
 #ifdef SONAR
@@ -1158,14 +1158,14 @@ static bool blackboxWriteSysinfo()
         break;
         case 10:
             if (testBlackboxCondition(FLIGHT_LOG_FIELD_CONDITION_VBAT)) {
-                blackboxPrintfHeaderLine("vbatscale:%u", masterConfig.batteryConfig.vbatscale);
+                blackboxPrintfHeaderLine("vbatscale:%u", batteryConfig.vbatscale);
             } else {
                 xmitState.headerIndex += 2; // Skip the next two vbat fields too
             }
         break;
         case 11:
-            blackboxPrintfHeaderLine("vbatcellvoltage:%u,%u,%u", masterConfig.batteryConfig.vbatmincellvoltage,
-                masterConfig.batteryConfig.vbatwarningcellvoltage, masterConfig.batteryConfig.vbatmaxcellvoltage);
+            blackboxPrintfHeaderLine("vbatcellvoltage:%u,%u,%u", batteryConfig.vbatmincellvoltage,
+                batteryConfig.vbatwarningcellvoltage, batteryConfig.vbatmaxcellvoltage);
         break;
         case 12:
             blackboxPrintfHeaderLine("vbatref:%u", vbatReference);
@@ -1173,7 +1173,7 @@ static bool blackboxWriteSysinfo()
         case 13:
             //Note: Log even if this is a virtual current meter, since the virtual meter uses these parameters too:
             if (feature(FEATURE_CURRENT_METER)) {
-                blackboxPrintfHeaderLine("currentMeter:%d,%d", masterConfig.batteryConfig.currentMeterOffset, masterConfig.batteryConfig.currentMeterScale);
+                blackboxPrintfHeaderLine("currentMeter:%d,%d", batteryConfig.currentMeterOffset, batteryConfig.currentMeterScale);
             }
         break;
         default:

src/main/config/config_master.h
@@ -46,8 +46,6 @@ typedef struct master_t {
     // Motor/ESC/Servo configuration
     flight3DConfig_t flight3DConfig;
 
-    batteryConfig_t batteryConfig;
-
     rxConfig_t rxConfig;
     inputFilteringMode_e inputFilteringMode;  // Use hardware input filtering, e.g. for OrangeRX PPM/PWM receivers.
 

src/main/config/parameter_group_ids.h
@@ -25,6 +25,7 @@
 #define PG_SENSOR_ALIGNMENT_CONFIG 8
 #define PG_SENSOR_TRIMS 9
 #define PG_GYRO_CONFIG 10
+#define PG_BATTERY_CONFIG 11
 
 // FIXME remove/adjust after migration
 #define PG_PROFILE 254

src/main/io/serial_cli.c
@@ -565,15 +565,15 @@ const clivalue_t valueTable[] = {
     { "hott_alarm_sound_interval",  VAR_UINT8  | MASTER_VALUE,  &masterConfig.telemetryConfig.hottAlarmSoundInterval, .config.minmax = { 0,  120 } },
 #endif
 
-    { "battery_capacity",           VAR_UINT16 | MASTER_VALUE,  &masterConfig.batteryConfig.batteryCapacity, .config.minmax = { 0,  20000 } },
-    { "vbat_scale",                 VAR_UINT8  | MASTER_VALUE,  &masterConfig.batteryConfig.vbatscale, .config.minmax = { VBAT_SCALE_MIN,  VBAT_SCALE_MAX } },
-    { "vbat_max_cell_voltage",      VAR_UINT8  | MASTER_VALUE,  &masterConfig.batteryConfig.vbatmaxcellvoltage, .config.minmax = { 10,  50 } },
-    { "vbat_min_cell_voltage",      VAR_UINT8  | MASTER_VALUE,  &masterConfig.batteryConfig.vbatmincellvoltage, .config.minmax = { 10,  50 } },
-    { "vbat_warning_cell_voltage",  VAR_UINT8  | MASTER_VALUE,  &masterConfig.batteryConfig.vbatwarningcellvoltage, .config.minmax = { 10,  50 } },
-    { "current_meter_scale",        VAR_INT16  | MASTER_VALUE,  &masterConfig.batteryConfig.currentMeterScale, .config.minmax = { -10000,  10000 } },
-    { "current_meter_offset",       VAR_UINT16 | MASTER_VALUE,  &masterConfig.batteryConfig.currentMeterOffset, .config.minmax = { 0,  3300 } },
-    { "multiwii_current_meter_output", VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP,  &masterConfig.batteryConfig.multiwiiCurrentMeterOutput, .config.lookup = { TABLE_OFF_ON } },
-    { "current_meter_type",         VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP,  &masterConfig.batteryConfig.currentMeterType, .config.lookup = { TABLE_CURRENT_SENSOR } },
+    { "battery_capacity",           VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  20000 } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, batteryCapacity)},
+    { "vbat_scale",                 VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { VBAT_SCALE_MIN,  VBAT_SCALE_MAX } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, vbatscale)},
+    { "vbat_max_cell_voltage",      VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 10,  50 } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, vbatmaxcellvoltage)},
+    { "vbat_min_cell_voltage",      VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 10,  50 } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, vbatmincellvoltage)},
+    { "vbat_warning_cell_voltage",  VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 10,  50 } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, vbatwarningcellvoltage)},
+    { "current_meter_scale",        VAR_INT16  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { -10000,  10000 } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, currentMeterScale)},
+    { "current_meter_offset",       VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  3300 } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, currentMeterOffset)},
+    { "multiwii_current_meter_output", VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, multiwiiCurrentMeterOutput)},
+    { "current_meter_type",         VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_CURRENT_SENSOR } , PG_BATTERY_CONFIG, offsetof(batteryConfig_t, currentMeterType)},
 
     { "align_gyro",                 VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_ALIGNMENT } , PG_SENSOR_ALIGNMENT_CONFIG, offsetof(sensorAlignmentConfig_t, gyro_align)},
     { "align_acc",                  VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_ALIGNMENT } , PG_SENSOR_ALIGNMENT_CONFIG, offsetof(sensorAlignmentConfig_t, acc_align)},
@@ -605,7 +605,6 @@ const clivalue_t valueTable[] = {
     { "airmode_saturation_limit",   VAR_UINT8  | MASTER_VALUE, &masterConfig.mixerConfig.airmode_saturation_limit, .config.minmax = { 0,  100 } },
     { "yaw_motor_direction",        VAR_INT8   | MASTER_VALUE, &masterConfig.mixerConfig.yaw_motor_direction, .config.minmax = { -1,  1 } },
     { "yaw_jump_prevention_limit",  VAR_UINT16 | MASTER_VALUE, &masterConfig.mixerConfig.yaw_jump_prevention_limit, .config.minmax = { YAW_JUMP_PREVENTION_LIMIT_LOW,  YAW_JUMP_PREVENTION_LIMIT_HIGH } },
-
 
 #ifdef USE_SERVOS
     { "tri_unarmed_servo",          VAR_INT8   | MASTER_VALUE | MODE_LOOKUP, &masterConfig.mixerConfig.tri_unarmed_servo, .config.lookup = { TABLE_OFF_ON } },

src/main/io/serial_msp.c
@@ -828,7 +828,7 @@ static bool processOutCommand(uint8_t cmdMSP)
         serialize8((uint8_t)constrain(vbat, 0, 255));
         serialize16((uint16_t)constrain(mAhDrawn, 0, 0xFFFF)); // milliamp hours drawn from battery
         serialize16(rssi);
-        if(masterConfig.batteryConfig.multiwiiCurrentMeterOutput) {
+        if(batteryConfig.multiwiiCurrentMeterOutput) {
             serialize16((uint16_t)constrain(amperage * 10, 0, 0xFFFF)); // send amperage in 0.001 A steps. Negative range is truncated to zero
         } else
             serialize16((int16_t)constrain(amperage, -0x8000, 0x7FFF)); // send amperage in 0.01 A steps, range is -320A to 320A
@@ -952,16 +952,16 @@ static bool processOutCommand(uint8_t cmdMSP)
         serialize8(0); // TODO gps_baudrate (an index, cleanflight uses a uint32_t
         serialize8(0); // gps_ubx_sbas
 #endif
-        serialize8(masterConfig.batteryConfig.multiwiiCurrentMeterOutput);
+        serialize8(batteryConfig.multiwiiCurrentMeterOutput);
         serialize8(masterConfig.rxConfig.rssi_channel);
         serialize8(0);
 
         serialize16(currentProfile->mag_declination / 10);
 
-        serialize8(masterConfig.batteryConfig.vbatscale);
-        serialize8(masterConfig.batteryConfig.vbatmincellvoltage);
-        serialize8(masterConfig.batteryConfig.vbatmaxcellvoltage);
-        serialize8(masterConfig.batteryConfig.vbatwarningcellvoltage);
+        serialize8(batteryConfig.vbatscale);
+        serialize8(batteryConfig.vbatmincellvoltage);
+        serialize8(batteryConfig.vbatmaxcellvoltage);
+        serialize8(batteryConfig.vbatwarningcellvoltage);
         break;
 
     case MSP_MOTOR_PINS:
@@ -1047,18 +1047,18 @@ static bool processOutCommand(uint8_t cmdMSP)
 
     case MSP_VOLTAGE_METER_CONFIG:
         headSerialReply(4);
-        serialize8(masterConfig.batteryConfig.vbatscale);
-        serialize8(masterConfig.batteryConfig.vbatmincellvoltage);
-        serialize8(masterConfig.batteryConfig.vbatmaxcellvoltage);
-        serialize8(masterConfig.batteryConfig.vbatwarningcellvoltage);
+        serialize8(batteryConfig.vbatscale);
+        serialize8(batteryConfig.vbatmincellvoltage);
+        serialize8(batteryConfig.vbatmaxcellvoltage);
+        serialize8(batteryConfig.vbatwarningcellvoltage);
         break;
 
     case MSP_CURRENT_METER_CONFIG:
         headSerialReply(7);
-        serialize16(masterConfig.batteryConfig.currentMeterScale);
-        serialize16(masterConfig.batteryConfig.currentMeterOffset);
-        serialize8(masterConfig.batteryConfig.currentMeterType);
-        serialize16(masterConfig.batteryConfig.batteryCapacity);
+        serialize16(batteryConfig.currentMeterScale);
+        serialize16(batteryConfig.currentMeterOffset);
+        serialize8(batteryConfig.currentMeterType);
+        serialize16(batteryConfig.batteryCapacity);
         break;
 
     case MSP_MIXER:
@@ -1107,8 +1107,8 @@ static bool processOutCommand(uint8_t cmdMSP)
         serialize16(boardAlignment.pitchDegrees);
         serialize16(boardAlignment.yawDegrees);
 
-        serialize16(masterConfig.batteryConfig.currentMeterScale);
-        serialize16(masterConfig.batteryConfig.currentMeterOffset);
+        serialize16(batteryConfig.currentMeterScale);
+        serialize16(batteryConfig.currentMeterOffset);
         break;
 
     case MSP_CF_SERIAL_CONFIG:
@@ -1404,16 +1404,16 @@ static bool processInCommand(void)
         read8(); // gps_baudrate
         read8(); // gps_ubx_sbas
 #endif
-        masterConfig.batteryConfig.multiwiiCurrentMeterOutput = read8();
+        batteryConfig.multiwiiCurrentMeterOutput = read8();
         masterConfig.rxConfig.rssi_channel = read8();
         read8();
 
         currentProfile->mag_declination = read16() * 10;
 
-        masterConfig.batteryConfig.vbatscale = read8();           // actual vbatscale as intended
-        masterConfig.batteryConfig.vbatmincellvoltage = read8();  // vbatlevel_warn1 in MWC2.3 GUI
-        masterConfig.batteryConfig.vbatmaxcellvoltage = read8();  // vbatlevel_warn2 in MWC2.3 GUI
-        masterConfig.batteryConfig.vbatwarningcellvoltage = read8();  // vbatlevel when buzzer starts to alert
+        batteryConfig.vbatscale = read8();           // actual vbatscale as intended
+        batteryConfig.vbatmincellvoltage = read8();  // vbatlevel_warn1 in MWC2.3 GUI
+        batteryConfig.vbatmaxcellvoltage = read8();  // vbatlevel_warn2 in MWC2.3 GUI
+        batteryConfig.vbatwarningcellvoltage = read8();  // vbatlevel when buzzer starts to alert
         break;
     case MSP_SET_MOTOR:
         for (i = 0; i < 8; i++) {
@@ -1586,17 +1586,17 @@ static bool processInCommand(void)
         break;
 
     case MSP_SET_VOLTAGE_METER_CONFIG:
-        masterConfig.batteryConfig.vbatscale = read8();           // actual vbatscale as intended
-        masterConfig.batteryConfig.vbatmincellvoltage = read8();  // vbatlevel_warn1 in MWC2.3 GUI
-        masterConfig.batteryConfig.vbatmaxcellvoltage = read8();  // vbatlevel_warn2 in MWC2.3 GUI
-        masterConfig.batteryConfig.vbatwarningcellvoltage = read8();  // vbatlevel when buzzer starts to alert
+        batteryConfig.vbatscale = read8();           // actual vbatscale as intended
+        batteryConfig.vbatmincellvoltage = read8();  // vbatlevel_warn1 in MWC2.3 GUI
+        batteryConfig.vbatmaxcellvoltage = read8();  // vbatlevel_warn2 in MWC2.3 GUI
+        batteryConfig.vbatwarningcellvoltage = read8();  // vbatlevel when buzzer starts to alert
         break;
 
     case MSP_SET_CURRENT_METER_CONFIG:
-        masterConfig.batteryConfig.currentMeterScale = read16();
-        masterConfig.batteryConfig.currentMeterOffset = read16();
-        masterConfig.batteryConfig.currentMeterType = read8();
-        masterConfig.batteryConfig.batteryCapacity = read16();
+        batteryConfig.currentMeterScale = read16();
+        batteryConfig.currentMeterOffset = read16();
+        batteryConfig.currentMeterType = read8();
+        batteryConfig.batteryCapacity = read16();
         break;
 
 #ifndef USE_QUAD_MIXER_ONLY
@@ -1654,8 +1654,8 @@ static bool processInCommand(void)
         boardAlignment.pitchDegrees = read16(); // board_align_pitch
         boardAlignment.yawDegrees = read16(); // board_align_yaw
 
-        masterConfig.batteryConfig.currentMeterScale = read16();
-        masterConfig.batteryConfig.currentMeterOffset = read16();
+        batteryConfig.currentMeterScale = read16();
+        batteryConfig.currentMeterOffset = read16();
         break;
 
     case MSP_SET_CF_SERIAL_CONFIG:

src/main/main.c
@@ -332,7 +332,7 @@ void init(void)
     const sonarHardware_t *sonarHardware = NULL;
 
     if (feature(FEATURE_SONAR)) {
-        sonarHardware = sonarGetHardwareConfiguration(&masterConfig.batteryConfig);
+        sonarHardware = sonarGetHardwareConfiguration(&batteryConfig);
         sonarGPIOConfig_t sonarGPIOConfig = {
             .gpio = SONAR_GPIO,
             .triggerPin = sonarHardware->echo_pin,
@@ -363,8 +363,10 @@ void init(void)
     pwm_params.useSoftSerial = feature(FEATURE_SOFTSERIAL);
     pwm_params.useParallelPWM = feature(FEATURE_RX_PARALLEL_PWM);
     pwm_params.useRSSIADC = feature(FEATURE_RSSI_ADC);
-    pwm_params.useCurrentMeterADC = feature(FEATURE_CURRENT_METER)
-        && masterConfig.batteryConfig.currentMeterType == CURRENT_SENSOR_ADC;
+    pwm_params.useCurrentMeterADC = (
+        feature(FEATURE_CURRENT_METER)
+        && batteryConfig.currentMeterType == CURRENT_SENSOR_ADC
+    );
     pwm_params.useLEDStrip = feature(FEATURE_LED_STRIP);
     pwm_params.usePPM = feature(FEATURE_RX_PPM);
     pwm_params.useSerialRx = feature(FEATURE_RX_SERIAL);
@@ -627,7 +629,7 @@ void init(void)
     // Now that everything has powered up the voltage and cell count be determined.
 
     if (feature(FEATURE_VBAT | FEATURE_CURRENT_METER))
-        batteryInit(&masterConfig.batteryConfig);
+        batteryInit();
 
 #ifdef DISPLAY
     if (feature(FEATURE_DISPLAY)) {

src/main/sensors/battery.c
@@ -26,6 +26,9 @@
 #include "drivers/adc.h"
 #include "drivers/system.h"
 
+#include "config/parameter_group.h"
+#include "config/parameter_group_ids.h"
+
 #include "config/runtime_config.h"
 #include "config/config.h"
 
@@ -50,7 +53,16 @@ uint16_t amperageLatestADC = 0;     // most recent raw reading from current ADC
 int32_t amperage = 0;               // amperage read by current sensor in centiampere (1/100th A)
 int32_t mAhDrawn = 0;               // milliampere hours drawn from the battery since start
 
-batteryConfig_t *batteryConfig;
+batteryConfig_t batteryConfig;
+
+static const pgRegistry_t batteryRegistry PG_REGISTRY_SECTION =
+{
+    .base = (uint8_t *)&batteryConfig,
+    .size = sizeof(batteryConfig),
+    .pgn = PG_BATTERY_CONFIG,
+    .format = 0,
+    .flags = PGC_SYSTEM
+};
 
 static batteryState_e batteryState;
 static biquad_t vbatFilterState;
@@ -60,7 +72,7 @@ uint16_t batteryAdcToVoltage(uint16_t src)
 {
     // calculate battery voltage based on ADC reading
     // result is Vbatt in 0.1V steps. 3.3V = ADC Vref, 0xFFF = 12bit adc, 110 = 11:1 voltage divider (10k:1k) * 10 for 0.1V
-    return ((((uint32_t)src * batteryConfig->vbatscale * 33 + (0xFFF * 5)) / (0xFFF * batteryConfig->vbatresdivval))/batteryConfig->vbatresdivmultiplier);
+    return ((((uint32_t)src * batteryConfig.vbatscale * 33 + (0xFFF * 5)) / (0xFFF * batteryConfig.vbatresdivval)) / batteryConfig.vbatresdivmultiplier);
 }
 
 static void updateBatteryVoltage(void)
@@ -92,14 +104,14 @@ void updateBattery(void)
         delay(VBATTERY_STABLE_DELAY);
         updateBatteryVoltage();
 
-        unsigned cells = (batteryAdcToVoltage(vbatLatestADC) / batteryConfig->vbatmaxcellvoltage) + 1;
+        unsigned cells = (batteryAdcToVoltage(vbatLatestADC) / batteryConfig.vbatmaxcellvoltage) + 1;
         if (cells > 8) {
             // something is wrong, we expect 8 cells maximum (and autodetection will be problematic at 6+ cells)
             cells = 8;
         }
         batteryCellCount = cells;
-        batteryWarningVoltage = batteryCellCount * batteryConfig->vbatwarningcellvoltage;
-        batteryCriticalVoltage = batteryCellCount * batteryConfig->vbatmincellvoltage;
+        batteryWarningVoltage = batteryCellCount * batteryConfig.vbatwarningcellvoltage;
+        batteryCriticalVoltage = batteryCellCount * batteryConfig.vbatmincellvoltage;
     }
     /* battery has been disconnected - can take a while for filter cap to disharge so we use a threshold of VBATT_PRESENT_THRESHOLD_MV */
     else if (batteryState != BATTERY_NOT_PRESENT && vbat <= VBATT_PRESENT_THRESHOLD_MV)
@@ -153,9 +165,8 @@ const char * getBatteryStateString(void)
     return batteryStateStrings[batteryState];
 }
 
-void batteryInit(batteryConfig_t *initialBatteryConfig)
+void batteryInit(void)
 {
-    batteryConfig = initialBatteryConfig;
     batteryState = BATTERY_NOT_PRESENT;
     batteryCellCount = 1;
     batteryWarningVoltage = 0;
@@ -171,9 +182,9 @@ int32_t currentSensorToCentiamps(uint16_t src)
     int32_t millivolts;
 
     millivolts = ((uint32_t)src * ADCVREF) / 4096;
-    millivolts -= batteryConfig->currentMeterOffset;
+    millivolts -= batteryConfig.currentMeterOffset;
 
-    return (millivolts * 1000) / (int32_t)batteryConfig->currentMeterScale; // current in 0.01A steps
+    return (millivolts * 1000) / (int32_t)batteryConfig.currentMeterScale; // current in 0.01A steps
 }
 
 void updateCurrentMeter(int32_t lastUpdateAt, throttleStatus_e throttleStatus)
@@ -183,19 +194,19 @@ void updateCurrentMeter(int32_t lastUpdateAt, throttleStatus_e throttleStatus)
     int32_t throttleOffset = (int32_t)rcCommand[THROTTLE] - 1000;
     int32_t throttleFactor = 0;
 
-    switch(batteryConfig->currentMeterType) {
+    switch(batteryConfig.currentMeterType) {
         case CURRENT_SENSOR_ADC:
             amperageRaw -= amperageRaw / 8;
             amperageRaw += (amperageLatestADC = adcGetChannel(ADC_CURRENT));
             amperage = currentSensorToCentiamps(amperageRaw / 8);
             break;
         case CURRENT_SENSOR_VIRTUAL:
-            amperage = (int32_t)batteryConfig->currentMeterOffset;
+            amperage = (int32_t)batteryConfig.currentMeterOffset;
             if (ARMING_FLAG(ARMED)) {
                 if (throttleStatus == THROTTLE_LOW && feature(FEATURE_MOTOR_STOP))
                     throttleOffset = 0;
                 throttleFactor = throttleOffset + (throttleOffset * throttleOffset / 50);
-                amperage += throttleFactor * (int32_t)batteryConfig->currentMeterScale  / 1000;
+                amperage += throttleFactor * (int32_t)batteryConfig.currentMeterScale  / 1000;
             }
             break;
         case CURRENT_SENSOR_NONE:
@@ -209,12 +220,12 @@ void updateCurrentMeter(int32_t lastUpdateAt, throttleStatus_e throttleStatus)
 
 uint8_t calculateBatteryPercentage(void)
 {
-    return (((uint32_t)vbat - (batteryConfig->vbatmincellvoltage * batteryCellCount)) * 100) / ((batteryConfig->vbatmaxcellvoltage - batteryConfig->vbatmincellvoltage) * batteryCellCount);
+    return (((uint32_t)vbat - (batteryConfig.vbatmincellvoltage * batteryCellCount)) * 100) / ((batteryConfig.vbatmaxcellvoltage - batteryConfig.vbatmincellvoltage) * batteryCellCount);
 }
 
 uint8_t calculateBatteryCapacityRemainingPercentage(void)
 {
-    uint16_t batteryCapacity = batteryConfig->batteryCapacity;
+    uint16_t batteryCapacity = batteryConfig.batteryCapacity;
 
     return constrain((batteryCapacity - constrain(mAhDrawn, 0, 0xFFFF)) * 100.0f / batteryCapacity , 0, 100);
 }

commit a26999f9771061d750d942df81cb5de2337182a9

fix alienwiif3 target broken by 87f160a8d1d30f7faa732b3ccfd5129a397fda1c

src/main/config/config.c
@@ -526,7 +526,7 @@ STATIC_UNIT_TESTED void resetConf(void)
     featureSet(FEATURE_MOTOR_STOP);
 #ifdef ALIENWIIF3
     masterConfig.serialConfig.portConfigs[2].functionMask = FUNCTION_RX_SERIAL;
-    masterConfig.batteryConfig.vbatscale = 20;
+    batteryConfig.vbatscale = 20;
 #else
     masterConfig.serialConfig.portConfigs[1].functionMask = FUNCTION_RX_SERIAL;
 #endif