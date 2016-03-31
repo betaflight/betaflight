betaflight/betaflight

commit 40bbbc9ace9cfe0141a5c62c85580b7647b1239f

extract imu configuration from master_t.

src/main/config/config.c
@@ -340,9 +340,13 @@ STATIC_UNIT_TESTED void resetConf(void)
 
     featureSet(FEATURE_FAILSAFE);
 
-    // global settings
-    masterConfig.small_angle = 25;
-    masterConfig.dcm_kp = 2500;                // 1.0 * 10000
+    // imu settings
+    imuConfig.small_angle = 25;
+    imuConfig.dcm_kp = 2500;                // 1.0 * 10000
+    imuConfig.looptime = 2000;
+    imuConfig.gyroSync = 1;
+    imuConfig.gyroSyncDenominator = 1;
+
     gyroConfig.gyro_lpf = 1;                 // supported by all gyro drivers now. In case of ST gyro, will default to 32Hz instead
     gyroConfig.soft_gyro_lpf_hz = 60;        // Software based lpf filter for gyro
 
@@ -396,10 +400,6 @@ STATIC_UNIT_TESTED void resetConf(void)
 #endif
 
     resetSerialConfig(&serialConfig);
-
-    masterConfig.looptime = 2000;
-    masterConfig.gyroSync = 1;
-    masterConfig.gyroSyncDenominator = 1;
 
     systemConfig.i2c_highspeed = 1;
 
@@ -648,11 +648,11 @@ void activateConfig(void)
         &masterConfig.rxConfig
     );
 
-    imuRuntimeConfig.dcm_kp = masterConfig.dcm_kp / 10000.0f;
-    imuRuntimeConfig.dcm_ki = masterConfig.dcm_ki / 10000.0f;
+    imuRuntimeConfig.dcm_kp = imuConfig.dcm_kp / 10000.0f;
+    imuRuntimeConfig.dcm_ki = imuConfig.dcm_ki / 10000.0f;
     imuRuntimeConfig.acc_cut_hz = currentProfile->acc_cut_hz;
     imuRuntimeConfig.acc_unarmedcal = currentProfile->acc_unarmedcal;
-    imuRuntimeConfig.small_angle = masterConfig.small_angle;
+    imuRuntimeConfig.small_angle = imuConfig.small_angle;
 
     imuConfigure(
         &imuRuntimeConfig,

src/main/config/config_master.h
@@ -22,14 +22,6 @@ typedef struct master_t {
     // Profile configuration
     uint8_t current_profile_index;
 
-    // IMU configuration
-    uint16_t looptime;                      // imu loop time in us
-    uint8_t gyroSync;                       // Enable interrupt based loop
-    uint8_t gyroSyncDenominator;            // Gyro sync Denominator
-    uint16_t dcm_kp;                        // DCM filter proportional gain ( x 10000)
-    uint16_t dcm_ki;                        // DCM filter integral gain ( x 10000)
-    uint8_t small_angle;                    // Angle used for mag hold threshold.
-
     // Motor/ESC/Servo configuration
     flight3DConfig_t flight3DConfig;
 

src/main/config/parameter_group_ids.h
@@ -37,6 +37,7 @@
 #define PG_FEATURE_CONFIG 19
 #define PG_MIXER_CONFIG 20
 #define PG_SERVO_MIXER 21
+#define PG_IMU_CONFIG 22
 
 // Driver configuration
 #define PG_DRIVER_PWM_RX_CONFIG 100

src/main/drivers/gyro_sync.c
@@ -51,7 +51,7 @@ void gyroUpdateSampleRate(uint32_t looptime, uint8_t lpf, uint8_t gyroSync, uint
             gyroSamplePeriod = 1000;
         }
 
-        mpuDividerDrops  = gyroSyncDenominator - 1;
+        mpuDividerDrops = gyroSyncDenominator - 1;
         targetLooptime = (mpuDividerDrops + 1) * gyroSamplePeriod;
     } else {
     	mpuDividerDrops = 0;

src/main/flight/imu.c
@@ -31,6 +31,7 @@
 #include "common/filter.h"
 
 #include "config/runtime_config.h"
+#include "config/parameter_group_ids.h"
 #include "config/parameter_group.h"
 
 #include "drivers/system.h"
@@ -73,6 +74,8 @@ static bool isAccelUpdatedAtLeastOnce = false;
 
 static imuRuntimeConfig_t *imuRuntimeConfig;
 static accDeadband_t *accDeadband;
+
+PG_REGISTER(imuConfig_t, imuConfig, PG_IMU_CONFIG, 0);
 
 STATIC_UNIT_TESTED float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;    // quaternion of sensor frame relative to earth frame
 static float rMat[3][3];

src/main/flight/imu.h
@@ -46,6 +46,18 @@ typedef struct accDeadband_s {
     uint8_t z;                  // set the acc deadband for z-Axis, this ignores small accelerations
 } accDeadband_t;
 
+typedef struct imuConfig_s {
+    // IMU configuration
+    uint16_t looptime;                      // imu loop time in us
+    uint8_t gyroSync;                       // Enable interrupt based loop
+    uint8_t gyroSyncDenominator;            // Gyro sync Denominator
+    uint16_t dcm_kp;                        // DCM filter proportional gain ( x 10000)
+    uint16_t dcm_ki;                        // DCM filter integral gain ( x 10000)
+    uint8_t small_angle;                    // Angle used for mag hold threshold.
+} imuConfig_t;
+
+PG_DECLARE(imuConfig_t, imuConfig);
+
 typedef struct imuRuntimeConfig_s {
     uint8_t acc_cut_hz;
     uint8_t acc_unarmedcal;

src/main/io/serial_cli.c
@@ -491,11 +491,11 @@ typedef struct {
 } __attribute__((packed)) clivalue_t;
 
 const clivalue_t valueTable[] = {
-    { "looptime",                   VAR_UINT16 | MASTER_VALUE,  &masterConfig.looptime, .config.minmax = {0, 9000} },
+    { "looptime",                   VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = {0, 9000} , PG_IMU_CONFIG, offsetof(imuConfig_t, looptime)},
     { "emf_avoidance",              VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_SYSTEM_CONFIG, offsetof(systemConfig_t, emf_avoidance)},
     { "i2c_highspeed",              VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_SYSTEM_CONFIG, offsetof(systemConfig_t, i2c_highspeed)},
-    { "gyro_sync",                  VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP,  &masterConfig.gyroSync, .config.lookup = { TABLE_OFF_ON } },
-    { "gyro_sync_denom",            VAR_UINT8  | MASTER_VALUE,  &masterConfig.gyroSyncDenominator, .config.minmax = { 1,  32 } },
+    { "gyro_sync",                  VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_IMU_CONFIG, offsetof(imuConfig_t, gyroSync)},
+    { "gyro_sync_denom",            VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 1,  32 } , PG_IMU_CONFIG, offsetof(imuConfig_t, gyroSyncDenominator)},
 
     { "mid_rc",                     VAR_UINT16 | MASTER_VALUE,  &masterConfig.rxConfig.midrc, .config.minmax = { 1200,  1700 } },
     { "min_check",                  VAR_UINT16 | MASTER_VALUE,  &masterConfig.rxConfig.mincheck, .config.minmax = { PWM_RANGE_ZERO,  PWM_RANGE_MAX } },
@@ -526,7 +526,7 @@ const clivalue_t valueTable[] = {
     { "auto_disarm_delay",          VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  60 } , PG_ARMING_CONFIG, offsetof(armingConfig_t, auto_disarm_delay)},
     { "max_arm_angle",              VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  180 } , PG_ARMING_CONFIG, offsetof(armingConfig_t, max_arm_angle)},
 
-    { "small_angle",                VAR_UINT8  | MASTER_VALUE,  &masterConfig.small_angle, .config.minmax = { 0,  180 } },
+    { "small_angle",                VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  180 } , PG_IMU_CONFIG, offsetof(imuConfig_t, small_angle)},
 
     { "fixedwing_althold_dir",      VAR_INT8   | MASTER_VALUE,  &masterConfig.airplaneConfig.fixedwing_althold_dir, .config.minmax = { -1,  1 } },
 
@@ -596,8 +596,8 @@ const clivalue_t valueTable[] = {
     { "gyro_lpf",                   VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_GYRO_LPF } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, gyro_lpf)},
     { "gyro_soft_lpf",              VAR_FLOAT  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  500 } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, soft_gyro_lpf_hz)},
     { "moron_threshold",            VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  128 } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, gyroMovementCalibrationThreshold)},
-    { "imu_dcm_kp",                 VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_kp, .config.minmax = { 0,  20000 } },
-    { "imu_dcm_ki",                 VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_ki, .config.minmax = { 0,  20000 } },
+    { "imu_dcm_kp",                 VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  20000 } , PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_kp)},
+    { "imu_dcm_ki",                 VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  20000 } , PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_ki)},
 
     { "alt_hold_deadband",          VAR_UINT8  | PROFILE_VALUE, 0,  .config.minmax = { 1,  250 } , PG_PROFILE, offsetof(profile_t, rcControlsConfig.alt_hold_deadband)},
     { "alt_hold_fast_change",       VAR_UINT8  | PROFILE_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_PROFILE, offsetof(profile_t, rcControlsConfig.alt_hold_fast_change)},

src/main/io/serial_msp.c
@@ -836,7 +836,7 @@ static bool processOutCommand(uint8_t cmdMSP)
         break;
     case MSP_LOOP_TIME:
         headSerialReply(2);
-        serialize16(masterConfig.looptime);
+        serialize16(imuConfig.looptime);
         break;
     case MSP_RC_TUNING:
         headSerialReply(11);
@@ -1288,7 +1288,7 @@ static bool processInCommand(void)
         armingConfig.disarm_kill_switch = read8();
         break;
     case MSP_SET_LOOP_TIME:
-        masterConfig.looptime = read16();
+        imuConfig.looptime = read16();
         break;
     case MSP_SET_PID_CONTROLLER:
         pidProfile->pidController = read8();

commit dfe414726c8d5e18c468bdc02df19d5fea66a011

fix collibri race target, broken by 40bbbc9ace9cfe0141a5c62c85580b7647b1239f

src/main/config/config.c
@@ -491,7 +491,7 @@ STATIC_UNIT_TESTED void resetConf(void)
 
     // alternative defaults settings for COLIBRI RACE targets
 #if defined(COLIBRI_RACE)
-    masterConfig.looptime = 1000;
+    imuConfig.looptime = 1000;
 
     pidProfile->pidController = 1;
 