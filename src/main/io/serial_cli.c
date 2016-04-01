e924957947c83d34cd7d5d5066cef0aa268f7f80
Dominic Clifton <me@dominicclifton.name>
2016-04-01 20:11:38 +0200

    fix incorrect parameter group for cli configuration of navigation profile.

--- src/main/io/serial_cli.c
+++ src/main/io/serial_cli.c
@@ -556,10 +556,10 @@ const clivalue_t valueTable[] = {
 
     { "gps_wp_radius",              VAR_UINT16 | PROFILE_VALUE, .config.minmax = { 0,  2000 }, PG_PID_PROFILE, offsetof(gpsProfile_t, gps_wp_radius) },
 
-    { "nav_controls_heading",       VAR_UINT8  | PROFILE_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_OFF_ON }, PG_PROFILE ,offsetof(gpsProfile_t, nav_controls_heading) },
-    { "nav_speed_min",              VAR_UINT16 | PROFILE_VALUE, .config.minmax = { 10,  2000 }, PG_PROFILE, offsetof(gpsProfile_t, nav_speed_min) },
-    { "nav_speed_max",              VAR_UINT16 | PROFILE_VALUE, .config.minmax = { 10,  2000 }, PG_PROFILE, offsetof(gpsProfile_t, nav_speed_max) },
-    { "nav_slew_rate",              VAR_UINT8  | PROFILE_VALUE, .config.minmax = { 0,  100 }, PG_PROFILE, offsetof(gpsProfile_t, nav_slew_rate) },
+    { "nav_controls_heading",       VAR_UINT8  | PROFILE_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_OFF_ON }, PG_NAVIGATION_CONFIG, offsetof(gpsProfile_t, nav_controls_heading) },
+    { "nav_speed_min",              VAR_UINT16 | PROFILE_VALUE, .config.minmax = { 10,  2000 }, PG_NAVIGATION_CONFIG, offsetof(gpsProfile_t, nav_speed_min) },
+    { "nav_speed_max",              VAR_UINT16 | PROFILE_VALUE, .config.minmax = { 10,  2000 }, PG_NAVIGATION_CONFIG, offsetof(gpsProfile_t, nav_speed_max) },
+    { "nav_slew_rate",              VAR_UINT8  | PROFILE_VALUE, .config.minmax = { 0,  100 }, PG_NAVIGATION_CONFIG, offsetof(gpsProfile_t, nav_slew_rate) },
 #endif
 
 #ifdef TELEMETRY
