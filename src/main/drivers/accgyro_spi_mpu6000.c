9000decdf7349413ed5d957b0b9562a9a99e6a60
borisbstyle <dreambb1982@gmail.com>
2016-04-15 15:54:15 +0200

    Fix SPI speed for MPU6000 for CC3D target and some new targets

--- src/main/drivers/accgyro_spi_mpu6000.c
+++ src/main/drivers/accgyro_spi_mpu6000.c
@@ -132,6 +132,8 @@ void mpu6000SpiGyroInit(uint8_t lpf)
     mpu6000WriteRegister(MPU6000_CONFIG, lpf);
     delayMicroseconds(1);
 
+    spiSetDivisor(MPU6000_SPI_INSTANCE, SPI_18MHZ_CLOCK_DIVIDER);  // 18 MHz SPI clock
+
     int16_t data[3];
     mpuGyroRead(data);
 
