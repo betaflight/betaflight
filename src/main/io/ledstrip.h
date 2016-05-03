3a3424a2354520e4b71a5d82249a0cf64a631f3b
gael <gaeljames@gmail.com>
2016-05-04 01:29:39 +0200

    LEDs: GPS fix, Sat count & RSSI
    New LED functions: G & S
    Isssue #709
    Isssue #1384

--- src/main/io/ledstrip.h
+++ src/main/io/ledstrip.h
@@ -49,6 +49,8 @@ typedef enum {
     LED_FUNCTION_THROTTLE    = (1 << 10),
     LED_FUNCTION_THRUST_RING = (1 << 11),
     LED_FUNCTION_COLOR       = (1 << 12),
+    LED_FUNCTION_GPS         = (1 << 13),
+    LED_FUNCTION_RSSI        = (1 << 14),
 } ledFlag_e;
 
 #define LED_DIRECTION_BIT_OFFSET 0
@@ -68,7 +70,9 @@ typedef enum {
     LED_FUNCTION_ARM_STATE | \
     LED_FUNCTION_THROTTLE | \
     LED_FUNCTION_THRUST_RING | \
-    LED_FUNCTION_COLOR \
+    LED_FUNCTION_COLOR | \
+    LED_FUNCTION_GPS | \
+    LED_FUNCTION_RSSI \
 )
 
 
@@ -100,3 +104,4 @@ void ledStripInit(void);
 void ledStripEnable(void);
 void reevalulateLedConfig(void);
 
+extern uint16_t rssi; // FIXME dependency on mw.c
