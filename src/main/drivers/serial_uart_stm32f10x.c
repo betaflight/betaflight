betaflight/betaflight

commit c031e8d4aa7383ff5a933be06d6355507e6eb552

Remove PORT103V support that was added in the commit for UART4/5 support.
The PORT103R also supports UART4/5 and we don't want to support another
development target right now.

Cleanup USART/UART inconsistencies in STMF1/F3 uart implementions.
Cleanup remove some F1/F3 specific code from PWM mapping code.
Relocate usart TX configuration code from system.c into serial_uart.c.

src/main/drivers/serial_uart_stm32f10x.c
@@ -35,32 +35,33 @@
 #include "serial.h"
 #include "serial_uart.h"
 #include "serial_uart_impl.h"
+#include "serial_uart_stm32f10x.h"
 
-#ifdef USE_USART1
+#ifdef USE_UART1
 static uartPort_t uartPort1;
 #endif
 
-#ifdef USE_USART2
+#ifdef USE_UART2
 static uartPort_t uartPort2;
 #endif
 
-#ifdef USE_USART3
+#ifdef USE_UART3
 static uartPort_t uartPort3;
 #endif
 
-#ifdef USE_USART4
+#ifdef USE_UART4
 static uartPort_t uartPort4;
 #endif
 
-#ifdef USE_USART5
+#ifdef USE_UART5
 static uartPort_t uartPort5;
 #endif
 
 // Using RX DMA disables the use of receive callbacks
-#define USE_USART1_RX_DMA
+#define USE_UART1_RX_DMA
 
 #if defined(CC3D) // FIXME move board specific code to target.h files.
-#undef USE_USART1_RX_DMA
+#undef USE_UART1_RX_DMA
 #endif
 
 void usartIrqCallback(uartPort_t *s)
@@ -90,9 +91,9 @@ void usartIrqCallback(uartPort_t *s)
     }
 }
 
-#ifdef USE_USART1
-// USART1 - Telemetry (RX/TX by DMA)
-uartPort_t *serialUSART1(uint32_t baudRate, portMode_t mode, portOptions_t options)
+#ifdef USE_UART1
+// UART1 - Telemetry (RX/TX by DMA)
+uartPort_t *serialUART1(uint32_t baudRate, portMode_t mode, portOptions_t options)
 {
     uartPort_t *s;
     static volatile uint8_t rx1Buffer[UART1_RX_BUFFER_SIZE];
@@ -113,34 +114,39 @@ uartPort_t *serialUSART1(uint32_t baudRate, portMode_t mode, portOptions_t optio
     s->USARTx = USART1;
 
 
-#ifdef USE_USART1_RX_DMA
+#ifdef USE_UART1_RX_DMA
     s->rxDMAChannel = DMA1_Channel5;
     s->rxDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
 #endif
     s->txDMAChannel = DMA1_Channel4;
     s->txDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+#ifdef UART1_APB1_PERIPHERALS
+    RCC_APB1PeriphClockCmd(UART1_APB1_PERIPHERALS, ENABLE);
+#endif
+#ifdef UART1_APB2_PERIPHERALS
+    RCC_APB2PeriphClockCmd(UART1_APB2_PERIPHERALS, ENABLE);
+#endif
+#ifdef UART1_AHB_PERIPHERALS
+    RCC_APB2PeriphClockCmd(UART1_AHB_PERIPHERALS, ENABLE);
+#endif
 
-    // USART1_TX    PA9
-    // USART1_RX    PA10
     gpio.speed = Speed_2MHz;
 
-    gpio.pin = Pin_9;
+    gpio.pin = UART1_TX_PIN;
     if (options & SERIAL_BIDIR) {
         gpio.mode = Mode_AF_OD;
-        gpioInit(GPIOA, &gpio);
+        gpioInit(UART1_GPIO, &gpio);
     } else {
         if (mode & MODE_TX) {
             gpio.mode = Mode_AF_PP;
-            gpioInit(GPIOA, &gpio);
+            gpioInit(UART1_GPIO, &gpio);
         }
 
         if (mode & MODE_RX) {
-            gpio.pin = Pin_10;
+            gpio.pin = UART1_RX_PIN;
             gpio.mode = Mode_IPU;
-            gpioInit(GPIOA, &gpio);
+            gpioInit(UART1_GPIO, &gpio);
         }
     }
 
@@ -151,7 +157,7 @@ uartPort_t *serialUSART1(uint32_t baudRate, portMode_t mode, portOptions_t optio
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 
-#ifndef USE_USART1_RX_DMA
+#ifndef USE_UART1_RX_DMA
     // RX/TX Interrupt
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = NVIC_PRIORITY_BASE(NVIC_PRIO_SERIALUART1);
@@ -164,7 +170,7 @@ uartPort_t *serialUSART1(uint32_t baudRate, portMode_t mode, portOptions_t optio
 }
 
 
-// USART1 Tx DMA Handler
+// UART1 Tx DMA Handler
 void DMA1_Channel4_IRQHandler(void)
 {
     uartPort_t *s = &uartPort1;
@@ -177,7 +183,7 @@ void DMA1_Channel4_IRQHandler(void)
         s->txDMAEmpty = true;
 }
 
-// USART1 Rx/Tx IRQ Handler
+// UART1 Rx/Tx IRQ Handler
 void USART1_IRQHandler(void)
 {
     uartPort_t *s = &uartPort1;
@@ -186,9 +192,9 @@ void USART1_IRQHandler(void)
 
 #endif
 
-#ifdef USE_USART2
-// USART2 - GPS or Spektrum or ?? (RX + TX by IRQ)
-uartPort_t *serialUSART2(uint32_t baudRate, portMode_t mode, portOptions_t options)
+#ifdef USE_UART2
+// UART2 - GPS or Spektrum or ?? (RX + TX by IRQ)
+uartPort_t *serialUART2(uint32_t baudRate, portMode_t mode, portOptions_t options)
 {
     uartPort_t *s;
     static volatile uint8_t rx2Buffer[UART2_RX_BUFFER_SIZE];
@@ -211,27 +217,32 @@ uartPort_t *serialUSART2(uint32_t baudRate, portMode_t mode, portOptions_t optio
     s->txDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
     s->rxDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
 
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+#ifdef UART2_APB1_PERIPHERALS
+    RCC_APB1PeriphClockCmd(UART2_APB1_PERIPHERALS, ENABLE);
+#endif
+#ifdef UART2_APB2_PERIPHERALS
+    RCC_APB2PeriphClockCmd(UART2_APB2_PERIPHERALS, ENABLE);
+#endif
+#ifdef UART2_AHB_PERIPHERALS
+    RCC_APB2PeriphClockCmd(UART2_AHB_PERIPHERALS, ENABLE);
+#endif
 
-    // USART2_TX    PA2
-    // USART2_RX    PA3
     gpio.speed = Speed_2MHz;
 
-    gpio.pin = Pin_2;
+    gpio.pin = UART2_TX_PIN;
     if (options & SERIAL_BIDIR) {
         gpio.mode = Mode_AF_OD;
-        gpioInit(GPIOA, &gpio);
+        gpioInit(UART2_GPIO, &gpio);
     } else {
         if (mode & MODE_TX) {
             gpio.mode = Mode_AF_PP;
-            gpioInit(GPIOA, &gpio);
+            gpioInit(UART2_GPIO, &gpio);
         }
 
         if (mode & MODE_RX) {
-            gpio.pin = Pin_3;
+            gpio.pin = UART2_RX_PIN;
             gpio.mode = Mode_IPU;
-            gpioInit(GPIOA, &gpio);
+            gpioInit(UART2_GPIO, &gpio);
         }
     }
 
@@ -246,7 +257,7 @@ uartPort_t *serialUSART2(uint32_t baudRate, portMode_t mode, portOptions_t optio
 }
 
 
-// USART2 Rx/Tx IRQ Handler
+// UART2 Rx/Tx IRQ Handler
 void USART2_IRQHandler(void)
 {
     uartPort_t *s = &uartPort2;
@@ -255,9 +266,9 @@ void USART2_IRQHandler(void)
 
 #endif
 
-#ifdef USE_USART3
-// USART3
-uartPort_t *serialUSART3(uint32_t baudRate, portMode_t mode, portOptions_t options)
+#ifdef USE_UART3
+// UART3
+uartPort_t *serialUART3(uint32_t baudRate, portMode_t mode, portOptions_t options)
 {
     uartPort_t *s;
     static volatile uint8_t rx3Buffer[UART3_RX_BUFFER_SIZE];
@@ -280,29 +291,29 @@ uartPort_t *serialUSART3(uint32_t baudRate, portMode_t mode, portOptions_t optio
     s->txDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
     s->rxDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
 
-#ifdef USART3_APB1_PERIPHERALS
-    RCC_APB1PeriphClockCmd(USART3_APB1_PERIPHERALS, ENABLE);
+#ifdef UART3_APB1_PERIPHERALS
+    RCC_APB1PeriphClockCmd(UART3_APB1_PERIPHERALS, ENABLE);
 #endif
-#ifdef USART3_APB2_PERIPHERALS
-    RCC_APB2PeriphClockCmd(USART3_APB2_PERIPHERALS, ENABLE);
+#ifdef UART3_APB2_PERIPHERALS
+    RCC_APB2PeriphClockCmd(UART3_APB2_PERIPHERALS, ENABLE);
 #endif
 
     gpio.speed = Speed_2MHz;
 
-    gpio.pin = USART3_TX_PIN;
+    gpio.pin = UART3_TX_PIN;
     if (options & SERIAL_BIDIR) {
         gpio.mode = Mode_AF_OD;
-        gpioInit(USART3_GPIO, &gpio);
+        gpioInit(UART3_GPIO, &gpio);
     } else {
         if (mode & MODE_TX) {
             gpio.mode = Mode_AF_PP;
-            gpioInit(USART3_GPIO, &gpio);
+            gpioInit(UART3_GPIO, &gpio);
         }
 
         if (mode & MODE_RX) {
-            gpio.pin = USART3_RX_PIN;
+            gpio.pin = UART3_RX_PIN;
             gpio.mode = Mode_IPU;
-            gpioInit(USART3_GPIO, &gpio);
+            gpioInit(UART3_GPIO, &gpio);
         }
     }
 
@@ -316,7 +327,7 @@ uartPort_t *serialUSART3(uint32_t baudRate, portMode_t mode, portOptions_t optio
     return s;
 }
 
-// USART2 Rx/Tx IRQ Handler
+// UART2 Rx/Tx IRQ Handler
 void USART3_IRQHandler(void)
 {
     uartPort_t *s = &uartPort3;
@@ -324,9 +335,9 @@ void USART3_IRQHandler(void)
 }
 #endif
 
-#ifdef USE_USART4
+#ifdef USE_UART4
 // UART4
-uartPort_t *serialUSART4(uint32_t baudRate, portMode_t mode, portOptions_t options)
+uartPort_t *serialUART4(uint32_t baudRate, portMode_t mode, portOptions_t options)
 {
     uartPort_t *s;
     static volatile uint8_t rx4Buffer[UART4_RX_BUFFER_SIZE];
@@ -349,29 +360,29 @@ uartPort_t *serialUSART4(uint32_t baudRate, portMode_t mode, portOptions_t optio
     s->txDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
     s->rxDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
 
-#ifdef USART4_APB1_PERIPHERALS
-    RCC_APB1PeriphClockCmd(USART4_APB1_PERIPHERALS, ENABLE);
+#ifdef UART4_APB1_PERIPHERALS
+    RCC_APB1PeriphClockCmd(UART4_APB1_PERIPHERALS, ENABLE);
 #endif
-#ifdef USART4_APB2_PERIPHERALS
-    RCC_APB2PeriphClockCmd(USART4_APB2_PERIPHERALS, ENABLE);
+#ifdef UART4_APB2_PERIPHERALS
+    RCC_APB2PeriphClockCmd(UART4_APB2_PERIPHERALS, ENABLE);
 #endif
 
     gpio.speed = Speed_2MHz;
 
-    gpio.pin = USART4_TX_PIN;
+    gpio.pin = UART4_TX_PIN;
     if (options & SERIAL_BIDIR) {
         gpio.mode = Mode_AF_OD;
-        gpioInit(USART4_GPIO, &gpio);
+        gpioInit(UART4_GPIO, &gpio);
     } else {
         if (mode & MODE_TX) {
             gpio.mode = Mode_AF_PP;
-            gpioInit(USART4_GPIO, &gpio);
+            gpioInit(UART4_GPIO, &gpio);
         }
 
         if (mode & MODE_RX) {
-            gpio.pin = USART4_RX_PIN;
+            gpio.pin = UART4_RX_PIN;
             gpio.mode = Mode_IPU;
-            gpioInit(USART4_GPIO, &gpio);
+            gpioInit(UART4_GPIO, &gpio);
         }
     }
 
@@ -393,9 +404,9 @@ void UART4_IRQHandler(void)
 }
 #endif
 
-#ifdef USE_USART5
+#ifdef USE_UART5
 // UART5
-uartPort_t *serialUSART5(uint32_t baudRate, portMode_t mode, portOptions_t options)
+uartPort_t *serialUART5(uint32_t baudRate, portMode_t mode, portOptions_t options)
 {
     uartPort_t *s;
     static volatile uint8_t rx5Buffer[UART5_RX_BUFFER_SIZE];
@@ -418,32 +429,32 @@ uartPort_t *serialUSART5(uint32_t baudRate, portMode_t mode, portOptions_t optio
     s->txDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
     s->rxDMAPeripheralBaseAddr = (uint32_t)&s->USARTx->DR;
 
-#ifdef USART5_APB1_PERIPHERALS
-    RCC_APB1PeriphClockCmd(USART5_APB1_PERIPHERALS, ENABLE);
+#ifdef UART5_APB1_PERIPHERALS
+    RCC_APB1PeriphClockCmd(UART5_APB1_PERIPHERALS, ENABLE);
 #endif
-#ifdef USART5_APB2_PERIPHERALS_TX
-    RCC_APB2PeriphClockCmd(USART5_APB2_PERIPHERALS_TX, ENABLE);
+#ifdef UART5_APB2_PERIPHERALS_TX
+    RCC_APB2PeriphClockCmd(UART5_APB2_PERIPHERALS_TX, ENABLE);
 #endif
-#ifdef USART5_APB2_PERIPHERALS_RX
-    RCC_APB2PeriphClockCmd(USART5_APB2_PERIPHERALS_TX, ENABLE);
+#ifdef UART5_APB2_PERIPHERALS_RX
+    RCC_APB2PeriphClockCmd(UART5_APB2_PERIPHERALS_TX, ENABLE);
 #endif
 
     gpio.speed = Speed_2MHz;
 
-    gpio.pin = USART5_TX_PIN;
+    gpio.pin = UART5_TX_PIN;
     if (options & SERIAL_BIDIR) {
         gpio.mode = Mode_AF_OD;
-        gpioInit(USART5_GPIO_TX, &gpio);
+        gpioInit(UART5_GPIO_TX, &gpio);
     } else {
         if (mode & MODE_TX) {
             gpio.mode = Mode_AF_PP;
-            gpioInit(USART5_GPIO_TX, &gpio);
+            gpioInit(UART5_GPIO_TX, &gpio);
         }
 
         if (mode & MODE_RX) {
-            gpio.pin = USART5_RX_PIN;
+            gpio.pin = UART5_RX_PIN;
             gpio.mode = Mode_IPU;
-            gpioInit(USART5_GPIO_RX, &gpio);
+            gpioInit(UART5_GPIO_RX, &gpio);
         }
     }
 

Makefile
@@ -35,7 +35,7 @@ FLASH_SIZE ?=
 
 FORKNAME			 = cleanflight
 
-VALID_TARGETS	 = ALIENWIIF1 ALIENWIIF3 CC3D CHEBUZZF3 CJMCU COLIBRI_RACE LUX_RACE EUSTM32F103RC MOTOLAB NAZE NAZE32PRO OLIMEXINO PORT103R PORT103V RMDO SPARKY SPRACINGF3 SPRACINGF3MINI STM32F3DISCOVERY 
+VALID_TARGETS	 = ALIENWIIF1 ALIENWIIF3 CC3D CHEBUZZF3 CJMCU COLIBRI_RACE LUX_RACE EUSTM32F103RC MOTOLAB NAZE NAZE32PRO OLIMEXINO PORT103R RMDO SPARKY SPRACINGF3 SPRACINGF3MINI STM32F3DISCOVERY 
 
 # Configure default flash sizes for the targets
 ifeq ($(FLASH_SIZE),)
@@ -43,7 +43,7 @@ ifeq ($(TARGET),$(filter $(TARGET),CJMCU))
 FLASH_SIZE = 64
 else ifeq ($(TARGET),$(filter $(TARGET),ALIENWIIF1 CC3D NAZE OLIMEXINO RMDO))
 FLASH_SIZE = 128
-else ifeq ($(TARGET),$(filter $(TARGET),ALIENWIIF3 CHEBUZZF3 COLIBRI_RACE LUX_RACE EUSTM32F103RC MOTOLAB NAZE32PRO PORT103R PORT103V SPARKY SPRACINGF3 SPRACINGF3MINI STM32F3DISCOVERY))
+else ifeq ($(TARGET),$(filter $(TARGET),ALIENWIIF3 CHEBUZZF3 COLIBRI_RACE LUX_RACE EUSTM32F103RC MOTOLAB NAZE32PRO PORT103R SPARKY SPRACINGF3 SPRACINGF3MINI STM32F3DISCOVERY))
 FLASH_SIZE = 256
 else
 $(error FLASH_SIZE not configured for target)
@@ -128,7 +128,7 @@ ifeq ($(TARGET),RMDO)
 TARGET_FLAGS := $(TARGET_FLAGS) -DSPRACINGF3
 endif
 
-else ifeq ($(TARGET),$(filter $(TARGET),EUSTM32F103RC PORT103R PORT103V))
+else ifeq ($(TARGET),$(filter $(TARGET),EUSTM32F103RC PORT103R))
 
 
 STDPERIPH_DIR	 = $(ROOT)/lib/main/STM32F10x_StdPeriph_Driver
@@ -387,8 +387,6 @@ EUSTM32F103RC_SRC = startup_stm32f10x_hd_gcc.S \
 		   $(COMMON_SRC)
 
 PORT103R_SRC = $(EUSTM32F103RC_SRC)
-
-PORT103V_SRC = $(EUSTM32F103RC_SRC)
 
 OLIMEXINO_SRC = startup_stm32f10x_md_gcc.S \
 		   drivers/accgyro_mpu.c \

src/main/drivers/nvic.h
@@ -9,21 +9,21 @@
 #define NVIC_PRIO_BARO_EXT                 NVIC_BUILD_PRIORITY(0x0f, 0x0f)
 #define NVIC_PRIO_WS2811_DMA               NVIC_BUILD_PRIORITY(1, 2)  // TODO - is there some reason to use high priority? (or to use DMA IRQ at all?)
 #define NVIC_PRIO_TRANSPONDER_DMA          NVIC_BUILD_PRIORITY(3, 0)
-#define NVIC_PRIO_SERIALUART1_TXDMA        NVIC_BUILD_PRIORITY(1, 1)
-#define NVIC_PRIO_SERIALUART1_RXDMA        NVIC_BUILD_PRIORITY(1, 1)
-#define NVIC_PRIO_SERIALUART1              NVIC_BUILD_PRIORITY(1, 1)
-#define NVIC_PRIO_SERIALUART2_TXDMA        NVIC_BUILD_PRIORITY(1, 0)
-#define NVIC_PRIO_SERIALUART2_RXDMA        NVIC_BUILD_PRIORITY(1, 1)
-#define NVIC_PRIO_SERIALUART2              NVIC_BUILD_PRIORITY(1, 2)
-#define NVIC_PRIO_SERIALUART3_TXDMA        NVIC_BUILD_PRIORITY(1, 0)
-#define NVIC_PRIO_SERIALUART3_RXDMA        NVIC_BUILD_PRIORITY(1, 1)
-#define NVIC_PRIO_SERIALUART3              NVIC_BUILD_PRIORITY(1, 2)
-#define NVIC_PRIO_SERIALUART4_TXDMA        NVIC_BUILD_PRIORITY(1, 0)
-#define NVIC_PRIO_SERIALUART4_RXDMA        NVIC_BUILD_PRIORITY(1, 1)
-#define NVIC_PRIO_SERIALUART4              NVIC_BUILD_PRIORITY(1, 2)
-#define NVIC_PRIO_SERIALUART5_TXDMA        NVIC_BUILD_PRIORITY(1, 0)
-#define NVIC_PRIO_SERIALUART5_RXDMA        NVIC_BUILD_PRIORITY(1, 1)
-#define NVIC_PRIO_SERIALUART5              NVIC_BUILD_PRIORITY(1, 2)
+#define NVIC_PRIO_SERIALUART1_TXDMA       NVIC_BUILD_PRIORITY(1, 1)
+#define NVIC_PRIO_SERIALUART1_RXDMA       NVIC_BUILD_PRIORITY(1, 1)
+#define NVIC_PRIO_SERIALUART1             NVIC_BUILD_PRIORITY(1, 1)
+#define NVIC_PRIO_SERIALUART2_TXDMA       NVIC_BUILD_PRIORITY(1, 0)
+#define NVIC_PRIO_SERIALUART2_RXDMA       NVIC_BUILD_PRIORITY(1, 1)
+#define NVIC_PRIO_SERIALUART2             NVIC_BUILD_PRIORITY(1, 2)
+#define NVIC_PRIO_SERIALUART3_TXDMA       NVIC_BUILD_PRIORITY(1, 0)
+#define NVIC_PRIO_SERIALUART3_RXDMA       NVIC_BUILD_PRIORITY(1, 1)
+#define NVIC_PRIO_SERIALUART3             NVIC_BUILD_PRIORITY(1, 2)
+#define NVIC_PRIO_SERIALUART4_TXDMA       NVIC_BUILD_PRIORITY(1, 0)
+#define NVIC_PRIO_SERIALUART4_RXDMA       NVIC_BUILD_PRIORITY(1, 1)
+#define NVIC_PRIO_SERIALUART4             NVIC_BUILD_PRIORITY(1, 2)
+#define NVIC_PRIO_SERIALUART5_TXDMA       NVIC_BUILD_PRIORITY(1, 0)
+#define NVIC_PRIO_SERIALUART5_RXDMA       NVIC_BUILD_PRIORITY(1, 1)
+#define NVIC_PRIO_SERIALUART5             NVIC_BUILD_PRIORITY(1, 2)
 #define NVIC_PRIO_I2C_ER                   NVIC_BUILD_PRIORITY(0, 0)
 #define NVIC_PRIO_I2C_EV                   NVIC_BUILD_PRIORITY(0, 0)
 #define NVIC_PRIO_USB                      NVIC_BUILD_PRIORITY(2, 0)

src/main/drivers/pwm_mapping.c
@@ -29,6 +29,13 @@
 #include "pwm_rx.h"
 #include "pwm_mapping.h"
 
+#ifdef STM32F10X
+#include "serial_uart_stm32f10x.h"
+#endif
+#ifdef STM32F303xC
+#include "serial_uart_stm32f30x.h"
+#endif
+
 void pwmBrushedMotorConfig(const timerHardware_t *timerHardware, uint8_t motorIndex, uint16_t motorPwmRate, uint16_t idlePulse);
 void pwmBrushlessMotorConfig(const timerHardware_t *timerHardware, uint8_t motorIndex, uint16_t motorPwmRate, uint16_t idlePulse);
 void pwmOneshotMotorConfig(const timerHardware_t *timerHardware, uint8_t motorIndex);
@@ -602,36 +609,35 @@ pwmIOConfiguration_t *pwmInit(drv_pwm_config_t *init)
             continue;
 #endif
 
-#ifdef STM32F10X
         // skip UART ports
-        if (init->useUART2 && (timerIndex == PWM3 || timerIndex == PWM4))
-            continue;
-
-#ifdef USE_USART3
-        if (init->useUART3 && timerHardwarePtr->gpio == USART3_GPIO && (timerHardwarePtr->pin == USART3_TX_PIN || timerHardwarePtr->pin == USART3_RX_PIN))
+#ifdef USE_UART2
+        if (init->useUART2 && timerHardwarePtr->gpio == UART2_GPIO && (timerHardwarePtr->pin == UART2_TX_PIN || timerHardwarePtr->pin == UART2_RX_PIN))
             continue;
 #endif
 
-#ifdef USE_USART4
-        if (init->useUART4 && timerHardwarePtr->gpio == USART4_GPIO && (timerHardwarePtr->pin == USART4_TX_PIN || timerHardwarePtr->pin == USART4_RX_PIN))
+#ifdef USE_UART3
+        if (init->useUART3 && timerHardwarePtr->gpio == UART3_GPIO && (timerHardwarePtr->pin == UART3_TX_PIN || timerHardwarePtr->pin == UART3_RX_PIN))
             continue;
 #endif
 
-#ifdef USE_USART5
-        if (init->useUART5 && ((timerHardwarePtr->gpio == USART5_GPIO_TX && timerHardwarePtr->pin == USART5_TX_PIN) || (timerHardwarePtr->gpio == USART5_GPIO_RX && timerHardwarePtr->pin == USART5_RX_PIN)))
+#ifdef USE_UART4
+        if (init->useUART4 && timerHardwarePtr->gpio == UART4_GPIO && (timerHardwarePtr->pin == UART4_TX_PIN || timerHardwarePtr->pin == UART4_RX_PIN))
             continue;
 #endif
+
+#ifdef USE_UART5
+        if (init->useUART5 &&
+            (
+                (timerHardwarePtr->gpio == UART5_GPIO_TX && timerHardwarePtr->pin == UART5_TX_PIN)
+                || (timerHardwarePtr->gpio == UART5_GPIO_RX && timerHardwarePtr->pin == UART5_RX_PIN)
+            )
+        )
+            continue;
 #endif
 
 #if defined(STM32F10X) && I2C_DEVICE == I2CDEV_1
         // skip I2C ports if device 1 is selected
         if (timerHardwarePtr->gpio == GPIOB && (timerHardwarePtr->pin == Pin_6 || timerHardwarePtr->pin == Pin_7))
-        	continue;
-#endif
-
-#if defined(STM32F303xC) && defined(USE_USART3)
-        // skip UART3 ports (PB10/PB11)
-        if (init->useUART3 && timerHardwarePtr->gpio == UART3_GPIO && (timerHardwarePtr->pin == UART3_TX_PIN || timerHardwarePtr->pin == UART3_RX_PIN))
             continue;
 #endif
 

src/main/drivers/pwm_mapping.h
@@ -58,14 +58,17 @@ typedef struct drv_pwm_config_s {
     bool useSerialRx;
     bool useRSSIADC;
     bool useCurrentMeterADC;
-#ifdef STM32F10X
+#if defined(USE_UART2)
     bool useUART2;
-    bool useUART3;
-    bool useUART4;
-    bool useUART5;
 #endif
-#ifdef STM32F303xC
+#if defined(USE_UART3)
     bool useUART3;
+#endif
+#if defined(USE_UART4)
+    bool useUART4;
+#endif
+#if defined(USE_UART5)
+    bool useUART5;
 #endif
     bool useVbat;
     bool useOneshot;

src/main/drivers/serial_uart.c
@@ -35,6 +35,65 @@
 #include "serial.h"
 #include "serial_uart.h"
 #include "serial_uart_impl.h"
+#ifdef STM32F10X
+#include "serial_uart_stm32f10x.h"
+#endif
+#ifdef STM32F303xC
+#include "serial_uart_stm32f30x.h"
+#endif
+
+void usartInitAllIOSignals(void)
+{
+#ifdef STM32F10X
+    // Set UART1 TX to output and high state to prevent a rs232 break condition on reset.
+    // See issue https://github.com/cleanflight/cleanflight/issues/1433
+    gpio_config_t gpio;
+
+    gpio.mode = Mode_Out_PP;
+    gpio.speed = Speed_2MHz;
+    gpio.pin = UART1_TX_PIN;
+    digitalHi(UART1_GPIO, gpio.pin);
+    gpioInit(UART1_GPIO, &gpio);
+
+    // Set TX of UART2 and UART3 to input with pull-up to prevent floating TX outputs.
+    gpio.mode = Mode_IPU;
+
+#ifdef USE_UART2
+    gpio.pin = UART2_TX_PIN;
+    gpioInit(UART2_GPIO, &gpio);
+#endif
+
+#ifdef USE_UART3
+    gpio.pin = UART3_TX_PIN;
+    gpioInit(UART3_GPIO, &gpio);
+#endif
+
+#endif
+
+#ifdef STM32F303
+    // Set TX for UART1, UART2 and UART3 to input with pull-up to prevent floating TX outputs.
+    gpio_config_t gpio;
+
+    gpio.mode = Mode_IPU;
+    gpio.speed = Speed_2MHz;
+
+#ifdef USE_UART1
+    gpio.pin = UART1_TX_PIN;
+    gpioInit(UART1_GPIO, &gpio);
+#endif
+
+//#ifdef USE_UART2
+//    gpio.pin = UART2_TX_PIN;
+//    gpioInit(UART2_GPIO, &gpio);
+//#endif
+
+#ifdef USE_UART3
+    gpio.pin = UART3_TX_PIN;
+    gpioInit(UART3_GPIO, &gpio);
+#endif
+
+#endif
+}
 
 static void usartConfigurePinInversion(uartPort_t *uartPort) {
 #if !defined(INVERTER) && !defined(STM32F303xC)
@@ -99,22 +158,22 @@ serialPort_t *uartOpen(USART_TypeDef *USARTx, serialReceiveCallbackPtr callback,
     uartPort_t *s = NULL;
 
     if (USARTx == USART1) {
-        s = serialUSART1(baudRate, mode, options);
-#ifdef USE_USART2
+        s = serialUART1(baudRate, mode, options);
+#ifdef USE_UART2
     } else if (USARTx == USART2) {
-        s = serialUSART2(baudRate, mode, options);
+        s = serialUART2(baudRate, mode, options);
 #endif
-#ifdef USE_USART3
+#ifdef USE_UART3
     } else if (USARTx == USART3) {
-        s = serialUSART3(baudRate, mode, options);
+        s = serialUART3(baudRate, mode, options);
 #endif
-#ifdef USE_USART4
+#ifdef USE_UART4
     } else if (USARTx == UART4) {
-        s = serialUSART4(baudRate, mode, options);
+        s = serialUART4(baudRate, mode, options);
 #endif
-#ifdef USE_USART5
+#ifdef USE_UART5
     } else if (USARTx == UART5) {
-        s = serialUSART5(baudRate, mode, options);
+        s = serialUART5(baudRate, mode, options);
 #endif
     } else {
         return (serialPort_t *)s;

src/main/drivers/serial_uart.h
@@ -54,6 +54,8 @@ typedef struct {
 
 serialPort_t *uartOpen(USART_TypeDef *USARTx, serialReceiveCallbackPtr callback, uint32_t baudRate, portMode_t mode, portOptions_t options);
 
+void usartInitAllIOSignals(void);
+
 // serialPort API
 void uartWrite(serialPort_t *instance, uint8_t ch);
 uint8_t uartTotalRxBytesWaiting(serialPort_t *instance);

src/main/drivers/serial_uart_impl.h
@@ -23,9 +23,9 @@ extern const struct serialPortVTable uartVTable[];
 
 void uartStartTxDMA(uartPort_t *s);
 
-uartPort_t *serialUSART1(uint32_t baudRate, portMode_t mode, portOptions_t options);
-uartPort_t *serialUSART2(uint32_t baudRate, portMode_t mode, portOptions_t options);
-uartPort_t *serialUSART3(uint32_t baudRate, portMode_t mode, portOptions_t options);
-uartPort_t *serialUSART4(uint32_t baudRate, portMode_t mode, portOptions_t options);
-uartPort_t *serialUSART5(uint32_t baudRate, portMode_t mode, portOptions_t options);
+uartPort_t *serialUART1(uint32_t baudRate, portMode_t mode, portOptions_t options);
+uartPort_t *serialUART2(uint32_t baudRate, portMode_t mode, portOptions_t options);
+uartPort_t *serialUART3(uint32_t baudRate, portMode_t mode, portOptions_t options);
+uartPort_t *serialUART4(uint32_t baudRate, portMode_t mode, portOptions_t options);
+uartPort_t *serialUART5(uint32_t baudRate, portMode_t mode, portOptions_t options);
 

commit 7cc578db12854af3764d6a8d73ff0423987c464d

fix uart1 initialisation on STM32F1 targets, broken in c031e8d4aa7383ff5a933be06d6355507e6eb552

src/main/drivers/serial_uart_stm32f10x.c
@@ -128,7 +128,7 @@ uartPort_t *serialUART1(uint32_t baudRate, portMode_t mode, portOptions_t option
     RCC_APB2PeriphClockCmd(UART1_APB2_PERIPHERALS, ENABLE);
 #endif
 #ifdef UART1_AHB_PERIPHERALS
-    RCC_APB2PeriphClockCmd(UART1_AHB_PERIPHERALS, ENABLE);
+    RCC_AHBPeriphClockCmd(UART1_AHB_PERIPHERALS, ENABLE);
 #endif
 
     gpio.speed = Speed_2MHz;