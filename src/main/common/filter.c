81656f6baaab52b1f64ab183baf72569aa3251bc
Morgan Leborgne <leborgne@zib.de>
2016-06-14 12:40:52 +0200

    filter: Fix indentation - use 4 spaces instead of tabs.

--- src/main/common/filter.c
+++ src/main/common/filter.c
@@ -33,10 +33,10 @@
 float filterApplyPt1(float input, filterStatePt1_t *filter, uint8_t f_cut, float dT)
 {
 
-	// Pre calculate and store RC
-	if (!filter->RC) {
-		filter->RC = 1.0f / ( 2.0f * (float)M_PI * f_cut );
-	}
+    // Pre calculate and store RC
+    if (!filter->RC) {
+        filter->RC = 1.0f / ( 2.0f * (float)M_PI * f_cut );
+    }
 
     filter->state = filter->state + dT / (filter->RC + dT) * (input - filter->state);
 
