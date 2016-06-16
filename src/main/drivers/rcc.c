9b707715394fceea6875f372b1d7957550f5994a
Martin Budden <mjbudden@gmail.com>
2016-06-16 11:47:02 +0100

    Converted tabs to spaces

--- src/main/drivers/rcc.c
+++ src/main/drivers/rcc.c
@@ -4,48 +4,48 @@
 
 void RCC_ClockCmd(rccPeriphTag_t periphTag, FunctionalState NewState)
 {
-	int tag = periphTag >> 5;
-	uint32_t mask = 1 << (periphTag & 0x1f);
-	switch (tag) {
+    int tag = periphTag >> 5;
+    uint32_t mask = 1 << (periphTag & 0x1f);
+    switch (tag) {
 #if defined(STM32F303xC)
-	case RCC_AHB:
-		RCC_AHBPeriphClockCmd(mask, NewState);
-		break;
+    case RCC_AHB:
+        RCC_AHBPeriphClockCmd(mask, NewState);
+        break;
 #endif
-	case RCC_APB2:
-		RCC_APB2PeriphClockCmd(mask, NewState);
-		break;
-	case RCC_APB1:
-		RCC_APB1PeriphClockCmd(mask, NewState);
-		break;	
+    case RCC_APB2:
+        RCC_APB2PeriphClockCmd(mask, NewState);
+        break;
+    case RCC_APB1:
+        RCC_APB1PeriphClockCmd(mask, NewState);
+        break;
 #if defined(STM32F40_41xxx) || defined(STM32F411xE)
-	case RCC_AHB1:
-		RCC_AHB1PeriphClockCmd(mask, NewState);
-		break;
+    case RCC_AHB1:
+        RCC_AHB1PeriphClockCmd(mask, NewState);
+        break;
 #endif
-	}
+    }
 }
 
 void RCC_ResetCmd(rccPeriphTag_t periphTag, FunctionalState NewState)
 {
-	int tag = periphTag >> 5;
-	uint32_t mask = 1 << (periphTag & 0x1f);
-	switch (tag) {
+    int tag = periphTag >> 5;
+    uint32_t mask = 1 << (periphTag & 0x1f);
+    switch (tag) {
 #if defined(STM32F303xC)
-	case RCC_AHB:
-		RCC_AHBPeriphResetCmd(mask, NewState);
-		break;
+    case RCC_AHB:
+        RCC_AHBPeriphResetCmd(mask, NewState);
+        break;
 #endif
-	case RCC_APB2:
-		RCC_APB2PeriphResetCmd(mask, NewState);
-		break;
-	case RCC_APB1:
-		RCC_APB1PeriphResetCmd(mask, NewState);
-		break;	
+    case RCC_APB2:
+        RCC_APB2PeriphResetCmd(mask, NewState);
+        break;
+    case RCC_APB1:
+        RCC_APB1PeriphResetCmd(mask, NewState);
+        break;
 #if defined(STM32F40_41xxx) || defined(STM32F411xE)
-	case RCC_AHB1:
-		RCC_AHB1PeriphResetCmd(mask, NewState);
-		break;
+    case RCC_AHB1:
+        RCC_AHB1PeriphResetCmd(mask, NewState);
+        break;
 #endif
-	}
+    }
 }
