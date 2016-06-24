389d74bd8d34ebddf88314544c630312931dfc50
Anders Hoglund <AndWho@gmail.com>
2016-06-24 13:41:55 +0200

    /PORT103R build failed. Target.mk fixed.

--- src/main/target/PORT103R/target.mk
+++ src/main/target/PORT103R/target.mk
@@ -11,6 +11,7 @@ TARGET_SRC = \
             drivers/accgyro_mpu.c \
             drivers/accgyro_mpu3050.c \
             drivers/accgyro_mpu6050.c \
+            drivers/accgyro_mpu6500.c \
             drivers/accgyro_spi_mpu6000.c \
             drivers/accgyro_spi_mpu6500.c \
             drivers/barometer_bmp085.c \
