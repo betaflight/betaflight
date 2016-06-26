c14af2506c5b7e85a71d9ffb7977e6ad7440e06c
rotcehdnih <Hector@3xa-gaming.net>
2016-06-26 23:43:44 +1000

    Fix whitespace to tab

--- fake_travis_build.sh
+++ fake_travis_build.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 targets=("PUBLISHMETA=True" \
-    "TARGET=CC3D" \
-    "TARGET=CC3D_OPBL" \
-    "TARGET=COLIBRI_RACE" \
-    "TARGET=LUX_RACE" \
-    "TARGET=SPRACINGF3" \
-    "TARGET=SPRACINGF3EVO" \
-    "TARGET=SPRACINGF3MINI" \
-    "TARGET=OMNIBUS" \
-    "TARGET=NAZE" \
-    "TARGET=AFROMINI" \
-    "TARGET=RMDO" \
-    "TARGET=SPARKY" \
-    "TARGET=MOTOLAB" \
-    "TARGET=PIKOBLX" \
-    "TARGET=IRCFUSIONF3" \
-    "TARGET=ALIENFLIGHTF1" \
-    "TARGET=ALIENFLIGHTF3" \
-    "TARGET=DOGE" \
-    "TARGET=SINGULARITY" \
-    "TARGET=SIRINFPV" \
+	"TARGET=CC3D" \
+	"TARGET=CC3D_OPBL" \
+	"TARGET=COLIBRI_RACE" \
+	"TARGET=LUX_RACE" \
+	"TARGET=SPRACINGF3" \
+	"TARGET=SPRACINGF3EVO" \
+	"TARGET=SPRACINGF3MINI" \
+	"TARGET=OMNIBUS" \
+	"TARGET=NAZE" \
+	"TARGET=AFROMINI" \
+	"TARGET=RMDO" \
+	"TARGET=SPARKY" \
+	"TARGET=MOTOLAB" \
+	"TARGET=PIKOBLX" \
+	"TARGET=IRCFUSIONF3" \
+	"TARGET=ALIENFLIGHTF1" \
+	"TARGET=ALIENFLIGHTF3" \
+	"TARGET=DOGE" \
+	"TARGET=SINGULARITY" \
+	"TARGET=SIRINFPV" \
 	"TARGET=X_RACERSPI")
+
+
 
 #fake a travis build environment
 export TRAVIS_BUILD_NUMBER=$(date +%s)
@@ -31,9 +33,9 @@ export TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG:=$USER/simulated}
 for target in "${targets[@]}"
 do
 	unset RUNTESTS PUBLISHMETA TARGET
-  echo
-  echo
-  echo "BUILDING '$target'"
+	echo
+	echo
+	echo "BUILDING '$target'"
 	eval "export $target"
 	make -f Makefile clean
 	./.travis.sh
