fdd416318c072a501e5e1027b28221859111152d
gaelj <gaeljames@gmail.com>
2016-07-16 18:14:57 +0200

    MSP header size fix

--- src/main/io/serial_msp.c
+++ src/main/io/serial_msp.c
@@ -1245,7 +1245,7 @@ static bool processOutCommand(uint8_t cmdMSP)
         break;
 
     case MSP_3D:
-        headSerialReply(2 * 4);
+        headSerialReply(2 * 3);
         serialize16(masterConfig.flight3DConfig.deadband3d_low);
         serialize16(masterConfig.flight3DConfig.deadband3d_high);
         serialize16(masterConfig.flight3DConfig.neutral3d);
@@ -1253,7 +1253,7 @@ static bool processOutCommand(uint8_t cmdMSP)
         break;
 
     case MSP_RC_DEADBAND:
-        headSerialReply(3);
+        headSerialReply(5);
         serialize8(masterConfig.rcControlsConfig.deadband);
         serialize8(masterConfig.rcControlsConfig.yaw_deadband);
         serialize8(masterConfig.rcControlsConfig.alt_hold_deadband);
