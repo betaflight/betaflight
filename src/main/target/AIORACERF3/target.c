de4a50594566b5bec2a5ceb04e4c6727f51e9bd1
爵爷 <tianbin4279@gmail.com>
2016-07-28 22:49:53 +0800

    Update target.c

--- src/main/target/AIORACERF3/target.c
+++ src/main/target/AIORACERF3/target.c
@@ -23,7 +23,6 @@
 
 const uint16_t multiPPM[] = {
     PWM1  | (MAP_TO_PPM_INPUT    << 8),  // PPM input
-
     PWM2  | (MAP_TO_MOTOR_OUTPUT << 8),
     PWM3  | (MAP_TO_MOTOR_OUTPUT << 8),
     PWM4  | (MAP_TO_MOTOR_OUTPUT << 8),
