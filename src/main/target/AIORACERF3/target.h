54ad5a29e3324a313211f2d8892d77c99140738e
爵爷 <tianbin4279@gmail.com>
2016-07-28 22:42:52 +0800

    Update target.h

--- src/main/target/AIORACERF3/target.h
+++ src/main/target/AIORACERF3/target.h
@@ -117,8 +117,8 @@
 #define BOARD_HAS_VOLTAGE_DIVIDER
 #define USE_ADC
 #define ADC_INSTANCE            ADC2
-#define VBAT_ADC_PIN            PA4
-#define CURRENT_METER_ADC_PIN   PA5
+#define VBAT_ADC_PIN            PA5
+#define CURRENT_METER_ADC_PIN   PA4
 #define RSSI_ADC_PIN            PB2
 
 #define LED_STRIP
