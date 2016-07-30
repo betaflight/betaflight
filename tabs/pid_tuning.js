9ce7f76f27a12bc8dd4e6da42d2efff3870e0d9f
nathan <nathan@vertile.com>
2016-07-30 01:22:14 -0700

    load the right config based on version and fix a typo

--- tabs/pid_tuning.js
+++ tabs/pid_tuning.js
@@ -17,15 +17,20 @@ TABS.pid_tuning.initialize = function (callback) {
         if (semver.gte(CONFIG.apiVersion, CONFIGURATOR.pidControllerChangeMinApiVersion)) {
             return MSP.promise(MSP_codes.MSP_PID_CONTROLLER);
         }
-        return true;
     }).then(function() {
         return MSP.promise(MSP_codes.MSP_PIDNAMES)
     }).then(function() {
         return MSP.promise(MSP_codes.MSP_PID);
+    }).then(function () {
+        if (semver.gte(CONFIG.apiVersion, "2.9.0") && semver.lte(CONFIG.apiVersion, "3.0.0")) {
+          return MSP.promise(MSP_codes.MSP_SPECIAL_PARAMETERS);
+        }
+    }).then(function() {
+        if (semver.gte(CONFIG.apiVersion, "3.0.0")) {
+          return MSP.promise(MSP_codes.MSP_PID_ADVANCED);
+        }
     }).then(function() {
         return MSP.promise(MSP_codes.MSP_RC_TUNING);
-    }).then(function() {
-        return MSP.promise(MSP_codes.MSP_PID_ADVANCED);
     }).then(function() {
         return MSP.promise(MSP_codes.MSP_FILTER_CONFIG);
     }).then(function() {
@@ -698,7 +703,7 @@ TABS.pid_tuning.initialize = function (callback) {
             }).then(function () {
                 return MSP.promise(MSP_codes.MSP_SET_PID, MSP.crunch(MSP_codes.MSP_SET_PID));
             }).then(function () {
-                if (semver.gte(CONFIG.apiVersion, "2.9.0") && smver.lte(CONFIG.apiVersion, "3.0.0")) {
+                if (semver.gte(CONFIG.apiVersion, "2.9.0") && semver.lte(CONFIG.apiVersion, "3.0.0")) {
                   return MSP.promise(MSP_codes.MSP_SET_SPECIAL_PARAMETERS, MSP.crunch(MSP_codes.MSP_SET_SPECIAL_PARAMETERS));
                 }
             }).then(function () {
