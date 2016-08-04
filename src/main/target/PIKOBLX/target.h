95d9d8f816c8d595befa7e58838b4cea92967077
NightHawk32 <nighthawk2244@gmail.com>
2016-08-04 09:09:40 -0400

    Changing ADC pin mapping to fit KOMBINI current sensor

--- src/main/target/PIKOBLX/target.h
+++ src/main/target/PIKOBLX/target.h
@@ -76,9 +76,8 @@
 #define BOARD_HAS_VOLTAGE_DIVIDER
 #define USE_ADC
 #define ADC_INSTANCE            ADC2
-#define CURRENT_METER_ADC_PIN   PA2
+#define CURRENT_METER_ADC_PIN   PB2
 #define VBAT_ADC_PIN            PA5
-#define RSSI_ADC_PIN            PB2
 
 #define LED_STRIP
 #if 1
