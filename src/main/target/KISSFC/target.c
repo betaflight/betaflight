3371cadd862b0d64e29d3e74a25a21e3f0deb628
borisbstyle <dreambb1982@gmail.com>
2016-08-05 00:02:38 +0200

    Add yaw smoothing back // adjust defaults // KISS fc remove some shared timers

--- src/main/target/KISSFC/target.c
+++ src/main/target/KISSFC/target.c
@@ -30,11 +30,6 @@ const uint16_t multiPPM[] = {
     PWM1  | (MAP_TO_MOTOR_OUTPUT << 8),
     PWM5  | (MAP_TO_MOTOR_OUTPUT << 8),
     PWM6  | (MAP_TO_MOTOR_OUTPUT << 8),
-    PWM8  | (MAP_TO_MOTOR_OUTPUT << 8),
-    PWM9  | (MAP_TO_MOTOR_OUTPUT << 8),
-    PWM10 | (MAP_TO_MOTOR_OUTPUT << 8),
-    PWM11 | (MAP_TO_MOTOR_OUTPUT << 8),
-    PWM12 | (MAP_TO_MOTOR_OUTPUT << 8),
     0xFFFF
 };
 
