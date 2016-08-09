f9354e53b3b41c71e0982ef65b01b26474d7af41
mikeller <github@ike.ch>
2016-08-10 00:00:40 +1200

    Graft of 'cli_diff_command' into 'master'.

--- src/main/config/config_master.h
+++ src/main/config/config_master.h
@@ -174,3 +174,5 @@ typedef struct master_t {
 extern master_t masterConfig;
 extern profile_t *currentProfile;
 extern controlRateConfig_t *currentControlRateProfile;
+
+void createDefaultConfig(master_t *config);
