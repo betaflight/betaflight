fb067244d8a4663804a1fbe852cefd67811d170d
Sami Korhonen <samik.korhonen@gmail.com>
2016-08-11 08:25:42 +0300

    Fix F4 ledstip

--- src/main/drivers/light_ws2811strip.c
+++ src/main/drivers/light_ws2811strip.c
@@ -39,7 +39,11 @@
 #include "drivers/dma.h"
 #include "drivers/light_ws2811strip.h"
 
+#if defined(STM32F4)
+uint32_t ledStripDMABuffer[WS2811_DMA_BUFFER_SIZE];
+#else
 uint8_t ledStripDMABuffer[WS2811_DMA_BUFFER_SIZE];
+#endif
 volatile uint8_t ws2811LedDataTransferInProgress = 0;
 
 static hsvColor_t ledColorBuffer[WS2811_LED_STRIP_LENGTH];
