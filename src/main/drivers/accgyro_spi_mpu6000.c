196baa4e8715de8edd0ef0a8f0bd8c4d34ebf828
blckmn <blackman@xtra.com.au>
2016-08-13 11:53:12 +1000

    Some minor updates to the SPI MPU6500 driver to disable i2c interface

--- src/main/drivers/accgyro_spi_mpu6000.c
+++ src/main/drivers/accgyro_spi_mpu6000.c
@@ -205,8 +205,8 @@ bool mpu6000SpiDetect(void)
     return false;
 }
 
-static void mpu6000AccAndGyroInit(void) {
-
+static void mpu6000AccAndGyroInit(void) 
+{
     if (mpuSpi6000InitDone) {
         return;
     }
