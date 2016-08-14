84dadb5cb7d27344283c77a79bfdfb6fc836fee1
kc10kevin <kc10kevin@yahoo.com>
2016-08-14 07:06:03 -0500

    Enable FuryF4 LED Strip

--- src/main/target/FURYF4/target.h
+++ src/main/target/FURYF4/target.h
@@ -147,6 +147,15 @@
 #define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_SBUS
 
+#define LED_STRIP
+#define WS2811_PIN                      PA0
+#define WS2811_TIMER                    TIM5
+#define WS2811_DMA_HANDLER_IDENTIFER    DMA1_ST2_HANDLER
+#define WS2811_DMA_STREAM               DMA1_Stream2
+#define WS2811_DMA_IT                   DMA_IT_TCIF2
+#define WS2811_DMA_CHANNEL              DMA_Channel_6
+#define WS2811_TIMER_CHANNEL            TIM_Channel_1
+
 #define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
 
 #define SPEKTRUM_BIND
