f2e4f9874f829c3d82bee06abfe8d12e3761c0ae
Sami Korhonen <samik.korhonen@gmail.com>
2016-08-27 16:33:08 +0300

    F7 enable FPU

--- src/main/startup/startup_stm32f745xx.s
+++ src/main/startup/startup_stm32f745xx.s
@@ -105,6 +105,12 @@ LoopFillZerobss:
   cmp  r2, r3
   bcc  FillZerobss
 
+/*FPU settings*/
+  ldr  r0, =0xE000ED88           /* Enable CP10,CP11 */
+  ldr  r1,[r0]
+  orr  r1,r1,#(0xF << 20)
+  str  r1,[r0]
+
 /* Call the clock system initialization function.*/
   bl  SystemInit   
 /* Call the application's entry point.*/
