42eef5813e17aa63f2c88449ac936ebcd202cbe7
Martin Budden <mjbudden@gmail.com>
2016-08-27 14:14:10 +0100

    Fix mmix cli parsing

--- src/main/io/serial_cli.c
+++ src/main/io/serial_cli.c
@@ -1552,22 +1552,35 @@ static void cliMotorMix(char *cmdline)
         if (i < MAX_SUPPORTED_MOTORS) {
             ptr = strchr(ptr, ' ');
             if (ptr) {
-                masterConfig.customMotorMixer[i].throttle = fastA2F(++ptr);
+            	// although fastA2F skips leading spaces, it does not increment pointer accordingly, so while loop below is required
+            	while (*ptr == ' ') {
+            		++ptr;
+            	}
+                masterConfig.customMotorMixer[i].throttle = fastA2F(ptr);
                 check++;
             }
             ptr = strchr(ptr, ' ');
             if (ptr) {
-                masterConfig.customMotorMixer[i].roll = fastA2F(++ptr);
+            	while (*ptr == ' ') {
+            		++ptr;
+            	}
+                masterConfig.customMotorMixer[i].roll = fastA2F(ptr);
                 check++;
             }
             ptr = strchr(ptr, ' ');
             if (ptr) {
-                masterConfig.customMotorMixer[i].pitch = fastA2F(++ptr);
+            	while (*ptr == ' ') {
+            		++ptr;
+            	}
+                masterConfig.customMotorMixer[i].pitch = fastA2F(ptr);
                 check++;
             }
             ptr = strchr(ptr, ' ');
             if (ptr) {
-                masterConfig.customMotorMixer[i].yaw = fastA2F(++ptr);
+            	while (*ptr == ' ') {
+            		++ptr;
+            	}
+                masterConfig.customMotorMixer[i].yaw = fastA2F(ptr);
                 check++;
             }
             if (check != 4) {
