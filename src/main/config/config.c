9e5f66909031d57e2f9d007a80fac568550c4dcc
borisbstyle <dreambb1982@gmail.com>
2016-09-02 22:15:43 +0200

    Bump EEPROM

--- src/main/config/config.c
+++ src/main/config/config.c
@@ -172,7 +172,7 @@ static uint32_t activeFeaturesLatch = 0;
 static uint8_t currentControlRateProfileIndex = 0;
 controlRateConfig_t *currentControlRateProfile;
 
-static const uint8_t EEPROM_CONF_VERSION = 144;
+static const uint8_t EEPROM_CONF_VERSION = 145;
 
 static void resetAccelerometerTrims(flightDynamicsTrims_t *accelerometerTrims)
 {
