62b0500f017ba99aadfaf66a55c63aa430f54ada
borisbstyle <dreambb1982@gmail.com>
2016-09-18 01:52:59 +0200

    Fix OMNIBUSF4 Timers

--- src/main/target/OMNIBUSF4/target.c
+++ src/main/target/OMNIBUSF4/target.c
@@ -30,11 +30,6 @@ const uint16_t multiPPM[] = {
     PWM10 | (MAP_TO_MOTOR_OUTPUT << 8),
     PWM11 | (MAP_TO_MOTOR_OUTPUT << 8),
     PWM12 | (MAP_TO_MOTOR_OUTPUT << 8),
-    PWM2  | (MAP_TO_MOTOR_OUTPUT << 8),  // Swap to servo if needed
-    PWM3  | (MAP_TO_MOTOR_OUTPUT << 8),  // Swap to servo if needed
-    PWM4  | (MAP_TO_MOTOR_OUTPUT << 8),  // Swap to servo if needed
-    PWM5  | (MAP_TO_MOTOR_OUTPUT << 8),  // Swap to servo if needed
-    PWM1  | (MAP_TO_MOTOR_OUTPUT << 8),  // Swap to servo if needed
     0xFFFF
 };
 
@@ -62,11 +57,6 @@ const uint16_t airPPM[] = {
     PWM10 | (MAP_TO_SERVO_OUTPUT  << 8),
     PWM11 | (MAP_TO_SERVO_OUTPUT  << 8),
     PWM12 | (MAP_TO_SERVO_OUTPUT  << 8),
-    PWM2  | (MAP_TO_SERVO_OUTPUT  << 8),
-    PWM3  | (MAP_TO_SERVO_OUTPUT  << 8),
-    PWM4  | (MAP_TO_SERVO_OUTPUT  << 8),
-    PWM5  | (MAP_TO_SERVO_OUTPUT  << 8),
-    PWM1  | (MAP_TO_SERVO_OUTPUT  << 8),
     0xFFFF
 };
 
@@ -88,17 +78,17 @@ const uint16_t airPWM[] = {
 
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-    { TIM12, IO_TAG(PB14), TIM_Channel_1, TIM8_BRK_TIM12_IRQn, IOCFG_IPD,      GPIO_Mode_AF, GPIO_AF_TIM12 }, // PPM (5th pin on FlexiIO port)
-    { TIM12, IO_TAG(PB15), TIM_Channel_2, TIM8_BRK_TIM12_IRQn, IOCFG_IPD,      GPIO_Mode_AF, GPIO_AF_TIM12 }, // S2_IN - GPIO_PartialRemap_TIM3
-    { TIM8,  IO_TAG(PC6),  TIM_Channel_1, TIM8_CC_IRQn,        IOCFG_IPD,      GPIO_Mode_AF, GPIO_AF_TIM8 }, // S3_IN
-    { TIM8,  IO_TAG(PC7),  TIM_Channel_2, TIM8_CC_IRQn,        IOCFG_IPD,      GPIO_Mode_AF, GPIO_AF_TIM8 }, // S4_IN
-    { TIM8,  IO_TAG(PC8),  TIM_Channel_3, TIM8_CC_IRQn,        IOCFG_IPD,      GPIO_Mode_AF, GPIO_AF_TIM8 }, // S5_IN
-    { TIM8,  IO_TAG(PC9),  TIM_Channel_4, TIM8_CC_IRQn,        IOCFG_IPD,      GPIO_Mode_AF, GPIO_AF_TIM8 }, // S6_IN
+    { TIM12, IO_TAG(PB14), TIM_Channel_1, TIM8_BRK_TIM12_IRQn, 0, IOCFG_IPD, GPIO_AF_TIM12 },// PPM (5th pin on FlexiIO port)
+    { TIM12, IO_TAG(PB15), TIM_Channel_2, TIM8_BRK_TIM12_IRQn, 0, IOCFG_IPD, GPIO_AF_TIM12 },// S2_IN - GPIO_PartialRemap_TIM3
+    { TIM8,  IO_TAG(PC6),  TIM_Channel_1, TIM8_CC_IRQn,        0, IOCFG_IPD, GPIO_AF_TIM8 }, // S3_IN
+    { TIM8,  IO_TAG(PC7),  TIM_Channel_2, TIM8_CC_IRQn,        0, IOCFG_IPD, GPIO_AF_TIM8 }, // S4_IN
+    { TIM8,  IO_TAG(PC8),  TIM_Channel_3, TIM8_CC_IRQn,        0, IOCFG_IPD, GPIO_AF_TIM8 }, // S5_IN
+    { TIM8,  IO_TAG(PC9),  TIM_Channel_4, TIM8_CC_IRQn,        0, IOCFG_IPD, GPIO_AF_TIM8 }, // S6_IN
 
-    { TIM3,  IO_TAG(PB0),  TIM_Channel_3, TIM3_IRQn,           IOCFG_AF_PP_PD, GPIO_Mode_AF, GPIO_AF_TIM3 }, // S1_OUT
-    { TIM3,  IO_TAG(PB1),  TIM_Channel_4, TIM3_IRQn,           IOCFG_AF_PP_PD, GPIO_Mode_AF, GPIO_AF_TIM3 }, // S2_OUT
-    { TIM9,  IO_TAG(PA3),  TIM_Channel_2, TIM1_BRK_TIM9_IRQn,  IOCFG_AF_PP_PD, GPIO_Mode_AF, GPIO_AF_TIM9 }, // S3_OUT
-    { TIM2,  IO_TAG(PA2),  TIM_Channel_3, TIM2_IRQn,           IOCFG_AF_PP_PD, GPIO_Mode_AF, GPIO_AF_TIM2 }, // S4_OUT
-    { TIM5,  IO_TAG(PA1),  TIM_Channel_2, TIM5_IRQn,           IOCFG_AF_PP_PD, GPIO_Mode_AF, GPIO_AF_TIM5 }, // S5_OUT - GPIO_PartialRemap_TIM3
-    { TIM5,  IO_TAG(PA0),  TIM_Channel_1, TIM5_IRQn,           IOCFG_AF_PP_PD, GPIO_Mode_AF, GPIO_AF_TIM5 }, // S6_OUT
+    { TIM3,  IO_TAG(PB0),  TIM_Channel_3, TIM3_IRQn,           1, IOCFG_AF_PP_PD, GPIO_AF_TIM3 }, // S1_OUT
+    { TIM3,  IO_TAG(PB1),  TIM_Channel_4, TIM3_IRQn,           1, IOCFG_AF_PP_PD, GPIO_AF_TIM3 }, // S2_OUT
+    { TIM9,  IO_TAG(PA3),  TIM_Channel_2, TIM1_BRK_TIM9_IRQn,  1, IOCFG_AF_PP_PD, GPIO_AF_TIM9 }, // S4_OUT
+    { TIM2,  IO_TAG(PA2),  TIM_Channel_3, TIM2_IRQn,           1, IOCFG_AF_PP_PD, GPIO_AF_TIM2 }, // S4_OUT
+    { TIM5,  IO_TAG(PA1),  TIM_Channel_2, TIM5_IRQn,           1, IOCFG_AF_PP_PD, GPIO_AF_TIM5 }, // S5_OUT - GPIO_PartialRemap_TIM3
+    { TIM5,  IO_TAG(PA0),  TIM_Channel_1, TIM5_IRQn,           1, IOCFG_AF_PP_PD, GPIO_AF_TIM5 }, // S6_OUT
 };
