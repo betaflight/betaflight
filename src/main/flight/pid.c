e9ee2bce28894eac24a2868778a7484c232876f3
Hydra <dominic.clifton@cleanflight.com>
2016-09-19 23:53:31 +0200

    Change the default pid controller to lux float.

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -76,7 +76,7 @@ pidControllerFuncPtr pid_controller = pidMultiWiiRewrite;
 PG_REGISTER_PROFILE_WITH_RESET_TEMPLATE(pidProfile_t, pidProfile, PG_PID_PROFILE, 0);
 
 PG_RESET_TEMPLATE(pidProfile_t, pidProfile,
-    .pidController = PID_CONTROLLER_MWREWRITE,
+    .pidController = PID_CONTROLLER_LUX_FLOAT,
     .P8[PIDROLL] = 40,
     .I8[PIDROLL] = 30,
     .D8[PIDROLL] = 23,
