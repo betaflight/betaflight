e962f8fbfdbb4a3d8d6e9d8e3e8c3d3fb64cd0d6
Hydra <dominic.clifton@cleanflight.com>
2016-09-21 12:00:23 +0200

    move gyro lpf definitions into accgyro.h

--- src/main/drivers/gyro_sync.h
+++ src/main/drivers/gyro_sync.h
@@ -15,14 +15,5 @@
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define GYRO_LPF_256HZ      0
-#define GYRO_LPF_188HZ      1
-#define GYRO_LPF_98HZ       2
-#define GYRO_LPF_42HZ       3
-#define GYRO_LPF_20HZ       4
-#define GYRO_LPF_10HZ       5
-#define GYRO_LPF_5HZ        6
-#define GYRO_LPF_NONE       7
-
 uint8_t gyroMPU6xxxCalculateDivider(void);
 uint32_t gyroSetSampleRate(uint8_t lpf, uint8_t gyroSyncDenominator);
