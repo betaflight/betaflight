6a9bb00e42ddbad0bd89ae8fa48ffe0f9e698589
Hydra <dominic.clifton@cleanflight.com>
2016-09-21 12:00:23 +0200

    move gyro lpf definitions into accgyro.h

--- src/main/drivers/accgyro.h
+++ src/main/drivers/accgyro.h
@@ -17,6 +17,15 @@
 
 #pragma once
 
+#define GYRO_LPF_256HZ      0
+#define GYRO_LPF_188HZ      1
+#define GYRO_LPF_98HZ       2
+#define GYRO_LPF_42HZ       3
+#define GYRO_LPF_20HZ       4
+#define GYRO_LPF_10HZ       5
+#define GYRO_LPF_5HZ        6
+#define GYRO_LPF_NONE       7
+
 typedef struct gyro_s {
     sensorGyroInitFuncPtr init;                             // initialize function
     sensorReadFuncPtr read;                                 // read 3 axis data function
