c75865814d4c6f6709ede008fe196feaa8cb2eeb
Hydra <dominic.clifton@cleanflight.com>
2016-09-29 03:55:41 +0200

    reorder some fields in the new version of the imuConfig PG.
    Things least likely to change are first.

--- src/main/flight/imu.h
+++ src/main/flight/imu.h
@@ -43,12 +43,12 @@ extern attitudeEulerAngles_t attitude;
 
 typedef struct imuConfig_s {
     // IMU configuration
-    uint8_t pid_process_denom;              // Processing denominator for PID controller vs gyro sampling rate
+    uint16_t max_angle_inclination;         // max inclination allowed in angle (level) mode. default 500 (50 degrees).
+    uint8_t small_angle;                    // Angle used for mag hold threshold.
     uint16_t dcm_kp;                        // DCM filter proportional gain ( x 10000)
     uint16_t dcm_ki;                        // DCM filter integral gain ( x 10000)
-    uint8_t small_angle;                    // Angle used for mag hold threshold.
-    uint16_t max_angle_inclination;         // max inclination allowed in angle (level) mode. default 500 (50 degrees).
     uint8_t gyro_sync;                      // Enable interrupt based loop
+    uint8_t pid_process_denom;              // Processing denominator for PID controller vs gyro sampling rate
     uint16_t gyro_sample_hz;                // The desired gyro sample frequency.
 } imuConfig_t;
 
