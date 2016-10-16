a96e1a78d2518b4047ac8aa16af55174462b8a99
oleost <oleost@gmail.com>
2016-10-16 10:57:51 +0200

    Adding spracing F3 mini (#673)
    Support for SPRacing F3 MINI board.
    No transponder support.

---
+++ src/main/target/SPRACINGF3MINI/target.mk
@@ -0,0 +1,15 @@
+F3_TARGETS  += $(TARGET)
+FEATURES    = VCP SDCARD 
+
+TARGET_SRC = \
+            drivers/accgyro_mpu.c \
+            drivers/accgyro_mpu6500.c \
+            drivers/barometer_bmp280.c \
+            drivers/compass_ak8963.c \
+            drivers/compass_hmc5883l.c \
+            drivers/flash_m25p16.c \
+            drivers/light_ws2811strip.c \
+            drivers/light_ws2811strip_stm32f30x.c \
+            drivers/serial_softserial.c \
+            drivers/serial_usb_vcp.c \
+            drivers/sonar_hcsr04.c \
