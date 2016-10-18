60e80c23d7369d76f10b56dd3afa80dc5c367bd6
Martin Budden <mjbudden@gmail.com>
2016-10-18 21:21:26 +0100

    Increased MSP inbuf size to 256

--- src/main/msp/msp_serial.h
+++ src/main/msp/msp_serial.h
@@ -37,7 +37,7 @@ typedef enum {
     MSP_SKIP_NON_MSP_DATA
 } mspEvaluateNonMspData_e;
 
-#define MSP_PORT_INBUF_SIZE 64
+#define MSP_PORT_INBUF_SIZE 256
 #ifdef USE_FLASHFS
 #define MSP_PORT_OUTBUF_SIZE (4096 + 16)
 #else
