b5ceab889e22dcdc19bc0914cf3805fe78df9545
Hydra <dominic.clifton@cleanflight.com>
2016-10-24 18:17:52 +0200

    Increase timeout for I2C transter. BMP280 is reading 24 bytes as a single I2C transaction and hits the timeout which causes the i2c driver to get stuck on sending START when doing the next transaction.
    credit to @digitalentity / 61b494d795731cd66c0e22b39b1f528161e794a0

--- src/main/drivers/bus_i2c.h
+++ src/main/drivers/bus_i2c.h
@@ -19,7 +19,7 @@
 
 #define I2C_SHORT_TIMEOUT            ((uint32_t)0x1000)
 #define I2C_LONG_TIMEOUT             ((uint32_t)(10 * I2C_SHORT_TIMEOUT))
-#define I2C_DEFAULT_TIMEOUT          I2C_SHORT_TIMEOUT
+#define I2C_DEFAULT_TIMEOUT          I2C_LONG_TIMEOUT
 
 typedef enum I2CDevice {
     I2CDEV_1,
