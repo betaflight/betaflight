kangsterizer/inav

filter initialization

src/main/fc/mw.c
@@ -498,7 +498,11 @@ void filterRc(bool isRXDataNew)
 
     // Calculate average cycle time (1Hz LPF on cycle time)
     if (!filterInitialised) {
-        biquadFilterInit(&filteredCycleTimeState, 1, gyro.targetLooptime);
+    #ifdef ASYNC_GYRO_PROCESSING
+        biquadFilterInit(&filteredCycleTimeState, 1, getPidUpdateRate());
+    #else
+        biquadFilterInit(&filteredCycleTimeState, 1, gyro.targetLooptime;
+    #endif
         filterInitialised = true;
     }
 
@@ -655,7 +659,11 @@ void taskGyro(void) {
 
     if (masterConfig.gyroSync) {
         while (1) {
+        #ifdef ASYNC_GYRO_PROCESSING
+            if (gyroSyncCheckUpdate() || ((currentDeltaTime + (micros() - currentTime)) >= (getGyroUpdateRate() + GYRO_WATCHDOG_DELAY))) {
+        #else
             if (gyroSyncCheckUpdate() || ((currentDeltaTime + (micros() - currentTime)) >= (gyro.targetLooptime + GYRO_WATCHDOG_DELAY))) {
+        #endif
                 break;
             }
         }

src/main/sensors/gyro.c
@@ -36,6 +36,8 @@
 #include "sensors/boardalignment.h"
 #include "sensors/gyro.h"
 
+#include "config/config.h"
+
 gyro_t gyro;                      // gyro access functions
 sensor_align_e gyroAlign = 0;
 
@@ -57,9 +59,17 @@ void gyroUseConfig(const gyroConfig_t *gyroConfigToUse, uint8_t gyro_soft_lpf_hz
 
 void gyroInit(void)
 {
-    if (gyroSoftLpfHz && gyro.targetLooptime) {  // Initialisation needs to happen once samplingrate is known
+    /*
+     * After refactoring this function is always called after gyro sampling rate is known, so
+     * no additional condition is required
+     */
+    if (gyroSoftLpfHz) {
         for (int axis = 0; axis < 3; axis++) {
+        #ifdef ASYNC_GYRO_PROCESSING
+            biquadFilterInit(&gyroFilterLPF[axis], gyroSoftLpfHz, getGyroUpdateRate());
+        #else
             biquadFilterInit(&gyroFilterLPF[axis], gyroSoftLpfHz, gyro.targetLooptime);
+        #endif
         }
     }
 }

betaflight/betaflight

src/main/fc/mw.c
@@ -492,7 +492,9 @@ void filterRc(bool isRXDataNew)
 
     // Calculate average cycle time (1Hz LPF on cycle time)
     if (!filterInitialised) {
-        biquadFilterInitLPF(&filteredCycleTimeState, 1, gyro.targetLooptime);
+    #ifdef ASYNC_GYRO_PROCESSING
+        biquadFilterInitLPF(&filteredCycleTimeState, 1, getPidUpdateRate());
+    #else
         filterInitialised = true;
     }
 
@@ -529,7 +531,11 @@ void taskGyro(void) {
 
     if (masterConfig.gyroSync) {
         while (1) {
+        #ifdef ASYNC_GYRO_PROCESSING
+            if (gyroSyncCheckUpdate() || ((currentDeltaTime + (micros() - currentTime)) >= (getGyroUpdateRate() + GYRO_WATCHDOG_DELAY))) {
+        #else
             if (gyroSyncCheckUpdate() || ((currentDeltaTime + (micros() - currentTime)) >= (gyro.targetLooptime + GYRO_WATCHDOG_DELAY))) {
+        #endif
                 break;
             }
         }

src/main/sensors/gyro.c
@@ -36,6 +36,8 @@
 #include "sensors/boardalignment.h"
 #include "sensors/gyro.h"
 
+#include "config/config.h"
+
 gyro_t gyro;                      // gyro access functions
 sensor_align_e gyroAlign = 0;
 
@@ -57,9 +59,17 @@ void gyroUseConfig(const gyroConfig_t *gyroConfigToUse, uint8_t gyro_soft_lpf_hz
 
 void gyroInit(void)
 {
-    if (gyroSoftLpfHz && gyro.targetLooptime) {  // Initialisation needs to happen once samplingrate is known
+    /*
+     * After refactoring this function is always called after gyro sampling rate is known, so
+     * no additional condition is required
+     */
+    if (gyroSoftLpfHz) {
         for (int axis = 0; axis < 3; axis++) {
+        #ifdef ASYNC_GYRO_PROCESSING
+            biquadFilterInitLPF(&gyroFilterLPF[axis], gyroSoftLpfHz, getGyroUpdateRate());
+        #else
             biquadFilterInitLPF(&gyroFilterLPF[axis], gyroSoftLpfHz, gyro.targetLooptime);
+        #endif
         }
     }
 }