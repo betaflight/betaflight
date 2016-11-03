383a7b5c40c8a808728d2a24824caf598af6f438
Martin Budden <mjbudden@gmail.com>
2016-11-03 16:38:20 +0000

    Added and used FC_FIRMWARE_NAME #define

--- src/main/io/serial_cli.c
+++ src/main/io/serial_cli.c
@@ -3608,7 +3608,8 @@ static void cliVersion(char *cmdline)
 {
     UNUSED(cmdline);
 
-    cliPrintf("# BetaFlight/%s %s %s / %s (%s)\r\n",
+    cliPrintf("# %s/%s %s %s / %s (%s)\r\n",
+        FC_FIRMWARE_NAME,
         targetName,
         FC_VERSION_STRING,
         buildDate,
