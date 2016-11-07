d572043b8afae3c22cab5239c7027af5af250727
borisbstyle <dreambb1982@gmail.com>
2016-11-07 12:33:12 +0100

    Remap TIM17 DMA on SPF3

--- src/main/target/SPRACINGF3/target.h
+++ src/main/target/SPRACINGF3/target.h
@@ -103,6 +103,7 @@
 #define RSSI_ADC_PIN            PB2
 
 #define USE_DSHOT
+#define REMAP_TIM17_DMA
 
 // UART1 TX uses DMA1_Channel4, which is also used by dshot on motor 4
 #if defined(USE_UART1_TX_DMA) && defined(USE_DSHOT)
