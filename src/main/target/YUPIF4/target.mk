89a1278d8572f16deb19b13e3c6cddb11575c767
ted-rcnet <ted@rcnet.com>
2016-11-22 23:47:16 +0100

    Fix i2c Target YuPiF4

--- src/main/target/YUPIF4/target.mk
+++ src/main/target/YUPIF4/target.mk
@@ -4,6 +4,8 @@ FEATURES        += SDCARD VCP
 TARGET_SRC = \
             drivers/accgyro_spi_mpu6500.c \
             drivers/accgyro_mpu6500.c \
+	    drivers/barometer_bmp280.c \
+	    drivers/compass_hmc5883l.c \
             drivers/light_ws2811strip.c \
             drivers/light_ws2811strip_stm32f4xx.c
 
