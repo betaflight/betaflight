2eaa53e8e9bf3ec72af1553368fb513d130d4c7e
fishpepper <fishpepper@gmail.com>
2016-11-22 20:44:18 +0100

    adding tinyFISH target

--- src/main/target/TINYFISH/target.mk
+++ src/main/target/TINYFISH/target.mk
@@ -3,6 +3,8 @@ FEATURES    = VCP ONBOARDFLASH
 
 TARGET_SRC = \
             drivers/accgyro_mpu.c \
+            drivers/flash_m25p16.c \
             drivers/accgyro_spi_mpu6000.c \
-            drivers/serial_usb_vcp.c \
-            drivers/flash_m25p16.c
+            drivers/light_ws2811strip.c \
+            drivers/light_ws2811strip_stm32f30x.c \
+            drivers/serial_usb_vcp.c 
