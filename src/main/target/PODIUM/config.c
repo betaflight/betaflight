c4447c6aa4af0ca3324d593f8b57fe5577e3c65d
HaseebNain <haseebnain@u.boisestate.edu>
2016-11-22 13:34:54 -0700

    Podium Target update
    Fixed Target information
    - Invertor set
    - mpu6500 driver
    - timer allocations

--- src/main/target/PODIUM/config.c
+++ src/main/target/PODIUM/config.c
@@ -16,18 +16,20 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <platform.h>
-#include "drivers/io.h"
 
-#include "drivers/timer.h"
+#include "config/config_master.h"
 
-const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-    { TIM8,  IO_TAG(PC6),  TIM_Channel_1, TIM_USE_PWM,   0, GPIO_AF_TIM8,  NULL,         0,             0  },  // S3_IN
-    { TIM8,  IO_TAG(PC7),  TIM_Channel_2, TIM_USE_PWM,   0, GPIO_AF_TIM8,  NULL,         0,             0  },  // S4_IN
-	{ TIM3,  IO_TAG(PB0),  TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM3  }, // S1_OUT
-    { TIM3,  IO_TAG(PB1),  TIM_Channel_4, TIM_USE_MOTOR, 1, GPIO_AF_TIM3  }, // S2_OUT
-    { TIM9,  IO_TAG(PA3),  TIM_Channel_2, TIM_USE_MOTOR, 1, GPIO_AF_TIM9  }, // S3_OUT
-    { TIM2,  IO_TAG(PA2),  TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM2  }, // S4_OUT
-};
-
+// alternative defaults settings for BlueJayF4 targets
+void targetConfiguration(master_t *config)
+{
+        
+        config->boardAlignment.rollDegrees = 180;
+		config->boardAlignment.pitchDegrees = 0;
+		config->boardAlignment.yawDegrees = 90;
+		
+		
+    
+}
