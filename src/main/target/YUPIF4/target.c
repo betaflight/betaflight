79ad21c813836203e35bb4869abd7dd7bed056e4
francoisduf <fdufourniaud@hotmail.com>
2016-11-25 20:02:56 +0100

    Dshot support

--- src/main/target/YUPIF4/target.c
+++ src/main/target/YUPIF4/target.c
@@ -20,15 +20,17 @@
 #include <platform.h>
 #include "drivers/io.h"
 #include "drivers/timer.h"
+#include "drivers/timer_def.h"
 #include "drivers/dma.h"
 
+
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-    { TIM8, IO_TAG(PC8), TIM_Channel_3, TIM_USE_PPM,   0, GPIO_AF_TIM8, NULL, 0, 0 }, // PPM IN
-    { TIM2, IO_TAG(PA0), TIM_Channel_1, TIM_USE_MOTOR, 1, GPIO_AF_TIM2, NULL, 0, 0 }, // MS1
-    { TIM2, IO_TAG(PA1), TIM_Channel_2, TIM_USE_MOTOR, 1, GPIO_AF_TIM2, NULL, 0, 0 }, // MS2
-    { TIM2, IO_TAG(PA2), TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM2, NULL, 0, 0 }, // MS3
-    { TIM2, IO_TAG(PA3), TIM_Channel_4, TIM_USE_MOTOR, 1, GPIO_AF_TIM2, NULL, 0, 0 }, // MS4
-    { TIM3, IO_TAG(PB0), TIM_Channel_3, TIM_USE_MOTOR, 1, GPIO_AF_TIM3, NULL, 0, 0 }, // MS5
-    { TIM3, IO_TAG(PB1), TIM_Channel_4, TIM_USE_MOTOR, 1, GPIO_AF_TIM3, DMA1_Stream2, DMA_Channel_5, DMA1_ST2_HANDLER }, // MS6
+    DEF_TIM(TIM8, CH2, PC7, TIM_USE_PPM,                 TIMER_INPUT_ENABLED,  0 ), // PPM IN
+    DEF_TIM(TIM5, CH1, PA0, TIM_USE_MOTOR,               TIMER_OUTPUT_ENABLED, 0 ), // S1_OUT - DMA1_ST2
+    DEF_TIM(TIM5, CH2, PA1, TIM_USE_MOTOR,               TIMER_OUTPUT_ENABLED, 0 ), // S2_OUT - DMA1_ST4
+    DEF_TIM(TIM2, CH3, PA2, TIM_USE_MOTOR,               TIMER_OUTPUT_ENABLED, 0 ), // S3_OUT - DMA1_ST1
+    DEF_TIM(TIM2, CH4, PA3, TIM_USE_MOTOR,               TIMER_OUTPUT_ENABLED, 1 ), // S4_OUT - DMA1_ST6
+    DEF_TIM(TIM3, CH3, PB0, TIM_USE_MOTOR | TIM_USE_LED, TIMER_OUTPUT_ENABLED, 0 ), // S5_OUT - DMA1_ST7
+    DEF_TIM(TIM3, CH4, PB1, TIM_USE_MOTOR,               TIMER_OUTPUT_ENABLED, 0 ), // S6_OUT - DMA1_ST2
 };
 
