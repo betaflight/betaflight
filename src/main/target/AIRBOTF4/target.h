24243750d693f48e53a029578269ffd4e55404a8
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2016-11-30 22:41:13 +1000

    Fix linker scripts in preparation to parameter groups; Align FLASH_CONFIG with STM32F4 reference manual; Deprecate CONFIG_START_FLASH_ADDRESS; Unify flash erasing interface on F4 boards

--- src/main/target/AIRBOTF4/target.h
+++ src/main/target/AIRBOTF4/target.h
@@ -18,7 +18,6 @@
 #pragma once
 
 #define TARGET_BOARD_IDENTIFIER "ABF4"
-#define CONFIG_START_FLASH_ADDRESS (0x08080000) //0x08080000 to 0x080A0000 (FLASH_Sector_8)
 #define USBD_PRODUCT_STRING     "Airbot F4 / Flip32 F4"
 
 #define LED0                    PB5
