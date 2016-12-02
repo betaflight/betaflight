b1be83c18e8ec5ef06cc19624209669600bdf142
jflyper <shinoda@jaist.ac.jp>
2016-12-02 10:48:03 +0900

    I2C disabled again for testing

--- src/main/target/OMNIBUS/target.h
+++ src/main/target/OMNIBUS/target.h
@@ -78,7 +78,7 @@
 #define UART3_TX_PIN            PB10 // PB10 (AF7)
 #define UART3_RX_PIN            PB11 // PB11 (AF7)
 
-#define USE_I2C
+//#define USE_I2C
 #define I2C_DEVICE (I2CDEV_1) // PB6/SCL, PB7/SDA
 
 #define USE_ESCSERIAL
@@ -200,7 +200,7 @@
 // RSSI_PWM uses interrupt based PWM input
 #define USE_RSSI_SOFTPWM       // Interrupt based measurements
 //#define RSSI_SOFTPWM_PIN      PB2 // Requires 1K inline to be bypassed?
-//#define RSSI_SOFTPWM_PIN      PB4 // Shared with PPM (OK)
-//#define RSSI_SOFTPWM_PIN      PB7 // Shared with PWM7 & I2C (OK)
-//#define RSSI_SOFTPWM_PIN      PB6 // Shared with PWM8 & I2C (OK)
+//#define RSSI_SOFTPWM_PIN      PB4 // Shared with PPM (OK, can be rsrc switched)
+//#define RSSI_SOFTPWM_PIN      PB7 // Shared with PWM7 & I2C (OK, disable I2C)
+//#define RSSI_SOFTPWM_PIN      PB6 // Shared with PWM8 & I2C (OK, disable I2C)
 #define RSSI_SOFTPWM_PIN      NONE
