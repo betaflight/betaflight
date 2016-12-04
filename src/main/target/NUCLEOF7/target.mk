7c6d3efa1337cc87b04b5cc006e0d40b6a1c2d9a
Sami Korhonen <samik.korhonen@gmail.com>
2016-12-04 09:48:47 +0200

    NucleoF7 board support

---
+++ src/main/target/NUCLEOF7/target.mk
@@ -0,0 +1,9 @@
+F7X6XG_TARGETS += $(TARGET)
+FEATURES       += SDCARD VCP
+
+TARGET_SRC = \
+            drivers/accgyro_mpu6050.c \
+            drivers/barometer_ms5611.c \
+            drivers/compass_hmc5883l.c \
+            drivers/light_ws2811strip.c \
+            drivers/light_ws2811strip_hal.c
