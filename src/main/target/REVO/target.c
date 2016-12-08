92ae5c96d34dede0f8cba1687c8ee6cdc249e83c
jflyper <shinoda@jaist.ac.jp>
2016-12-08 14:58:46 +0900

    Fixed indentation for pre-processor directives

--- src/main/target/REVO/target.c
+++ src/main/target/REVO/target.c
@@ -43,10 +43,10 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM2,  CH4, PA3,  TIM_USE_MOTOR, 1, 1),  // S3_OUT D1_ST6
     DEF_TIM(TIM2,  CH3, PA2,  TIM_USE_MOTOR, 1, 0),  // S4_OUT D1_ST1
     DEF_TIM(TIM5,  CH2, PA1,  TIM_USE_MOTOR | TIM_USE_LED, 1, 0),  // S5_OUT / LED for REVO D1_ST4
- #ifdef AIRBOTF4
+#ifdef AIRBOTF4
     DEF_TIM(TIM1,  CH1, PA8,  TIM_USE_MOTOR, 1, 0),  // S6_OUT
- #else
+#else
     DEF_TIM(TIM5,  CH1, PA0,  TIM_USE_MOTOR, 1, 0),  // S6_OUT D1_ST2
- #endif
+#endif
 #endif
 };
