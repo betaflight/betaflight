liutairan/inav

Pitotmeter support

src/main/sensors/initialisation.c
@@ -469,6 +469,7 @@ static bool detectBaro(baroSensor_e baroHardwareToUse)
 static bool detectPitot()
 {
     pitotSensor_e pitotHardware = PITOT_DEFAULT;
+    requestedSensors[SENSOR_INDEX_PITOT] = PITOT_DEFAULT;   // FIXME
 
     switch (pitotHardware) {
         case PITOT_DEFAULT:

betaflight/betaflight

src/main/sensors/initialisation.c
@@ -463,6 +463,7 @@ static bool detectBaro(baroDev_t *dev, baroSensor_e baroHardwareToUse)
 static bool detectPitot(pitotDev_t *dev, uint8_t pitotHardwareToUse)
 {
     pitotSensor_e pitotHardware = pitotHardwareToUse;
+    requestedSensors[SENSOR_INDEX_PITOT] = pitotHardwareToUse;   // FIXME
 
     switch (pitotHardware) {
         case PITOT_DEFAULT: