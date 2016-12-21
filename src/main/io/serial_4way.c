cleanflight/cleanflight

Prevent serial overflow in BLHeli Passthrough

src/main/io/serial_4way.c
@@ -314,6 +314,8 @@ static uint8_t readByteCrc(void)
 
 static void writeByte(uint8_t b)
 {
+    while (!serialTxBytesFree(instance)) {
+    };
     serialWrite(port, b);
 }
 

betaflight/betaflight

src/main/io/serial_4way.c
@@ -314,6 +314,8 @@ static uint8_t readByteCrc(void)
 
 static void writeByte(uint8_t b)
 {
+    while (!serialTxBytesFree(port)) {
+    };
     serialWrite(port, b);
 }
 