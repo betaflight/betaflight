29fe740af068976a6f8f166e3a165d6ae777df80
johnlemon <ms@flyinglemon.eu>
2016-12-23 21:17:08 +0100

    spaces cleanup

--- src/main/target/KIWIF4/target.mk
+++ src/main/target/KIWIF4/target.mk
@@ -5,6 +5,6 @@ TARGET_SRC = \
             drivers/accgyro_spi_mpu6000.c \
             drivers/light_ws2811strip.c \
             drivers/light_ws2811strip_stm32f4xx.c\
-			drivers/max7456.c \
+            drivers/max7456.c \
             io/osd.c
 
