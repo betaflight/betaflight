betaflight/betaflight

commit e1ef91c0bc56b468fb2012d212a5f02524972ac2

Temporary disable softserial on CC3D due to code size issues.

src/main/target/CC3D/target.h
@@ -75,7 +75,7 @@
 #define USE_VCP
 #define USE_UART1
 #define USE_UART3
-#define USE_SOFTSERIAL1
+//#define USE_SOFTSERIAL1
 #define SERIAL_PORT_COUNT 4
 
 #define SOFTSERIAL_1_TIMER TIM3

commit a8ccc57445f192d104aff4e2e344228636135479

CC3D - Fix duplicate UART1 in configurator.  Caused by e1ef91c0bc56b468fb2012d212a5f02524972ac2.

src/main/target/CC3D/target.h
@@ -78,8 +78,7 @@
 #define USE_VCP
 #define USE_UART1
 #define USE_UART3
-//#define USE_SOFTSERIAL1
-#define SERIAL_PORT_COUNT 4
+#define SERIAL_PORT_COUNT 3
 
 #define USE_UART1_TX_DMA
 