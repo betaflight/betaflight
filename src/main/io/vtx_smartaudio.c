9d6fb10da45a587f4a6a2d6862cf142cb9de574e
jflyper <shinoda@jaist.ac.jp>
2016-12-30 00:41:59 +0900

    Add copyright notice

--- src/main/io/vtx_smartaudio.c
+++ src/main/io/vtx_smartaudio.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Created by jflyper */
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <ctype.h>
@@ -11,6 +30,7 @@
 
 #include "string.h"
 #include "common/printf.h"
+#include "common/utils.h"
 #include "drivers/system.h"
 #include "drivers/serial.h"
 #include "io/serial.h"
@@ -20,11 +40,6 @@
 #include "fc/runtime_config.h"
 
 #include "flight/pid.h"
-#if 0
-#include "config/config.h"
-#include "config/config_eeprom.h"
-#include "config/config_profile.h"
-#endif
 #include "config/config_master.h"
 
 #include "build/build_config.h"
