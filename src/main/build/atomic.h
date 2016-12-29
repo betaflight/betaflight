6870a9b88324cc736ec48cac45d1b83f7e6aa165
Petr Ledvina <ledvinap@gmail.com>
2016-12-29 19:31:02 +0100

    Fix atomic.h
    `=m` output operand means that value is write only, gcc may discard previous value because it assumes that it will be overwritten. This bug was not triggered in gcc v4 because `asm volatile` triggered full memory barrier.
    
    With old version, this code will increase `markme` only by 2, not 3:
    ```
    static int markme = 0;
    markme++;
    ATOMIC_BLOCK_NB(0xff) {
      ATOMIC_BARRIER(markme);  // markme is marked as overwritten, previous increment can be discarded
      markme++;
    }
    markme++;
    ```

--- src/main/build/atomic.h
+++ src/main/build/atomic.h
@@ -104,7 +104,7 @@ static inline uint8_t __basepriSetRetVal(uint8_t prio)
         __asm__ volatile ("\t# barier(" #data ")  end\n" : : "m" (**__d));                          \
     }                                                                   \
     typeof(data)  __attribute__((__cleanup__(__UNIQL(__barrierEnd)))) *__UNIQL(__barrier) = &data; \
-    __asm__ volatile ("\t# barier (" #data ") start\n" : "=m" (*__UNIQL(__barrier)))
+    __asm__ volatile ("\t# barier (" #data ") start\n" : "+m" (*__UNIQL(__barrier)))
 
 #endif //__arm__
 
