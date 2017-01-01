3154dcea1001931fb49f75bcd6a01417e835815d
jflyper <shinoda@jaist.ac.jp>
2017-01-01 12:48:00 +0900

    Renamed cli variable oled_bus to i2c_bus_oled
    So “set/get i2c” will display all i2c prefixed variables (including bus
    numbers).

--- src/main/io/serial_cli.c
+++ src/main/io/serial_cli.c
@@ -991,7 +991,7 @@ const clivalue_t valueTable[] = {
     { "vcd_v_offset",               VAR_INT8    | MASTER_VALUE, &vcdProfile()->v_offset, .config.minmax = { -15, 16 } },
 #endif
 #ifdef USE_OLEDI2C
-    { "oled_bus",                   VAR_INT8    | MASTER_VALUE, &oledi2cConfig()->bus, .config.minmax = { I2CINVALID, I2CDEV_COUNT - 1 } },
+    { "i2c_bus_oled",               VAR_INT8    | MASTER_VALUE, &oledi2cConfig()->bus, .config.minmax = { I2CINVALID, I2CDEV_COUNT - 1 } },
 #endif
 };
 
