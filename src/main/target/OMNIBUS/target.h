51bb3a6df687d1f427b01827fdec882c5251316c
jflyper <shinoda@jaist.ac.jp>
2017-01-03 18:16:47 +0900

    Rename uartStartTxDMA to uartTryStartTxDMA
    serial_uart.h now defines uartTryStartTxDMA.
    Untested, but should be okay (name change only).

--- src/main/target/OMNIBUS/target.h
+++ src/main/target/OMNIBUS/target.h
@@ -73,11 +73,13 @@
 
 #define UART2_TX_PIN            PA14 // PA14 / SWCLK
 #define UART2_RX_PIN            PA15
-#define USE_UART1_TX_DMA
-#define USE_UART2_TX_DMA
 
 #define UART3_TX_PIN            PB10 // PB10 (AF7)
 #define UART3_RX_PIN            PB11 // PB11 (AF7)
+
+#define USE_UART1_TX_DMA
+#define USE_UART2_TX_DMA
+#define USE_UART3_TX_DMA
 
 #define USE_I2C
 #define I2C_DEVICE (I2CDEV_1) // PB6/SCL, PB7/SDA
