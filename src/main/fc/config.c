ccc7292b6c9800eb96fa86745128e11a53df0034
blckmn <j@blck.mn>
2017-01-11 12:24:34 +1100

    Removed VCP default to using second serial (UART1 in most cases) to MSP

--- src/main/fc/config.c
+++ src/main/fc/config.c
@@ -452,10 +452,6 @@ void resetSerialConfig(serialConfig_t *serialConfig)
     }
 
     serialConfig->portConfigs[0].functionMask = FUNCTION_MSP;
-#if defined(USE_VCP)
-    // This allows MSP connection via USART & VCP so the board can be reconfigured.
-    serialConfig->portConfigs[1].functionMask = FUNCTION_MSP;
-#endif
 }
 
 void resetRcControlsConfig(rcControlsConfig_t *rcControlsConfig)
