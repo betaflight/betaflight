671e1dc16dc958be802ec4407e9e0ab9c493a019
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2017-01-24 20:45:36 +1000

    Remove ASYNC_GYRO_PROCESSING, BOOTLOG and TASK_STATISTICS from 128K targets

--- src/main/target/NAZE/target.h
+++ src/main/target/NAZE/target.h
@@ -60,7 +60,7 @@
     #define UART3_RX_PIN            PB11
     #define UART3_TX_PIN            PB10
 
-    #define SERIAL_PORT_COUNT       5
+    #define SERIAL_PORT_COUNT       4
 
     #define MPU6500_CS_GPIO_CLK_PERIPHERAL  NAZE_CS_GPIO_CLK_PERIPHERAL
     #define MPU6500_CS_GPIO                 NAZE_SPI_CS_GPIO
@@ -74,7 +74,7 @@
     #define M25P16_CS_PIN           NAZE_SPI_CS_PIN
     #define M25P16_SPI_INSTANCE     NAZE_SPI_INSTANCE
 
-    #define SERIAL_PORT_COUNT       4
+    #define SERIAL_PORT_COUNT       5
 
     #define USE_FLASHFS
     #define USE_FLASH_M25P16
@@ -184,7 +184,6 @@
 //#define USE_SERIAL_4WAY_BLHELI_INTERFACE
 
 #define TARGET_MOTOR_COUNT      6
-#define DISABLE_UNCOMMON_MIXERS
 
 #define DEFAULT_FEATURES        FEATURE_VBAT
 #define DEFAULT_RX_FEATURE      FEATURE_RX_PPM
