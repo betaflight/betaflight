fb383dc4a52632c677d4690fdb4551ec5b2712a3
jamming <jianbing.pan@holybro.com>
2017-02-08 10:00:32 +0800

    changes log

--- src/main/target/KAKUTEF4/target.h
+++ src/main/target/KAKUTEF4/target.h
@@ -129,7 +129,15 @@
 #define RSSI_ADC_CHANNEL            ADC_Channel_11
 
 #define LED_STRIP
-#define LED_STRIP_TIMER         TIM5
+#define WS2811_PIN                      PA0
+#define WS2811_TIMER                    TIM5
+#define WS2811_DMA_HANDLER_IDENTIFER    DMA1_ST2_HANDLER
+#define WS2811_DMA_STREAM               DMA1_Stream2
+#define WS2811_DMA_IT                   DMA_IT_TCIF2
+#define WS2811_DMA_CHANNEL              DMA_Channel_6
+#define WS2811_TIMER_CHANNEL            TIM_Channel_1
+#define WS2811_TIMER_GPIO_AF            GPIO_AF_TIM5
+
 
 #define DEFAULT_FEATURES        FEATURE_BLACKBOX
 #define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
@@ -142,6 +150,6 @@
 #define TARGET_IO_PORTB 0xffff
 #define TARGET_IO_PORTC 0xffff
 
-#define USABLE_TIMER_CHANNEL_COUNT 7
+#define USABLE_TIMER_CHANNEL_COUNT 8
 #define USED_TIMERS  ( TIM_N(2) | TIM_N(3) | TIM_N(5)  |  TIM_N(8))
 
