e8d8c8453bc0a2da7e2744458d48076e5c953388
jamming <jianbing.pan@holybro.com>
2017-02-13 09:27:43 +0800

    fix kakuteF4 led_trip final changes

--- src/main/target/KAKUTEF4/target.h
+++ src/main/target/KAKUTEF4/target.h
@@ -129,15 +129,6 @@
 #define RSSI_ADC_CHANNEL            ADC_Channel_11
 
 #define LED_STRIP
-#define WS2811_PIN                      PA0
-#define WS2811_TIMER                    TIM5
-#define WS2811_DMA_HANDLER_IDENTIFER    DMA1_ST2_HANDLER
-#define WS2811_DMA_STREAM               DMA1_Stream2
-#define WS2811_DMA_IT                   DMA_IT_TCIF2
-#define WS2811_DMA_CHANNEL              DMA_Channel_6
-#define WS2811_TIMER_CHANNEL            TIM_Channel_1
-#define WS2811_TIMER_GPIO_AF            GPIO_AF_TIM5
-
 
 #define DEFAULT_FEATURES        FEATURE_BLACKBOX
 #define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
