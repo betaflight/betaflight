23d9ccc77ba8c7027e575efa1edc11a562bd28dc
Michael Keller <github@ike.ch>
2017-02-17 10:05:58 +1300

    Fixed timer definitions for REVO / REVOLT / AIRBOTF4.

--- src/main/target/REVO/target.h
+++ src/main/target/REVO/target.h
@@ -208,8 +208,13 @@
 
 #ifdef REVOLT
 #define USABLE_TIMER_CHANNEL_COUNT 11
+#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(8) | TIM_N(12) )
 #else
 #define USABLE_TIMER_CHANNEL_COUNT 12
-#endif
+#ifdef AIRBOTF4
+#define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) | TIM_N(12) )
+#else
+#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) | TIM_N(12) )
+#endif // AIRBOTF4
+#endif // REVOLT
 
-#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(12) | TIM_N(8) | TIM_N(9) )
