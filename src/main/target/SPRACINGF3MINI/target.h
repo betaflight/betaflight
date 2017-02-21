7ed1e1914ce3bb73bccd2673003474d74b42d644
mikeller <github@ike.ch>
2017-02-22 01:00:33 +1300

    Re-add Spektrum bind pin for SPRACINGF3MINI / TINYBEEF3.

--- src/main/target/SPRACINGF3MINI/target.h
+++ src/main/target/SPRACINGF3MINI/target.h
@@ -183,8 +183,11 @@
 #define BUTTON_B_PIN            PB0
 
 #define HARDWARE_BIND_PLUG
-#define BINDPLUG_PIN            PB0
+#define BINDPLUG_PIN            BUTTON_B_PIN
 #endif
+
+#define SPEKTRUM_BIND
+#define BIND_PIN                UART2_RX_PIN
 
 #define TARGET_IO_PORTA         0xffff
 #define TARGET_IO_PORTB         0xffff
