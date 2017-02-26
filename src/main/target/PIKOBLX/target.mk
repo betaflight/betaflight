9ad094f9aa792f3e1132c6390688cd2644677ddc
mikeller <github@ike.ch>
2017-02-27 00:20:52 +1300

    Moved hardware specific parts into 'transponder_ir.c'.

--- src/main/target/PIKOBLX/target.mk
+++ src/main/target/PIKOBLX/target.mk
@@ -5,6 +5,5 @@ TARGET_SRC = \
             drivers/accgyro_mpu.c \
             drivers/accgyro_spi_mpu6000.c \
             drivers/transponder_ir.c \
-            drivers/transponder_ir_stm32f30x.c \
             io/transponder_ir.c
 
