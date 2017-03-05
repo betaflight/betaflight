001dc05864a27674aa06404bd6e366922d6095de
Michael Keller <github@ike.ch>
2017-03-06 11:48:59 +1300

    Disabled SOFTSERIAL for SPRACINGF3EVO and variants to make the targets fit the flash size in 3.1.7.

--- src/main/target/SPRACINGF3EVO/target.h
+++ src/main/target/SPRACINGF3EVO/target.h
@@ -71,10 +71,12 @@
 #define USE_UART1
 #define USE_UART2
 #define USE_UART3
-#define USE_SOFTSERIAL1
-#define USE_SOFTSERIAL2
+// Disabling SOFTSERIAL to make the targets fit the flash for 3.1.7
+//#define USE_SOFTSERIAL1
+//#define USE_SOFTSERIAL2
 
-#define SERIAL_PORT_COUNT       6
+//#define SERIAL_PORT_COUNT       6
+#define SERIAL_PORT_COUNT       4
 
 #define USE_ESCSERIAL
 #define ESCSERIAL_TIMER_TX_HARDWARE 0 // PWM 1
