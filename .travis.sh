959f981ac47260f2762a53e3c08e838d21fb21b4
Magnus Ivarsson <magnus@unitware.se>
2017-03-07 20:29:48 +0100

    try to reduce noise

--- .travis.sh
+++ .travis.sh
@@ -67,7 +67,7 @@ elif [ $GOAL ] ; then
     make V=0 $GOAL || exit $?
     
     if [ "test" == "$GOAL" ] ; then
-        lcov --directory . -b src/test --capture --output-file coverage.info # capture coverage info
+        lcov --directory . -b src/test --capture --output-file coverage.info 2>&1 | grep -E ":version '402\*', prefer.*'406\*" --invert-match
         lcov --remove coverage.info 'lib/test/*' 'src/test/*' '/usr/*' --output-file coverage.info # filter out system and test code
         lcov --list coverage.info # debug before upload
         coveralls-lcov coverage.info # uploads to coveralls
