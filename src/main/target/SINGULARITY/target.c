a4b19e98c5153942ccf8ee6af4edce374a247b60
Thomas Calvert <thomas@calvert.ch>
2017-03-15 14:26:56 +0100

    Usable DSHOT on Singularity target (motors 1-4 only)

--- src/main/target/SINGULARITY/target.c
+++ src/main/target/SINGULARITY/target.c
@@ -27,11 +27,11 @@
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM2,  CH1, PA15, TIM_USE_PWM | TIM_USE_PPM,   0), // PPM/SERIAL RX
     DEF_TIM(TIM3,  CH1, PB4,  TIM_USE_PWM,                 0), // PWM1
-    DEF_TIM(TIM3,  CH2, PB5,  TIM_USE_PWM,                 0), // PWM2
+    DEF_TIM(TIM17, CH1, PB5,  TIM_USE_PWM,                 0), // PWM2
     DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_PWM,                 0), // PWM3
     DEF_TIM(TIM3,  CH4, PB1,  TIM_USE_PWM,                 0), // PWM4
     DEF_TIM(TIM16, CH1, PB8,  TIM_USE_MOTOR,               1), // PWM5
-    DEF_TIM(TIM17, CH1, PB9,  TIM_USE_MOTOR,               1), // PWM6
+    DEF_TIM(TIM4,  CH4, PB9,  TIM_USE_MOTOR,               1), // PWM6
     DEF_TIM(TIM15, CH1, PA2,  TIM_USE_MOTOR,               1), // SOFTSERIAL1 RX (NC)
     DEF_TIM(TIM15, CH2, PA3,  TIM_USE_MOTOR,               1), // SOFTSERIAL1 TX
     DEF_TIM(TIM1,  CH1, PA8,  TIM_USE_MOTOR | TIM_USE_LED, 1), // LED_STRIP
