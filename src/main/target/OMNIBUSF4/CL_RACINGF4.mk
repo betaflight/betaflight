40eccea4a9ed9eec15ef09c0d687fba88a13fa40
Cheng Lin <bnn1044@gmail.com>
2017-03-15 10:23:53 -0400

    CREATE CL_RACINGF4.mk for CL_RACING TARGET

---
+++ src/main/target/OMNIBUSF4/CL_RACINGF4.mk
@@ -0,0 +1,11 @@
+#==CL_RACING F4 PDB rev 1.0==
+#Owner: CL_RACING desgin base on OmnibusF4 board
+#Board information:
+#- CPU - STM32F405RGT6
+#- MPU-6000
+#- Full push in SD Card Slot 
+#- Build in 5V BEC 
+#- Build in LC filter for camera and vtx
+#- Build in OSD
+#- Built in current meter, PDB
+FEATURES       += VCP SDCARD
