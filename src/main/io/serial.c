2e982793699441e2e9eafab63d79c8c119fbbc52
DieHertz <diehertz@gmail.com>
2017-03-17 02:08:01 +0300

    Set default GPS baud rate back to 57600

--- src/main/io/serial.c
+++ src/main/io/serial.c
@@ -110,7 +110,7 @@ void pgResetFn_serialConfig(serialConfig_t *serialConfig)
     for (int i = 0; i < SERIAL_PORT_COUNT; i++) {
         serialConfig->portConfigs[i].identifier = serialPortIdentifiers[i];
         serialConfig->portConfigs[i].msp_baudrateIndex = BAUD_115200;
-        serialConfig->portConfigs[i].gps_baudrateIndex = BAUD_38400;
+        serialConfig->portConfigs[i].gps_baudrateIndex = BAUD_57600;
         serialConfig->portConfigs[i].telemetry_baudrateIndex = BAUD_AUTO;
         serialConfig->portConfigs[i].blackbox_baudrateIndex = BAUD_115200;
     }
