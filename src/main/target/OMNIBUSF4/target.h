416feaed0d6eaa3adb361564f29264e0820eb2f7
Cheng Lin <bnn1044@gmail.com>
2017-03-15 21:48:52 -0400

    Update target.h

--- src/main/target/OMNIBUSF4/target.h
+++ src/main/target/OMNIBUSF4/target.h
@@ -169,6 +169,7 @@
 #define USE_ADC
 #define CURRENT_METER_ADC_PIN   PC1
 #define VBAT_ADC_PIN            PC2
+//#define RSSI_ADC_PIN            PA0
 #ifdef CL_RACINGF3
  #define RSSI_ADC_PIN            PC3
 #endif
