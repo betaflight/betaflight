0c57c7cd05d221461499aac882b340a1c31c4712
Faduf <francois.dufourniaud@gadz.org>
2017-03-19 21:04:25 +0100

    Add OSD support to YuPiF4 target

--- src/main/target/YUPIF4/target.h
+++ src/main/target/YUPIF4/target.h
@@ -128,6 +128,15 @@
 #define SPI3_MOSI_PIN           PC12
 
 
+// OSD
+#define OSD
+#define USE_MAX7456
+#define MAX7456_SPI_INSTANCE    SPI1
+#define MAX7456_SPI_CS_PIN      PA14
+#define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD*2)
+#define MAX7456_RESTORE_CLK     (SPI_CLOCK_FAST)
+
+
 #define USE_I2C
 #define USE_I2C_DEVICE_1
 #define I2C_DEVICE              (I2CDEV_1)
