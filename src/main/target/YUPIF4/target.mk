e666a1f1b15a45fc1ab1bd1edf374c6e854a0473
Faduf <francois.dufourniaud@gadz.org>
2017-03-20 22:38:05 +0100

    Add OSD support to YuPiF4 target

--- src/main/target/YUPIF4/target.mk
+++ src/main/target/YUPIF4/target.mk
@@ -4,5 +4,7 @@ FEATURES        += SDCARD VCP
 TARGET_SRC = \
             drivers/accgyro_mpu6500.c \
             drivers/accgyro_spi_mpu6500.c \
-            drivers/accgyro_spi_icm20689.c
+            drivers/accgyro_spi_icm20689.c\
+            drivers/max7456.c \
+            io/osd.c
 
