1fceed7bab69e248217a58bb2928b9430e7d2a2d
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2017-03-27 17:42:28 +1000

    More baud rates (1200 to 921600)

--- src/main/io/serial.h
+++ src/main/io/serial.h
@@ -45,6 +45,8 @@ typedef enum {
 
 typedef enum {
     BAUD_AUTO = 0,
+    BAUD_1200,
+    BAUD_2400,
     BAUD_9600,
     BAUD_19200,
     BAUD_38400,
@@ -52,6 +54,8 @@ typedef enum {
     BAUD_115200,
     BAUD_230400,
     BAUD_250000,
+    BAUD_460800,
+    BAUD_921600,
 } baudRate_e;
 
 extern const uint32_t baudRates[];
