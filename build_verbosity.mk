9e77f9c4c87a7ae19b35a2ee71a0068fe6a0d6a2
Magnus Ivarsson <magnus@unitware.se>
2017-03-31 16:57:56 +0200

    control build verbosity for the unit tests

---
+++ build_verbosity.mk
@@ -0,0 +1,22 @@
+
+
+## V                 : Set verbosity level based on the V= parameter
+##                     V=0 Low
+##                     V=1 High
+export AT := @
+
+ifndef V
+export V0    :=
+export V1    := $(AT)
+export STDOUT   :=
+else ifeq ($(V), 0)
+export V0    := $(AT)
+export V1    := $(AT)
+export STDOUT:= "> /dev/null"
+export MAKE  := $(MAKE) --no-print-directory
+else ifeq ($(V), 1)
+export V0    :=
+export V1    :=
+export STDOUT   :=
+endif
+
