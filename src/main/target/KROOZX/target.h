48efae5c88f459efa3faea887d629ab8b5af84c6
Sergey Krukovski <softsr@yahoo.de>
2017-04-02 21:52:27 +0200

    Change KroozX I2C setup

--- src/main/target/KROOZX/target.h
+++ src/main/target/KROOZX/target.h
@@ -57,6 +57,7 @@
 
 #define BARO
 #define USE_BARO_MS5611
+#define BARO_I2C_INSTANCE       I2CDEV_3
 
 #define USE_SDCARD
 #define SDCARD_DETECT_INVERTED
@@ -114,8 +115,8 @@
 #define SERIAL_PORT_COUNT       6
 
 #define USE_I2C
-#define I2C_DEVICE              (I2CDEV_3)
-#define I2C_DEVICE_EXT          (I2CDEV_1)
+#define I2C_DEVICE              (I2CDEV_1)
+#define I2C_DEVICE_EXT          (I2CDEV_3)
 #define I2C1_SCL                PB6
 #define I2C1_SDA                PB7
 #define I2C3_SCL                PA8
