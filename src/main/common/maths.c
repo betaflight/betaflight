70101693bc784d5216d6e6cd32ec184e12e679bf
Manors <manors@live.cn>
2017-04-06 00:45:44 +0800

    Tidy scaleRange code

--- src/main/common/maths.c
+++ src/main/common/maths.c
@@ -155,7 +155,7 @@ float degreesToRadians(int16_t degrees)
 int scaleRange(int x, int srcMin, int srcMax, int destMin, int destMax) {
     long int a = ((long int) destMax - (long int) destMin) * ((long int) x - (long int) srcMin);
     long int b = (long int) srcMax - (long int) srcMin;
-    return ((a / b) - (destMax - destMin)) + destMax;
+    return (a / b) + destMin;
 }
 
 // Normalize a vector
