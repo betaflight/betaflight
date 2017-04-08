ba75dc4f1dff408779086ac20f91bc508b759a37
Hydra <dominic.clifton@cleanflight.com>
2017-04-08 18:51:15 +0100

    CF/BF - SPRacingF3OSD - Allow cli and bootloader again.

--- src/main/fc/fc_tasks.c
+++ src/main/fc/fc_tasks.c
@@ -114,11 +114,7 @@ static void taskHandleSerial(timeUs_t currentTimeUs)
         return;
     }
 #endif
-#ifdef USE_OSD_SLAVE
-    mspSerialProcess(MSP_SKIP_NON_MSP_DATA, mspFcProcessCommand, mspFcProcessReply);
-#else
     mspSerialProcess(ARMING_FLAG(ARMED) ? MSP_SKIP_NON_MSP_DATA : MSP_EVALUATE_NON_MSP_DATA, mspFcProcessCommand, mspFcProcessReply);
-#endif
 }
 
 void taskBatteryAlerts(timeUs_t currentTimeUs)
