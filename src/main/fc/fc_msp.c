e1e4312f1271ae24626ac1170187871c9ba862d1
Hydra <dominic.clifton@cleanflight.com>
2017-04-09 21:32:30 +0100

    CF/BF - Fix more compiler warnings.

--- src/main/fc/fc_msp.c
+++ src/main/fc/fc_msp.c
@@ -62,6 +62,7 @@
 #include "fc/rc_adjustments.h"
 #include "fc/runtime_config.h"
 
+#ifdef USE_FC
 #include "flight/altitudehold.h"
 #include "flight/failsafe.h"
 #include "flight/imu.h"
@@ -69,6 +70,7 @@
 #include "flight/navigation.h"
 #include "flight/pid.h"
 #include "flight/servos.h"
+#endif
 
 #include "io/asyncfatfs/asyncfatfs.h"
 #include "io/beeper.h"
@@ -89,14 +91,18 @@
 #include "msp/msp_protocol.h"
 #include "msp/msp_serial.h"
 
+#ifdef USE_FC
 #include "rx/msp.h"
 #include "rx/rx.h"
+#endif
 
 #include "scheduler/scheduler.h"
 
+#include "sensors/battery.h"
+
+#ifdef USE_FC
 #include "sensors/acceleration.h"
 #include "sensors/barometer.h"
-#include "sensors/battery.h"
 #include "sensors/boardalignment.h"
 #include "sensors/compass.h"
 #include "sensors/gyro.h"
@@ -104,6 +110,7 @@
 #include "sensors/sonar.h"
 
 #include "telemetry/telemetry.h"
+#endif
 
 #ifdef USE_HARDWARE_REVISION_DETECTION
 #include "hardware_revision.h"
