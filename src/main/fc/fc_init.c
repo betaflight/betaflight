0b163382493cdc89b3517aa2e2ff62313dfd82be
Hydra <dominic.clifton@cleanflight.com>
2017-04-17 15:12:41 +0100

    CF/BF - Remove editor mishap.

--- src/main/fc/fc_init.c
+++ src/main/fc/fc_init.c
@@ -1,5 +1,5 @@
 /*
-5 * This file is part of Cleanflight.
+ * This file is part of Cleanflight.
  *
  * Cleanflight is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
