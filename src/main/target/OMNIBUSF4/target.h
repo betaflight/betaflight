93e80f029da8b5a02ca29f85b881e8822a41897e
Cheng Lin <bnn1044@gmail.com>
2017-04-19 09:01:29 -0400

    ADD pwm beeper and timer for pwm

--- src/main/target/OMNIBUSF4/target.h
+++ src/main/target/OMNIBUSF4/target.h
@@ -40,7 +40,7 @@
 #define LED0                    PB5
 //#define LED1                    PB4 // Remove this at the next major release
 #if defined(CL_RACINGF4)
-#define BEEPER_PWM_HZ           2200 // Beeper PWM frequency in Hz
+#define BEEPER_PWM_HZ           3800 // Beeper PWM frequency in Hz
 #endif
 #define BEEPER                  PB4
 
@@ -215,9 +215,6 @@
 #define TARGET_IO_PORTC (0xffff & ~(BIT(15)|BIT(14)|BIT(13)))
 #define TARGET_IO_PORTD BIT(2)
 
-//#ifdef CL_RACINGF4
-//#define USABLE_TIMER_CHANNEL_COUNT 6
-//#define USED_TIMERS  ( TIM_N(4) | TIM_N(8)|TIM_N(3) )
 #if defined(OMNIBUSF4SD) || defined(CL_RACINGF4)
 #define USABLE_TIMER_CHANNEL_COUNT 13
 #else
