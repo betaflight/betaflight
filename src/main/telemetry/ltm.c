aeac0f3d1602f084b285ff2a1570aeeb606edcbd
Krzysztof Matula <km@apnet.pl>
2017-04-27 00:57:21 +0200

    The MAG mode is usually a long-term setting, while ALTHOLD rather short-term. Changing the order of if-s, so ALTHOLD is reported first, then MAG mode only if ALTHOLD is not enabled.

--- src/main/telemetry/ltm.c
+++ src/main/telemetry/ltm.c
@@ -192,10 +192,10 @@ void ltm_sframe(sbuf_t *dst)
         lt_flightmode = 13;
     else if (FLIGHT_MODE(NAV_POSHOLD_MODE))
         lt_flightmode = 9;
-    else if (FLIGHT_MODE(HEADFREE_MODE) || FLIGHT_MODE(HEADING_MODE))
-        lt_flightmode = 11;
     else if (FLIGHT_MODE(NAV_ALTHOLD_MODE))
         lt_flightmode = 8;
+    else if (FLIGHT_MODE(HEADFREE_MODE) || FLIGHT_MODE(HEADING_MODE))
+        lt_flightmode = 11;
     else if (FLIGHT_MODE(ANGLE_MODE))
         lt_flightmode = 2;
     else if (FLIGHT_MODE(HORIZON_MODE))
