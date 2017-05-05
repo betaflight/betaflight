b1654e335eda308de40c88a6dd12f5ac2b3a1e70
Cheng Lin <bnn1044@gmail.com>
2017-05-05 10:37:31 -0400

    Update target.c file for clracingf7 target
    Change tab to space for spacing

--- src/main/target/CLRACINGF7/target.h
+++ src/main/target/CLRACINGF7/target.h
@@ -47,11 +47,6 @@
 
 #define USE_SDCARD
 #define SDCARD_DETECT_PIN                   PB7
-#define SDCARD_DETECT_EXTI_LINE             EXTI_Line3
-#define SDCARD_DETECT_EXTI_PIN_SOURCE       EXTI_PinSource3
-#define SDCARD_DETECT_EXTI_PORT_SOURCE      EXTI_PortSourceGPIOD
-#define SDCARD_DETECT_EXTI_IRQn             EXTI3_IRQn
-
 #define SDCARD_SPI_INSTANCE                 SPI2
 #define SDCARD_SPI_CS_PIN                   SPI2_NSS_PIN
 
