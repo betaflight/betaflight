8a2e70bc3a0dd96f06d255165f448148894f4c9a
Sami Korhonen <samik.korhonen@gmail.com>
2017-05-06 14:43:11 +0300

    F7 HAL update V1.2.2

--- lib/main/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_conf_template.h
+++ lib/main/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_conf_template.h
@@ -2,8 +2,8 @@
   ******************************************************************************
   * @file    stm32f7xx_hal_conf_template.h
   * @author  MCD Application Team
-  * @version V1.2.1
-  * @date    24-March-2017
+  * @version V1.2.2
+  * @date    14-April-2017
   * @brief   HAL configuration template file. 
   *          This file should be copied to the application folder and renamed
   *          to stm32f7xx_hal_conf.h.
--- lib/main/STM32F7xx_HAL_Driver/Src/stm32f7xx_ll_tim.c
+++ lib/main/STM32F7xx_HAL_Driver/Src/stm32f7xx_ll_tim.c
@@ -2,8 +2,8 @@
   ******************************************************************************
   * @file    stm32f7xx_ll_tim.c
   * @author  MCD Application Team
-  * @version V1.2.1
-  * @date    24-March-2017
+  * @version V1.2.2
+  * @date    14-April-2017
   * @brief   TIM LL module driver.
   ******************************************************************************
   * @attention
