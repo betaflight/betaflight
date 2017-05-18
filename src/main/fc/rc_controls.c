65476c506cc5c8aa9934fc5a07149a9a028d17b5
DieHertz <diehertz@gmail.com>
2017-05-18 19:06:12 +0300

    Added stick commands for camera control

--- src/main/fc/rc_controls.c
+++ src/main/fc/rc_controls.c
@@ -34,6 +34,8 @@
 #include "config/parameter_group.h"
 #include "config/parameter_group_ids.h"
 
+#include "drivers/camera_control.h"
+
 #include "fc/config.h"
 #include "fc/fc_core.h"
 #include "fc/rc_controls.h"
@@ -299,6 +301,22 @@ void processRcStickPositions(throttleStatus_e throttleStatus)
     }
     if (rcSticks ==  THR_HI + YAW_HI + PIT_CE + ROL_LO) {
         vtxDecrementChannel();
+    }
+#endif
+
+#ifdef USE_CAMERA_CONTROL
+    if (rcSticks == THR_LO + YAW_CE + PIT_LO + ROL_CE) {
+        cameraControlKeyPress(CAMERA_CONTROL_KEY_ENTER, 0);
+    } else if (rcSticks == THR_CE + YAW_CE + PIT_CE + ROL_LO) {
+        cameraControlKeyPress(CAMERA_CONTROL_KEY_LEFT, 0);
+    } else if (rcSticks == THR_CE + YAW_CE + PIT_HI + ROL_CE) {
+        cameraControlKeyPress(CAMERA_CONTROL_KEY_UP, 0);
+    } else if (rcSticks == THR_CE + YAW_CE + PIT_CE + ROL_HI) {
+        cameraControlKeyPress(CAMERA_CONTROL_KEY_RIGHT, 0);
+    } else if (rcSticks == THR_CE + YAW_CE + PIT_LO + ROL_CE) {
+        cameraControlKeyPress(CAMERA_CONTROL_KEY_DOWN, 0);
+    } else if (rcSticks == THR_LO + YAW_CE + PIT_HI + ROL_CE) {
+        cameraControlKeyPress(CAMERA_CONTROL_KEY_UP, 2000);
     }
 #endif
 }
