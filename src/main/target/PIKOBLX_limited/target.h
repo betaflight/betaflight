9a61c594e2961f7423e6c0ca9cb14aa20b954dbd
Konstantin (DigitalEntity) Sharlaimov <konstantin.sharlaimov@gmail.com>
2017-06-09 20:47:44 +0300

    Fix PICOBLX target

--- src/main/target/PIKOBLX_limited/target.h
+++ src/main/target/PIKOBLX_limited/target.h
@@ -79,32 +79,13 @@
 #define RSSI_ADC_PIN            PB2
 
 #define LED_STRIP
-#if 1
-
 #define USE_LED_STRIP_ON_DMA1_CHANNEL3
 #define WS2811_PIN                      PB8 // TIM16_CH1
 #define WS2811_TIMER                    TIM16
-#define WS2811_DMA_CHANNEL              DMA1_Channel3
+#define WS2811_DMA_STREAM               DMA1_Channel3
 #define WS2811_IRQ                      DMA1_Channel3_IRQn
 #define WS2811_DMA_TC_FLAG              DMA1_FLAG_TC3
 #define WS2811_DMA_HANDLER_IDENTIFER    DMA1_CH3_HANDLER
-#endif
-
-#if 0
-// Alternate LED strip pin
-// FIXME DMA IRQ Transfer Complete is never called because the  TIM17_DMA_RMP needs to be set in SYSCFG_CFGR1
-#define LED_STRIP_TIMER TIM17
-#define USE_LED_STRIP_ON_DMA1_CHANNEL7
-#define WS2811_GPIO                     GPIOA
-#define WS2811_GPIO_AHB_PERIPHERAL      RCC_AHBPeriph_GPIOA
-#define WS2811_GPIO_AF                  GPIO_AF_1
-#define WS2811_PIN                      GPIO_Pin_7 // TIM17_CH1
-#define WS2811_PIN_SOURCE               GPIO_PinSource7
-#define WS2811_TIMER                    TIM17
-#define WS2811_TIMER_APB2_PERIPHERAL    RCC_APB2Periph_TIM17
-#define WS2811_DMA_CHANNEL              DMA1_Channel7
-#define WS2811_IRQ                      DMA1_Channel7_IRQn
-#endif
 
 #define TRANSPONDER
 #define TRANSPONDER_GPIO                     GPIOA
