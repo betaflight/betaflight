670282e0a1f61a3a06c56e87ba59df8ea6ce5bcc
Krzysztof Matula <km@apnet.pl>
2017-06-09 23:29:04 +0200

    stats: don't record for flioghts < 10s
    +stats doc in Cli.md

--- src/main/fc/stats.c
+++ src/main/fc/stats.c
@@ -12,6 +12,9 @@
 #include "config/parameter_group.h"
 #include "config/parameter_group_ids.h"
 
+#define MIN_FLIGHT_TIME_TO_RECORD_STATS_S 10    //prevent recording stats for that short "flights" [s]
+
+
 PG_REGISTER_WITH_RESET_FN(statsConfig_t, statsConfig, PG_STATS_CONFIG, 0);
 
 void pgResetFn_statsConfig(statsConfig_t *instance)
@@ -33,9 +36,12 @@ void statsOnArm(void)
 void statsOnDisarm(void)
 {
     if (statsConfig()->stats_enabled) {
-        statsConfigMutable()->stats_total_time += (millis() - arm_millis) / 1000;	//[s]
-        statsConfigMutable()->stats_total_dist += (getTotalTravelDistance() - arm_distance_cm) / 100;	//[m]
-        writeEEPROM();
+        uint32_t dt = (millis() - arm_millis) / 1000;
+        if (dt >= MIN_FLIGHT_TIME_TO_RECORD_STATS_S) {
+            statsConfigMutable()->stats_total_time += dt;   //[s]
+            statsConfigMutable()->stats_total_dist += (getTotalTravelDistance() - arm_distance_cm) / 100;   //[m]
+            writeEEPROM();
+        }
     }
 }
 
