86fca0509fa1fe57cd6eb6e6f909127a6d38e2b0
Konstantin (DigitalEntity) Sharlaimov <konstantin.sharlaimov@gmail.com>
2017-06-10 11:17:29 +0300

    Fix CC3D typos

--- src/main/target/CC3D/target.c
+++ src/main/target/CC3D/target.c
@@ -24,24 +24,24 @@
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
 #ifdef CC3D_PPM1
-    { TIM4, IO_TAG(PB6), TIM_Channel_1, TIM4_IRQn,    0, IOCFG_IPD,  TIM_USE_PPM | TIM_USE_PWM },   // S1_IN
+    { TIM4, IO_TAG(PB6), TIM_Channel_1, 0, IOCFG_IPD,  TIM_USE_PPM | TIM_USE_PWM },   // S1_IN
 #else
-    { TIM4, IO_TAG(PB6), TIM_Channel_1, TIM4_IRQn,    0, IOCFG_IPD,  TIM_USE_PWM },   // S1_IN
+    { TIM4, IO_TAG(PB6), TIM_Channel_1, 0, IOCFG_IPD,  TIM_USE_PWM },   // S1_IN
 #endif
-    { TIM3, IO_TAG(PB5), TIM_Channel_2, TIM3_IRQn,    0, IOCFG_IPD,  TIM_USE_PWM },   // S2_IN - SoftSerial TX - GPIO_PartialRemap_TIM3 / Sonar trigger
-    { TIM3, IO_TAG(PB0), TIM_Channel_3, TIM3_IRQn,    0, IOCFG_IPD,  TIM_USE_PWM },   // S3_IN - SoftSerial RX / Sonar echo / RSSI ADC
-    { TIM3, IO_TAG(PB1), TIM_Channel_4, TIM3_IRQn,    0, IOCFG_IPD,  TIM_USE_PWM },   // S4_IN - Current
-    { TIM2, IO_TAG(PA0), TIM_Channel_1, TIM2_IRQn,    0, IOCFG_IPD,  TIM_USE_PWM },   // S5_IN - Vbattery
+    { TIM3, IO_TAG(PB5), TIM_Channel_2, 0, IOCFG_IPD,  TIM_USE_PWM },   // S2_IN - SoftSerial TX - GPIO_PartialRemap_TIM3 / Sonar trigger
+    { TIM3, IO_TAG(PB0), TIM_Channel_3, 0, IOCFG_IPD,  TIM_USE_PWM },   // S3_IN - SoftSerial RX / Sonar echo / RSSI ADC
+    { TIM3, IO_TAG(PB1), TIM_Channel_4, 0, IOCFG_IPD,  TIM_USE_PWM },   // S4_IN - Current
+    { TIM2, IO_TAG(PA0), TIM_Channel_1, 0, IOCFG_IPD,  TIM_USE_PWM },   // S5_IN - Vbattery
 #ifdef CC3D_PPM1
-    { TIM2, IO_TAG(PA1), TIM_Channel_2, TIM2_IRQn,    0, IOCFG_IPD,  TIM_USE_PWM },   // S6_IN - PPM IN
+    { TIM2, IO_TAG(PA1), TIM_Channel_2, 0, IOCFG_IPD,  TIM_USE_PWM },   // S6_IN - PPM IN
 #else
-    { TIM2, IO_TAG(PA1), TIM_Channel_2, TIM2_IRQn,    0, IOCFG_IPD,  TIM_USE_PPM | TIM_USE_PWM },   // S6_IN - PPM IN
+    { TIM2, IO_TAG(PA1), TIM_Channel_2, 0, IOCFG_IPD,  TIM_USE_PPM | TIM_USE_PWM },   // S6_IN - PPM IN
 #endif
 
-    { TIM4, IO_TAG(PB9), TIM_Channel_4, TIM4_IRQn,    1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_MOTOR}, // S1_OUT
-    { TIM4, IO_TAG(PB8), TIM_Channel_3, TIM4_IRQn,    1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_MOTOR}, // S1_OUT
-    { TIM4, IO_TAG(PB7), TIM_Channel_2, TIM4_IRQn,    1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_SERVO}, // S3_OUT
-    { TIM1, IO_TAG(PA8), TIM_Channel_1, TIM1_CC_IRQn, 1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_SERVO}, // S3_OUT
-    { TIM3, IO_TAG(PB4), TIM_Channel_1, TIM3_IRQn,    1, IOCFG_AF_PP, TIM_USE_MC_MOTOR | TIM_USE_MC_SERVO | TIM_USE_FW_SERVO}, // S5_OUT
-    { TIM2, IO_TAG(PA2), TIM_Channel_3, TIM2_IRQn,    1, IOCFG_AF_PP, TIM_USE_MC_MOTOR | TIM_USE_MC_SERVO | TIM_USE_FW_SERVO}, // S6_OUT
+    { TIM4, IO_TAG(PB9), TIM_Channel_4, 1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_MOTOR}, // S1_OUT
+    { TIM4, IO_TAG(PB8), TIM_Channel_3, 1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_MOTOR}, // S1_OUT
+    { TIM4, IO_TAG(PB7), TIM_Channel_2, 1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_SERVO}, // S3_OUT
+    { TIM1, IO_TAG(PA8), TIM_Channel_1, 1, IOCFG_AF_PP, TIM_USE_MC_MOTOR |                    TIM_USE_FW_SERVO}, // S3_OUT
+    { TIM3, IO_TAG(PB4), TIM_Channel_1, 1, IOCFG_AF_PP, TIM_USE_MC_MOTOR | TIM_USE_MC_SERVO | TIM_USE_FW_SERVO}, // S5_OUT
+    { TIM2, IO_TAG(PA2), TIM_Channel_3, 1, IOCFG_AF_PP, TIM_USE_MC_MOTOR | TIM_USE_MC_SERVO | TIM_USE_FW_SERVO}, // S6_OUT
 };
