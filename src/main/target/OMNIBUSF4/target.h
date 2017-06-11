5f9f23a134fab69ad505da72748dd57041e8f86a
Cheng Lin <bnn1044@gmail.com>
2017-06-10 22:30:55 -0400

    fix LED strip no DMA timer by swap to PPM and Timer def in target.h

--- src/main/target/OMNIBUSF4/target.h
+++ src/main/target/OMNIBUSF4/target.h
@@ -216,7 +216,7 @@
 
 #ifdef CL_RACINGF4
 #define USABLE_TIMER_CHANNEL_COUNT 7
-#define USED_TIMERS  (  TIM_N(3) | TIM_N(4) | TIM_N(4) | TIM_N(8) )
+#define USED_TIMERS  (  TIM_N(2) | TIM_N(3) | TIM_N(4)  )
 #else
 #ifdef OMNIBUSF4SD
 #define USABLE_TIMER_CHANNEL_COUNT 13
