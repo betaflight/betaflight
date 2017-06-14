c39c51f09969637da19e446d19b8b9f835bc6a0f
Steffen Windoffer <steffen@wind0r.de>
2017-06-14 13:19:42 +0200

    fix iterm acclerator gain scale

--- src/main/fc/fc_rc.c
+++ src/main/fc/fc_rc.c
@@ -166,7 +166,7 @@ static void scaleRcCommandToFpvCamAngle(void) {
     const int16_t rcCommandSpeed = rcCommand[THROTTLE] - rcCommandThrottlePrevious[index];
 
     if(ABS(rcCommandSpeed) > throttleVelocityThreshold)
-        pidSetItermAccelerator(0.0001f * currentPidProfile->itermAcceleratorGain);
+        pidSetItermAccelerator(0.001f * currentPidProfile->itermAcceleratorGain);
     else
         pidSetItermAccelerator(1.0f);
 }
