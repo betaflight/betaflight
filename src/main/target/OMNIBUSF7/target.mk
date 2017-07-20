a9471ac3e806341a119691cf8775f6534d4b7293
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2017-07-20 21:05:09 +0200

    Added MAG and I2C BARO support if needed

--- src/main/target/OMNIBUSF7/target.mk
+++ src/main/target/OMNIBUSF7/target.mk
@@ -6,8 +6,14 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_spi_bmp280.c \
+            drivers/barometer/barometer_ms56xx.c \
+            drivers/compass/compass_ak8963.c \
+            drivers/compass/compass_ak8975.c \
+            drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_mag3110.c \
             drivers/light_ws2811strip.c \
             drivers/light_ws2811strip_hal.c \
             drivers/max7456.c
