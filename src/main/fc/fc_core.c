ceeda0c9b355a1dbc6a78fc25fb786a0b91bccc4
Adrian Miriuta <adrian.miriuta@gmail.com>
2017-07-22 21:03:41 +0200

    cleanup

--- src/main/fc/fc_core.c
+++ src/main/fc/fc_core.c
@@ -469,10 +469,10 @@ void processRx(timeUs_t currentTimeUs)
         }
         if (IS_RC_MODE_ACTIVE(BOXHEADADJ)) {
            // rebase earth to body
-           q0 = 1.0; 
-           q1 = 0.0; 
+           q0 = 1.0;
+           q1 = 0.0;
            q2 = 0.0;
-           q3 = 0.0; 
+           q3 = 0.0;
         }
     }
 #endif
