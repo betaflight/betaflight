d3e251d4806d6d0432d2a95a491bb44cc6825791
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2017-08-06 00:20:02 +1000

    Bump up MSP API_VERSION_MINOR

--- src/main/msp/msp_protocol.h
+++ src/main/msp/msp_protocol.h
@@ -60,7 +60,7 @@
 #define MSP_PROTOCOL_VERSION                0
 
 #define API_VERSION_MAJOR                   1 // increment when major changes are made
-#define API_VERSION_MINOR                   26 // increment when any change is made, reset to zero when major changes are released after changing API_VERSION_MAJOR
+#define API_VERSION_MINOR                   27 // increment when any change is made, reset to zero when major changes are released after changing API_VERSION_MAJOR
 
 #define API_VERSION_LENGTH                  2
 
