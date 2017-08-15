96d28a3f135e845a9f5465f58a33940760e90d43
mikeller <github@ike.ch>
2017-08-16 01:17:21 +1200

    Renamed 'FRSKYD' to 'FRSKY_D' for consistency with other SPI RX providers.

--- src/main/rx/rx_spi.c
+++ src/main/rx/rx_spi.c
@@ -109,8 +109,8 @@ STATIC_UNIT_TESTED bool rxSpiSetProtocol(rx_spi_protocol_e protocol)
         protocolSetRcDataFromPayload = inavNrf24SetRcDataFromPayload;
         break;
 #endif
-#ifdef USE_RX_FRSKYD
-    case RX_SPI_FRSKYD:
+#ifdef USE_RX_FRSKY_D
+    case RX_SPI_FRSKY_D:
         protocolInit = frskyD_Rx_Init;
         protocolDataReceived = frskyD_Rx_DataReceived;
         protocolSetRcDataFromPayload = frskyD_Rx_SetRCdata;
