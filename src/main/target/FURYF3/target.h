54f2c041c18f4f38bddbbcbbb30e14960cb90ca3
jflyper <shinoda@jaist.ac.jp>
2017-08-21 15:09:45 +0900

    MAX7456 to run at 10MHz

--- src/main/target/FURYF3/target.h
+++ src/main/target/FURYF3/target.h
@@ -84,7 +84,7 @@
     #define USE_MAX7456
     #define MAX7456_SPI_INSTANCE    SPI1
     #define MAX7456_SPI_CS_PIN      PC13
-    #define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD*2)
+    #define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD) // 10MHz
     #define MAX7456_RESTORE_CLK     (SPI_CLOCK_FAST)
 
     #define USE_FLASHFS
