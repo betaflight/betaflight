816a42c7336a50971565b06494024b77f0ede462
Michael Keller <github@ike.ch>
2017-08-25 20:12:13 +1200

    Remove unused MW_VERSION define.

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -26,8 +26,6 @@
 #define STR(x) STR_HELPER(x)
 #define FC_VERSION_STRING STR(FC_VERSION_MAJOR) "." STR(FC_VERSION_MINOR) "." STR(FC_VERSION_PATCH_LEVEL)
 
-#define MW_VERSION              231
-
 extern const char* const targetName;
 
 #define GIT_SHORT_REVISION_LENGTH   7 // lower case hexadecimal digits.
