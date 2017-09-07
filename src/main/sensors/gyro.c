0d678d99d5fba57fe30f778a95d8c45beb234a0b
Martin Budden <mjbudden@gmail.com>
2017-09-07 06:42:19 +0100

    Fix setting USE_GYRO_INVERSION_FILTER

--- src/main/sensors/gyro.c
+++ src/main/sensors/gyro.c
@@ -69,7 +69,7 @@
 #include "hardware_revision.h"
 #endif
 
-#if !defined(USE_GYRO_SPI_MPU6000) && (FLASH_SIZE > 128)
+#if ((FLASH_SIZE > 128) && (defined(USE_GYRO_SPI_MPU6500) ||  defined(USE_GYRO_SPI_MPU9250) || defined(USE_GYRO_SPI_ICM20601) || defined(USE_GYRO_SPI_ICM20689)
 #define USE_GYRO_INVERSION_FILTER
 #endif
 
