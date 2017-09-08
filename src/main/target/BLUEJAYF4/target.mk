74068bf4688951c9d3192496a7c340a1b16de9cd
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2017-09-09 00:17:57 +1000

    Fix BlueJayF4 gyro not detected issue

--- src/main/target/BLUEJAYF4/target.mk
+++ src/main/target/BLUEJAYF4/target.mk
@@ -3,7 +3,7 @@ FEATURES        += SDCARD VCP ONBOARDFLASH
 
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu6500.c \
-            drivers/accgyro/accgyro_spi_mpu9250.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_ms56xx.c \
