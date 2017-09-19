fd29a8eff08a268372a7ce9a9d29ca57004d92ef
dangeljs <dangel.jason@gmail.com>
2017-09-18 20:24:25 -0400

    Reinstall the PPM pin on Timer2 CH2.  Issue #2177

--- src/main/target/CC3D/target.c
+++ src/main/target/CC3D/target.c
@@ -31,7 +31,7 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM3, CH3, PB0, TIM_USE_PWM,               0),   // S3_IN - SoftSerial RX / Sonar echo / RSSI ADC
     DEF_TIM(TIM3, CH4, PB1, TIM_USE_PWM,               0),   // S4_IN - Current
     DEF_TIM(TIM2, CH1, PA0, TIM_USE_PWM,               0),   // S5_IN - Vbattery
-    DEF_TIM(TIM2, CH2, PA1, TIM_USE_PWM,			   0),   // S6_IN - PPM IN
+    DEF_TIM(TIM2, CH2, PA1, TIM_USE_PWM| TIM_USE_PPM,  0),   // S6_IN - PPM IN
     DEF_TIM(TIM3, CH1, PB4, TIM_USE_PWM,               1), // S5_OUT - GPIO_PartialRemap_TIM3 - LED Strip
     DEF_TIM(TIM4, CH4, PB9, TIM_USE_MOTOR,             1), // S1_OUT
     DEF_TIM(TIM4, CH3, PB8, TIM_USE_MOTOR,             1), // S2_OUT
