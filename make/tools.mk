8abed72792d9189aa2e5e604591cbbbe16cbdfe8
I-Fly-Anything <austinwessels@gmail.com>
2017-10-05 14:16:44 -0400

    Update tools.mk

--- make/tools.mk
+++ make/tools.mk
@@ -293,7 +293,7 @@ else ifeq (,$(findstring _install,$(MAKECMDGOALS)))
   ifeq ($(GCC_VERSION),)
     $(error **ERROR** arm-none-eabi-gcc not in the PATH. Run 'make arm_sdk_install' to install automatically in the tools folder of this repo)
   else ifneq ($(GCC_VERSION), $(GCC_REQUIRED_VERSION))
-    $(error **ERROR** your arm-none-eabi-gcc is '$(GCC_VERSION)', but '$(GCC_REQUIRED_VERSION)' is expected. Override with 'GCC_REQUIRED_VERSION' in make/local.mk or run 'make arm_sdk_install' to install the right version automatically in the tools folder of this repo)
+#    $(error **ERROR** your arm-none-eabi-gcc is '$(GCC_VERSION)', but '$(GCC_REQUIRED_VERSION)' is expected. Override with 'GCC_REQUIRED_VERSION' in make/local.mk or run 'make arm_sdk_install' to install the right version automatically in the tools folder of this repo)
   endif
 
   # ARM tookchain is in the path, and the version is what's required.
