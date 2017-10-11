86fa88d0d8a6f3607983bc1422229028c412772b
Michael Keller <github@ike.ch>
2017-10-12 00:09:57 +1300

    Merge pull request #4255 from rotcehdnih/xracerf4
    XRACERF4 Target

--- src/main/target/OMNIBUSF4/target.h
+++ src/main/target/OMNIBUSF4/target.h
@@ -23,6 +23,8 @@
 #define TARGET_BOARD_IDENTIFIER "LUX4"
 #elif defined(DYSF4PRO)
 #define TARGET_BOARD_IDENTIFIER "DYS4"
+#elif defined(XRACERF4)
+#define TARGET_BOARD_IDENTIFIER "XRF4"
 #else
 #define TARGET_BOARD_IDENTIFIER "OBF4"
 #define OMNIBUSF4BASE // For config.c
@@ -32,6 +34,8 @@
 #define USBD_PRODUCT_STRING "LuxF4osd"
 #elif defined(DYSF4PRO)
 #define USBD_PRODUCT_STRING "DysF4Pro"
+#elif defined(XRACERF4)
+#define USBD_PRODUCT_STRING "XRACERF4"
 #else
 #define USBD_PRODUCT_STRING "OmnibusF4"
 #endif
@@ -71,6 +75,9 @@
 #if defined(OMNIBUSF4SD)
 #define GYRO_MPU6000_ALIGN       CW270_DEG
 #define ACC_MPU6000_ALIGN        CW270_DEG
+#elif defined(XRACERF4)
+#define GYRO_MPU6000_ALIGN       CW90_DEG
+#define ACC_MPU6000_ALIGN        CW90_DEG 
 #else
 #define GYRO_MPU6000_ALIGN       CW180_DEG
 #define ACC_MPU6000_ALIGN        CW180_DEG
