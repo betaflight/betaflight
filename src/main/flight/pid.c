3d0b2ec8f031423ea44daf63d8c72d380b21ed63
Vidalcris <vidalcrisssss16@gmail.com>
2017-10-19 21:10:12 +0200

    Update pid.c

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -461,15 +461,19 @@ void pidController(const pidProfile_t *pidProfile, const rollAndPitchTrims_t *an
             // and ITerm windup during crash recovery can be extreme, especially on yaw axis
             axisPID_I[axis] = 0.0f;
             if (cmpTimeUs(currentTimeUs, crashDetectedAtUs) > crashTimeLimitUs || !ARMING_FLAG(ARMED)
-		|| (ABS(attitude.raw[FD_ROLL] - angleTrim->raw[FD_ROLL]) < crashRecoveryAngleDeciDegrees
-                       && ABS(attitude.raw[FD_PITCH] - angleTrim->raw[FD_PITCH]) < crashRecoveryAngleDeciDegrees)
                 || (motorMixRange < 1.0f
                        && ABS(gyro.gyroADCf[FD_ROLL]) < crashRecoveryRate
                        && ABS(gyro.gyroADCf[FD_PITCH]) < crashRecoveryRate
                        && ABS(gyro.gyroADCf[FD_YAW]) < crashRecoveryRate)) {
+                    // check aircraft nearly level
+                    if (ABS(attitude.raw[FD_ROLL] - angleTrim->raw[FD_ROLL]) < crashRecoveryAngleDeciDegrees
+                       && ABS(attitude.raw[FD_PITCH] - angleTrim->raw[FD_PITCH]) < crashRecoveryAngleDeciDegrees) {
+                        inCrashRecoveryMode = false;
+                        BEEP_OFF;
+                    }
+                } else {
                     inCrashRecoveryMode = false;
                     BEEP_OFF;
-                }
             }
         }
 
