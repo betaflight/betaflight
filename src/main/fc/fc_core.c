a39b18a573dc99e46d86e5579df7d336ffc89282
Steffen Windoffer <steffen@wind0r.de>
2017-10-19 23:31:45 +0200

    deactivate crash recovery on disarm

--- src/main/fc/fc_core.c
+++ src/main/fc/fc_core.c
@@ -235,6 +235,8 @@ void updateArmingStatus(void)
     }
 }
 
+extern bool inCrashRecoveryMode;
+
 void disarm(void)
 {
     if (ARMING_FLAG(ARMED)) {
@@ -245,6 +247,9 @@ void disarm(void)
             blackboxFinish();
         }
 #endif
+
+        inCrashRecoveryMode = false;
+
         BEEP_OFF;
         beeper(BEEPER_DISARMING);      // emit disarm tone
     }
