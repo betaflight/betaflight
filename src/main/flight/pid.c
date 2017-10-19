23593c01fa4dc45970dc659f3628a0ef422e253e
Vidalcris <vidalcrisssss16@gmail.com>
2017-10-19 23:01:00 +0200

    Update pid.c

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -517,21 +517,22 @@ void pidController(const pidProfile_t *pidProfile, const rollAndPitchTrims_t *an
             // if crash recovery is on and accelerometer enabled then check for a crash
             if (pidProfile->crash_recovery) {
                 if (ARMING_FLAG(ARMED)) {
-                if (motorMixRange >= 1.0f && inCrashRecoveryMode == false
-                        && ABS(delta) > crashDtermThreshold
-                        && ABS(errorRate) > crashGyroThreshold
-                        && ABS(getSetpointRate(axis)) < crashSetpointThreshold) {
-                    inCrashRecoveryMode = true;
-                    crashDetectedAtUs = currentTimeUs;
-                }
-                if (cmpTimeUs(currentTimeUs, crashDetectedAtUs) < crashTimeDelayUs && (ABS(errorRate) < crashGyroThreshold
-                    || ABS(getSetpointRate(axis)) > crashSetpointThreshold)) {
+                    if (motorMixRange >= 1.0f && inCrashRecoveryMode == false
+                       && ABS(delta) > crashDtermThreshold
+                       && ABS(errorRate) > crashGyroThreshold
+                       && ABS(getSetpointRate(axis)) < crashSetpointThreshold) {
+                        inCrashRecoveryMode = true;
+                        crashDetectedAtUs = currentTimeUs;
+                    }
+                    if (cmpTimeUs(currentTimeUs, crashDetectedAtUs) < crashTimeDelayUs && (ABS(errorRate) < crashGyroThreshold
+                       || ABS(getSetpointRate(axis)) > crashSetpointThreshold)) {
+                        inCrashRecoveryMode = false;
+                    }
+                } else {
                     inCrashRecoveryMode = false;
+                    BEEP_OFF;
                 }
-            } else {
-	    	inCrashRecoveryMode = false;
-	    }
-	}
+            }
             axisPID_D[axis] = Kd[axis] * delta * tpaFactor;
         }
 
