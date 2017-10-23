85efcf4960f45248acaf8dc047f2d825334b3992
I-Fly-Anything <austinwessels@gmail.com>
2017-10-23 15:10:55 -0400

    Update mixer.c

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -123,8 +123,8 @@ float pidSumLimitYaw;
 
 static const motorMixer_t mixerQuadX[] = {
     { 1.0f, -1.0f,  1.0f, -1.0f },          // REAR_R
-    { -1.0f, 1.0f, 1.0f,  -1.0f },          // FRONT_R
-    { -1.0f,  -1.0f,  -1.0f,  -1.0f },          // REAR_L
+    { 1.0f, -1.0f, -1.0f,  1.0f },          // FRONT_R
+    { 1.0f,  1.0f,  1.0f,  1.0f },          // REAR_L
     { 1.0f,  1.0f, -1.0f, -1.0f },          // FRONT_L
 };
 #ifndef USE_QUAD_MIXER_ONLY
