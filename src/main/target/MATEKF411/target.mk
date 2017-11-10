db0134bf8676edeaa74e56fd95929fd2fbd3170f
Michael Keller <github@ike.ch>
2017-11-11 12:34:58 +1300

    Merge pull request #4534 from mikeller/added_matekf411_target
    Added target definition for MATEKF411.

---
+++ src/main/target/MATEKF411/target.mk
@@ -0,0 +1,11 @@
+F411_TARGETS    += $(TARGET)
+FEATURES        += SDCARD VCP
+
+TARGET_SRC = \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/barometer/barometer_bmp085.c \
+            drivers/barometer/barometer_bmp280.c \
+            drivers/barometer/barometer_ms5611.c \
+            drivers/max7456.c
