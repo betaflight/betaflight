3454a7bfcdb3b169652a0e114a8d3dec40384799
Martin Budden <mjbudden@gmail.com>
2017-11-10 07:37:07 +0000

    Merge pull request #4527 from mikeller/update_matekf722
    Updates to the MATEKF722 target.

--- src/main/target/MATEKF722/target.mk
+++ src/main/target/MATEKF722/target.mk
@@ -2,5 +2,10 @@ F7X2RE_TARGETS += $(TARGET)
 FEATURES       += SDCARD VCP
 
 TARGET_SRC = \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_icm20689.c \
+            drivers/barometer/barometer_bmp085.c \
+            drivers/barometer/barometer_bmp280.c \
+            drivers/barometer/barometer_ms5611.c \
             drivers/max7456.c
