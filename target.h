bc4972bc0a65632da0d3a35b1fbcdcd0e50486fc
FatsharkFC <33479028+FatsharkFC@users.noreply.github.com>
2017-11-13 08:35:24 +0800

    Change default settings for FS101

---
+++ target.h
@@ -0,0 +1,138 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TARGET_BOARD_IDENTIFIER "FSFC"
+#define USE_TARGET_CONFIG
+
+#define BRUSHED_MOTORS
+
+
+#undef USE_UNCOMMON_MIXERS // no space left
+
+#undef USE_TELEMETRY_JETIEXBUS // no space left
+#undef USE_SERIALRX_JETIEXBUS // no space left 
+#undef USE_DASHBOARD // no space left
+#undef USE_RTC_TIME // no space left
+
+#define CONFIG_FASTLOOP_PREFERRED_ACC ACC_DEFAULT
+
+#define USE_BRUSHED_ESC_AUTODETECT
+
+#define LED0_PIN                PB8
+#define LED2                    PA5	//Used to switch camera to black and white mode as battery low voltage indicator
+
+#define BEEPER                  PC15
+#define BEEPER_INVERTED
+
+#define USE_EXTI
+#define MPU_INT_EXTI            PC13
+#define USE_MPU_DATA_READY_SIGNAL
+#define ENSURE_MPU_DATA_READY_IS_LOW
+
+#define USE_MAG_DATA_READY_SIGNAL
+#define ENSURE_MAG_DATA_READY_IS_HIGH
+
+#define USE_ESC_SENSOR
+
+#define USE_GYRO
+#define USE_GYRO_SPI_MPU6500
+
+#define USE_ACC
+#define USE_ACC_SPI_MPU6500
+
+#define ACC_MPU6500_ALIGN       CW180_DEG
+#define GYRO_MPU6500_ALIGN      CW180_DEG
+
+
+//#define USE_SONAR
+
+#define USE_VCP
+#define USE_UART1
+#define USE_UART2
+#define USE_UART3
+
+
+#define SOFTSERIAL1_RX_PIN      PA6 // PWM 5
+#define SOFTSERIAL1_TX_PIN      PA7 // PWM 6
+
+#define SOFTSERIAL2_RX_PIN      PB0 // PWM 7
+#define SOFTSERIAL2_TX_PIN      PB1 // PWM 8
+
+#define SERIAL_PORT_COUNT       4
+
+#define USE_ESCSERIAL
+#define ESCSERIAL_TIMER_TX_PIN  PA15  // (HARDARE=0,PPM)
+
+#define UART1_TX_PIN            PA9
+#define UART1_RX_PIN            PA10
+
+#define UART2_TX_PIN            PA14 // PA14 / SWCLK
+#define UART2_RX_PIN            PA15
+
+#define UART3_TX_PIN            PB10 // PB10 (AF7)
+#define UART3_RX_PIN            PB11 // PB11 (AF7)
+
+#define USE_I2C
+#define USE_I2C_DEVICE_1
+#define I2C_DEVICE              (I2CDEV_1)
+
+#define USE_SPI
+#define USE_SPI_DEVICE_1 // PB9,3,4,5 on AF5 SPI1 (MPU)
+#define USE_SPI_DEVICE_2 // PB12,13,14,15 on AF5 SPI2 (SDCard)
+
+#define SPI1_NSS_PIN            PB9
+#define SPI1_SCK_PIN            PB3
+#define SPI1_MISO_PIN           PB4
+#define SPI1_MOSI_PIN           PB5
+
+#define SPI2_NSS_PIN            PB12
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
+
+
+#define MPU6500_CS_PIN                   PB9
+#define MPU6500_SPI_INSTANCE             SPI1
+
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+
+#define USE_ADC
+#define ADC_INSTANCE            ADC2
+#define RSSI_ADC_PIN            PB2
+
+#define VBAT_ADC_PIN            PA4
+#define CURRENT_METER_ADC_PIN   PA5
+
+#define LED_STRIP
+
+#define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
+#define DEFAULT_FEATURES        (FEATURE_LED_STRIP)
+
+#define USE_SERIAL_4WAY_BLHELI_INTERFACE
+
+// IO - stm32f303cc in 48pin package
+#define TARGET_IO_PORTA         0xffff
+#define TARGET_IO_PORTB         0xffff
+#define TARGET_IO_PORTC         (BIT(13)|BIT(14)|BIT(15))
+#define TARGET_IO_PORTF         (BIT(0)|BIT(1)|BIT(4))
+
+#define USABLE_TIMER_CHANNEL_COUNT 12 // PPM, 8 PWM, UART3 RX/TX, LED Strip
+
+#define USED_TIMERS             (TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(8) | TIM_N(15) | TIM_N(16))
+
