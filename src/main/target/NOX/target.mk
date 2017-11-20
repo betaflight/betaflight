5d12a026886c92accb284191a3cbf5894627316d
jflyper <shinoda@jaist.ac.jp>
2017-11-20 16:10:04 +0900

    Added mpu6000 defs

--- src/main/target/NOX/target.mk
+++ src/main/target/NOX/target.mk
@@ -3,5 +3,6 @@ FEATURES       += VCP
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/max7456.c
