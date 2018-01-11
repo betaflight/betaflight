19bb4b70c8ef75f72c406a7176442232239c2a10
Michael Keller <github@ike.ch>
2018-01-12 01:29:33 +1300

    Merge pull request #4941 from mikeller/fix_beebrain_v2_target
    Fixed BEEBRAIN_V2F target definition.

--- src/main/target/BEEBRAIN_V2F/config.c
+++ src/main/target/BEEBRAIN_V2F/config.c
@@ -138,6 +138,11 @@ void targetConfiguration(void)
     osdConfigMutable()->item_pos[OSD_ESC_TMP]            &= ~VISIBLE_FLAG;
     osdConfigMutable()->item_pos[OSD_ESC_RPM]            &= ~VISIBLE_FLAG;
 
+    modeActivationConditionsMutable(0)->modeId           = BOXANGLE;
+    modeActivationConditionsMutable(0)->auxChannelIndex  = AUX2 - NON_AUX_CHANNEL_COUNT;
+    modeActivationConditionsMutable(0)->range.startStep  = CHANNEL_VALUE_TO_STEP(900);
+    modeActivationConditionsMutable(0)->range.endStep    = CHANNEL_VALUE_TO_STEP(2100);
+
 #if defined(BEEBRAIN_V2D)
     // DSM version
     for (uint8_t rxRangeIndex = 0; rxRangeIndex < NON_AUX_CHANNEL_COUNT; rxRangeIndex++) {
