94df2648d1bace3d6e47b6695e6a6b1da86a2464
blckmn <j@blck.mn>
2018-01-25 09:11:11 +1100

    Corrected target source

--- src/main/target/STM32F7X2/target.mk
+++ src/main/target/STM32F7X2/target.mk
@@ -1,4 +1,4 @@
 F7X2RE_TARGETS += $(TARGET)
 FEATURES       += SDCARD VCP
 
-TARGET_SRC = $(subst ./src/main/,,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c)))
+TARGET_SRC = $(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c)))
