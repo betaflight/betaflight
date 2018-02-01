b187a9e4083cc713d2d8079425cb409eeb85ce00
Steve Evans <steve@SteveMacPro.scevans.com>
2018-02-01 01:02:15 +0000

    Failsfafe must be detected rather than just reporting dropped frames

--- src/main/rx/sbus_channels.c
+++ src/main/rx/sbus_channels.c
@@ -68,15 +68,15 @@ uint8_t sbusChannelsDecode(rxRuntimeConfig_t *rxRuntimeConfig, const sbusChannel
         sbusChannelData[17] = SBUS_DIGITAL_CHANNEL_MIN;
     }
 
-    if (channels->flags & SBUS_FLAG_SIGNAL_LOSS) {
-    	// The received data is a repeat of the last valid data so can be considered complete.
-    	return RX_FRAME_COMPLETE | RX_FRAME_DROPPED;
-    }
-
     if (channels->flags & SBUS_FLAG_FAILSAFE_ACTIVE) {
         // internal failsafe enabled and rx failsafe flag set
         // RX *should* still be sending valid channel data, so use it.
         return RX_FRAME_COMPLETE | RX_FRAME_FAILSAFE;
+    }
+
+    if (channels->flags & SBUS_FLAG_SIGNAL_LOSS) {
+    	// The received data is a repeat of the last valid data so can be considered complete.
+    	return RX_FRAME_COMPLETE | RX_FRAME_DROPPED;
     }
 
     return RX_FRAME_COMPLETE;
