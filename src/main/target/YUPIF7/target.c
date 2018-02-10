114b3010007d0366c38ff35921056dec9c98c611
Faduf <francois.dufourniaud@gadz.org>
2018-02-10 20:28:44 +0100

    Update YupiF7 target

--- src/main/target/YUPIF7/target.c
+++ src/main/target/YUPIF7/target.c
@@ -30,8 +30,8 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM5,  CH2, PA1,  TIM_USE_MOTOR,               0, 0 ), // S2_OUT - DMA1_ST4
     DEF_TIM(TIM2,  CH3, PA2,  TIM_USE_MOTOR,               0, 0 ), // S3_OUT - DMA1_ST1
     DEF_TIM(TIM2,  CH4, PA3,  TIM_USE_MOTOR,               0, 1 ), // S4_OUT - DMA1_ST6
-    DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_MOTOR | TIM_USE_LED, 0, 0 ), // S5_OUT - DMA1_ST7 - LED Control
-    DEF_TIM(TIM4,  CH2, PB7,  TIM_USE_ANY,                 0, 0 ), // S6_OUT - DMA1_ST3 - Camera Control
-    DEF_TIM(TIM3,  CH4, PB1,  TIM_USE_MOTOR,               0, 0 ), // S7_OUT
+    DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_MOTOR,               0, 0 ), // S5_OUT - DMA1_ST7 - LED Control
+    DEF_TIM(TIM3,  CH4, PB1,  TIM_USE_MOTOR | TIM_USE_LED, 0, 0 ), // S6_OUT
+    DEF_TIM(TIM4,  CH2, PB7,  TIM_USE_ANY,                 0, 0 ), // Camera Control
     DEF_TIM(TIM12, CH1, PB14, TIM_USE_BEEPER,              0, 0 ), // BEEPER PWM
 };
