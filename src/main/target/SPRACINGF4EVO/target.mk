488dcedd2af46d73d425e46fd14e097505dc9140
Dominic Clifton <dominic.clifton@cleanflight.com>
2018-02-22 20:02:41 +0100

    CF/BF - Add SPRacingF4EVODG target.
    This target adds a second gyro to the board using the SPI pads on the back of the board.

--- src/main/target/SPRACINGF4EVO/target.mk
+++ src/main/target/SPRACINGF4EVO/target.mk
@@ -10,3 +10,9 @@ TARGET_SRC = \
             drivers/compass/compass_hmc5883l.c \
             drivers/max7456.c \
             drivers/vtx_rtc6705.c
+
+ifeq ($(TARGET), SPRACINGF4EVODG)
+TARGET_SRC += \
+            drivers/accgyro/accgyro_spi_icm20689.c
+endif
+
