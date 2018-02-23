502f7a76cb9d8cea36fe66d9d17a69b178f0db32
Lucas Moreira <lucas@moreiradevelopment.io>
2018-02-23 08:20:44 -0500

    Restructured common_fc_pre to fit FKF on F3 targets

--- src/main/target/common_fc_pre.h
+++ src/main/target/common_fc_pre.h
@@ -109,6 +109,7 @@
 #define USE_SERIALRX_SBUS       // Frsky and Futaba receivers
 #define USE_SERIALRX_SPEKTRUM   // SRXL, DSM2 and DSMX protocol
 #define USE_SERIALRX_SUMD       // Graupner Hott protocol
+//#define USE_SERIALRX_SUMH       // Graupner legacy protocol
 #define USE_SERIALRX_XBUS       // JR
 
 #if (FLASH_SIZE > 64)
@@ -119,8 +120,10 @@
 
 #if (FLASH_SIZE > 64)
 #define USE_BLACKBOX
+//#define USE_LED_STRIP
 #define USE_RESOURCE_MGMT
 #define USE_RUNAWAY_TAKEOFF     // Runaway Takeoff Prevention (anti-taz)
+//#define USE_SERVOS
 #define USE_TELEMETRY
 #define USE_TELEMETRY_FRSKY_HUB
 #define USE_TELEMETRY_HOTT
@@ -131,6 +134,7 @@
 
 #if (FLASH_SIZE > 128)
 #define USE_SERIALRX_SUMH       // Graupner legacy protocol
+#define USE_SERVOS
 #define USE_LED_STRIP
 #define USE_CAMERA_CONTROL
 #define USE_CMS
@@ -184,5 +188,5 @@
 #define USE_TELEMETRY_JETIEXBUS
 #define USE_TELEMETRY_MAVLINK
 #define USE_UNCOMMON_MIXERS
-#define USE_GYRO_FAST_KALMAN
+//#define USE_GYRO_FAST_KALMAN
 #endif
