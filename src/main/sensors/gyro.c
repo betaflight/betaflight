debb5964f47a557ddcb14b7d668ff449bf1396c8
Adrian Miriuta <a.miriuta@ledvance.com>
2018-02-26 09:26:26 +0100

    DieHertz requested changes

--- src/main/sensors/gyro.c
+++ src/main/sensors/gyro.c
@@ -629,7 +629,7 @@ static bool isOnFinalGyroCalibrationCycle(const gyroCalibration_t *gyroCalibrati
 
 static uint16_t gyroCalculateCalibratingCycles(void)
 {
-    return (CALIBRATING_GYRO_TIME / gyro.targetLooptime);
+    return (CALIBRATING_GYRO_TIME_US / gyro.targetLooptime);
 }
 
 static bool isOnFirstGyroCalibrationCycle(const gyroCalibration_t *gyroCalibration)
@@ -669,7 +669,7 @@ STATIC_UNIT_TESTED void performGyroCalibration(gyroSensor_t *gyroSensor, uint8_t
             gyroSensor->gyroDev.gyroZero[axis] = 0;
         }
 
-        // Sum up CALIBRATING_GYRO_TIME readings
+        // Sum up CALIBRATING_GYRO_TIME_US readings
         gyroSensor->calibration.sum[axis] += gyroSensor->gyroDev.gyroADCRaw[axis];
         devPush(&gyroSensor->calibration.var[axis], gyroSensor->gyroDev.gyroADCRaw[axis]);
 
