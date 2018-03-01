53d0c834442fd107775031c5d4923a0c2ed81b21
brucesdad13@gmail.com <brucesdad13@gmail.com>
2018-02-28 20:27:04 -0500

    Don't close blackbox log on disarm if mode is ALWAYS_ON

--- src/main/blackbox/blackbox.h
+++ src/main/blackbox/blackbox.h
@@ -33,6 +33,12 @@ typedef enum BlackboxDevice {
     BLACKBOX_DEVICE_SERIAL = 3
 } BlackboxDevice_e;
 
+typedef enum BlackboxMode {
+    BLACKBOX_MODE_NORMAL = 0,
+    BLACKBOX_MODE_MOTOR_TEST,
+    BLACKBOX_MODE_ALWAYS_ON
+} BlackboxMode;
+
 typedef enum FlightLogEvent {
     FLIGHT_LOG_EVENT_SYNC_BEEP = 0,
     FLIGHT_LOG_EVENT_INFLIGHT_ADJUSTMENT = 13,
