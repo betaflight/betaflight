dd2b10db3a3708616dd9f6baf07ddfc2b9366f2f
Michael Keller <github@ike.ch>
2018-03-04 11:49:15 +1300

    Enabled Dshot burst by default for DALRCF405. (#5362)

--- src/main/target/DALRCF405/target.h
+++ src/main/target/DALRCF405/target.h
@@ -20,9 +20,6 @@
 #define USBD_PRODUCT_STRING  "DALRCF405"
 //----------------------------------------
 
-//#define USE_TARGET_CONFIG
-#define USE_DSHOT_DMAR
-
 //LED & BEE------------------------------- 
 #define LED0_PIN                PC14
 
@@ -139,6 +136,8 @@
  
 #define DEFAULT_VOLTAGE_METER_SOURCE    VOLTAGE_METER_ADC
 #define DEFAULT_CURRENT_METER_SOURCE    CURRENT_METER_ADC
+
+#define ENABLE_DSHOT_DMAR       true
 
 #define USE_ESCSERIAL
 #define ESCSERIAL_TIMER_TX_PIN PA3
