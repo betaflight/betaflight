11076d9151a695a99ee34db2caf78ebe468d4652
SteveCEvans <Steve@SCEvans.com>
2018-03-20 20:36:03 +0000

    Restore debug code

--- src/main/rx/sbus.c
+++ src/main/rx/sbus.c
@@ -141,6 +141,19 @@ static uint8_t sbusFrameStatus(rxRuntimeConfig_t *rxRuntimeConfig)
     }
     sbusFrameData->done = false;
 
+    DEBUG_SET(DEBUG_SBUS, DEBUG_SBUS_FRAME_FLAGS, sbusFrameData->frame.frame.channels.flags);
+
+    if (sbusFrameData->frame.frame.channels.flags & SBUS_FLAG_SIGNAL_LOSS) {
+        sbusFrameData->stateFlags |= SBUS_STATE_SIGNALLOSS;
+        DEBUG_SET(DEBUG_SBUS, DEBUG_SBUS_STATE_FLAGS, sbusFrameData->stateFlags);
+    }
+    if (sbusFrameData->frame.frame.channels.flags & SBUS_FLAG_FAILSAFE_ACTIVE) {
+        sbusFrameData->stateFlags |= SBUS_STATE_FAILSAFE;
+        DEBUG_SET(DEBUG_SBUS, DEBUG_SBUS_STATE_FLAGS, sbusFrameData->stateFlags);
+    }
+
+    DEBUG_SET(DEBUG_SBUS, DEBUG_SBUS_STATE_FLAGS, sbusFrameData->stateFlags);
+
     return sbusChannelsDecode(rxRuntimeConfig, &sbusFrameData->frame.frame.channels);
 }
 
