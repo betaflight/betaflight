7b8dad9ec08fb9567120efa8a1265bebf3095115
Andrey Mironov <hertz@yandex-team.ru>
2018-03-20 14:50:12 +0300

    Fixed indentantion shifts

--- src/main/cms/cms_menu_imu.c
+++ src/main/cms/cms_menu_imu.c
@@ -366,10 +366,10 @@ static uint16_t cmsx_yaw_lowpass_hz;
 static long cmsx_FilterPerProfileRead(void)
 {
     const pidProfile_t *pidProfile = pidProfiles(pidProfileIndex);
-    cmsx_dterm_lowpass_hz =       pidProfile->dterm_lowpass_hz;
+    cmsx_dterm_lowpass_hz =   pidProfile->dterm_lowpass_hz;
     cmsx_dterm_notch_hz =     pidProfile->dterm_notch_hz;
     cmsx_dterm_notch_cutoff = pidProfile->dterm_notch_cutoff;
-    cmsx_yaw_lowpass_hz =         pidProfile->yaw_lowpass_hz;
+    cmsx_yaw_lowpass_hz =     pidProfile->yaw_lowpass_hz;
 
     return 0;
 }
@@ -379,10 +379,10 @@ static long cmsx_FilterPerProfileWriteback(const OSD_Entry *self)
     UNUSED(self);
 
     pidProfile_t *pidProfile = currentPidProfile;
-    pidProfile->dterm_lowpass_hz =       cmsx_dterm_lowpass_hz;
+    pidProfile->dterm_lowpass_hz =   cmsx_dterm_lowpass_hz;
     pidProfile->dterm_notch_hz =     cmsx_dterm_notch_hz;
     pidProfile->dterm_notch_cutoff = cmsx_dterm_notch_cutoff;
-    pidProfile->yaw_lowpass_hz =         cmsx_yaw_lowpass_hz;
+    pidProfile->yaw_lowpass_hz =     cmsx_yaw_lowpass_hz;
 
     return 0;
 }
@@ -391,10 +391,10 @@ static OSD_Entry cmsx_menuFilterPerProfileEntries[] =
 {
     { "-- FILTER PP  --", OME_Label, NULL, NULL, 0 },
 
-    { "DTERM LPF",  OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_dterm_lowpass_hz,         0, 500, 1 }, 0 },
+    { "DTERM LPF",  OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_dterm_lowpass_hz,     0, 500, 1 }, 0 },
     { "DTERM NF",   OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_dterm_notch_hz,       0, 500, 1 }, 0 },
     { "DTERM NFCO", OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_dterm_notch_cutoff,   0, 500, 1 }, 0 },
-    { "YAW LPF",    OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_yaw_lowpass_hz,           0, 500, 1 }, 0 },
+    { "YAW LPF",    OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_yaw_lowpass_hz,       0, 500, 1 }, 0 },
 
     { "BACK", OME_Back, NULL, NULL, 0 },
     { NULL, OME_END, NULL, NULL, 0 }
