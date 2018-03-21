7c7d5ea8e62ae5ad7b62419072caaf0c165d5471
Chris <krzyshockuba@gmail.com>
2018-03-21 03:53:12 +0100

    Fix headers

--- src/main/drivers/usb_msc_f4xx.c
+++ src/main/drivers/usb_msc_f4xx.c
@@ -1,8 +1,21 @@
 /*
- * usb_msc_f4xx.c
+ * This file is part of Cleanflight.
  *
- *  Created on: Feb 15, 2018
- *      Author: khockuba
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Chris Hockuba (https://github.com/conkerkh)
+ *
  */
 
 #include <stdint.h>
