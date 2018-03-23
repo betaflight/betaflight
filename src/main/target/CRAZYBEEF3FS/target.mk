8974b8c6455ef5faa5f8fa3d3bf8d67bbb55ca03
githubDLG <oneshot.triplekill@qq.com>
2018-03-23 14:42:30 +0800

    merge crazybeef3fs to crazybeef3fr

--- src/main/target/CRAZYBEEF3FS/target.mk
+++
@@ -1,8 +0,0 @@
-F3_TARGETS  += $(TARGET)
-FEATURES  = VCP 
-TARGET_SRC = \
-            drivers/accgyro/accgyro_mpu.c \
-            drivers/accgyro/accgyro_spi_mpu6000.c \
-			drivers/rx/rx_a7105.c \
-			rx/flysky.c \
-            drivers/max7456.c
