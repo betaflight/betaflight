759c636069023b1ce5c1e6f2040974fcd5eb4d1d
githubDLG <oneshot.triplekill@qq.com>
2018-03-23 15:11:59 +0800

    add new target CrazyBee F3 FS (#5495)
    * add new target CrazyBee F3 FS
    
    add new target CrazyBee F3 FS
    
    * merge crazybeef3fs to crazybeef3fr

--- src/main/target/CRAZYBEEF3FR/target.h
+++ src/main/target/CRAZYBEEF3FR/target.h
@@ -17,8 +17,14 @@
 
 #pragma once
 
-#define TARGET_BOARD_IDENTIFIER "CBFR" //
+#if defined(CRAZYBEEF3FS)
+#define TARGET_BOARD_IDENTIFIER "CBFS"
+#define USBD_PRODUCT_STRING     "CrazyBee F3 FS"
+#else //CRAZYBEEF3FS
+#define TARGET_BOARD_IDENTIFIER "CBFR"
 #define USBD_PRODUCT_STRING     "CrazyBee F3 FR"
+#endif
+
 #define LED0_PIN                PB3
 #define BEEPER                  PC15
 #define BEEPER_INVERTED
@@ -54,6 +60,18 @@
 #define SPI2_MISO_PIN           PB14
 #define SPI2_MOSI_PIN           PB15
 
+#if defined(CRAZYBEEF3FS)
+#define USE_RX_SPI
+#define USE_RX_FLYSKY
+#define RX_SPI_DEFAULT_PROTOCOL RX_SPI_A7105_FLYSKY_2A
+#define FLYSKY_2A_CHANNEL_COUNT 14
+#define RX_SPI_INSTANCE         SPI2
+#define RX_NSS_PIN              SPI2_NSS_PIN
+#define RX_IRQ_PIN              PA8
+#define BINDPLUG_PIN            PA9
+#define USE_RX_FLYSKY_SPI_LED
+#define RX_FLYSKY_SPI_LED_PIN   PA10
+#else
 #define USE_RX_SPI
 #define USE_RX_FRSKY_SPI_D
 #define USE_RX_FRSKY_SPI_X
@@ -68,6 +86,7 @@
 #define RX_FRSKY_SPI_GDO_0_PIN  PA8
 #define RX_FRSKY_SPI_LED_PIN    PA10
 #define BINDPLUG_PIN            PA9
+#endif
 
 #define USE_OSD
 #define USE_MAX7456
@@ -79,15 +98,16 @@
 #define USE_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
-#define VBAT_ADC_PIN             PA0
-#define CURRENT_METER_ADC_PIN    PA1
-#define ADC_INSTANCE             ADC1
+#define VBAT_ADC_PIN            PA0
+#define CURRENT_METER_ADC_PIN   PA1
+#define ADC_INSTANCE            ADC1
+#define CURRENT_METER_SCALE_DEFAULT 2350
 
-#define DEFAULT_FEATURES        (FEATURE_TELEMETRY | FEATURE_OSD)
+#define DEFAULT_FEATURES        (FEATURE_TELEMETRY | FEATURE_OSD | FEATURE_RX_SPI | FEATURE_MOTOR_STOP)
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
-#define TARGET_IO_PORTA          0xffff
-#define TARGET_IO_PORTB          0xffff
-#define TARGET_IO_PORTC          (BIT(13)|BIT(14)|BIT(15))
-#define TARGET_IO_PORTF          (BIT(0)|BIT(1)|BIT(4))
+#define TARGET_IO_PORTA         0xffff
+#define TARGET_IO_PORTB         0xffff
+#define TARGET_IO_PORTC         (BIT(13)|BIT(14)|BIT(15))
+#define TARGET_IO_PORTF         (BIT(0)|BIT(1)|BIT(4))
 #define USABLE_TIMER_CHANNEL_COUNT 4
-#define USED_TIMERS              (TIM_N(2) | TIM_N(8) | TIM_N(15))
+#define USED_TIMERS             (TIM_N(2) | TIM_N(8) | TIM_N(15))
