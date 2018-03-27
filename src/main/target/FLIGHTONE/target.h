da24bf495fb2170aea26106dfc0c09d0728ab9ae
Preston Garrison III <pg3@raceflight.com>
2018-03-26 19:22:03 -0700

    timer fixes

--- src/main/target/FLIGHTONE/target.h
+++ src/main/target/FLIGHTONE/target.h
@@ -133,4 +133,4 @@
 #define TARGET_IO_PORTD         (BIT(2))
 
 #define USABLE_TIMER_CHANNEL_COUNT 11
-#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(8) | TIM_N(12) )
+#define USED_TIMERS             ( TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(11) )
