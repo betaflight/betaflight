41de00907ddb4a2e302b19fb01e13a92029926fc
Tim Sweet <timothyswt@gmail.com>
2018-03-28 13:17:48 -0700

    skipping every other cycle when running faster than 16k

--- src/main/fc/fc_rc.c
+++ src/main/fc/fc_rc.c
@@ -57,6 +57,7 @@ static float rcStepSize[4] = { 0, 0, 0, 0 };
 static float inverseRcInt;
 static uint8_t interpolationChannels;
 volatile bool isRXDataNew;
+volatile bool skipNextInterpolate;
 volatile int16_t rcInterpolationStepCount;
 volatile uint16_t rxRefreshRate;
 volatile uint16_t currentRxRefreshRate;
@@ -189,6 +190,12 @@ static void checkForThrottleErrorResetState(void)
 
 void processRcCommand(void)
 {
+    if (skipNextInterpolate && !isRXDataNew) {
+        skipNextInterpolate = false;
+        return;
+    }
+    skipNextInterpolate = targetPidLooptime < 62;
+
     int updatedChannel = 0;
     if (isRXDataNew && isAntiGravityModeActive()) {
         checkForThrottleErrorResetState();
