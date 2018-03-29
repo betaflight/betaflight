79ce7f6ef8812db78b3ec6b80f59e750b2feeb01
Lucas Moreira <lucas@moreiradevelopment.io>
2018-03-29 09:12:07 -0400

    Increment version for release

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -21,14 +21,14 @@
 
 #define FC_FIRMWARE_NAME            "ButterFlight"
 #define FC_VERSION_MAJOR            3  // increment when a major release is made (big new feature, etc)
-#define FC_VERSION_MINOR            4  // increment when a minor release is made (small new feature, change etc)
-#define FC_VERSION_PATCH_LEVEL      2  // increment when a bug is fixed
+#define FC_VERSION_MINOR            5  // increment when a minor release is made (small new feature, change etc)
+#define FC_VERSION_PATCH_LEVEL      0  // increment when a bug is fixed
 
 #if defined(USE_GYRO_IMUF9001)
     #define IMUF_VERSION_MAJOR       1
     #define IMUF_VERSION_MINOR       0
     #define IMUF_VERSION_PATCH_LEVEL 4
-    #define FC_VERSION_STRING STR(FC_VERSION_MAJOR) "." STR(FC_VERSION_MINOR) "." STR(FC_VERSION_PATCH_LEVEL) " | IMUF: " STR(IMUF_VERSION_MAJOR) "." STR(IMUF_VERSION_MINOR) "." STR(IMUF_VERSION_PATCH_LEVEL) 
+    #define FC_VERSION_STRING STR(FC_VERSION_MAJOR) "." STR(FC_VERSION_MINOR) "." STR(FC_VERSION_PATCH_LEVEL) " | IMUF: " STR(IMUF_VERSION_MAJOR) "." STR(IMUF_VERSION_MINOR) "." STR(IMUF_VERSION_PATCH_LEVEL)
 #else
     #define FC_VERSION_STRING STR(FC_VERSION_MAJOR) "." STR(FC_VERSION_MINOR) "." STR(FC_VERSION_PATCH_LEVEL)
 #endif //USE_GYRO_IMUF9001
