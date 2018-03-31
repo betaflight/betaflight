0038bd41d52f965a1419c1545dc481870b83abe9
Michael Keller <github@ike.ch>
2018-04-01 11:00:03 +1200

    Fixed DMA conflicts for CRAZYBEEF3 targets. (#5590)

--- src/main/target/CRAZYBEEF3FR/target.h
+++ src/main/target/CRAZYBEEF3FR/target.h
@@ -25,6 +25,8 @@
 #define USBD_PRODUCT_STRING     "CrazyBee F3 FR"
 #endif
 
+#define ENABLE_DSHOT_DMAR       true
+
 #define LED0_PIN                PB3
 #define BEEPER                  PC15
 #define BEEPER_INVERTED
