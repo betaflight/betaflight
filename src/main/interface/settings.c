jflyper/cleanflight

Add open drain mode

src/main/interface/settings.c
@@ -784,6 +784,7 @@ const clivalue_t valueTable[] = {
 #ifdef USE_LED_STRIP
     { "ledstrip_visual_beeper",     VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_OFF_ON }, PG_LED_STRIP_CONFIG, offsetof(ledStripConfig_t, ledstrip_visual_beeper) },
     { "ledstrip_grb_rgb",           VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_RGB_GRB }, PG_LED_STRIP_CONFIG, offsetof(ledStripConfig_t, ledstrip_grb_rgb) },
+    { "ledstrip_od_enable",         VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_OFF_ON }, PG_LED_STRIP_CONFIG, offsetof(ledStripConfig_t, odEnable) },
 #endif
 
 // PG_SDCARD_CONFIG

src/main/io/ledstrip.c
@@ -178,6 +178,7 @@ void pgResetFn_ledStripConfig(ledStripConfig_t *ledStripConfig)
         }
     }
     ledStripConfig->ioTag = IO_TAG_NONE;
+    ledStripConfig->odEnable = 0;
 }
 
 static int scaledThrottle;
@@ -1203,7 +1204,7 @@ void ledStripEnable(void)
     reevaluateLedConfig();
     ledStripInitialised = true;
 
-    ws2811LedStripInit(ledStripConfig()->ioTag);
+    ws2811LedStripInit(ledStripConfig()->ioTag, ledStripConfig()->odEnable);
 }
 
 static void ledStripDisable(void)

src/main/io/ledstrip.h
@@ -153,6 +153,7 @@ typedef struct ledStripConfig_s {
     uint8_t ledstrip_aux_channel;
     ioTag_t ioTag;
     ledStripFormatRGB_e ledstrip_grb_rgb;
+    uint8_t odEnable;
 } ledStripConfig_t;
 
 PG_DECLARE(ledStripConfig_t, ledStripConfig);

betaflight/betaflight

src/main/interface/settings.c
@@ -784,6 +784,7 @@ const clivalue_t valueTable[] = {
 #ifdef USE_LED_STRIP
     { "ledstrip_visual_beeper",     VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_OFF_ON }, PG_LED_STRIP_CONFIG, offsetof(ledStripConfig_t, ledstrip_visual_beeper) },
     { "ledstrip_grb_rgb",           VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_RGB_GRB }, PG_LED_STRIP_CONFIG, offsetof(ledStripConfig_t, ledstrip_grb_rgb) },
+    { "ledstrip_od",                VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_OFF_ON }, PG_LED_STRIP_CONFIG, offsetof(ledStripConfig_t, isOpenDrain) },
 #endif
 
 // PG_SDCARD_CONFIG

src/main/io/ledstrip.c
@@ -178,6 +178,7 @@ void pgResetFn_ledStripConfig(ledStripConfig_t *ledStripConfig)
         }
     }
     ledStripConfig->ioTag = IO_TAG_NONE;
+    ledStripConfig->isOpenDrain = 0;
 }
 
 static int scaledThrottle;
@@ -1203,7 +1204,7 @@ void ledStripEnable(void)
     reevaluateLedConfig();
     ledStripInitialised = true;
 
-    ws2811LedStripInit(ledStripConfig()->ioTag);
+    ws2811LedStripInit(ledStripConfig()->ioTag, ledStripConfig()->isOpenDrain);
 }
 
 static void ledStripDisable(void)

src/main/io/ledstrip.h
@@ -153,6 +153,7 @@ typedef struct ledStripConfig_s {
     uint8_t ledstrip_aux_channel;
     ioTag_t ioTag;
     ledStripFormatRGB_e ledstrip_grb_rgb;
+    uint8_t isOpenDrain;
 } ledStripConfig_t;
 
 PG_DECLARE(ledStripConfig_t, ledStripConfig);