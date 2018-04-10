834a186da78c2e22faeccb11831de59b7187a355
chibaron <chibaron@suneo.jp>
2018-04-10 21:57:04 +0900

    add target BeeStorm

---
+++ src/main/target/BEESTORM/target.mk
@@ -0,0 +1,8 @@
+F3_TARGETS  += $(TARGET)
+FEATURES    = VCP
+
+TARGET_SRC = \
+            drivers/accgyro/accgyro_mpu.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/max7456.c
