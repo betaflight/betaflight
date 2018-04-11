a8579d62a86772b418ccc23f4fbc1e2834fa108f
chibaron <chibaron@suneo.jp>
2018-04-11 23:37:04 +0900

    Revert "add target BeeStorm"
    This reverts commit 834a186da78c2e22faeccb11831de59b7187a355.

--- src/main/target/BEESTORM/target.mk
+++
@@ -1,8 +0,0 @@
-F3_TARGETS  += $(TARGET)
-FEATURES    = VCP
-
-TARGET_SRC = \
-            drivers/accgyro/accgyro_mpu.c \
-            drivers/accgyro/accgyro_mpu6500.c \
-            drivers/accgyro/accgyro_spi_mpu6500.c \
-            drivers/max7456.c
