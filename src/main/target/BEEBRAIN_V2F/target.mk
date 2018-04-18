4a5e79a5346caca50c1871bc5fded614cb296066
chibaron <chibaron@suneo.jp>
2018-04-18 22:39:25 +0900

    add target BeeStorm (#5662)
    * add target BeeStorm
    
    * Revert "add target BeeStorm"
    
    This reverts commit 834a186da78c2e22faeccb11831de59b7187a355.
    
    * derived model of BEEBRAIN_V2F

--- src/main/target/BEEBRAIN_V2F/target.mk
+++ src/main/target/BEEBRAIN_V2F/target.mk
@@ -5,5 +5,9 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
-            drivers/vtx_rtc6705_soft_spi.c \
             drivers/max7456.c
+
+ifeq ($(TARGET), BEESTORM)
+else
+TARGET_SRC += drivers/vtx_rtc6705_soft_spi.c
+endif
