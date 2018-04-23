cd5256bc0aa54a541afdea71e930d85cab882417
jflyper <shinoda@jaist.ac.jp>
2018-04-23 23:10:26 +0900

    Workaround pattern for empty function array.

--- src/main/target/OMNIBUSF7/config.c
+++ src/main/target/OMNIBUSF7/config.c
@@ -32,6 +32,8 @@ static targetSerialPortFunction_t targetSerialPortFunction[] = {
 #if defined(OMNIBUSF7V2) && defined(ESC_SENSOR_UART)
     // OMNIBUS F7 V2 has an option to connect UART7_RX to ESC telemetry
     { ESC_SENSOR_UART, FUNCTION_ESC_SENSOR },
+#else
+    { SERIAL_PORT_NONE, FUNCTION_NONE },
 #endif
 };
 
