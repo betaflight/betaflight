3364c094e1286179d7c3869cf0594307854d0f66
jflyper <shinoda@jaist.ac.jp>
2018-05-04 18:05:09 +0900

    Move hard coded defaults out of pgResetFn

--- src/main/pg/max7456.h
+++ src/main/pg/max7456.h
@@ -23,14 +23,15 @@
 #include "drivers/io_types.h"
 #include "pg/pg.h"
 
-#define MAX7456_CLOCK_CONFIG_HALF 0
-#define MAX7456_CLOCK_CONFIG_OC   1
-#define MAX7456_CLOCK_CONFIG_FULL 2
-
 typedef struct max7456Config_s {
-    uint8_t clockConfig; // 0 = force half clock, 1 = half if OC, 2 = force full
+    uint8_t clockConfig; // SPI clock based on device type and overclock state (MAX7456_CLOCK_CONFIG_xxxx)
     ioTag_t csTag;
     uint8_t spiDevice;
 } max7456Config_t;
+
+// clockConfig values
+#define MAX7456_CLOCK_CONFIG_HALF 0  // Force half clock
+#define MAX7456_CLOCK_CONFIG_OC   1  // Half clock if OC
+#define MAX7456_CLOCK_CONFIG_FULL 2  // Force full clock
 
 PG_DECLARE(max7456Config_t, max7456Config);
