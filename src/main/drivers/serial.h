d7665014afa9619c36a5eca897283d3eea21e42c
Michael Keller <github@ike.ch>
2018-05-10 10:54:14 +1200

    Moved pointers back.

--- src/main/drivers/serial.h
+++ src/main/drivers/serial.h
@@ -68,6 +68,8 @@ typedef struct serialPort_s {
 
     uint32_t rxBufferSize;
     uint32_t txBufferSize;
+    volatile uint8_t *rxBuffer;
+    volatile uint8_t *txBuffer;
     uint32_t rxBufferHead;
     uint32_t rxBufferTail;
     uint32_t txBufferHead;
@@ -77,9 +79,6 @@ typedef struct serialPort_s {
     void *rxCallbackData;
 
     uint8_t identifier;
-
-    volatile uint8_t *rxBuffer;
-    volatile uint8_t *txBuffer;
 } serialPort_t;
 
 #if defined(USE_SOFTSERIAL1) || defined(USE_SOFTSERIAL2)
