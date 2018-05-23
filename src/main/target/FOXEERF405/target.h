1baacd30058125b96a02f356cdafdab9b86bed52
nyway <niway@vip.qq.com>
2018-05-23 16:38:12 +0800

    Update target.h
    Delete TIM4  in  USED_TIMERS list

--- src/main/target/FOXEERF405/target.h
+++ src/main/target/FOXEERF405/target.h
@@ -158,7 +158,7 @@
 
 
 #define USABLE_TIMER_CHANNEL_COUNT 9
-#define USED_TIMERS             (TIM_N(1)|TIM_N(2)|TIM_N(3)|TIM_N(4)|TIM_N(8)|TIM_N(12))
+#define USED_TIMERS             (TIM_N(1)|TIM_N(2)|TIM_N(3)|TIM_N(8)|TIM_N(12))
 
 
 
