9492899aebb0934371ab56d9a465c0aa5e6bc5f9
mikeller <github@ike.ch>
2018-06-17 13:42:30 +1200

    Made it fit on SIRINFPV.

--- src/main/target/SIRINFPV/target.h
+++ src/main/target/SIRINFPV/target.h
@@ -22,6 +22,14 @@
 
 #define TARGET_BOARD_IDENTIFIER "SIRF"
 
+// Removed to make the firmware fit into flash (in descending order of priority):
+
+#undef USE_EXTENDED_CMS_MENUS
+#undef USE_RTC_TIME
+#undef USE_RX_MSP
+#undef USE_ESC_SENSOR_INFO
+
+
 #define LED0_PIN                PB2
 #define USE_BEEPER
 #define BEEPER_PIN              PA1
