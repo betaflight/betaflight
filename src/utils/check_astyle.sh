e93094a7018a7f7ca4b7fb3f19b01c51d72652bc
mikeller <github@ike.ch>
2018-06-19 00:24:26 +1200

    Improvements from review.

--- src/utils/check_astyle.sh
+++ src/utils/check_astyle.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+git diff-files --quiet 2>/dev/null
+UNSTAGED=$?
+
+git diff-index --cached --quiet HEAD 2>/dev/null
+UNCOMMITTED=$?
+
+if [ ${UNSTAGED} -gt 0 -o ${UNCOMMITTED} -gt 0 ]; then
+    echo "Unstaged / uncommitted changes, aborting!"
+
+    exit 2
+fi
+
 ./src/utils/astyle.sh
 
 git diff
 
 git diff --quiet HEAD 2>/dev/null
+DIFF=$?
 
-if [ $? -gt 0 ]; then
+if [ ${DIFF} -gt 0 ]; then
      echo "Style error! Run astyle: ./src/utils/astyle.sh"
 
      git reset --hard 2>/dev/null 1>&2
