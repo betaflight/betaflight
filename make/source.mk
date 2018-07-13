09ee3fb0cb58cfae0e3fd18469a29b2a418f9b60
MJ666 <michael.p.jakob@web.de>
2018-07-13 22:06:51 +0200

    Suppport for W25N01GVIG and W25N02GVIG NAND SPI flash chips

--- make/source.mk
+++ make/source.mk
@@ -354,6 +354,7 @@ SRC += \
             drivers/flash.c \
             drivers/flash_m25p16.c \
             drivers/flash_w25m.c \
+            drivers/flash_w25n.c \
             io/flashfs.c \
             pg/flash.c \
             $(MSC_SRC)
