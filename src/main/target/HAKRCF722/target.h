3d6ab87a84e2110a14e34e708d2453eac6a89f40
loopur <x82780567@qq.com>
2018-08-25 19:29:23 +0800

    Delete this gyro of icm20689

--- src/main/target/HAKRCF722/target.h
+++ src/main/target/HAKRCF722/target.h
@@ -50,9 +50,6 @@
 #define MPU6000_CS_PIN          PA4
 #define MPU6000_SPI_INSTANCE    SPI1
 
-#define ICM20689_CS_PIN         PA4
-#define ICM20689_SPI_INSTANCE   SPI1
-
 #define USE_EXTI
 #define MPU_INT_EXTI            PC4
 #define USE_MPU_DATA_READY_SIGNAL
@@ -61,15 +58,10 @@
 #define USE_GYRO_SPI_MPU6000
 #define GYRO_MPU6000_ALIGN      CW270_DEG
 
-#define USE_GYRO_SPI_ICM20689
-#define GYRO_ICM20689_ALIGN     CW90_DEG
-
 #define USE_ACC
 #define USE_ACC_SPI_MPU6000
 #define ACC_MPU6000_ALIGN       CW270_DEG
 
-#define USE_ACC_SPI_ICM20689
-#define ACC_ICM20689_ALIGN      CW90_DEG
 
 #define LED0_PIN                PC14
 
--- src/main/target/HAKRCF722/target.mk
+++ src/main/target/HAKRCF722/target.mk
@@ -2,7 +2,6 @@ F7X2RE_TARGETS += $(TARGET)
 FEATURES       += VCP ONBOARDFLASH
 TARGET_SRC = \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-            drivers/accgyro/accgyro_spi_icm20689.c \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_ms5611.c \
