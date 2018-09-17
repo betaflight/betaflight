9e838e1451b41ce4cca748bdffe13ab43e0a4e2f
Amir Guindehi <amir@datacore.ch>
2018-09-17 20:45:42 +0200

    Corrected USED_TIMERS in the UAVPNG030MINI target.

--- src/main/target/UAVPNG030MINI/target.h
+++ src/main/target/UAVPNG030MINI/target.h
@@ -146,4 +146,8 @@
 #define TARGET_IO_PORTE (0xffff & ~(BIT(15)))
 
 #define USABLE_TIMER_CHANNEL_COUNT 10
-#define USED_TIMERS ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(12) | TIM_N(8) | TIM_N(9))
+
+// Used Timers
+//#define USED_TIMERS ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(9) | TIM_N(12))
+// Defined Timers in timer.c
+#define USED_TIMERS ( TIM_N(1) | TIM_N(3) | TIM_N(4))
