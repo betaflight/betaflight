13463e63df7feb32e2c371761c7de86c771f1c3c
Blaine <b.forbort@gmail.com>
2018-09-19 21:56:50 -0700

    Cleanup

--- src/main/target/ELLE0/target.mk
+++ src/main/target/ELLE0/target.mk
@@ -6,4 +6,4 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
-            drivers/compass/compass_ak8963.c \
+            drivers/compass/compass_ak8963.c
