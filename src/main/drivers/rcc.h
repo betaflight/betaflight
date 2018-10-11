1b62bedbaba48226bac61e11f6f9a0241fcb2a03
jflyper <shinoda@jaist.ac.jp>
2018-10-11 15:44:43 +0900

    [H7] RCC and IO

--- src/main/drivers/rcc.h
+++ src/main/drivers/rcc.h
@@ -22,12 +22,29 @@
 
 #include "rcc_types.h"
 
+// XXX rcc module is not actively used by HAL based platforms (F7 and H7),
+// XXX as peripherals are turned on statically in enableGPIOPowerUsageAndNoiseReductions()
+// XXX during initialization.
+
 enum rcc_reg {
     RCC_EMPTY = 0,   // make sure that default value (0) does not enable anything
+#ifdef STM32H7
+    RCC_AHB,
+    RCC_APB2,
+    RCC_APB1L,
+    RCC_APB1H,
+    RCC_AHB1,
+    RCC_AHB2,
+    RCC_AHB3,
+    RCC_APB3,
+    RCC_AHB4,
+    RCC_APB4,
+#else
     RCC_AHB,
     RCC_APB2,
     RCC_APB1,
-    RCC_AHB1
+    RCC_AHB1,
+#endif
 };
 
 #define RCC_ENCODE(reg, mask) (((reg) << 5) | LOG2_32BIT(mask))
@@ -35,6 +52,17 @@ enum rcc_reg {
 #define RCC_APB2(periph) RCC_ENCODE(RCC_APB2, RCC_APB2ENR_ ## periph ## EN)
 #define RCC_APB1(periph) RCC_ENCODE(RCC_APB1, RCC_APB1ENR_ ## periph ## EN)
 #define RCC_AHB1(periph) RCC_ENCODE(RCC_AHB1, RCC_AHB1ENR_ ## periph ## EN)
+
+#ifdef STM32H7
+#define RCC_AHB2(periph) RCC_ENCODE(RCC_AHB2, RCC_AHB2ENR_ ## periph ## EN)
+#define RCC_AHB3(periph) RCC_ENCODE(RCC_AHB3, RCC_AHB3ENR_ ## periph ## EN)
+#define RCC_APB3(periph) RCC_ENCODE(RCC_APB3, RCC_APB3ENR_ ## periph ## EN)
+#define RCC_AHB4(periph) RCC_ENCODE(RCC_AHB4, RCC_AHB4ENR_ ## periph ## EN)
+#define RCC_APB4(periph) RCC_ENCODE(RCC_APB4, RCC_APB4ENR_ ## periph ## EN)
+#undef  RCC_APB1
+#define RCC_APB1L(periph) RCC_ENCODE(RCC_APB1L, RCC_APB1LENR_ ## periph ## EN)
+#define RCC_APB1H(periph) RCC_ENCODE(RCC_APB1H, RCC_APB1HENR_ ## periph ## EN)
+#endif
 
 void RCC_ClockCmd(rccPeriphTag_t periphTag, FunctionalState NewState);
 void RCC_ResetCmd(rccPeriphTag_t periphTag, FunctionalState NewState);
--- src/main/drivers/rcc_types.h
+++ src/main/drivers/rcc_types.h
@@ -20,4 +20,8 @@
 
 #pragma once
 
+#if defined(STM32H7)
+typedef uint16_t rccPeriphTag_t;
+#else
 typedef uint8_t rccPeriphTag_t;
+#endif
