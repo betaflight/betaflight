31db76b856d0d7e55795f6ffabec5b1c48c60515
Asizon <43983086+Asizon@users.noreply.github.com>
2018-11-25 09:39:40 +0100

    Add target OMNIBUSF4V6
    I add target of Airbot Omnibus F4 V6, it is tested and functional at 100%
    
    Update target OmnibusF4V6 to Fireworks variant
    
    Update target.h
    
    Update target.c
    
    Update target.c
    
    Fix motor output resources for Typhoon32 V2
    
    Revert motor changes
    
    Enable SCL and SDA pins
    
    I have enabled the resources I2C2_SCL and I2C2_SDA, in Omnibus F4 V6 they are independent, they do not share with UART3TX / UART3RX
    
    Requested changes
    
    I have reviewed the requested changes, default gyro first and swapping of gyro1 and 2
    
    Removing two linefeed
    
    Add I2C Definitions and Timers
    
    Added definitions and timers for I2C1 device(Baro, Mag)
    
    Update target.c target.h
    
    Some requested changes
    
    Remove I2C Timers
    
    Waiting to find compatible timers for SCL and SDA(I2C)
    
    Update USABLE_TIMER_CHANNEL_COUNT

--- src/main/target/OMNIBUSF4FW/config.c
+++ src/main/target/OMNIBUSF4FW/config.c
@@ -32,8 +32,12 @@
 #include "config_helper.h"
 
 static targetSerialPortFunction_t targetSerialPortFunction[] = {
+#ifdef OMNIBUSF4V6
+    { SERIAL_PORT_USART6, FUNCTION_RX_SERIAL },   
+#else
     { SERIAL_PORT_USART1, FUNCTION_RX_SERIAL },
     { SERIAL_PORT_UART4,  FUNCTION_ESC_SENSOR },    
+#endif
 };
 
 void targetConfiguration(void)
