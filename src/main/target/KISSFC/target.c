44504f173fba04757412f7f8b20cc395e4c578dc
Robert Lacroix <mail@robertlacroix.com>
2018-11-29 21:08:43 -0800

    KISSCC: Fix soft serial on TLM pad
    _Legal disclaimer: I am making my contributions/submissions to this project solely in my personal capacity and am not conveying any rights to any intellectual property of any third parties._

--- src/main/target/KISSFC/target.c
+++ src/main/target/KISSFC/target.c
@@ -36,7 +36,6 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
 
     DEF_TIM(TIM3,  CH1,  PA6,  TIM_USE_MOTOR,               0),
     DEF_TIM(TIM17, CH1,  PA7,  TIM_USE_MOTOR,               0),
-    DEF_TIM(TIM16, CH1N, PA13, TIM_USE_PWM,                 0),
 #else
     DEF_TIM(TIM1,  CH2N, PB14, TIM_USE_MOTOR,               TIMER_OUTPUT_INVERTED),
     DEF_TIM(TIM8,  CH2N, PB0,  TIM_USE_MOTOR,               TIMER_OUTPUT_INVERTED),
@@ -44,8 +43,8 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM1,  CH1,  PA8,  TIM_USE_MOTOR,               TIMER_OUTPUT_INVERTED),
     DEF_TIM(TIM3,  CH1,  PA6,  TIM_USE_MOTOR | TIM_USE_LED, TIMER_OUTPUT_INVERTED),
     DEF_TIM(TIM17, CH1,  PA7,  TIM_USE_MOTOR,               TIMER_OUTPUT_INVERTED),
-    DEF_TIM(TIM4,  CH3,  PA13, TIM_USE_PWM,                 0), // On KISSFC TIM16 did not work, using TIM4 works
 #endif
+    DEF_TIM(TIM4,  CH3,  PA13, TIM_USE_PWM,                 0),
     DEF_TIM(TIM2,  CH2,  PB3,  TIM_USE_PWM | TIM_USE_PPM,   0),
     DEF_TIM(TIM2,  CH1,  PA15, TIM_USE_PWM,                 0),
     DEF_TIM(TIM2,  CH3,  PA2,  TIM_USE_PWM,                 0),
