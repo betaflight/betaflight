40247b0a1b46155fec2b038a903dd0d04e101d0a
Cheng Lin <bnn1044@gmail.com>
2018-12-04 20:39:41 -0500

    add I2C support, and remove blackbox flash define
    Signed-off-by: Cheng Lin <bnn1044@gmail.com>
    
    remove blackbox support

--- src/main/target/CLRACINGF7/target.mk
+++ src/main/target/CLRACINGF7/target.mk
@@ -1,5 +1,5 @@
 F7X2RE_TARGETS  += $(TARGET)
-FEATURES        +=  VCP ONBOARDFLASH SDCARD_SPI
+FEATURES        +=  VCP ONBOARDFLASH
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6500.c \
@@ -7,4 +7,6 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/light_ws2811strip.c \
             drivers/light_ws2811strip_hal.c \
+            drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_qmc5883l.c \
             drivers/max7456.c
