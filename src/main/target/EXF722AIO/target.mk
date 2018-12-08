59a8ca262aba9d38c9d82ea07fca49a7e83f69d1
LinJieqiang <517503838@qq.com>
2018-12-08 13:20:51 +0800

    TARGET: Add support code for EXF722AIO board.

---
+++ src/main/target/EXF722AIO/target.mk
@@ -0,0 +1,10 @@
+F7X2RE_TARGETS += $(TARGET)
+FEATURES       += VCP ONBOARDFLASH
+
+TARGET_SRC = \
+        drivers/accgyro/accgyro_mpu.c \
+        drivers/accgyro/accgyro_spi_mpu6000.c \
+        drivers/accgyro/accgyro_spi_icm20689.c \
+        drivers/light_ws2811strip.c \
+        drivers/light_ws2811strip_hal.c \
+        drivers/max7456.c
