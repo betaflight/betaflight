e55ec290cffe32521bb9eec48c32204585189f95
nyway <niway@vip.qq.com>
2019-01-05 09:22:53 +0800

    Update target.h

--- src/main/target/RUSHCORE7/target.h
+++ src/main/target/RUSHCORE7/target.h
@@ -142,7 +142,7 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 10
+#define USABLE_TIMER_CHANNEL_COUNT 9
 #define USED_TIMERS  (TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(8) | TIM_N(9) )
 
 
