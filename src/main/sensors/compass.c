1a9c2891e7759f75616f3341d7199977df77638a
MJ666 <michael.p.jakob@web.de>
2019-01-07 15:50:58 +0100

    Fix indentation

--- src/main/sensors/compass.c
+++ src/main/sensors/compass.c
@@ -176,7 +176,7 @@ bool compassDetect(magDev_t *dev)
     case MAG_HMC5883:
 #if defined(USE_MAG_HMC5883) || defined(USE_MAG_SPI_HMC5883)
         if (busdev->bustype == BUSTYPE_I2C) {
-                busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
+            busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
         }
 
         if (hmc5883lDetect(dev)) {
@@ -192,7 +192,7 @@ bool compassDetect(magDev_t *dev)
     case MAG_LIS3MDL:
 #if defined(USE_MAG_LIS3MDL)
         if (busdev->bustype == BUSTYPE_I2C) {
-                busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
+            busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
         }
 
         if (lis3mdlDetect(dev)) {
@@ -208,7 +208,7 @@ bool compassDetect(magDev_t *dev)
     case MAG_AK8975:
 #ifdef USE_MAG_AK8975
         if (busdev->bustype == BUSTYPE_I2C) {
-                busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
+            busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
         }
 
         if (ak8975Detect(dev)) {
@@ -245,7 +245,7 @@ bool compassDetect(magDev_t *dev)
     case MAG_QMC5883:
 #ifdef USE_MAG_QMC5883
         if (busdev->bustype == BUSTYPE_I2C) {
-                busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
+            busdev->busdev_u.i2c.address = compassConfig()->mag_i2c_address;
         }
 
         if (qmc5883lDetect(dev)) {
