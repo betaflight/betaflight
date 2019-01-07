83abbbf8f2b79ee98aa35fc81d4dce78eb731ef7
elin-neo <neodrone@neosystems.co.kr>
2019-01-07 12:49:13 +0900

    Move target file to REVO

--- src/main/target/REVO/config.c
+++ src/main/target/REVO/config.c
@@ -18,21 +18,24 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "platform.h"
-#include "drivers/io.h"
-#include "drivers/dma.h"
-#include "drivers/timer.h"
-#include "drivers/timer_def.h"
+#include "drivers/serial.h"
+#include "pg/rx.h"
+#include "pg/piniobox.h"
+#include "rx/rx.h"
+#include "telemetry/telemetry.h"
+#include "fc/config.h"
 
-const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
-    DEF_TIM(TIM11, CH1,  PB9, TIM_USE_ANY,   0, 0),
-    DEF_TIM(TIM8,  CH2N, PB0, TIM_USE_MOTOR, 0, 0),
-    DEF_TIM(TIM8,  CH3N, PB1, TIM_USE_MOTOR, 0, 0),
-    DEF_TIM(TIM2,  CH4,  PA3, TIM_USE_MOTOR, 0, 0),
-    DEF_TIM(TIM2,  CH3,  PA2, TIM_USE_MOTOR, 0, 0),
-    DEF_TIM(TIM1,  CH1,  PA8, TIM_USE_MOTOR, 0, 0),
-    DEF_TIM(TIM3,  CH4,  PC9, TIM_USE_MOTOR, 0, 0),
-    DEF_TIM(TIM4,  CH1,  PB6, TIM_USE_LED,   0, 0),
-};
+#ifdef USE_TARGET_CONFIG
+#include "pg/pg.h"
+
+void targetConfiguration(void)
+{
+    rxConfigMutable()->halfDuplex = true;
+    pinioBoxConfigMutable()->permanentId[0] = 40;
+    pinioBoxConfigMutable()->permanentId[1] = 41;
+}
+#endif
--- src/main/target/REVO/target.mk
+++ src/main/target/REVO/target.mk
@@ -13,4 +13,5 @@ TARGET_SRC = \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/compass/compass_hmc5883l.c \
-            drivers/compass/compass_qmc5883l.c
+            drivers/compass/compass_qmc5883l.c \
+            drivers/max7456.c
