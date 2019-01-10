c94f7cb8f28464def82e58906ed436810d628666
mikeller <github@ike.ch>
2019-01-11 03:56:58 +1300

    Fixed flash overflow on RG_SSD_F3, RMDO, SPRACINGF3.

--- src/main/target/RG_SSD_F3/target.h
+++ src/main/target/RG_SSD_F3/target.h
@@ -23,9 +23,30 @@
 #define TARGET_BOARD_IDENTIFIER "RGF3" // rgSSD_F3
 
 // Removed to make the firmware fit into flash (in descending order of priority):
+//#undef USE_GYRO_OVERFLOW_CHECK
+//#undef USE_GYRO_LPF2
+
+//#undef USE_ITERM_RELAX
+//#undef USE_RC_SMOOTHING_FILTER
+
+//#undef USE_LED_STRIP
+
+//#undef USE_HUFFMAN
+//#undef USE_PINIO
+//#undef USE_PINIOBOX
+
+//#undef USE_TELEMETRY_HOTT
+//#undef USE_TELEMETRY_MAVLINK
+//#undef USE_TELEMETRY_LTM
+//#undef USE_SERIALRX_XBUS
+//#undef USE_SERIALRX_SUMH
+//#undef USE_PWM
+
+//#undef USE_BOARD_INFO
+//#undef USE_EXTENDED_CMS_MENUS
 //#undef USE_RTC_TIME
 #undef USE_RX_MSP
-//#undef USE_ESC_SENSOR_INFO
+#undef USE_ESC_SENSOR_INFO
 
 #define LED0_PIN                 PC1
 #define LED1_PIN                 PC0
