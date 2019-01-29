8250dcc8611db938c736ed50c4e551cb435080df
Tim Eckel <tim@leethost.com>
2019-01-29 09:09:40 -0500

    Added back the `OK` status, tried to keep a similar spirit of the code changes
    The `OK` status is it shows the craft is ready to arm (no errors and not waiting for GPS).  The flight modes are only when armed, showing the currently active flight mode.  This way, it's clear when armed and when not armed.  This confirmation of armed/unarmed is important to trigger events and modes.  For example, when to start a timer or monitor signal strength.

--- src/main/telemetry/crsf.c
+++ src/main/telemetry/crsf.c
@@ -266,31 +266,33 @@ void crsfFrameFlightMode(sbuf_t *dst)
     uint8_t *lengthPtr = sbufPtr(dst);
     sbufWriteU8(dst, 0);
     sbufWriteU8(dst, CRSF_FRAMETYPE_FLIGHT_MODE);
-
-    // Acro is the default mode
-    const char *flightMode = "ACRO";
-
+    const char *flightMode = "";
+    
+    if (ARMING_FLAG(ARMED)) {
+        // Armed flight modes in decreasing order of importance
+        if (FLIGHT_MODE(FAILSAFE_MODE)) {
+            flightMode = "!FS!";
+        } else if (FLIGHT_MODE(GPS_RESCUE_MODE)) {
+            flightMode = "RTH";
+        } else if (FLIGHT_MODE(PASSTHRU_MODE)) {
+            flightMode = "MANU";
+        } else if (FLIGHT_MODE(ANGLE_MODE)) {
+            flightMode = "STAB";
+        } else if (FLIGHT_MODE(HORIZON_MODE)) {
+            flightMode = "HOR";
+        } else if (airmodeIsEnabled()) {
+            flightMode = "AIR";
+        } else {
+            flightMode = "ACRO";
+        }
 #ifdef USE_GPS
-    if (!ARMING_FLAG(ARMED) && featureIsEnabled(FEATURE_GPS) && (!STATE(GPS_FIX) || !STATE(GPS_FIX_HOME))) {
+    } else if (featureIsEnabled(FEATURE_GPS) && (!STATE(GPS_FIX) || !STATE(GPS_FIX_HOME))) {
         flightMode = "WAIT"; // Waiting for GPS lock
-    } else
 #endif
-
-    // Flight modes in decreasing order of importance
-    if (FLIGHT_MODE(FAILSAFE_MODE)) {
-        flightMode = "!FS!";
     } else if (isArmingDisabled()) {
         flightMode = "!ERR";
-    } else if (FLIGHT_MODE(GPS_RESCUE_MODE)) {
-        flightMode = "RTH";
-    } else if (FLIGHT_MODE(PASSTHRU_MODE)) {
-        flightMode = "MANU";
-    } else if (FLIGHT_MODE(ANGLE_MODE)) {
-        flightMode = "STAB";
-    } else if (FLIGHT_MODE(HORIZON_MODE)) {
-        flightMode = "HOR";
-    } else if (airmodeIsEnabled()) {
-        flightMode = "AIR";
+    } else {
+        flightMode = "OK"; // Ready to arm
     }
 
     sbufWriteString(dst, flightMode);
