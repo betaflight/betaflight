abf4f3bc7f4c80d04e20d5434866d18d7c436a04
SteveCEvans <Steve@SCEvans.com>
2019-02-07 20:10:19 +0000

    FreeRTOS requires the use of all interrupt priority bits for pre-emption priority and none for subpriority

--- src/main/drivers/nvic.h
+++ src/main/drivers/nvic.h
@@ -67,13 +67,13 @@
 
 #ifdef USE_HAL_DRIVER
 // utility macros to join/split priority
-#define NVIC_PRIORITY_GROUPING NVIC_PRIORITYGROUP_2
+#define NVIC_PRIORITY_GROUPING NVIC_PRIORITYGROUP_4
 #define NVIC_BUILD_PRIORITY(base,sub) (((((base)<<(4-(7-(NVIC_PRIORITY_GROUPING))))|((sub)&(0x0f>>(7-(NVIC_PRIORITY_GROUPING)))))<<4)&0xf0)
 #define NVIC_PRIORITY_BASE(prio) (((prio)>>(4-(7-(NVIC_PRIORITY_GROUPING))))>>4)
 #define NVIC_PRIORITY_SUB(prio) (((prio)&(0x0f>>(7-(NVIC_PRIORITY_GROUPING))))>>4)
 #else
 // utility macros to join/split priority
-#define NVIC_PRIORITY_GROUPING NVIC_PriorityGroup_2
+#define NVIC_PRIORITY_GROUPING NVIC_PriorityGroup_4
 #define NVIC_BUILD_PRIORITY(base,sub) (((((base)<<(4-(7-(NVIC_PRIORITY_GROUPING>>8))))|((sub)&(0x0f>>(7-(NVIC_PRIORITY_GROUPING>>8)))))<<4)&0xf0)
 #define NVIC_PRIORITY_BASE(prio) (((prio)>>(4-(7-(NVIC_PRIORITY_GROUPING>>8))))>>4)
 #define NVIC_PRIORITY_SUB(prio) (((prio)&(0x0f>>(7-(NVIC_PRIORITY_GROUPING>>8))))>>4)
