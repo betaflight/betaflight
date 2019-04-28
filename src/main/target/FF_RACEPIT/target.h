37cd86e4fcd67748e478d00de657990a11f9a131
MJ666 <MJ666@users.noreply.github.com>
2019-04-28 06:42:51 +0200

    Merge pull request #8046 from AlienWiiBF/RACEPIT
    FuriousFPV RacePIT target updates

--- src/main/target/FF_RACEPIT/target.h
+++ src/main/target/FF_RACEPIT/target.h
@@ -30,19 +30,20 @@
 
 /*------------BEEPER---------------*/
 #define USE_BEEPER
-#define BEEPER_PIN              PB3
+#define BEEPER_PIN              PC3
 #define BEEPER_INVERTED
 /*---------------------------------*/
 
 /*---------- VTX POWER SWITCH---------*/
 #define USE_PINIO
 #define PINIO1_PIN              PC0 // VTX power switcher
+#define PINIO2_PIN              PC8 // 2xCamera switcher
 #define USE_PINIOBOX
 
 /*------------SENSORS--------------*/
 // MPU interrupt
 #define USE_EXTI
-#define GYRO_1_INT_EXTI_PIN            PC4
+#define GYRO_1_INT_EXTI_PIN     PC4
 
 #define USE_MPU_DATA_READY_SIGNAL
 #define ENSURE_MPU_DATA_READY_IS_LOW
@@ -53,24 +54,24 @@
 #define USE_GYRO_SPI_MPU6000
 #define USE_ACC_SPI_MPU6000
 
-#define GYRO_1_CS_PIN          SPI1_NSS_PIN
-#define GYRO_1_SPI_INSTANCE    SPI1
+#define GYRO_1_CS_PIN           SPI1_NSS_PIN
+#define GYRO_1_SPI_INSTANCE     SPI1
 
-#define GYRO_1_ALIGN      CW90_DEG_FLIP
+#define GYRO_1_ALIGN            CW90_DEG_FLIP
 /*---------------------------------*/
 
 
 /*-------------OSD-----------------*/
 #define USE_MAX7456
-#define MAX7456_SPI_INSTANCE    SPI2  
-#define MAX7456_SPI_CS_PIN      SPI2_NSS_PIN 
-#define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD) // 10MHz
-#define MAX7456_RESTORE_CLK     (SPI_CLOCK_FAST)
+#define MAX7456_SPI_INSTANCE     SPI2
+#define MAX7456_SPI_CS_PIN       SPI2_NSS_PIN
+#define MAX7456_SPI_CLK          (SPI_CLOCK_STANDARD) // 10MHz
+#define MAX7456_RESTORE_CLK      (SPI_CLOCK_FAST)
 /*---------------------------------*/
 
 /*------------FLASH----------------*/
-#define FLASH_CS_PIN            PA15
-#define FLASH_SPI_INSTANCE      SPI3
+#define FLASH_CS_PIN             PA15
+#define FLASH_SPI_INSTANCE       SPI3
 
 #define USE_FLASHFS
 #define USE_FLASH_M25P16
@@ -80,62 +81,62 @@
 #define USE_VCP
 
 #define USE_UART1
-#define UART1_RX_PIN            PB7	
-#define UART1_TX_PIN            PA9
+#define UART1_RX_PIN             PB7
+#define UART1_TX_PIN             PA9
 
 #define USE_UART2
-#define UART2_RX_PIN            PA3
-#define UART2_TX_PIN            PA2
+#define UART2_RX_PIN             PA3
+#define UART2_TX_PIN             PA2
 
 #define USE_UART3
-#define UART3_RX_PIN            PC11 
-#define UART3_TX_PIN            PC10 
-#define INVERTER_PIN_UART3      PC15
+#define UART3_RX_PIN             PC11
+#define UART3_TX_PIN             PC10
+#define INVERTER_PIN_UART3       PC15
 
 #define USE_UART4
-#define UART4_TX_PIN            PA0
-#define UART4_RX_PIN            PA1
+#define UART4_TX_PIN             PA0
+#define UART4_RX_PIN             PA1
 
 #define USE_UART5
-#define UART5_RX_PIN            PD2
-#define UART5_TX_PIN            PC12
+#define UART5_RX_PIN             PD2
+#define UART5_TX_PIN             PC12
 
 #define USE_UART6
-#define UART6_RX_PIN            PC7
-#define UART6_TX_PIN            PC6
+#define UART6_RX_PIN             PC7
+#define UART6_TX_PIN             PC6
 
-#define SERIAL_PORT_COUNT       7 
+#define SERIAL_PORT_COUNT        7
 /*---------------------------------*/
 
 /*-------------SPIs----------------*/
 #define USE_SPI
 
 #define USE_SPI_DEVICE_1
-#define SPI1_NSS_PIN            PA4
-#define SPI1_SCK_PIN            PA5
-#define SPI1_MISO_PIN           PA6
-#define SPI1_MOSI_PIN           PA7
+#define SPI1_NSS_PIN             PA4
+#define SPI1_SCK_PIN             PA5
+#define SPI1_MISO_PIN            PA6
+#define SPI1_MOSI_PIN            PA7
 
 #define USE_SPI_DEVICE_2
-#define SPI2_NSS_PIN            PB12
-#define SPI2_SCK_PIN            PB13
-#define SPI2_MISO_PIN           PB14
-#define SPI2_MOSI_PIN           PB15
+#define SPI2_NSS_PIN             PB12
+#define SPI2_SCK_PIN             PB13
+#define SPI2_MISO_PIN            PB14
+#define SPI2_MOSI_PIN            PB15
 
 #define USE_SPI_DEVICE_3
-#define SPI3_NSS_PIN            PA15
-#define SPI3_SCK_PIN            PB3
-#define SPI3_MISO_PIN           PB4
-#define SPI3_MOSI_PIN           PB5
+#define SPI3_NSS_PIN             PA15
+#define SPI3_SCK_PIN             PB3
+#define SPI3_MISO_PIN            PB4
+#define SPI3_MOSI_PIN            PB5
 /*---------------------------------*/
 
 /*-------------I2C-----------------*/
 #define USE_I2C
 #define USE_I2C_PULLUP
 #define USE_I2C_DEVICE_3
-#define I2C_DEVICE              (I2CDEV_3)
-#define I2C3_SCL                PA8
-#define I2C3_SDA                PC9
+#define I2C_DEVICE               (I2CDEV_3)
+#define I2C3_SCL                 PA8
+#define I2C3_SDA                 PC9
 /*---------------------------------*/
 
 /*-------------ADCs----------------*/
@@ -149,22 +150,22 @@
 /*-------------ESCs----------------*/
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
 #define USE_ESCSERIAL
-#define ESCSERIAL_TIMER_TX_PIN  PB0  // (HARDWARE=0)
+#define ESCSERIAL_TIMER_TX_PIN   PB0  // (HARDWARE=0)
 /*---------------------------------*/
 
 /*--------DEFAULT VALUES-----------*/
-#define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
-#define SERIALRX_PROVIDER       SERIALRX_SBUS
-#define SERIALRX_UART           SERIAL_PORT_USART3
-#define DEFAULT_FEATURES        ( FEATURE_LED_STRIP | FEATURE_OSD | FEATURE_MOTOR_STOP )
+#define DEFAULT_RX_FEATURE       FEATURE_RX_SERIAL
+#define SERIALRX_PROVIDER        SERIALRX_SBUS
+#define SERIALRX_UART            SERIAL_PORT_USART3
+#define DEFAULT_FEATURES         FEATURE_OSD
 
-#define TARGET_IO_PORTA         0xffff
-#define TARGET_IO_PORTB         0xffff
-#define TARGET_IO_PORTC         0xffff
-#define TARGET_IO_PORTD         (BIT(2))
+#define TARGET_IO_PORTA          0xffff
+#define TARGET_IO_PORTB          0xffff
+#define TARGET_IO_PORTC          0xffff
+#define TARGET_IO_PORTD          (BIT(2))
 /*---------------------------------*/
 
 /*--------------TIMERS-------------*/
 #define USABLE_TIMER_CHANNEL_COUNT  6
-#define USED_TIMERS             (TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) )
+#define USED_TIMERS              (TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) )
 /*---------------------------------*/
