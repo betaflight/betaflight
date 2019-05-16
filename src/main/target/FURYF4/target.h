ede066fbeee5128a840256975c8fd261e52274bf
Michael Keller <github@ike.ch>
2019-05-17 00:04:22 +1200

    Add camera control for Mamba Fury F4 Mini using PB9 (#8264)
    Add camera control for Mamba Fury F4 Mini using PB9

--- src/main/target/FURYF4/target.h
+++ src/main/target/FURYF4/target.h
@@ -154,5 +154,5 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 6
-#define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) )
+#define USABLE_TIMER_CHANNEL_COUNT 7
+#define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) | TIM_N(11) )
