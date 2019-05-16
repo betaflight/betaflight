237ad5a68e8c7d982ce1c010b5af64aa011a9e44
Asizon <43983086+Asizon@users.noreply.github.com>
2019-05-16 13:49:11 +0200

    Remove deprecated ACC_ALIGN

--- src/main/target/AIRBOTF7/target.h
+++ src/main/target/AIRBOTF7/target.h
@@ -59,8 +59,6 @@
 #define USE_ACC
 #define USE_ACC_SPI_MPU6000
 #define USE_ACC_SPI_MPU6500
-#define ACC_1_ALIGN             CW90_DEG
-#define ACC_2_ALIGN             CW0_DEG
 
 // *************** OSD **************************
 
