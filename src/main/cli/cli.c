babad40dcbec5ec35b58966f012ebe3997d4fcb4
mikeller <github@ike.ch>
2019-05-19 12:30:37 +1200

    Really fixed flash erasing.

--- src/main/cli/cli.c
+++ src/main/cli/cli.c
@@ -2239,7 +2239,6 @@ static void cliFlashErase(char *cmdline)
 
     bufWriterFlush(cliWriter);
     flashfsEraseCompletely();
-    flashfsInit();
 
     while (!flashfsIsReady()) {
 #ifndef MINIMAL_CLI
