2e93b138a07d905b8275b2e34ae161a18251e6d1
jflyper <shinoda@jaist.ac.jp>
2019-05-26 10:30:51 +0900

    Assign correct pins for I2C1

--- src/main/target/SPARKY2/target.h
+++ src/main/target/SPARKY2/target.h
@@ -112,6 +112,8 @@
 
 #define USE_I2C
 #define USE_I2C_DEVICE_1
+#define I2C1_SCL                PB8
+#define I2C1_SDA                PB9
 #define I2C_DEVICE              (I2CDEV_1)
 
 #define USE_ADC
