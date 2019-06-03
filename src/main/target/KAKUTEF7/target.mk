bd3cecd6d373fc039dffc0378d1ce3d2bc558566
jamming <jianbing.pan@holybro.com>
2019-06-03 13:35:02 +0800

    Add mpu6000 compatible to KakuteF7 target

--- src/main/target/KAKUTEF7/target.mk
+++ src/main/target/KAKUTEF7/target.mk
@@ -12,6 +12,7 @@ endif
 
 TARGET_SRC = \
             drivers/accgyro/accgyro_mpu.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/accgyro/accgyro_spi_icm20689.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/compass/compass_hmc5883l.c \
