114fc6cee4389befb49f2526100863ea5afa1a80
Michael Keller <github@ike.ch>
2019-06-18 21:42:20 +1200

    increase SA_MAX_RCVLEN (#8438)
    increase SA_MAX_RCVLEN

--- src/main/io/vtx_smartaudio.c
+++ src/main/io/vtx_smartaudio.c
@@ -150,7 +150,7 @@ static uint8_t saLockMode = SA_MODE_SET_UNLOCK; // saCms variable?
 bool saDeferred = true; // saCms variable?
 
 // Receive frame reassembly buffer
-#define SA_MAX_RCVLEN 15
+#define SA_MAX_RCVLEN 21
 static uint8_t sa_rbuf[SA_MAX_RCVLEN+4]; // XXX delete 4 byte guard
 
 //
