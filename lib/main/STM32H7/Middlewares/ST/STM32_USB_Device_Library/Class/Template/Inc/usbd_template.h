2f911c116327ff5121a3874ed09073b4b1de3af7
jflyper <shinoda@jaist.ac.jp>
2019-06-24 12:59:45 +0900

    [H7][LIB] Import V1.4.0 library

---
+++ lib/main/STM32H7/Middlewares/ST/STM32_USB_Device_Library/Class/Template/Inc/usbd_template.h
@@ -0,0 +1,101 @@
+/**
+  ******************************************************************************
+  * @file    usbd_template_core.h
+  * @author  MCD Application Team
+  * @brief   Header file for the usbd_template_core.c file.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2015 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                      http://www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __USB_TEMPLATE_CORE_H
+#define __USB_TEMPLATE_CORE_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include  "usbd_ioreq.h"
+
+/** @addtogroup STM32_USB_DEVICE_LIBRARY
+  * @{
+  */
+
+/** @defgroup USBD_TEMPLATE
+  * @brief This file is the header file for usbd_template_core.c
+  * @{
+  */
+
+
+/** @defgroup USBD_TEMPLATE_Exported_Defines
+  * @{
+  */
+#define TEMPLATE_EPIN_ADDR                 0x81
+#define TEMPLATE_EPIN_SIZE                 0x10
+
+#define USB_TEMPLATE_CONFIG_DESC_SIZ       64
+
+/**
+  * @}
+  */
+
+
+/** @defgroup USBD_CORE_Exported_TypesDefinitions
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+
+
+/** @defgroup USBD_CORE_Exported_Macros
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CORE_Exported_Variables
+  * @{
+  */
+
+extern USBD_ClassTypeDef  USBD_TEMPLATE_ClassDriver;
+/**
+  * @}
+  */
+
+/** @defgroup USB_CORE_Exported_Functions
+  * @{
+  */
+/**
+  * @}
+  */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __USB_TEMPLATE_CORE_H */
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
