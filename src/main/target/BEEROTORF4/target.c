56e7c8225a8df5e0bae3208f76e3f28cbaa978c1
mikeller <github@ike.ch>
2019-07-07 13:53:56 +1200

    Fixed DMA resource conflicts for target BEEROTORF4.

--- src/main/target/BEEROTORF4/target.c
+++ src/main/target/BEEROTORF4/target.c
@@ -30,14 +30,14 @@
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM9, CH2,  PA3, TIM_USE_PPM,                       0, 0), // PPM IN
 
-    DEF_TIM(TIM1, CH2N, PB0, TIM_USE_MOTOR,                     0, 0), // M1 - DMAR: DMA2_ST5
-    DEF_TIM(TIM1, CH3N, PB1, TIM_USE_MOTOR,                     0, 0), // M2 -
-    DEF_TIM(TIM2, CH2,  PA1, TIM_USE_MOTOR,                     0, 0), // M3 - DMAR: DMA1_ST7
-    DEF_TIM(TIM2, CH1,  PA0, TIM_USE_MOTOR,                     0, 0), // M4 -
-    DEF_TIM(TIM8, CH1,  PC6, TIM_USE_MOTOR,                     0, 0), // M5 - DMAR: DMA2_ST1
-    DEF_TIM(TIM8, CH2,  PC7, TIM_USE_MOTOR,                     0, 0), // M6 -
-    DEF_TIM(TIM3, CH2,  PB5, TIM_USE_MOTOR,                     0, 0), // M7 - DMAR: DMA1_ST2
-    DEF_TIM(TIM4, CH4,  PB9, TIM_USE_MOTOR,                     0, 0), // M8 - DMAR: DMA1_ST6
+    DEF_TIM(TIM1, CH2N, PB0, TIM_USE_MOTOR,                     0, 0), // M1 - DMA2_ST6
+    DEF_TIM(TIM8, CH3N, PB1, TIM_USE_MOTOR,                     0, 1), // M2 - DMA2_ST4
+    DEF_TIM(TIM2, CH2,  PA1, TIM_USE_MOTOR,                     0, 0), // M3 - DMA1_ST6
+    DEF_TIM(TIM5, CH1,  PA0, TIM_USE_MOTOR,                     0, 0), // M4 - DMA1_ST2
+    DEF_TIM(TIM3, CH1,  PC6, TIM_USE_MOTOR,                     0, 0), // M5 - DMA1_ST4 (conflicts with SDCard, switch off SDCard DMA if used for DShot)
+    DEF_TIM(TIM8, CH2,  PC7, TIM_USE_MOTOR,                     0, 0), // M6 - DMA2_ST2
+    DEF_TIM(TIM3, CH2,  PB5, TIM_USE_MOTOR,                     0, 0), // M7 - DMA1_ST5 (doesn't work for DShot)
+    DEF_TIM(TIM4, CH4,  PB9, TIM_USE_MOTOR,                     0, 0), // M8 - no DMA
 
     DEF_TIM(TIM4, CH3,  PB8, TIM_USE_LED | TIM_USE_TRANSPONDER, 0, 0), // LED_STRIP / TRANSPONDER - DMA1_ST7 (can be used for DShot, conflicts with OSD TX)
 };
