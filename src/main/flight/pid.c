2042e3162f1b6d6cc5a05b4e9fa07338b29ea7e6
ctzsnooze <chris.thompson@sydney.edu.au>
2019-07-15 11:46:27 +1000

    CamelCase fixes, change boost factor, limit ranges

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -205,7 +205,7 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .auto_profile_cell_count = AUTO_PROFILE_CELL_COUNT_STAY,
         .transient_throttle_limit = 15,
         .profileName = { 0 },
-        .ff_boost = 150,
+        .ff_boost = 15,
         .ff_boost_hz = 20,
     );
 #ifndef USE_D_MIN
@@ -303,8 +303,8 @@ static FAST_RAM_ZERO_INIT pt1Filter_t airmodeThrottleLpf2;
 
 static FAST_RAM_ZERO_INIT pt1Filter_t antiGravityThrottleLpf;
 
-static FAST_RAM_ZERO_INIT pt1Filter_t ff_lpf[XYZ_AXIS_COUNT];
-static FAST_RAM_ZERO_INIT float ff_boost_factor;
+static FAST_RAM_ZERO_INIT pt1Filter_t ffLpf[XYZ_AXIS_COUNT];
+static FAST_RAM_ZERO_INIT float ffBoostFactor;
 
 void pidInitFilters(const pidProfile_t *pidProfile)
 {
@@ -444,12 +444,12 @@ void pidInitFilters(const pidProfile_t *pidProfile)
     pt1FilterInit(&antiGravityThrottleLpf, pt1FilterGain(ANTI_GRAVITY_THROTTLE_FILTER_CUTOFF, dT));
 
     for (int axis = 0; axis < (XYZ_AXIS_COUNT - 1); axis++){
-        pt1FilterInit(&ff_lpf[axis], pt1FilterGain((float)pidProfile->ff_boost_hz, dT) );
+        pt1FilterInit(&ffLpf[axis], pt1FilterGain((float)pidProfile->ff_boost_hz, dT) );
     }
     if (pidProfile->ff_boost > 0 ){
-        ff_boost_factor = (float)pidProfile->ff_boost / 100.0f;
+        ffBoostFactor = (float)pidProfile->ff_boost / 10.0f;
     } else {
-        ff_boost_factor = 0;
+        ffBoostFactor = 0;
     }
 
 }
@@ -1431,9 +1431,9 @@ void FAST_CODE pidController(const pidProfile_t *pidProfile, timeUs_t currentTim
             // no transition if feedForwardTransition == 0
             float transition = feedForwardTransition > 0 ? MIN(1.f, getRcDeflectionAbs(axis) * feedForwardTransition) : 1;
             pidData[axis].F = feedforwardGain * transition * pidSetpointDelta * pidFrequency;
-            if (ff_boost_factor && (axis <= FD_PITCH)) {
-                const float F_hpf = pidData[axis].F - pt1FilterApply(&ff_lpf[axis], pidData[axis].F);
-                pidData[axis].F += F_hpf * ff_boost_factor;
+            if (ffBoostFactor && (axis <= FD_PITCH)) {
+                const float ffHpf = pidData[axis].F - pt1FilterApply(&ffLpf[axis], pidData[axis].F);
+                pidData[axis].F += ffHpf * ffBoostFactor;
             }
         } else {
             pidData[axis].F = 0;
