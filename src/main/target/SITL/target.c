9017f98f4135c61825a3361d0fa40cf1feae3303
Gene Merewether <mereweth@jpl.nasa.gov>
2019-08-22 09:49:41 -0700

    Get SITL building again

--- src/main/target/SITL/target.c
+++ src/main/target/SITL/target.c
@@ -70,6 +70,8 @@ static udpLink_t stateLink, pwmLink;
 static pthread_mutex_t updateLock;
 static pthread_mutex_t mainLoopLock;
 
+serialPinConfig_t serialPinConfig_System;
+
 int timeval_sub(struct timespec *result, struct timespec *x, struct timespec *y);
 
 int lockMainPID(void) {
