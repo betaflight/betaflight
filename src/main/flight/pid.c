445122a9977a1b809d9e258f4678f32862c89209
ctzsnooze <chris.thompson@sydney.edu.au>
2019-08-31 11:19:03 +1000

    FF defaults increased to 90's
    Higher feed forward improves stick responsiveness and reduces bounce back on lower authority quads.
    
    Feed forward defaults in 4.0 were slightly conservative even for typical quads.
    
    In association with ff_boost and ff_max_rate_limit, overshoot with even higher feed forward is less of a problem than before.
    
    I think these values should be about right for 4.1

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -136,9 +136,9 @@ void resetPidProfile(pidProfile_t *pidProfile)
 {
     RESET_CONFIG(pidProfile_t, pidProfile,
         .pid = {
-            [PID_ROLL] =  { 42, 85, 35, 70 },
-            [PID_PITCH] = { 46, 90, 38, 75 },
-            [PID_YAW] =   { 30, 90, 0, 70 },
+            [PID_ROLL] =  { 42, 85, 35, 90 },
+            [PID_PITCH] = { 46, 90, 38, 95 },
+            [PID_YAW] =   { 30, 90, 0, 90 },
             [PID_LEVEL] = { 50, 50, 75, 0 },
             [PID_MAG] =   { 40, 0, 0, 0 },
         },
