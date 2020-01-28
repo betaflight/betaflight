5e35eb0d396ccc615641ac0491a977b18b26c42e
Michael Keller <github@ike.ch>
2020-01-28 21:18:27 +1300

    Merge pull request #9418 from mikeller/fix_stm32f411_pin_init
    Fixed initialisation of unused pins on STM32F411.

--- src/main/drivers/timer_stm32f4xx.c
+++ src/main/drivers/timer_stm32f4xx.c
@@ -154,8 +154,10 @@ const timerHardware_t fullTimerHardware[FULL_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM9, CH2, PE6, TIM_USE_ANY, 0, 0),
 
 //PORTF
+#if !defined(STM32F411xE)
     DEF_TIM(TIM10, CH1, PF6, TIM_USE_ANY, 0, 0),
     DEF_TIM(TIM11, CH1, PF7, TIM_USE_ANY, 0, 0),
+#endif
 
 //PORTH
     DEF_TIM(TIM5, CH1, PH10, TIM_USE_ANY, 0, 0),
