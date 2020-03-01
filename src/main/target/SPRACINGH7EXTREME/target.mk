4bebbf817a79fdb2f4cfec32070dbcf2ae1e8e6e
R. Perkins <senatorperkins@imac.local>
2020-03-01 11:37:26 -0800

    Added support to SPRACING H7 EXTREME flight controller target for QMC-5883 compass as found in Matek M8Q-5883 GPS Compass package

--- src/main/target/SPRACINGH7EXTREME/target.mk
+++ src/main/target/SPRACINGH7EXTREME/target.mk
@@ -16,6 +16,7 @@ TARGET_SRC += \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_qmc5883l.c \
             drivers/barometer/barometer_bmp388.c \
             drivers/vtx_rtc6705.c \
             drivers/vtx_rtc6705_soft_spi.c
