9c99eed602255fd94b45c448f6d80e27534a9d45
ctzsnooze <chris.thompson@sydney.edu.au>
2020-04-14 17:38:57 +1000

    Default Yaw P to 45

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -147,7 +147,7 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .pid = {
             [PID_ROLL] =  { 42, 85, 35, 90 },
             [PID_PITCH] = { 46, 90, 38, 95 },
-            [PID_YAW] =   { 30, 90, 0, 90 },
+            [PID_YAW] =   { 45, 90, 0, 90 },
             [PID_LEVEL] = { 50, 50, 75, 0 },
             [PID_MAG] =   { 40, 0, 0, 0 },
         },
