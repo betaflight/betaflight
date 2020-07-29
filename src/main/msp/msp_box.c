5f5309f82bff2f6276f9760726c52eff78f2c6a7
Michael Keller <github@ike.ch>
2020-07-30 00:33:44 +1200

    Merge pull request #10057 from etracer65/gps_rescue_fixed_wing_disable
    Disable GPS RESCUE if mixer is fixed-wing type

--- src/main/msp/msp_box.c
+++ src/main/msp/msp_box.c
@@ -213,7 +213,7 @@ void initActiveBoxIds(void)
 #ifdef USE_GPS
     if (featureIsEnabled(FEATURE_GPS)) {
 #ifdef USE_GPS_RESCUE
-        if (!featureIsEnabled(FEATURE_3D)) {
+        if (!featureIsEnabled(FEATURE_3D) && !isFixedWing()) {
             BME(BOXGPSRESCUE);
         }
 #endif
