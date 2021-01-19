fd7f1e699199d76238e72573172c8b2126ca8b34
Krzysztof Kuczek <krzysztof.kuczek@gmail.com>
2021-01-19 20:05:52 +0100

    cosmetic changes 2

--- src/main/io/dashboard.c
+++ src/main/io/dashboard.c
@@ -612,32 +612,6 @@ static void showDebugPage(void)
 }
 #endif
 
-static void showBBPage(void)
-{
-    uint8_t rowIndex = PAGE_TITLE_LINE_COUNT;
-    int8_t fileNo;
-    if (isBlackboxDeviceWorking()){
-        switch (blackboxConfig()->device) {
-            case BLACKBOX_DEVICE_SDCARD:
-                fileNo = blackboxGetLogFileNo();
-                if( fileNo > 0){
-                    tfp_sprintf(lineBuffer, "File no: %d", fileNo);
-                }else{
-                    tfp_sprintf(lineBuffer, "Log not started!");
-                }
-                break;
-            default:
-                tfp_sprintf(lineBuffer, "Not supp. dev.");
-                break;
-        }
-    }else{
-        tfp_sprintf(lineBuffer, "BB not working");
-    }
-
-    padLineBuffer();
-    i2c_OLED_set_line(bus, rowIndex++);
-    i2c_OLED_send_string(bus, lineBuffer);
-}
 
 static const pageEntry_t pages[PAGE_COUNT] = {
     { PAGE_WELCOME, FC_FIRMWARE_NAME,  showWelcomePage,    PAGE_FLAGS_SKIP_CYCLING },
