4aa4d8f0b730c153c2f79f9d0f4359521bff1bca
Michael Keller <github@ike.ch>
2021-03-22 15:30:18 +1300

    Merge pull request #10616 from Linjieqiang/IFLIGHT_H743_AIO_V2
    Add support for board "IFLIGHT_H743_AIO_V2"

--- src/main/target/IFLIGHT_H743_AIO_V2/target.h
+++ src/main/target/IFLIGHT_H743_AIO_V2/target.h
@@ -20,37 +20,14 @@
 
 #pragma once
 
-#define TARGET_BOARD_IDENTIFIER "IFH7"
-#define USBD_PRODUCT_STRING     "IFLIGHT_H7_TWING"
+#define TARGET_BOARD_IDENTIFIER "IF7A"
+#define USBD_PRODUCT_STRING     "IFLIGHT_H743_AIO_V2"
 
-#define LED0_PIN                PC2
-#define LED1_PIN                PC3
+#define LED0_PIN                PC13
 
 #define USE_BEEPER
-#define BEEPER_PIN              PC13
-#define BEEPER_INVERTED
-
-#define USE_QUADSPI
-#define USE_QUADSPI_DEVICE_1
-
-#define QUADSPI1_SCK_PIN        PB2
-
-#define QUADSPI1_BK1_IO0_PIN    PD11
-#define QUADSPI1_BK1_IO1_PIN    PD12
-#define QUADSPI1_BK1_IO2_PIN    PE2
-#define QUADSPI1_BK1_IO3_PIN    PD13
-#define QUADSPI1_BK1_CS_PIN     PB10
-
-#define QUADSPI1_BK2_IO0_PIN    PE7
-#define QUADSPI1_BK2_IO1_PIN    PE8
-#define QUADSPI1_BK2_IO2_PIN    PE9
-#define QUADSPI1_BK2_IO3_PIN    PE10
-#define QUADSPI1_BK2_CS_PIN     NONE
-
-#define QUADSPI1_MODE           QUADSPI_MODE_BK1_ONLY
-#define QUADSPI1_CS_FLAGS       (QUADSPI_BK1_CS_HARDWARE | QUADSPI_BK2_CS_NONE | QUADSPI_CS_MODE_LINKED)
-
-#define USE_FLASH_CHIP
+#define BEEPER_PIN              PD2
+#define BEEPER_INVERTED 
 
 #define USE_UART
 
@@ -63,24 +40,20 @@
 #define UART2_TX_PIN            PA2
 
 #define USE_UART3
-#define UART3_RX_PIN            PD9
-#define UART3_TX_PIN            PD8
+#define UART3_RX_PIN            PB11
+#define UART3_TX_PIN            PB10
 
 #define USE_UART4
-#define UART4_RX_PIN            PD0
-#define UART4_TX_PIN            PD1
-
-#define USE_UART5
-#define UART5_RX_PIN            PD2
-#define UART5_TX_PIN            PC12
+#define UART4_RX_PIN            PA1
+#define UART4_TX_PIN            PA0
 
 #define USE_UART6
 #define UART6_RX_PIN            PC7
 #define UART6_TX_PIN            PC6
 
 #define USE_UART7
-#define UART7_RX_PIN            PB3
-#define UART7_TX_PIN            PB4
+#define UART7_RX_PIN            PE7
+#define UART7_TX_PIN            PE8
 
 #define USE_UART8
 #define UART8_RX_PIN            PE0
@@ -88,7 +61,7 @@
 
 #define USE_VCP
 
-#define SERIAL_PORT_COUNT       9
+#define SERIAL_PORT_COUNT       8
 
 #define USE_SPI
 
@@ -104,55 +77,77 @@
 #define SPI2_MOSI_PIN           PB15
 #define SPI2_NSS_PIN            PB12
 
+#define USE_SPI_DEVICE_3
+#define SPI3_SCK_PIN            PC10
+#define SPI3_MISO_PIN           PC11
+#define SPI3_MOSI_PIN           PC12
+#define SPI3_NSS_PIN            PA15
+
 #define USE_SPI_DEVICE_4
-#define SPI4_SCK_PIN            PE12
-#define SPI4_MISO_PIN           PE13
-#define SPI4_MOSI_PIN           PE14
-#define SPI4_NSS_PIN            PE11
+#define SPI4_SCK_PIN            PE2
+#define SPI4_MISO_PIN           PE5
+#define SPI4_MOSI_PIN           PE6
+#define SPI4_NSS_PIN            PE4
 
 #define USE_I2C
 #define USE_I2C_DEVICE_1
+#define I2C_DEVICE_1            (I2CDEV_1)
 #define I2C1_SCL                PB8
 #define I2C1_SDA                PB9
-#define I2C_DEVICE              (I2CDEV_1)
 
+#define USE_I2C_DEVICE_2
+#define I2C_DEVICE_2            (I2CDEV_2)
+#define I2C2_SCL                PB10
+#define I2C2_SDA                PB11
+
+#define MAG_I2C_INSTANCE        (I2CDEV_1)
 #define USE_MAG
 #define USE_MAG_HMC5883
+#define USE_MAG_QMC5883
+#define USE_MAG_LIS3MDL
 
+#define BARO_I2C_INSTANCE       (I2CDEV_1)
 #define USE_BARO
-#define USE_BARO_BMP388
+#define DEFAULT_BARO_DPS310I2CDEV_1
+#define USE_BARO_MS5611
+#define USE_BARO_BMP280
+#define USE_BARO_DPS310
 
 #define USE_GYRO
-#define USE_GYRO_SPI_MPU6500
-#define USE_MULTI_GYRO
-#undef  USE_GYRO_REGISTER_DUMP
+#define USE_ACC
+#define USE_GYRO_SPI_MPU6000
+#define USE_ACC_SPI_MPU6000
+#define USE_ACCGYRO_BMI270
+#define USE_ACCGYRO_LSM6DSO
 
 #define USE_EXTI
 #define USE_GYRO_EXTI
-#define GYRO_1_EXTI_PIN         PC5
-#define GYRO_2_EXTI_PIN         PB11
+#define GYRO_1_EXTI_PIN         PD0
+#define GYRO_2_EXTI_PIN         PD8
 #define USE_MPU_DATA_READY_SIGNAL
 #define ENSURE_MPU_DATA_READY_IS_LOW
 
 #define GYRO_1_CS_PIN           SPI1_NSS_PIN
 #define GYRO_1_SPI_INSTANCE     SPI1
+#define GYRO_1_ALIGN            CW0_DEG 
 
 #define GYRO_2_CS_PIN           SPI2_NSS_PIN
 #define GYRO_2_SPI_INSTANCE     SPI2
-
-#define USE_ACC
-#define USE_ACC_SPI_MPU6500
-
-#define GYRO_1_ALIGN            CW0_DEG
-#define GYRO_2_ALIGN            CW90_DEG
+#define GYRO_2_ALIGN            CW0_DEG
 
 #define GYRO_CONFIG_USE_GYRO_DEFAULT GYRO_CONFIG_USE_GYRO_BOTH
 
 #define USE_FLASHFS
 #define USE_FLASH_TOOLS
-#define USE_FLASH_W25N01G
-#define FLASH_QUADSPI_INSTANCE  QUADSPI
-
+#define USE_FLASH_M25P16
+#define USE_FLASH_W25N01G          // 1Gb NAND flash support
+#define USE_FLASH_W25M             // Stacked die support
+#define USE_FLASH_W25M512          // 512Kb (256Kb x 2 stacked) NOR flash support
+#define USE_FLASH_W25M02G          // 2Gb (1Gb x 2 stacked) NAND flash support
+#define FLASH_CS_PIN            SPI3_NSS_PIN
+#define FLASH_SPI_INSTANCE      SPI3
+#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
+ 
 #define USE_MAX7456
 #define MAX7456_SPI_INSTANCE    SPI4
 #define MAX7456_SPI_CS_PIN      SPI4_NSS_PIN
@@ -173,8 +168,6 @@
 #define ADC3_DMA_STREAM             DMA2_Stream1
 #endif
 
-#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
-
 #define USE_ADC
 #define USE_ADC_INTERNAL
 
@@ -182,12 +175,15 @@
 #define ADC2_INSTANCE               ADC2
 #define ADC3_INSTANCE               ADC3
 
-#define RSSI_ADC_PIN                PC4
-#define VBAT_ADC_PIN                PC1
-#define CURRENT_METER_ADC_PIN       PC0
+#define RSSI_ADC_PIN                PC5
+#define VBAT_ADC_PIN                PC3
+#define CURRENT_METER_ADC_PIN       PC2
+#define EXTERNAL1_ADC_PIN           PC1
 
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+
+#define CURRENT_METER_SCALE_DEFAULT 200
 
 #define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
 #define DEFAULT_FEATURES        (FEATURE_RSSI_ADC | FEATURE_TELEMETRY | FEATURE_OSD | FEATURE_LED_STRIP)
@@ -200,6 +196,6 @@
 #define TARGET_IO_PORTF 0xffff
 #define TARGET_IO_PORTG 0xffff
 
-#define USABLE_TIMER_CHANNEL_COUNT 11
+#define USABLE_TIMER_CHANNEL_COUNT 16
 
-#define USED_TIMERS  ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(15) )
+#define USED_TIMERS  ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) )
