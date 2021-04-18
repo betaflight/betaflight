721224c08a79de90f37523fdc9dddc6a701c7f0b
Dominic Clifton <me@dominicclifton.name>
2021-04-18 15:51:12 +0200

    SPRacingH7RF - Add SPRacingPixelOSD support.

---
+++ src/main/target/SPRACINGH7RF/SPRACINGH7RF.mk
@@ -0,0 +1,13 @@
+FEATURES       += VCP ONBOARDFLASH SDCARD_SDIO SPRACING_PIXEL_OSD
+
+ifneq ($(EXST),)
+EXST = yes
+LD_SCRIPT       = $(LINKER_DIR)/stm32_ram_h730_exst_spracingpixelosd.ld
+endif
+
+TARGET_SRC += \
+            drivers/accgyro/accgyro_mpu.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/compass/compass_hmc5883l.c \
+            drivers/barometer/barometer_bmp388.c \
