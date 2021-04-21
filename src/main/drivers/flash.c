1abc95c6d67e2ded3ebf540e62e001643b1aa1e9
Dominic Clifton <me@dominicclifton.name>
2021-04-22 00:52:24 +0200

    Require explicit usage of firmware partition in preparation for memory mapped EXST targets with a second flash chip for data storage and config.

--- src/main/drivers/flash.c
+++ src/main/drivers/flash.c
@@ -519,7 +519,7 @@ static void flashConfigurePartitions(void)
     }
 #endif
 
-#if defined(FIRMWARE_SIZE)
+#if defined(FIRMWARE_SIZE) && defined(USE_FIRMWARE_PARTITION)
     const uint32_t firmwareSize = (FIRMWARE_SIZE * 1024);
     flashSector_t firmwareSectors = (firmwareSize / flashGeometry->sectorSize);
 
