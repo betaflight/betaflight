3a787dd390d51d2764e746a282bf52a7878cd9ed
mikeller <github@ike.ch>
2021-04-28 02:57:44 +1200

    Removed non-existent drivers from IFLIGHT_H743_AIO_V2 for 4.2.

--- src/main/target/IFLIGHT_H743_AIO_V2/target.mk
+++ src/main/target/IFLIGHT_H743_AIO_V2/target.mk
@@ -9,11 +9,8 @@ TARGET_SRC += \
             drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-            drivers/accgyro/accgyro_spi_lsm6dso_init.c \
-            drivers/accgyro/accgyro_spi_lsm6dso.c \
             $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270.c \
             drivers/accgyro/accgyro_spi_bmi270.c \
-            drivers/barometer/barometer_dps310.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_ms5611.c \
             drivers/compass/compass_hmc5883l.c \
