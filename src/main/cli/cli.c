e5d98757cfa6200acdb228456f2bb2acb64ba604
eggsbenedict <itrapspam@gmail.com>
2021-06-01 13:07:35 -0400

    CLI based warning for new rate type default and diff/dump pasting warning.

--- src/main/cli/cli.c
+++ src/main/cli/cli.c
@@ -6798,6 +6798,9 @@ void cliEnter(serialPort_t *serialPort)
 
 #ifndef MINIMAL_CLI
     cliPrintLine("\r\nEntering CLI Mode, type 'exit' to return, or 'help'");
+    cliPrintLine("\r\n**Default RATE type now 'ACTUAL': DO NOT paste RATES without");
+    cliPrintLine("confirming values in configurator!**");
+    cliPrintLine("\r\n**DO NOT paste DIFFs or DUMPs between versions!**");
 #else
     cliPrintLine("\r\nCLI");
 #endif
