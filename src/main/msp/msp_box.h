579e8005afa3ba79a6fc6f83fc4e3236eef76c5c
Štěpán Dalecký <daleckystepan@gmail.com>
2021-12-26 19:49:14 +0100

    Reorder box_t to save few hundreds of bytes

--- src/main/msp/msp_box.h
+++ src/main/msp/msp_box.h
@@ -23,8 +23,8 @@
 #include "fc/rc_modes.h"
 
 typedef struct box_s {
-    const uint8_t boxId;            // see boxId_e
     const char *boxName;            // GUI-readable box name
+    const uint8_t boxId;            // see boxId_e
     const uint8_t permanentId;      // permanent ID used to identify BOX. This ID is unique for one function, DO NOT REUSE IT
 } box_t;
 
