46ed50ec6dd3e521cf1f8f583746aca35d2db0d3
EmaxModel <2734115161@qq.com>
2022-04-28 13:53:08 +0800

    add EMAXF411 target and more gyroscope

---
+++ src/main/target/EMAXF411RX/target.mk
@@ -0,0 +1,17 @@
+F411_TARGETS    += $(TARGET)
+
+FEATURES        += VCP SDCARD_SPI
+
+TARGET_SRC = \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
+	    drivers/accgyro/accgyro_spi_icm20689.c \
+	    $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
+	    drivers/accgyro/accgyro_spi_bmi270.c \
+            drivers/max7456.c \
+            drivers/rx/rx_cc2500.c \
+            rx/cc2500_common.c \
+            rx/cc2500_frsky_shared.c \
+            rx/cc2500_frsky_d.c \
+            rx/cc2500_frsky_x.c \
+            rx/cc2500_sfhss.c \
+            rx/cc2500_redpine.c
