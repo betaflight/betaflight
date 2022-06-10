b0d089ec7061584ba99ff70e92d8d912eb5f1255
Steve Evans <Steve@SCEvans.com>
2022-06-10 17:32:01 +0100

    Add STM32F411SX1280 unified target
    (cherry picked from commit 6d17f3f765208bbf5d4add55efd485ea6f5f4e7e)

--- make/targets_list.mk
+++ make/targets_list.mk
@@ -84,7 +84,8 @@ UNIFIED_TARGETS := STM32F405 \
 	STM32F7X2 \
 	STM32F745 \
 	STM32G47X \
-	STM32H743
+	STM32H743 \
+	STM32F411SX1280
 
 # Legacy targets are targets that have been replaced by Unified Target configurations
 LEGACY_TARGETS := MATEKF405 \
--- src/main/target/STM32_UNIFIED/target.mk
+++ src/main/target/STM32_UNIFIED/target.mk
@@ -1,9 +1,39 @@
+RX_SRC = \
+    rx/cc2500_common.c \
+    rx/cc2500_frsky_shared.c \
+    rx/cc2500_frsky_d.c \
+    rx/cc2500_frsky_x.c \
+    rx/cc2500_sfhss.c \
+    rx/cc2500_redpine.c \
+    rx/a7105_flysky.c \
+    rx/cyrf6936_spektrum.c \
+    drivers/rx/expresslrs_driver.c \
+    rx/expresslrs.c \
+    rx/expresslrs_common.c \
+    rx/expresslrs_telemetry.c \
+    drivers/rx/rx_cc2500.c \
+    drivers/rx/rx_a7105.c \
+    drivers/rx/rx_cyrf6936.c \
+    drivers/rx/rx_sx127x.c \
+    drivers/rx/rx_sx1280.c \
+
 ifeq ($(TARGET), STM32F405)
 F405_TARGETS += $(TARGET)
 
 else
 ifeq ($(TARGET), STM32F411)
 F411_TARGETS += $(TARGET)
+
+else
+ifeq ($(TARGET), STM32F411SX1280)
+F411_TARGETS += $(TARGET)
+RX_SRC = \
+    drivers/rx/expresslrs_driver.c \
+    drivers/rx/rx_sx127x.c \
+    drivers/rx/rx_sx1280.c \
+    rx/expresslrs_telemetry.c \
+    rx/expresslrs_common.c \
+    rx/expresslrs.c \
 
 else
 ifeq ($(TARGET), STM32F7X2)
@@ -25,6 +55,7 @@ endif
 endif
 endif
 endif
+endif
 
 ifeq ($(TARGET), $(filter $(TARGET), STM32F405 STM32F745 STM32H743))
 # Use a full block (16 kB) of flash for custom defaults - with 1 MB flash we have more than we know how to use anyway
@@ -39,27 +70,11 @@ FEATURES       += VCP SDCARD_SPI SDCARD_SDIO ONBOARDFLASH
 endif
 
 TARGET_SRC = \
-    $(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
-    $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
-    $(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
-    $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
-    drivers/max7456.c \
-    drivers/vtx_rtc6705.c \
-    drivers/vtx_rtc6705_soft_spi.c \
-    rx/cc2500_common.c \
-    rx/cc2500_frsky_shared.c \
-    rx/cc2500_frsky_d.c \
-    rx/cc2500_frsky_x.c \
-    rx/cc2500_sfhss.c \
-    rx/cc2500_redpine.c \
-    rx/a7105_flysky.c \
-    rx/cyrf6936_spektrum.c \
-    drivers/rx/expresslrs_driver.c \
-    rx/expresslrs.c \
-    rx/expresslrs_common.c \
-    rx/expresslrs_telemetry.c \
-    drivers/rx/rx_cc2500.c \
-    drivers/rx/rx_a7105.c \
-    drivers/rx/rx_cyrf6936.c \
-    drivers/rx/rx_sx127x.c \
-    drivers/rx/rx_sx1280.c \
+	$(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
+	$(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
+	$(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
+	$(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
+	drivers/max7456.c \
+	drivers/vtx_rtc6705.c \
+	drivers/vtx_rtc6705_soft_spi.c \
+	$(RX_SRC)
