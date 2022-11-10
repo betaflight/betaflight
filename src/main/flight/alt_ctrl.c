f71daabb5852dd679da25a479f503453af131094
meijiahao <1406988209@qq.com>
2022-11-10 22:00:06 +0800

    Modified undefined variables

--- src/main/flight/alt_ctrl.c
+++ src/main/flight/alt_ctrl.c
@@ -13,7 +13,7 @@
 #include "sensors/rangefinder.h"
 
 static int32_t TFMINI_Altitude = 0;
-static int32_t TFMINI_estimatedAltitude = 0;
+//static int32_t TFMINI_estimatedAltitude = 0;
 
 void alt_ctrl_run(timeUs_t currentTimeUs, float z_ref)
 {
