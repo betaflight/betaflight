67fee1d89b07f86f9991716272f16befd69635ce
meijiahao <1406988209@qq.com>
2022-11-17 18:22:52 +0800

    modify mixer 0.25

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -271,7 +271,7 @@ static void calculateThrottleAndCurrentMotorEndpoints(timeUs_t currentTimeUs)
         DISABLE_FLIGHT_MODE(RANGEFINDER_MODE);     
     }else{
         ENABLE_FLIGHT_MODE(RANGEFINDER_MODE);
-        throttle = 0.2;
+        throttle = 0.25;
         //beeper(BEEPER_ALL);
     }
 }
