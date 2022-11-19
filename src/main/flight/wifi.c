19545a8d43fc36acc3e42a5aa8714a85eeed096d
May <1406988209@qq.com>
2022-11-19 01:44:48 -0800

    write wifi drivers in wifi.c.h

--- src/main/flight/wifi.c
+++ src/main/flight/wifi.c
@@ -5,7 +5,87 @@
 
 #include "wifi.h"
 
-void wifi_init()
+#include "io/serial.h"
+
+#include "drivers/time.h"
+
+
+static serialPort_t *wifiSerialPort = NULL;
+
+wifiDev_t wifiDev;
+
+static char wificmd01[] = "AT";
+static char wificmd02[] = "AT+CWMODE=1";
+//static char wificmd03[] = "AT+CWJAP="Xiaomi12","11111111a"";
+static char wificmd04[] = "AT";
+static char wificmd05[] = "AT";
+static char wificmd06[] = "AT";
+
+bool wifi_init()
 {
+    if(!wifiATK8266Detect(&wifiDev.dev))
+    {
+        return false;
+    }
+
+    wifiDev.lastValidResponseTimeMs = millis();
+
+    return true;
+}
+
+void atk_8266_send_InitCmd()
+{
+    serialWriteBuf(wifiSerialPort, wificmd01, sizeof(wificmd01));
+}
+
+static bool WifiDetect(wifiDev_t *dev)
+{
+    const serialPortConfig_t *portConfig = findSerialPortConfig(FUNCTION_WIFI_ESP8266);
+
+    if (!portConfig) {
+        return false;
+    }
+
+    wifiSerialPort = openSerialPort(portConfig->identifier, FUNCTION_WIFI_ESP8266, NULL, NULL, 115200, MODE_RXTX, 0);
+
+    if (wifiSerialPort == NULL) {
+        return false;
+    }
+
+    return true;
+}
+
+bool wifiATK8266Detect(wifiDev_t *dev)
+{
+    if(WifiDetect(dev))
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+void wifiUpdate(wifiDev_t *dev)
+{
+    UNUSED(dev);
+    static timeMs_t lastFrameReceivedMs = 0;
+    const timeMs_t timeNowMs = millis();
+
+    if(wifiSerialPort == NULL){
+        return;
+    }
+    // while (serialRxBytesWaiting(wifiSerialPort))
+    // {
+    //     uint8_t c = serialRead(wifiSerialPort);
+
+    // }
+
+    lastFrameReceivedMs = timeNowMs;
+
+    // if (timeNowMs - lastFrameReceivedMs > TF_TIMEOUT_MS) {
+    atk_8266_send_InitCmd();
+    // }
 
 }
--- src/main/flight/wifi.h
+++ src/main/flight/wifi.h
@@ -2,6 +2,32 @@
 
 #include "pg/pg.h"
 #include "common/time.h"
+#include "drivers/io_types.h"
 
+struct wifiDev_s;
+typedef void (*wifiOpInitFuncPtr)(struct wifiDev_s * dev);
+typedef void (*wifiOpStartFuncPtr)(struct wifiDev_s * dev);
+typedef int32_t (*wifiOpReadFuncPtr)(struct wifiDev_s * dev);
 
-void wifi_init();
+typedef enum {
+    WIFI_NONE        = 0,
+    WIFI_ATK_MW8266D      = 1,
+} wifiType_t;
+
+typedef struct wifiDev_s{
+    wifiType_t dev;
+    timeMs_t delayMs;
+
+    timeMs_t lastValidResponseTimeMs;
+    // function pointers
+    wifiOpInitFuncPtr init;
+    wifiOpStartFuncPtr update;
+    wifiOpReadFuncPtr read;
+} wifiDev_t;
+
+static bool WifiDetect(wifiDev_t *dev);
+bool wifiATK8266Detect(wifiDev_t *dev);
+
+bool wifi_init();
+void atk_8266_send_InitCmd();
+void wifiUpdate(wifiDev_t *dev);
