1bb075db32a74424cfe0f1f834a4aada5056a5df
May <1406988209@qq.com>
2022-11-18 19:39:24 -0800

    add two files wifi.c wifi.h

---
+++ src/main/flight/wifi.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdint.h>
+
+#include "platform.h"
+
+#include "wifi.h"
+
+void wifi_init()
+{
+
+}
---
+++ src/main/flight/wifi.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "pg/pg.h"
+#include "common/time.h"
+
+
+void wifi_init();
