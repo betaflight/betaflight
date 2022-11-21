3587bdd53fd82337abee0c9d78604cf9a093f260
blckmn <blackman@xtra.com.au>
2022-11-22 08:59:31 +1100

    Adding additional files needed, and removing custom defaults.

--- src/main/target/STM32H750/target.mk
+++ src/main/target/STM32H750/target.mk
@@ -34,6 +34,10 @@ endif
 FEATURES       += VCP ONBOARDFLASH SDCARD_SDIO
 
 TARGET_SRC = \
+    msc/usbd_storage_sd_spi.c \
+    drivers/sdcard_spi.c \
+    drivers/bus_quadspi_hal.c \
+    drivers/bus_quadspi.c \
 	$(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
 	$(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
 	$(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
