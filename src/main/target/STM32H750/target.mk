a09b3ceb19fd0f1b1a7021ded57950b27bca3def
blckmn <blackman@xtra.com.au>
2022-11-22 14:27:02 +1100

    Corrected indentation

--- src/main/target/STM32H750/target.mk
+++ src/main/target/STM32H750/target.mk
@@ -38,11 +38,11 @@ TARGET_SRC = \
     drivers/sdcard_spi.c \
     drivers/bus_quadspi_hal.c \
     drivers/bus_quadspi.c \
-	$(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
-	$(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
-	$(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
-	$(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
-	drivers/max7456.c \
-	drivers/vtx_rtc6705.c \
-	drivers/vtx_rtc6705_soft_spi.c \
-	$(RX_SRC)
+    $(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
+    $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
+    $(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
+    $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
+    drivers/max7456.c \
+    drivers/vtx_rtc6705.c \
+    drivers/vtx_rtc6705_soft_spi.c \
+    $(RX_SRC)
