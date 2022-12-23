0a46ef37f83cc894a477218047a936f8b4c8d27f
J Blackman <blckmn@users.noreply.github.com>
2022-12-23 12:00:42 +1100

    FIX: Include the legacy drivers (gated) (#12069)

--- src/main/target/STM32F411/target.mk
+++ src/main/target/STM32F411/target.mk
@@ -23,6 +23,7 @@ FEATURES       += VCP SDCARD_SPI SDCARD_SDIO ONBOARDFLASH
 
 TARGET_SRC = \
 	$(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
+	$(addprefix drivers/accgyro_legacy/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro_legacy/*.c))) \
 	$(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
 	$(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
 	$(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
