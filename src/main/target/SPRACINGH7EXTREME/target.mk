4bdce0f8c4edd86b582cfda8ff9f26da09fa33d8
Dominic Clifton <dominic.clifton@cleanflight.com>
2022-12-25 12:15:13 +0100

    SPRacingH7EXTREME - Temporarily restore legacy target.
    Build with:
    
    ```
    make TARGET=SPRACINGH7EXTREME OPTIONS=CLOUD_BUILD
    ```

--- src/main/target/SPRACINGH7EXTREME/target.mk
+++ src/main/target/SPRACINGH7EXTREME/target.mk
@@ -15,8 +15,8 @@ TARGET_SRC += \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
-            drivers/compass/compass_hmc5883l.c \
-            drivers/compass/compass_qmc5883l.c \
-            drivers/barometer/barometer_bmp388.c \
             drivers/vtx_rtc6705.c \
-            drivers/vtx_rtc6705_soft_spi.c
+            drivers/vtx_rtc6705_soft_spi.c \
+           	$(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
+	        $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
+            
