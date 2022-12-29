6f574f5709f80d0c245a2d85c32d9d69ffd3f5fd
Ivan Efimov <gendalf44@yandex.ru>
2022-12-29 11:02:30 -0600

    Funny OSD

--- src/main/config/config.h
+++ src/main/config/config.h
@@ -35,6 +35,11 @@ typedef enum {
 typedef struct pilotConfig_s {
     char craftName[MAX_NAME_LENGTH + 1];
     char pilotName[MAX_NAME_LENGTH + 1];
+    char extra100Throttle[MAX_NAME_LENGTH + 1];
+    char extraFcHotWarning[MAX_NAME_LENGTH + 1];
+    char extraTurtleModeWarning[MAX_NAME_LENGTH + 1];
+    char extraLowBatteryWarning[MAX_NAME_LENGTH + 1];
+    char extraArmedWarning[MAX_NAME_LENGTH + 1];
 } pilotConfig_t;
 
 PG_DECLARE(pilotConfig_t, pilotConfig);
--- src/main/osd/osd_elements.h
+++ src/main/osd/osd_elements.h
@@ -67,3 +67,4 @@ bool osdElementsNeedAccelerometer(void);
 #ifdef USE_SPEC_PREARM_SCREEN
 void osdDrawSpec(displayPort_t *osdDisplayPort);
 #endif // USE_SPEC_PREARM_SCREEN
+void toUpperCase(char* dest, const char* src, unsigned int maxSrcLength);
