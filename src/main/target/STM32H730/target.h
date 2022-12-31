1b98917418f71ac4ee1e704145ef5eeb0b2abe1b
Dominic Clifton <dominic.clifton@cleanflight.com>
2022-12-31 04:28:12 +0100

    STM32H730 base target support. (#12093)
    * STM32H750 - Don't define a default config storage subsystem.
    * STM32H730 - Add base target based on the H750 unified target.

--- src/main/target/STM32H730/target.h
+++ src/main/target/STM32H730/target.h
@@ -28,7 +28,7 @@
  * 2) *must* define the storage subsystem used to boot, e.g. see CONFIG_IN_xxx.
  * 3) *must* define all the settings required for the config storage system to
  *    be able to load the config at boot time BEFORE it has actually loaded a config.
- *    e.g. for QSPI define the QSPI instance, pins, mode, etc,
+ *    e.g. for OCTOQSPI define the QSPI instance, pins, mode, etc,
  *         for SDCARD define the SD card bus (SPI/SDIO), pins, etc.
  *
  */
@@ -36,11 +36,11 @@
 #pragma once
 
 #ifndef TARGET_BOARD_IDENTIFIER
-#define TARGET_BOARD_IDENTIFIER "S750"
+#define TARGET_BOARD_IDENTIFIER "S730"
 #endif
 
 #ifndef USBD_PRODUCT_STRING
-#define USBD_PRODUCT_STRING     "Betaflight STM32H750"
+#define USBD_PRODUCT_STRING     "Betaflight STM32H730"
 #endif
 
 #if !defined(USE_I2C)
@@ -64,18 +64,6 @@
 #define SPI_FULL_RECONFIGURABILITY
 #endif
 
-// Provide a default so that this target builds on the build server.
-#if !defined(USE_SPI)
-#define USE_SPI
-#define USE_SPI_DEVICE_1
-#define USE_SPI_DEVICE_2
-#define USE_SPI_DEVICE_3
-#define USE_SPI_DEVICE_4
-#define USE_SPI_DEVICE_5
-#define USE_SPI_DEVICE_6
-#define SPI_FULL_RECONFIGURABILITY
-#endif
-
 #define USE_UART1
 #define USE_UART2
 #define USE_UART3
@@ -84,26 +72,31 @@
 #define USE_UART6
 #define USE_UART7
 #define USE_UART8
+#define USE_UART9
+#define USE_UART10
 #define USE_LP_UART1
 
-#define SERIAL_PORT_COUNT       (UNIFIED_SERIAL_PORT_COUNT + 8)
+#define SERIAL_PORT_COUNT       (UNIFIED_SERIAL_PORT_COUNT + 11)
+
+// Disable OCTOSPI pins PB2/CLK, PB6/NCS, PD11/IO0, PD12/IO1, PD13/IO3, PE2/IO2
+// PE7/IO4, PE8/IO5, PE9/IO6, PE10/IO7
 
 #define TARGET_IO_PORTA 0xffff
-#define TARGET_IO_PORTB 0xffff
+#define TARGET_IO_PORTB (0xffff & ~(BIT(2)|BIT(6)))
 #define TARGET_IO_PORTC 0xffff
-#define TARGET_IO_PORTD 0xffff
-#define TARGET_IO_PORTE 0xffff
+#define TARGET_IO_PORTD (0xffff & ~(BIT(11)|BIT(12)|BIT(13)))
+#define TARGET_IO_PORTE (0xffff & ~(BIT(2)|BIT(7)|BIT(8)|BIT(9)|BIT(10)))
 #define TARGET_IO_PORTF 0xffff
+#define TARGET_IO_PORTG 0xffff
+#define TARGET_IO_PORTH 0xffff
 
 #define USE_BEEPER
 
 #define USE_VCP
 
-#define USE_SOFTSERIAL1
-#define USE_SOFTSERIAL2
+#define UNIFIED_SERIAL_PORT_COUNT       1
 
-#define UNIFIED_SERIAL_PORT_COUNT       3
-
+#define USE_USB_ID
 #define USE_USB_DETECT
 
 #define USE_ESCSERIAL
