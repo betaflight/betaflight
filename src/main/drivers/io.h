15083a33b900ea762acb784af87216cb1b8086e2
blckmn <blackman@xtra.com.au>
2023-01-08 18:17:10 +1100

    IO and Timer Updates

--- src/main/drivers/io.h
+++ src/main/drivers/io.h
@@ -71,15 +71,25 @@
 #define IOCFG_IN_FLOATING    IO_CONFIG(GPIO_Mode_IN,  0, 0,             GPIO_PuPd_NOPULL)
 #define IOCFG_IPU_25         IO_CONFIG(GPIO_Mode_IN,  GPIO_Speed_25MHz, 0, GPIO_PuPd_UP)
 
+#elif defined(AT32F4)
+
+#define IOCFG_OUT_PP         0
+#define IOCFG_OUT_OD         0
+#define IOCFG_AF_PP          0
+#define IOCFG_AF_OD          0
+#define IOCFG_IPD            0
+#define IOCFG_IPU            0
+#define IOCFG_IN_FLOATING    0
+
 #elif defined(UNIT_TEST) || defined(SIMULATOR_BUILD)
 
-# define IOCFG_OUT_PP         0
-# define IOCFG_OUT_OD         0
-# define IOCFG_AF_PP          0
-# define IOCFG_AF_OD          0
-# define IOCFG_IPD            0
-# define IOCFG_IPU            0
-# define IOCFG_IN_FLOATING    0
+#define IOCFG_OUT_PP         0
+#define IOCFG_OUT_OD         0
+#define IOCFG_AF_PP          0
+#define IOCFG_AF_OD          0
+#define IOCFG_IPD            0
+#define IOCFG_IPU            0
+#define IOCFG_IN_FLOATING    0
 
 #else
 # warning "Unknown TARGET"
--- src/main/drivers/timer.h
+++ src/main/drivers/timer.h
@@ -37,14 +37,10 @@
 
 typedef uint16_t captureCompare_t;        // 16 bit on both 103 and 303, just register access must be 32bit sometimes (use timCCR_t)
 
-#if defined(STM32F4) || defined(STM32F7) || defined(STM32H7) || defined(STM32G4) || defined(UNIT_TEST) || defined(SIMULATOR_BUILD)
 typedef uint32_t timCCR_t;
 typedef uint32_t timCCER_t;
 typedef uint32_t timSR_t;
 typedef uint32_t timCNT_t;
-#else
-#error "Unknown CPU defined"
-#endif
 
 typedef enum {
     TIM_USE_ANY            = 0x0,
