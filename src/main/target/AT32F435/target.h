ba196e73d7d9578efbc58b236a31cbc6e1894440
J Blackman <blckmn@users.noreply.github.com>
2023-01-09 11:15:12 +1100

    AT32F435: new target (#12159)
    * AT32F435: New target (WIP)
    * IO and Timer Updates

---
+++ src/main/target/AT32F435/target.h
@@ -0,0 +1,69 @@
+/*
+ * This file is part of Cleanflight and Betaflight.
+ *
+ * Cleanflight and Betaflight are free software. You can redistribute
+ * this software and/or modify this software under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Cleanflight and Betaflight are distributed in the hope that they
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TARGET_BOARD_IDENTIFIER "A435"
+
+#define USBD_PRODUCT_STRING     "Betaflight AT32F435"
+
+#ifndef AT32F435
+#define AT32F435
+#endif
+
+//#define USE_I2C_DEVICE_1
+
+//#define USE_UART1
+//#define USE_UART2
+//#define USE_UART3
+
+#define SERIAL_PORT_COUNT       (UNIFIED_SERIAL_PORT_COUNT + 3)
+
+#define USE_INVERTER
+
+//#define USE_SPI_DEVICE_1
+//#define USE_SPI_DEVICE_2
+
+#define TARGET_IO_PORTA 0xffff
+#define TARGET_IO_PORTB 0xffff
+#define TARGET_IO_PORTC 0xffff
+#define TARGET_IO_PORTD 0xffff
+
+//#define USE_I2C
+//#define I2C_FULL_RECONFIGURABILITY
+
+//#define USE_SPI
+//#define SPI_FULL_RECONFIGURABILITY
+
+//#define USE_USB_DETECT
+//#define USE_VCP
+#define USE_SOFTSERIAL1
+#define USE_SOFTSERIAL2
+
+#define UNIFIED_SERIAL_PORT_COUNT       3
+
+#define USE_ADC
+
+#define USE_CUSTOM_DEFAULTS
+
+#undef USE_TIMER
+#undef USE_GPIO
+#undef USE_DMA
+#undef USE_PWM
