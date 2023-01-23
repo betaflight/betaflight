cb2edcee2dabe71e2d9f3c30896db8bbafda7e7b
blckmn <blackman@xtra.com.au>
2023-01-24 06:39:26 +1100

    Adding comment

--- src/main/target/common_pre.h
+++ src/main/target/common_pre.h
@@ -534,6 +534,9 @@ extern uint8_t _dmaram_end__;
 #define USE_CRAFTNAME_MSGS
 
 #if !defined(CORE_BUILD)
+// CORE_BUILD is only hardware drivers, and the bare minimum
+// any thing defined here will be in the standard (git hub actions)
+// builds or included in CLOUD_BUILD by default.
 
 #if !defined(USE_LAUNCH_CONTROL)
 #define USE_LAUNCH_CONTROL
