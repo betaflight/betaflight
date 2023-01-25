d94b07baa5c9f036bae432bdf9b407ac7f37560a
blckmn <blackman@xtra.com.au>
2023-01-25 19:50:55 +1100

    Update version for next release

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -25,7 +25,7 @@
 #define FC_FIRMWARE_NAME            "Betaflight"
 #define FC_FIRMWARE_IDENTIFIER      "BTFL"
 #define FC_VERSION_MAJOR            4  // increment when a major release is made (big new feature, etc)
-#define FC_VERSION_MINOR            4  // increment when a minor release is made (small new feature, change etc)
+#define FC_VERSION_MINOR            5  // increment when a minor release is made (small new feature, change etc)
 #define FC_VERSION_PATCH_LEVEL      0  // increment when a bug is fixed
 
 #define FC_VERSION_STRING STR(FC_VERSION_MAJOR) "." STR(FC_VERSION_MINOR) "." STR(FC_VERSION_PATCH_LEVEL)
@@ -35,10 +35,10 @@ extern const char* const targetName;
 #define GIT_SHORT_REVISION_LENGTH   7 // lower case hexadecimal digits.
 extern const char* const shortGitRevision;
 
-#define BUILD_DATE_LENGTH 11
+#define BUILD_DATE_LENGTH           11
 extern const char* const buildDate;  // "MMM DD YYYY" MMM = Jan/Feb/...
 
-#define BUILD_TIME_LENGTH 8
+#define BUILD_TIME_LENGTH           8
 extern const char* const buildTime;  // "HH:MM:SS"
 
 #define MSP_API_VERSION_STRING STR(API_VERSION_MAJOR) "." STR(API_VERSION_MINOR)
