5ab279aafcfaf4374a9607f366c60ae45e756e95
Dominic Clifton <dominic.clifton@cleanflight.com>
2023-01-30 15:43:39 +0100

    Fix missing CUSTOM_DEFAULTS for H730/H750 targets.

--- src/main/target/STM32H730/target.h
+++ src/main/target/STM32H730/target.h
@@ -107,3 +107,7 @@
 #if !defined(CONFIG_IN_RAM) && !defined(CONFIG_IN_SDCARD) && !defined(CONFIG_IN_EXTERNAL_FLASH)
 #define CONFIG_IN_RAM
 #endif
+
+#ifdef USE_EXST
+#define USE_CUSTOM_DEFAULTS
+#endif
