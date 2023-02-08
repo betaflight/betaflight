cf475e6806c4a668a49f9f13de86080ae5de5ac4
Dominic Clifton <dominic.clifton@cleanflight.com>
2023-02-08 22:59:09 +0100

    Revert "Fix missing CUSTOM_DEFAULTS for H730/H750 targets. (#12281)" (#12334)
    This reverts commit ec20d399c5e7a97b01c80f80ee8987f6179109d9.

--- src/main/target/STM32H730/target.h
+++ src/main/target/STM32H730/target.h
@@ -107,7 +107,3 @@
 #if !defined(CONFIG_IN_RAM) && !defined(CONFIG_IN_SDCARD) && !defined(CONFIG_IN_EXTERNAL_FLASH)
 #define CONFIG_IN_RAM
 #endif
-
-#ifdef USE_EXST
-#define USE_CUSTOM_DEFAULTS
-#endif
