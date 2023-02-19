03da124c743fc359b10938ceeb8b0f1486db0cfe
Ivan Efimov <gendalf44@yandex.ru>
2023-02-19 17:06:38 -0600

    change rpm_limiter defaults for kv and motors

--- src/main/flight/mixer_init.c
+++ src/main/flight/mixer_init.c
@@ -58,8 +58,8 @@ PG_RESET_TEMPLATE(mixerConfig_t, mixerConfig,
     .rpm_limiter_p = 25,
     .rpm_limiter_i = 10,
     .rpm_limiter_d = 8,
-    .rpm_limiter_rpm_limit = 185,
-    .motor_kv = 2070,
+    .rpm_limiter_rpm_limit = 180,
+    .motor_kv = 1960,
 );
 
 PG_REGISTER_ARRAY(motorMixer_t, MAX_SUPPORTED_MOTORS, customMotorMixer, PG_MOTOR_MIXER, 0);
