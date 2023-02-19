59a35a4c6350eee2e3e155e69606e046bb134e43
Ivan Efimov <gendalf44@yandex.ru>
2023-02-19 17:01:15 -0600

    force define USE_RPM_LIMIT

--- src/main/target/common_pre.h
+++ src/main/target/common_pre.h
@@ -40,7 +40,7 @@
     case of common settings for a given MCU group)
 
 */
-
+#define USE_RPM_LIMIT
 #define USE_PARAMETER_GROUPS
 // type conversion warnings.
 // -Wconversion can be turned on to enable the process of eliminating these warnings
