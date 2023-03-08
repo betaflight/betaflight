e57f06e6200636d4d16545eef60fc23d00ae33e5
blckmn <blackman@xtra.com.au>
2023-03-08 16:51:20 +1100

    Removing Brushed ESC auto detect
    - minimise pin "activation" on init.

--- make/source.mk
+++ make/source.mk
@@ -75,7 +75,6 @@ COMMON_SRC = \
             fc/controlrate_profile.c \
             drivers/camera_control.c \
             drivers/accgyro/gyro_sync.c \
-            drivers/pwm_esc_detect.c \
             drivers/rx/rx_spi.c \
             drivers/rx/rx_xn297.c \
             drivers/rx/rx_pwm.c \
--- src/main/pg/motor.c
+++ src/main/pg/motor.c
@@ -26,7 +26,6 @@
 
 #ifdef USE_MOTOR
 
-#include "drivers/pwm_esc_detect.h"
 #include "drivers/pwm_output.h"
 
 #include "pg/pg.h"
@@ -43,19 +42,9 @@ void pgResetFn_motorConfig(motorConfig_t *motorConfig)
     motorConfig->dev.motorPwmProtocol = PWM_TYPE_BRUSHED;
     motorConfig->dev.useUnsyncedPwm = true;
 #else
-#ifdef USE_BRUSHED_ESC_AUTODETECT
-    if (getDetectedMotorType() == MOTOR_BRUSHED) {
-        motorConfig->minthrottle = 1000;
-        motorConfig->dev.motorPwmRate = BRUSHED_MOTORS_PWM_RATE;
-        motorConfig->dev.motorPwmProtocol = PWM_TYPE_BRUSHED;
-        motorConfig->dev.useUnsyncedPwm = true;
-    } else
-#endif // USE_BRUSHED_ESC_AUTODETECT
-    {
-        motorConfig->minthrottle = 1070;
-        motorConfig->dev.motorPwmRate = BRUSHLESS_MOTORS_PWM_RATE;
-        motorConfig->dev.motorPwmProtocol = PWM_TYPE_DISABLED;
-    }
+    motorConfig->minthrottle = 1070;
+    motorConfig->dev.motorPwmRate = BRUSHLESS_MOTORS_PWM_RATE;
+    motorConfig->dev.motorPwmProtocol = PWM_TYPE_DISABLED;
 #endif // BRUSHED_MOTORS
 
     motorConfig->maxthrottle = 2000;
