9dac46f2ecb591d82317414f4e4fa1bd4bd9e4d1
TheIsotopes <theisotopes@byom.de>
2023-03-09 23:22:20 +0100

    FIX CONFIG: MATEKF405TE /MINI

--- src/config/MATEKF405TEMINI/config.h
+++ src/config/MATEKF405TEMINI/config.h
@@ -66,13 +66,10 @@
 #define LED1_PIN             PA13
 #define SPI1_SCK_PIN         PA5
 #define SPI2_SCK_PIN         PB13
-#define SPI3_SCK_PIN         NONE
 #define SPI1_SDI_PIN         PB4
 #define SPI2_SDI_PIN         PC2
-#define SPI3_SDI_PIN         NONE
 #define SPI1_SDO_PIN         PA7
 #define SPI2_SDO_PIN         PC3
-#define SPI3_SDO_PIN         NONE
 #define ADC_VBAT_PIN         PC4
 #define ADC_RSSI_PIN         PB0
 #define ADC_CURR_PIN         PC5
