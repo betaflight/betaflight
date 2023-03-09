0c7061016f4434f8d53798dae26c47be747ca80f
tbolin <tobias_bolin@hotmail.com>
2023-03-09 23:21:14 +0100

    Make dyn idle startup increase configurable (#12432)
    * Make dyn idle startup increase configurable
    
    Replace fixed 5 percent max increase from dynamic idle
    when airmode is active with a configurable value.
    
    -Default value is still 5.0 percent (50 in cli)
    -Add dyn_idle_start_increase cli setting
    -Add dyn_idle_start_increase BB header field
    
    * Increse PG reset to ver 6 for pidProfile
    
    * Indentation
    
    ---------
    
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -119,7 +119,7 @@ PG_RESET_TEMPLATE(pidConfig_t, pidConfig,
 
 #define LAUNCH_CONTROL_YAW_ITERM_LIMIT 50 // yaw iterm windup limit when launch mode is "FULL" (all axes)
 
-PG_REGISTER_ARRAY_WITH_RESET_FN(pidProfile_t, PID_PROFILE_COUNT, pidProfiles, PG_PID_PROFILE, 5);
+PG_REGISTER_ARRAY_WITH_RESET_FN(pidProfile_t, PID_PROFILE_COUNT, pidProfiles, PG_PID_PROFILE, 6);
 
 void resetPidProfile(pidProfile_t *pidProfile)
 {
@@ -199,6 +199,7 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .dyn_idle_i_gain = 50,
         .dyn_idle_d_gain = 50,
         .dyn_idle_max_increase = 150,
+        .dyn_idle_start_increase = 50,
         .feedforward_averaging = FEEDFORWARD_AVERAGING_OFF,
         .feedforward_max_rate_limit = 90,
         .feedforward_smooth_factor = 25,
