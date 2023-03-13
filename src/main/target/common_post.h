53dbad7e1ac111f1fb2ce65a15e80e5f5c80c4ae
Mark Haslinghuis <mark@numloq.nl>
2023-03-13 10:15:32 +0100

    CONFIG: Fix blackbox (#12511)
    * CONFIG: Fix blackbox
    
    * Remove blackbox from MATEKH743

--- src/main/target/common_post.h
+++ src/main/target/common_post.h
@@ -273,6 +273,10 @@
 #undef USE_ADC_INTERNAL
 #endif
 
+#if (defined(USE_SDCARD) || defined(USE_FLASH)) && !defined(USE_BLACKBOX)
+#define USE_BLACKBOX
+#endif
+
 #ifdef USE_FLASH
 #define USE_FLASH_TOOLS
 #define USE_FLASHFS
