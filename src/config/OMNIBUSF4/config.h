02ddc74139787be6595303001c90d3457e487262
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-03-20 16:35:43 -0500

    CONFIG: define W25Q128FV for OMNIBUSF4 (#12547)

--- src/config/OMNIBUSF4/config.h
+++ src/config/OMNIBUSF4/config.h
@@ -33,6 +33,8 @@
 #define USE_GYRO_SPI_MPU6000
 #define USE_ACC_SPI_MPU6000
 #define USE_BARO_BMP280
+#define USE_FLASH
+#define USE_FLASH_W25Q128FV
 #define USE_MAX7456
 
 #define BEEPER_PIN           PB4
