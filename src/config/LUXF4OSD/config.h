a813ea424d9f47a3aaf37f34d816de401f97c197
ot0tot <36753790+ot0tot@users.noreply.github.com>
2023-04-04 12:01:19 -0400

    Add missing LUXF4OSD flash define (#12609)

--- src/config/LUXF4OSD/config.h
+++ src/config/LUXF4OSD/config.h
@@ -33,6 +33,8 @@
 #define USE_GYRO_SPI_MPU6000
 #define USE_ACC_SPI_MPU6000
 #define USE_MAX7456
+#define USE_FLASH
+#define USE_FLASH_W25Q128FV
 
 #define BEEPER_PIN           PB4
 #define MOTOR1_PIN           PB0
