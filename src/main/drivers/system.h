2792d518d1f8a82ec8da4afc40902cbe23c4d2fd
Jan Post <post@stud.tu-darmstadt.de>
2023-04-07 02:16:43 +0200

    Implement Stopwatch (#12623)
    * Implement stopwatch to measure time periods
    
    * Add float version of stopwatchGetMicros()

--- src/main/drivers/system.h
+++ src/main/drivers/system.h
@@ -66,6 +66,7 @@ void systemResetToBootloader(bootloaderRequestType_e requestType);
 bool isMPUSoftReset(void);
 void cycleCounterInit(void);
 int32_t clockCyclesToMicros(int32_t clockCycles);
+float clockCyclesToMicrosf(int32_t clockCycles);
 int32_t clockCyclesTo10thMicros(int32_t clockCycles);
 uint32_t clockMicrosToCycles(uint32_t micros);
 uint32_t getCycleCounter(void);
