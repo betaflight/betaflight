218c2abf9b53ffdd2603ebe6bb75632f986f2525
Steve Evans <Steve@SCEvans.com>
2023-04-24 20:19:03 +0100

    Fix GYRO_1_EXTI_PIN definition

--- src/config/NEUTRONRCF435/config.h
+++ src/config/NEUTRONRCF435/config.h
@@ -19,7 +19,7 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define FC_TARGET_MCU     AT32F435G
+#define FC_TARGET_MCU     AT32F435M
 
 #define BOARD_NAME        NEUTRONRCF435
 #define MANUFACTURER_ID   NERC
@@ -33,10 +33,10 @@
 #define GYRO_1_SPI_INSTANCE  SPI1
 #define GYRO_1_ALIGN         CW270_DEG
 
-// MPU6000 interrupts
+// Gyro interrupts
 #define USE_EXTI
 #define USE_GYRO_EXTI
-#define GYRO_1_EXTI_PIN      PB8
+#define GYRO_1_EXTI_PIN      PA8
 
 #define SPI1_SCK_PIN         PA5
 #define SPI2_SCK_PIN         PB13
