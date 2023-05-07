5910848518d066dd685a5800b247b85a007fa1e9
Eike Ahmels <eike.ahmels@tu-dortmund.de>
2023-05-07 08:09:18 +0200

    start of implement m10 code

--- src/main/io/gps/gps_config.c
+++ src/main/io/gps/gps_config.c
@@ -10,30 +10,8 @@
 
 ubloxVersion_e ubloxVersion = UNDEF;
 
-static void ubloxDetectVersion(serialPort_t *instance) {
+static void ubloxDetectVersion(uint16_t * c) {
     ubloxVersion = M8;
-}
-
-static void ubloxSendMessage(const uint8_t *data, uint8_t len, gpsData_t *gpsData) {
-
-    switch (ubloxVersion)
-    {
-    case M8:
-        uint8_t checksumA = 0, checksumB = 0;
-        serialWrite(gpsPort, data[0]);
-        serialWrite(gpsPort, data[1]);
-        ubloxSendDataUpdateChecksum(&data[2], len - 2, &checksumA, &checksumB);
-        serialWrite(gpsPort, checksumA);
-        serialWrite(gpsPort, checksumB);
-
-        // Save state for ACK waiting
-        gpsData->ackWaitingMsgId = data[3]; //save message id for ACK
-        gpsData->ackTimeoutCounter = 0;
-        gpsData->ackState = UBLOX_ACK_WAITING;
-        break;
-    default:
-        break;
-    }
 }
 
 #endif
--- src/main/io/gps/gps_config.h
+++ src/main/io/gps/gps_config.h
@@ -1,14 +1,10 @@
 #pragma once
 
+#define USE_GPS
+
 #include "platform.h"
 
 #include "io/gps/gps.h"
 
-typedef enum {
-    UNDEF,
-    M8,
-    M10
-} ubloxVersion_e;
 
 ubloxVersion_e ubloxDetectVersion(serialPort_t *instance);
-void sendMessage(const uint8_t *data, uint8_t len);
