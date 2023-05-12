31ea163b8a60073e1e8d675083e4169d90c3bad0
Eike Ahmels <eike.ahmels@tu-dortmund.de>
2023-05-12 11:09:10 +0200

    offload detect to config file

--- src/main/io/gps/gps_config.c
+++ src/main/io/gps/gps_config.c
@@ -1,16 +1,19 @@
 
-#include <ctype.h>
 #include <string.h>
-#include <math.h>
 #include "gps_config.h"
 
 #include "platform.h"
 
 #ifdef USE_GPS
 
-ubloxVersion_e ubloxDetectVersion(uint16_t * c) {
-    UNUSED(c);
-    return UNDEF;
+ubloxVersion_e ubloxDetectVersion(char * c) {
+    ubloxVersion_e ret = UNDEF;
+    if (strcmp(c, "000A0000") == 0) {
+        ret = M10;
+    } else if (strcmp(c, "00800000") == 0) {
+        ret = M8;
+    }
+    return ret;
 }
 
 #endif
