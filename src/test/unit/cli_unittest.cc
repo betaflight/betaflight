a51a36ef6be3d2321fd36ffd81a246720cf77c73
Eike Ahmels <eike.ahmels@tu-dortmund.de>
2023-05-12 15:24:54 +0200

    fix cli_unittest gps include

--- src/test/unit/cli_unittest.cc
+++ src/test/unit/cli_unittest.cc
@@ -39,7 +39,7 @@ extern "C" {
     #include "flight/pid.h"
     #include "flight/servos.h"
     #include "io/beeper.h"
-    #include "io/gps.h"
+    #include "io/gps/gps.h"
     #include "io/ledstrip.h"
     #include "io/serial.h"
     #include "io/vtx.h"
