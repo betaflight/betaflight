ee35b6afe3159535a46dbc7f38e2113342358154
Petr Ledvina <ledvinap@gmail.com>
2023-06-10 09:06:47 +0200

    Update gtest to newer version (#12871)
    Update gtest
    
    Copy of debian/stable libgtest-dev
    
    Co-authored-by: Petr Ledvina <ledvinap@hp124.ekotip.cz>

--- lib/test/gtest/src/gtest_main.cc
+++ lib/test/gtest/src/gtest_main.cc
@@ -27,12 +27,39 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <stdio.h>
+#include <cstdio>
 
 #include "gtest/gtest.h"
 
+#if defined(GTEST_OS_ESP8266) || defined(GTEST_OS_ESP32)
+// Arduino-like platforms: program entry points are setup/loop instead of main.
+
+#ifdef GTEST_OS_ESP8266
+extern "C" {
+#endif
+
+void setup() { testing::InitGoogleTest(); }
+
+void loop() { RUN_ALL_TESTS(); }
+
+#ifdef GTEST_OS_ESP8266
+}
+#endif
+
+#elif defined(GTEST_OS_QURT)
+// QuRT: program entry point is main, but argc/argv are unusable.
+
+GTEST_API_ int main() {
+  printf("Running main() from %s\n", __FILE__);
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
+}
+#else
+// Normal platforms: program entry point is main, argc/argv are initialized.
+
 GTEST_API_ int main(int argc, char **argv) {
-  printf("Running main() from gtest_main.cc\n");
+  printf("Running main() from %s\n", __FILE__);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+#endif
