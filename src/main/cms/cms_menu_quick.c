38e42f11196c5feea7992bf0dd70e82ef8db40e3
Ivan Efimov <gendalf44@yandex.ru>
2023-07-25 17:42:06 -0500

    Changed .c/.h license headers to a modern one

--- src/main/cms/cms_menu_quick.c
+++ src/main/cms/cms_menu_quick.c
@@ -1,26 +1,23 @@
 /*
- * This file is part of Cleanflight and Betaflight.
+ * This file is part of Betaflight.
  *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
  *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this software.
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
  *
  * If not, see <http://www.gnu.org/licenses/>.
  */
-
-//
-// Main menu structure and support functions
-//
 
 #include <stdbool.h>
 #include <string.h>
@@ -50,8 +47,6 @@
 
 #include "cms_menu_quick.h"
 
-// Features
-
 static controlRateConfig_t rateProfile;
 static uint8_t rateProfileIndex;
 static batteryConfig_t batteryProfile;
@@ -71,7 +66,6 @@ static const void *quickMenuOnEnter(displayPort_t *pDisp)
 
     cmsx_motorOutputLimit = pidProfile->motor_output_limit;
 
-    // fill variables
     return NULL;
 }
 
