94e58259220d5a8b6d16ab003ceb4c1d22c9aed0
pichim <michernstpet@gmail.com>
2023-10-04 16:46:35 +0200

    Added define to chirp.c

--- src/main/common/chirp.c
+++ src/main/common/chirp.c
@@ -1,7 +1,30 @@
-#include "chirp.h"
-#include "maths.h"
+/*
+ * This file is part of Cleanflight and Betaflight.
+ *
+ * Cleanflight and Betaflight are free software. You can redistribute
+ * this software and/or modify this software under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Cleanflight and Betaflight are distributed in the hope that they
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <math.h>
+
+#include "platform.h"
+
+#include "maths.h"
+
+#include "chirp.h"
 
 void chirpInit(chirp_t *chirp, const float f0, const float f1, const float t1, const uint32_t looptimeUs)
 {
