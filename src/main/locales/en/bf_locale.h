7fff99873c41940d07a22dd42cc4005568c63103
HThuren <thuren.henrik@gmail.com>
2023-10-28 23:46:00 +0200

    rebase

--- src/main/locales/en/bf_locale.h
+++ src/main/locales/en/bf_locale.h
@@ -109,8 +109,13 @@
 =======
 #define LOCALE                       "en"
 
+#define STR_COMMA	                 ","
+#define STR_PERIOD	                 "."
 #define STR_THOUSAND 	             "."
 #define STR_SECONDS                  "seconds"
+
+#define STR_VISUAL_BEEP              "  * * * *"
+#define STR_MSP_API_NAME             "MSP API:"
 
 // CMS
 #define CMS_STARTUP_HELP_TEXT1       "MENU:THR MID"
