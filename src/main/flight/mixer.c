59dd40e156eacde83ac48c83410b2878709019d5
Tdogb <tdogbeard@gmail.com>
2023-11-14 18:53:22 -0800

    test

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -367,7 +367,7 @@ static void applyRpmLimiter(mixerRuntime_t *mixer)
         mixer->rpmLimiterThrottleScale *= 1.0f + 3.2f * pidGetDT();
     }
     mixer->rpmLimiterThrottleScale = constrainf(mixer->rpmLimiterThrottleScale, 0.01f, 1.0f);
-    float rpm_theoretical_max = (float)getBatteryVoltage() * (float)motorConfig()->kv;
+    float rpm_theoretical_max = (float)getBatteryVoltage() * 0.01f * (float)motorConfig()->kv;
     float rpm_derating = -5.44e-6 * rpm_theoretical_max + 0.944;
     throttle *= constrainf(mixer->rpmLimiterThrottleScale + constrainf(mixer->rpmLimiterRpmLimit / (rpm_theoretical_max * rpm_derating),0.0f, 1.0f) - mixer->rpmLimiterInitialThrottleScale, 0.0f, 1.0f);
     // Output
@@ -377,7 +377,7 @@ static void applyRpmLimiter(mixerRuntime_t *mixer)
 
     DEBUG_SET(DEBUG_RPM_LIMIT, 0, lrintf(averageRpm));
     DEBUG_SET(DEBUG_RPM_LIMIT, 1, lrintf(constrainf(mixer->rpmLimiterRpmLimit / (rpm_theoretical_max * rpm_derating),0.0f, 1.0f)*100.0f));
-    DEBUG_SET(DEBUG_RPM_LIMIT, 2, lrintf( mixer->rpmLimiterInitialThrottleScale * 100.0f));
+    DEBUG_SET(DEBUG_RPM_LIMIT, 2, lrintf(mixer->rpmLimiterInitialThrottleScale * 100.0f));
     DEBUG_SET(DEBUG_RPM_LIMIT, 3, lrintf(throttle * 100.0f));
     DEBUG_SET(DEBUG_RPM_LIMIT, 4, lrintf(error));
     DEBUG_SET(DEBUG_RPM_LIMIT, 5, lrintf(p * 100.0f));
