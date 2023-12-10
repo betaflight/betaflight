6ac08cdd58c1a17673b19c5065ed9fbaef456959
Ivan Efimov <gendalf44@yandex.ru>
2023-12-10 17:40:34 -0600

    Fix: RPM Limit - reset I-term at arm (#13214)
    * RPM Limiter - reset I-term at arm
    
    * Update src/main/flight/mixer.c
    
    Co-authored-by: Jan Post <Rm2k-Freak@web.de>
    
    ---------
    
    Co-authored-by: Jan Post <Rm2k-Freak@web.de>

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -551,6 +551,9 @@ void tryArm(void)
 #ifdef USE_OSD
         osdSuppressStats(false);
 #endif
+#ifdef USE_RPM_LIMIT
+        mixerResetRpmLimiter();
+#endif
         ENABLE_ARMING_FLAG(ARMED);
 
 #ifdef USE_RC_STATS
--- src/main/flight/mixer_init.c
+++ src/main/flight/mixer_init.c
@@ -361,6 +361,7 @@ void mixerInitProfile(void)
     mixerRuntime.rpmLimiterPGain = mixerConfig()->rpm_limit_p * 15e-6f;
     mixerRuntime.rpmLimiterIGain = mixerConfig()->rpm_limit_i * 1e-3f * pidGetDT();
     mixerRuntime.rpmLimiterDGain = mixerConfig()->rpm_limit_d * 3e-7f * pidGetPidFrequency();
+    mixerRuntime.rpmLimiterI = 0.0;
     pt1FilterInit(&mixerRuntime.rpmLimiterAverageRpmFilter, pt1FilterGain(6.0f, pidGetDT()));
     pt1FilterInit(&mixerRuntime.rpmLimiterThrottleScaleOffsetFilter, pt1FilterGain(2.0f, pidGetDT()));
     mixerResetRpmLimiter();
@@ -373,6 +374,7 @@ void mixerInitProfile(void)
 #ifdef USE_RPM_LIMIT
 void mixerResetRpmLimiter(void)
 {
+    mixerRuntime.rpmLimiterI = 0.0;
     mixerRuntime.rpmLimiterThrottleScale = constrainf(mixerRuntime.rpmLimiterRpmLimit / motorEstimateMaxRpm(), 0.0f, 1.0f);
     mixerRuntime.rpmLimiterInitialThrottleScale = mixerRuntime.rpmLimiterThrottleScale;
 }
