4edca26a3bbe15db587492b6a6825d3ee92c6a22
Mark Haslinghuis <mark@numloq.nl>
2023-12-22 07:58:36 +0100

    Remove redundant directives

--- src/main/sensors/gyro_init.c
+++ src/main/sensors/gyro_init.c
@@ -35,63 +35,21 @@
 #include "config/config.h"
 
 #include "drivers/accgyro/accgyro.h"
-#include "drivers/accgyro/accgyro_mpu.h"
-
-#ifdef USE_VIRTUAL_GYRO
 #include "drivers/accgyro/accgyro_virtual.h"
-#endif
-
-#ifdef USE_GYRO_MPU3050
+#include "drivers/accgyro/accgyro_mpu.h"
 #include "drivers/accgyro/accgyro_mpu3050.h"
-#endif
-
-#ifdef USE_GYRO_MPU6050
 #include "drivers/accgyro/accgyro_mpu6050.h"
-#endif
-
-#if defined(USE_GYRO_MPU6500)
 #include "drivers/accgyro/accgyro_mpu6500.h"
-#endif
-
-#ifdef USE_ACCGYRO_BMI160
 #include "drivers/accgyro/accgyro_spi_bmi160.h"
-#endif
-
-#ifdef USE_ACCGYRO_BMI270
 #include "drivers/accgyro/accgyro_spi_bmi270.h"
-#endif
-
-#ifdef USE_GYRO_SPI_ICM20649
 #include "drivers/accgyro/accgyro_spi_icm20649.h"
-#endif
-
-#ifdef USE_GYRO_SPI_ICM20689
 #include "drivers/accgyro/accgyro_spi_icm20689.h"
-#endif
-
-#if defined(USE_GYRO_SPI_ICM42605) || defined(USE_GYRO_SPI_ICM42688P)
 #include "drivers/accgyro/accgyro_spi_icm426xx.h"
-#endif
-
-#ifdef USE_ACCGYRO_LSM6DSO
 #include "drivers/accgyro/accgyro_spi_lsm6dso.h"
-#endif
-
-#ifdef USE_GYRO_SPI_MPU6000
 #include "drivers/accgyro/accgyro_spi_mpu6000.h"
-#endif
-
-#if defined(USE_GYRO_SPI_MPU6500) || defined(USE_GYRO_SPI_MPU9250) || defined(USE_GYRO_SPI_ICM20601) || defined(USE_GYRO_SPI_ICM20602) || defined(USE_GYRO_SPI_ICM20608G)
 #include "drivers/accgyro/accgyro_spi_mpu6500.h"
-#endif
-
-#ifdef USE_GYRO_SPI_MPU9250
 #include "drivers/accgyro/accgyro_spi_mpu9250.h"
-#endif
-
-#ifdef USE_ACCGYRO_LSM6DSV16X
 #include "drivers/accgyro/accgyro_spi_lsm6dsv16x.h"
-#endif
 
 #ifdef USE_GYRO_L3GD20
 #include "drivers/accgyro/accgyro_spi_l3gd20.h"
