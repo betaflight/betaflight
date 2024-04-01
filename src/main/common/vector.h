cd8b6aa5210bcb2907b8c09ca5580ce471b309e1
ctzsnooze <chris.thompson@sydney.edu.au>
2024-04-02 06:42:06 +1100

    Improve IMU responsiveness, refactor (#13373)
    * ensure that division is a float
    
    * change logging, improve logic for recalibration
    
    * Increase sensitivity, smooth center, higher max cogYawGain, renaming
    
    * use non thrust vector maths, revise and add comments
    
    * three factors to reduce ez_ef - speed, thrust vector, stick movement
    
    re-naming and comments
    
    * fix unittest, update comment
    
    * normalize heading magnitude, add pitch and roll suppression factors
    
    * ignore pitch factor on wings
    
    because they 'normally' have forward velocity while flat
    and their path while rolled usually does not change abruptly
    
    * fix unit test
    
    * fix dumb mistake blocking pitch suppression for non-wings
    
    * fixes from review thanks PL
    
    * refactor vector2normalize - thanks to karate
    
    * Use X and Y for 0 and 1 in rMat
    
    * Revert debugs, update comments from review
    
    * lrintf for floats in debugs
    
    * vector.h - implement some vector2* code as vector*
    
    * Refactor IMU code
    
    Move compass and Cog code into separate functions, separate gain
    heuristics
    
    - imuCalcGroundspeedGain
    - imuCalcCourseErr
    - imuCalcMagErr
    
    Temporary refactor od imuDebug_GPS_RESCUE_HEADING, to be removed of optimized
    
    * Simplify simulator build conditional compilation
    
    * Adapt unittests to IMU after refactoring
    
    * Maybe fix the test?
    
    =
    
    * Update src/main/flight/imu.c
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    * Update src/main/flight/imu.c
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    * Update src/main/flight/imu.c
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    * Update src/main/flight/imu.c
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    * Update src/main/flight/imu.c
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    ---------
    
    Co-authored-by: Petr Ledvina <ledvinap@hp124.ekotip.cz>
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>

--- src/main/common/vector.h
+++ src/main/common/vector.h
@@ -109,6 +109,40 @@ static inline fpVector3_t * vectorNormalize(fpVector3_t *result, const fpVector3
     }
 }
 
+
+static inline float vector2NormSquared(const fpVector2_t *a)
+{
+    return sq(a->x) + sq(a->y);
+}
+
+static inline float vector2Norm(const fpVector2_t *a)
+{
+    return sqrtf(vector2NormSquared(a));
+}
+
+
+static inline fpVector2_t * vector2Scale(fpVector2_t *result, const fpVector2_t *a, const float b)
+{
+    fpVector2_t ab;
+
+    ab.x = a->x * b;
+    ab.y = a->y * b;
+
+    *result = ab;
+    return result;
+}
+
+static inline fpVector2_t * vector2Normalize(fpVector2_t *result, const fpVector2_t *v)
+{
+    float normSq = vector2NormSquared(v);
+    if (normSq > 0.0f) {
+        return vector2Scale(result, v, 1.0f / sqrtf(normSq));
+    } else {
+        *result = (fpVector2_t){.x = 0.0f, .y = 0.0f};
+        return result;
+    }
+}
+
 static inline fpVector3_t * matrixVectorMul(fpVector3_t * result, const fpMat33_t * mat, const fpVector3_t * a)
 {
     fpVector3_t r;
@@ -163,7 +197,3 @@ static inline float vector2Dot(const fpVector2_t *a, const fpVector2_t *b)
     return a->x * b->x + a->y * b->y;
 }
 
-static inline float vector2Mag(const fpVector2_t *a)
-{
-    return sqrtf(sq(a->x) + sq(a->y));
-}
