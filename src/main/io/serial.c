c002f84bc05971ae8ac9839a318fe723ec9b61ec
Mark Haslinghuis <mark@numloq.nl>
2024-04-23 19:41:43 +0200

    Cloud Build API does not allow define size > 32

--- src/main/io/serial.c
+++ src/main/io/serial.c
@@ -347,7 +347,7 @@ bool isSerialConfigValid(serialConfig_t *serialConfigToCheck)
             // Ensure MSP or serial RX is not enabled on soft serial ports
             serialConfigToCheck->portConfigs[index].functionMask &= ~(FUNCTION_MSP | FUNCTION_RX_SERIAL);
             // Ensure that the baud rate on soft serial ports is limited to 19200
-#ifndef USE_OVERRIDE_SOFTSERIAL_BAUDRATE_RESTRICTION
+#ifndef USE_OVERRIDE_SOFTSERIAL_BAUDRATE
             serialConfigToCheck->portConfigs[index].gps_baudrateIndex = constrain(portConfig->gps_baudrateIndex, BAUD_AUTO, BAUD_19200);
             serialConfigToCheck->portConfigs[index].blackbox_baudrateIndex = constrain(portConfig->blackbox_baudrateIndex, BAUD_AUTO, BAUD_19200);
             serialConfigToCheck->portConfigs[index].telemetry_baudrateIndex = constrain(portConfig->telemetry_baudrateIndex, BAUD_AUTO, BAUD_19200);
@@ -437,7 +437,7 @@ serialPort_t *openSerialPort(
 
     serialPort_t *serialPort = NULL;
 
-#if defined(USE_SOFTSERIAL) && !defined(USE_OVERRIDE_SOFTSERIAL_BAUDRATE_RESTRICTION)
+#if defined(USE_SOFTSERIAL) && !defined(USE_OVERRIDE_SOFTSERIAL_BAUDRATE)
     if (((identifier == SERIAL_PORT_SOFTSERIAL1) || (identifier == SERIAL_PORT_SOFTSERIAL2)) && (baudRate > 19200)) {
         // Don't continue if baud rate requested is higher then the limit set on soft serial ports
         return NULL;
