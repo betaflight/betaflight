13a064baebbd3c6775d56e267b2dcca4bb7861c0
limonspb <gendalf44@yandex.ru>
2024-05-08 23:23:12 -0500

    Print OSD version on initialization closer to the middle of the screen

--- src/main/osd/osd.c
+++ src/main/osd/osd.c
@@ -494,7 +494,7 @@ static void osdCompleteInitialization(void)
 
     char string_buffer[30];
     tfp_sprintf(string_buffer, "V%s", FC_VERSION_STRING);
-    displayWrite(osdDisplayPort, midCol + 5, midRow, DISPLAYPORT_SEVERITY_NORMAL, string_buffer);
+    displayWrite(osdDisplayPort, midCol, midRow, DISPLAYPORT_SEVERITY_NORMAL, string_buffer);
 #ifdef USE_CMS
     displayWrite(osdDisplayPort, midCol - 8, midRow + 2,  DISPLAYPORT_SEVERITY_NORMAL, CMS_STARTUP_HELP_TEXT1);
     displayWrite(osdDisplayPort, midCol - 4, midRow + 3, DISPLAYPORT_SEVERITY_NORMAL, CMS_STARTUP_HELP_TEXT2);
