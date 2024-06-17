bc00641f3d491f38f56cf9c371903904042ef588
blckmn <blackman@xtra.com.au>
2024-06-17 14:16:35 +1000

    Expilicit source file declartion F7, G4, H7

--- mk/mcu/STM32F7.mk
+++ mk/mcu/STM32F7.mk
@@ -11,82 +11,65 @@ CMSIS_DIR      := $(ROOT)/lib/main/CMSIS
 
 #STDPERIPH
 STDPERIPH_DIR   = $(ROOT)/lib/main/STM32F7/Drivers/STM32F7xx_HAL_Driver
-STDPERIPH_SRC   = $(notdir $(wildcard $(STDPERIPH_DIR)/Src/*.c))
-EXCLUDES        = stm32f7xx_hal_can.c \
-                  stm32f7xx_hal_cec.c \
-                  stm32f7xx_hal_crc.c \
-                  stm32f7xx_hal_crc_ex.c \
-                  stm32f7xx_hal_cryp.c \
-                  stm32f7xx_hal_cryp_ex.c \
-                  stm32f7xx_hal_dcmi.c \
-                  stm32f7xx_hal_dcmi_ex.c \
-                  stm32f7xx_hal_dfsdm.c \
-                  stm32f7xx_hal_dma2d.c \
-                  stm32f7xx_hal_dsi.c \
-                  stm32f7xx_hal_eth.c \
-                  stm32f7xx_hal_hash.c \
-                  stm32f7xx_hal_hash_ex.c \
-                  stm32f7xx_hal_hcd.c \
-                  stm32f7xx_hal_i2s.c \
-                  stm32f7xx_hal_irda.c \
-                  stm32f7xx_hal_iwdg.c \
-                  stm32f7xx_hal_jpeg.c \
-                  stm32f7xx_hal_lptim.c \
-                  stm32f7xx_hal_ltdc.c \
-                  stm32f7xx_hal_ltdc_ex.c \
-                  stm32f7xx_hal_mdios.c \
-                  stm32f7xx_hal_mmc.c \
-                  stm32f7xx_hal_msp_template.c \
-                  stm32f7xx_hal_nand.c \
-                  stm32f7xx_hal_nor.c \
-                  stm32f7xx_hal_qspi.c \
-                  stm32f7xx_hal_rng.c \
-                  stm32f7xx_hal_sai.c \
-                  stm32f7xx_hal_sai_ex.c \
-                  stm32f7xx_hal_sd.c \
-                  stm32f7xx_hal_sdram.c \
-                  stm32f7xx_hal_smartcard.c \
-                  stm32f7xx_hal_smartcard_ex.c \
-                  stm32f7xx_hal_smbus.c \
-                  stm32f7xx_hal_spdifrx.c \
-                  stm32f7xx_hal_sram.c \
-                  stm32f7xx_hal_timebase_rtc_alarm_template.c \
-                  stm32f7xx_hal_timebase_rtc_wakeup_template.c \
-                  stm32f7xx_hal_timebase_tim_template.c \
-                  stm32f7xx_hal_wwdg.c \
-                  stm32f7xx_ll_adc.c \
-                  stm32f7xx_ll_crc.c \
-                  stm32f7xx_ll_dac.c \
-                  stm32f7xx_ll_exti.c \
-                  stm32f7xx_ll_fmc.c \
-                  stm32f7xx_ll_i2c.c \
-                  stm32f7xx_ll_lptim.c \
-                  stm32f7xx_ll_pwr.c \
-                  stm32f7xx_ll_rng.c \
-                  stm32f7xx_ll_rtc.c \
-                  stm32f7xx_ll_sdmmc.c \
-                  stm32f7xx_ll_usart.c
-
-STDPERIPH_SRC   := $(filter-out ${EXCLUDES}, $(STDPERIPH_SRC))
+STDPERIPH_SRC   = \
+            stm32f7xx_hal_adc.c \
+            stm32f7xx_hal_adc_ex.c \
+            stm32f7xx_hal.c \
+            stm32f7xx_hal_cortex.c \
+            stm32f7xx_hal_dac.c \
+            stm32f7xx_hal_dac_ex.c \
+            stm32f7xx_hal_dma.c \
+            stm32f7xx_hal_dma_ex.c \
+            stm32f7xx_hal_exti.c \
+            stm32f7xx_hal_flash.c \
+            stm32f7xx_hal_flash_ex.c \
+            stm32f7xx_hal_gpio.c \
+            stm32f7xx_hal_i2c.c \
+            stm32f7xx_hal_i2c_ex.c \
+            stm32f7xx_hal_pcd.c \
+            stm32f7xx_hal_pcd_ex.c \
+            stm32f7xx_hal_pwr.c \
+            stm32f7xx_hal_pwr_ex.c \
+            stm32f7xx_hal_rcc.c \
+            stm32f7xx_hal_rcc_ex.c \
+            stm32f7xx_hal_rtc.c \
+            stm32f7xx_hal_rtc_ex.c \
+            stm32f7xx_hal_spi.c \
+            stm32f7xx_hal_spi_ex.c \
+            stm32f7xx_hal_tim.c \
+            stm32f7xx_hal_tim_ex.c \
+            stm32f7xx_hal_uart.c \
+            stm32f7xx_hal_uart_ex.c \
+            stm32f7xx_hal_usart.c \
+            stm32f7xx_ll_dma2d.c \
+            stm32f7xx_ll_dma.c \
+            stm32f7xx_ll_gpio.c \
+            stm32f7xx_ll_rcc.c \
+            stm32f7xx_ll_spi.c \
+            stm32f7xx_ll_tim.c \
+            stm32f7xx_ll_usb.c \
+            stm32f7xx_ll_utils.c
 
 #USB
 USBCORE_DIR = $(ROOT)/lib/main/STM32F7/Middlewares/ST/STM32_USB_Device_Library/Core
-USBCORE_SRC = $(notdir $(wildcard $(USBCORE_DIR)/Src/*.c))
-EXCLUDES    = usbd_conf_template.c
-USBCORE_SRC := $(filter-out ${EXCLUDES}, $(USBCORE_SRC))
+USBCORE_SRC = \
+            usbd_core.c \
+            usbd_ctlreq.c \
+            usbd_ioreq.c
 
 USBCDC_DIR = $(ROOT)/lib/main/STM32F7/Middlewares/ST/STM32_USB_Device_Library/Class/CDC
-USBCDC_SRC = $(notdir $(wildcard $(USBCDC_DIR)/Src/*.c))
-EXCLUDES   = usbd_cdc_if_template.c
-USBCDC_SRC := $(filter-out ${EXCLUDES}, $(USBCDC_SRC))
+USBCDC_SRC = usbd_cdc.c
+
 
 USBHID_DIR = $(ROOT)/lib/main/STM32F7/Middlewares/ST/STM32_USB_Device_Library/Class/HID
-USBHID_SRC = $(notdir $(wildcard $(USBHID_DIR)/Src/*.c))
+USBHID_SRC = usbd_hid.c
 
 USBMSC_DIR = $(ROOT)/lib/main/STM32F7/Middlewares/ST/STM32_USB_Device_Library/Class/MSC
-USBMSC_SRC = $(notdir $(wildcard $(USBMSC_DIR)/Src/*.c))
-EXCLUDES   = usbd_msc_storage_template.c
-USBMSC_SRC := $(filter-out ${EXCLUDES}, $(USBMSC_SRC))
+USBMSC_SRC = \
+            usbd_msc_bot.c \
+            usbd_msc.c \
+            usbd_msc_data.c \
+            usbd_msc_scsi.c
 
 VPATH := $(VPATH):$(USBCDC_DIR)/Src:$(USBCORE_DIR)/Src:$(USBHID_DIR)/Src:$(USBMSC_DIR)/Src:$(STDPERIPH_DIR)/src
 
--- mk/mcu/STM32G4.mk
+++ mk/mcu/STM32G4.mk
@@ -11,66 +11,37 @@ CMSIS_DIR      := $(ROOT)/lib/main/CMSIS
 
 #STDPERIPH
 STDPERIPH_DIR   = $(ROOT)/lib/main/STM32G4/Drivers/STM32G4xx_HAL_Driver
-STDPERIPH_SRC   = $(notdir $(wildcard $(STDPERIPH_DIR)/Src/*.c))
-EXCLUDES        = \
-                stm32g4xx_hal_comp.c \
-                stm32g4xx_hal_crc.c \
-                stm32g4xx_hal_crc_ex.c \
-                stm32g4xx_hal_cryp.c \
-                stm32g4xx_hal_cryp_ex.c \
-                stm32g4xx_hal_dac.c \
-                stm32g4xx_hal_dac_ex.c \
-                stm32g4xx_hal_dma_ex.c \
-                stm32g4xx_hal_flash_ramfunc.c \
-                stm33g4xx_hal_fmac.c \
-                stm32g4xx_hal_hrtim.c \
-                stm32g4xx_hal_i2s.c \
-                stm32g4xx_hal_irda.c \
-                stm32g4xx_hal_iwdg.c \
-                stm32g4xx_hal_lptim.c \
-                stm32g4xx_hal_msp_template.c \
-                stm32g4xx_hal_nand.c \
-                stm32g4xx_hal_nor.c \
-                stm32g4xx_hal_opamp.c \
-                stm32g4xx_hal_opamp_ex.c \
-                stm32g4xx_hal_qspi.c \
-                stm32g4xx_hal_rng.c \
-                stm32g4xx_hal_sai.c \
-                stm32g4xx_hal_sai_ex.c \
-                stm32g4xx_hal_smartcard.c \
-                stm32g4xx_hal_smartcard_ex.c \
-                stm32g4xx_hal_smbus.c \
-                stm32g4xx_hal_spi.c \
-                stm32g4xx_hal_spi_ex.c \
-                stm32g4xx_hal_sram.c \
-                stm32g4xx_hal_timebase_tim_template.c \
-                stm32g4xx_hal_usart.c \
-                stm32g4xx_hal_usart_ex.c \
-                stm32g4xx_hal_wwdg.c \
-                stm32g4xx_ll_adc.c \
-                stm32g4xx_ll_comp.c \
-                stm32g4xx_ll_cordic.c \
-                stm32g4xx_ll_crc.c \
-                stm32g4xx_ll_crs.c \
-                stm32g4xx_ll_dac.c \
-                stm32g4xx_ll_exti.c \
-                stm32g4xx_ll_fmac.c \
-                stm32g4xx_ll_fmc.c \
-                stm32g4xx_ll_gpio.c \
-                stm32g4xx_ll_hrtim.c \
-                stm32g4xx_ll_i2c.c \
-                stm32g4xx_ll_lptim.c \
-                stm32g4xx_ll_lpuart.c \
-                stm32g4xx_ll_opamp.c \
-                stm32g4xx_ll_pwr.c \
-                stm32g4xx_ll_rcc.c \
-                stm32g4xx_ll_rng.c \
-                stm32g4xx_ll_rtc.c \
-                stm32g4xx_ll_ucpd.c \
-                stm32g4xx_ll_usart.c \
-                stm32g4xx_ll_utils.c
-
-STDPERIPH_SRC   := $(filter-out ${EXCLUDES}, $(STDPERIPH_SRC))
+STDPERIPH_SRC   = \
+            stm32g4xx_hal_adc.c \
+            stm32g4xx_hal_adc_ex.c \
+            stm32g4xx_hal.c \
+            stm32g4xx_hal_cordic.c \
+            stm32g4xx_hal_cortex.c \
+            stm32g4xx_hal_dma.c \
+            stm32g4xx_hal_exti.c \
+            stm32g4xx_hal_fdcan.c \
+            stm32g4xx_hal_flash.c \
+            stm32g4xx_hal_flash_ex.c \
+            stm32g4xx_hal_fmac.c \
+            stm32g4xx_hal_gpio.c \
+            stm32g4xx_hal_i2c.c \
+            stm32g4xx_hal_i2c_ex.c \
+            stm32g4xx_hal_pcd.c \
+            stm32g4xx_hal_pcd_ex.c \
+            stm32g4xx_hal_pwr.c \
+            stm32g4xx_hal_pwr_ex.c \
+            stm32g4xx_hal_rcc.c \
+            stm32g4xx_hal_rcc_ex.c \
+            stm32g4xx_hal_rtc.c \
+            stm32g4xx_hal_rtc_ex.c \
+            stm32g4xx_hal_tim.c \
+            stm32g4xx_hal_tim_ex.c \
+            stm32g4xx_hal_uart.c \
+            stm32g4xx_hal_uart_ex.c \
+            stm32g4xx_ll_dma.c \
+            stm32g4xx_ll_spi.c \
+            stm32g4xx_ll_tim.c \
+            stm32g4xx_ll_usb.c
 
 #USB
 USBCORE_DIR = $(ROOT)/lib/main/STM32G4/Middlewares/ST/STM32_USB_Device_Library/Core
