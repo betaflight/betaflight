38da0aa701a5df2ca2527e14198e17d620816441
jpmreece <jon@conficio.design>
2024-07-17 12:31:23 +0100

    LED Dimmer
    Build with EXTRA_FLAGS=-DUSE_LED_STRIP
    
    Tested on SpeedyBeeF7V3
    
    Added adjrange to update the ledstrip_brightness variable. This allows for easy changing of LEDs without having to connect to the configurator.

--- src/main/fc/rc_adjustments.c
+++ src/main/fc/rc_adjustments.c
@@ -226,7 +226,11 @@ static const adjustmentConfig_t defaultAdjustmentConfigs[ADJUSTMENT_FUNCTION_COU
         .adjustmentFunction = ADJUSTMENT_LED_PROFILE,
         .mode = ADJUSTMENT_MODE_SELECT,
         .data = { .switchPositions = 3 }
-    }
+    }, {
+		.adjustmentFunction = ADJUSTMENT_LED_DIMMER,
+		.mode = ADJUSTMENT_MODE_SELECT,
+		.data = { .switchPositions = 100 }
+	}
 };
 
 #if defined(USE_OSD) && defined(USE_OSD_ADJUSTMENTS)
@@ -264,6 +268,8 @@ static const char * const adjustmentLabels[] = {
     "ROLL F",
     "YAW F",
     "OSD PROFILE",
+	"LED PROFILE",
+	"LED DIMMER"
 };
 
 static int adjustmentRangeNameIndex = 0;
@@ -643,6 +649,14 @@ static uint8_t applySelectAdjustment(adjustmentFunction_e adjustmentFunction, ui
         }
 #endif
         break;
+	case ADJUSTMENT_LED_DIMMER:
+#ifdef USE_LED_STRIP
+
+		if (getLedBrightness() != position) {
+			setLedBrightness(position);
+		}
+#endif
+		break;
 
     default:
         break;
