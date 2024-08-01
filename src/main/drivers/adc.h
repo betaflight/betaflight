cf3605b4113947026f97744f0c6cb5c81f6b5904
MorroMaker <linwei.idea@outlook.com>
2024-08-01 09:50:32 +0800

    Implement ADC on the APM32

--- src/main/drivers/adc.h
+++ src/main/drivers/adc.h
@@ -29,7 +29,7 @@
 #define ADC_INSTANCE                ADC1
 #endif
 
-#if defined(STM32F4) || defined(STM32F7)
+#if defined(STM32F4) || defined(STM32F7) || defined(APM32F4)
 #ifndef ADC1_DMA_STREAM
 #define ADC1_DMA_STREAM DMA2_Stream4 // ST0 or ST4
 #endif
