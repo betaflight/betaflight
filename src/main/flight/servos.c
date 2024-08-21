377ab9a50fb2f3b2850e49d0be9a4af5472a2a45
Mark Haslinghuis <mark@numloq.nl>
2024-08-21 13:49:01 +0200

    Half solution

--- src/main/flight/servos.c
+++ src/main/flight/servos.c
@@ -209,7 +209,11 @@ int16_t determineServoMiddleOrForwardFromChannel(servoIndex_e servoIndex)
     const uint8_t channelToForwardFrom = servoParams(servoIndex)->forwardFromChannel;
 
     if (channelToForwardFrom != CHANNEL_FORWARDING_DISABLED && channelToForwardFrom < rxRuntimeState.channelCount) {
+#ifdef USE_SERVO180
+        return scaleRangef(constrainf(rcData[channelToForwardFrom], PWM_RANGE_MIN, PWM_RANGE_MAX), PWM_RANGE_MIN, PWM_RANGE_MAX, servoParams(servoIndex)->min, servoParams(servoIndex)->max);
+#else
         return rcData[channelToForwardFrom];
+#endif
     }
 
     return servoParams(servoIndex)->middle;
