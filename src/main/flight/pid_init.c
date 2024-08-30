43f977828fe7a5cd4603d3e3e492c43ef99706b6
ctzsnooze <chris.thompson@sydney.edu.au>
2024-08-30 23:10:01 +1000

    Fix angle mode rate profile change bug (#13862)
    fix angle mode rate profile change bug

--- src/main/flight/pid_init.c
+++ src/main/flight/pid_init.c
@@ -251,7 +251,6 @@ void pidInitFilters(const pidProfile_t *pidProfile)
     for (int axis = 0; axis < 2; axis++) {  // ROLL and PITCH only
         pt3FilterInit(&pidRuntime.attitudeFilter[axis], k);
         pt3FilterInit(&pidRuntime.angleFeedforwardPt3[axis], k2);
-        pidRuntime.maxRcRateInv[axis] = 1.0f / getMaxRcRate(axis);
     }
     pidRuntime.angleYawSetpoint = 0.0f;
 #endif
