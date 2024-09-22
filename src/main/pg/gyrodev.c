e96dbdd8524f94d2fe2b06ab0da42acd1d32e317
Vitaliy Nimych <vitaliy.nimych@gmail.com>
2024-09-22 11:27:38 +0300

    refactor gyro reset function

--- src/main/pg/gyrodev.c
+++ src/main/pg/gyrodev.c
@@ -72,7 +72,7 @@
 #endif
 
 #if defined(USE_SPI_GYRO) && (defined(GYRO_1_SPI_INSTANCE) || defined(GYRO_2_SPI_INSTANCE))
-static void gyroResetSpiDeviceConfig(gyroDeviceConfig_t *devconf, SPI_TypeDef *instance, ioTag_t csnTag, ioTag_t extiTag, uint8_t alignment, sensorAlignment_t customAlignment)
+static void gyroResetSpiDeviceConfig(gyroDeviceConfig_t *devconf, SPI_TypeDef *instance, ioTag_t csnTag, ioTag_t extiTag, ioTag_t clkInTag, uint8_t alignment, sensorAlignment_t customAlignment)
 {
     devconf->busType = BUS_TYPE_SPI;
     devconf->spiBus = SPI_DEV_TO_CFG(spiDeviceByInstance(instance));
@@ -80,7 +80,7 @@ static void gyroResetSpiDeviceConfig(gyroDeviceConfig_t *devconf, SPI_TypeDef *i
     devconf->extiTag = extiTag;
     devconf->alignment = alignment;
     devconf->customAlignment = customAlignment;
-    devconf->clkIn = IO_TAG(GYRO_CLKIN_PIN);
+    devconf->clkIn = clkInTag;
 }
 #endif
 
@@ -111,7 +111,7 @@ void pgResetFn_gyroDeviceConfig(gyroDeviceConfig_t *devconf)
     // All multi-gyro boards use SPI based gyros.
 #ifdef USE_SPI_GYRO
 #ifdef GYRO_1_SPI_INSTANCE
-    gyroResetSpiDeviceConfig(&devconf[0], GYRO_1_SPI_INSTANCE, IO_TAG(GYRO_1_CS_PIN), IO_TAG(GYRO_1_EXTI_PIN), GYRO_1_ALIGN, customAlignment1);
+    gyroResetSpiDeviceConfig(&devconf[0], GYRO_1_SPI_INSTANCE, IO_TAG(GYRO_1_CS_PIN), IO_TAG(GYRO_1_EXTI_PIN), IO_TAG(GYRO_CLKIN_PIN), GYRO_1_ALIGN, customAlignment1);
 #else
     devconf[0].busType = BUS_TYPE_NONE;
 #endif
@@ -125,7 +125,7 @@ void pgResetFn_gyroDeviceConfig(gyroDeviceConfig_t *devconf)
 #endif // GYRO_2_CUSTOM_ALIGN
 
 #ifdef GYRO_2_SPI_INSTANCE
-    gyroResetSpiDeviceConfig(&devconf[1], GYRO_2_SPI_INSTANCE, IO_TAG(GYRO_2_CS_PIN), IO_TAG(GYRO_2_EXTI_PIN), GYRO_2_ALIGN, customAlignment2);
+    gyroResetSpiDeviceConfig(&devconf[1], GYRO_2_SPI_INSTANCE, IO_TAG(GYRO_2_CS_PIN), IO_TAG(GYRO_2_EXTI_PIN), IO_TAG(GYRO_CLKIN_PIN), GYRO_2_ALIGN, customAlignment2);
 #else
     devconf[1].busType = BUS_TYPE_NONE;
 #endif
