8fb8389ebc918ae129b5b32b6ff7b4cd4fc91926
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-10-31 20:58:04 +0100

    kalman filter  sensor fusion added to altitude estimation

---
+++ src/main/flight/kalman_filter_1d.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Betaflight. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "platform.h"
+#include "math.h"
+
+
+#include "kalman_filter_1d.h"
+
+void kf_init(KalmanFilter *kf, float initialValue, float initialVariance, float processVariance) {
+    kf->estimatedValue = initialValue;
+    kf->estimatedVariance = initialVariance;
+    kf->processVariance = processVariance;
+}
+
+void kf_update_variance(KalmanFilter *kf) {
+    // Increase the estimated variance by the process variance
+    kf->estimatedVariance += kf->processVariance;
+}
+
+void kf_update(KalmanFilter *kf, SensorMeasurement sensorMeas) {
+    if (sensorMeas.value == -1) {
+        return; // Skip update if measurement is invalid
+    }
+
+    float kalmanGain = kf->estimatedVariance / (kf->estimatedVariance + sensorMeas.variance);
+
+    kf->estimatedValue += kalmanGain * (sensorMeas.value - kf->estimatedValue);
+
+    kf->estimatedVariance = (1 - kalmanGain) * kf->estimatedVariance;
+}
+
+#ifdef USE_BARO
+void updateBaroVariance(SensorMeasurement * sensorMeas) {
+    static uint16_t n = 0;
+    if (n == 0) {
+        sensorMeas->variance = 0.0f;
+    }
+    sensorMeas->variance = (sensorMeas->variance * n + powf(sensorMeas->value, 2)) / (n + 1);
+    n++;
+}
+#endif
