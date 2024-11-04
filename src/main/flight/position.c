1a8af2ce300729d66d19e341e3622303b1bf374c
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-11-05 00:50:45 +0100

    include the 0 range finder value

--- src/main/flight/position.c
+++ src/main/flight/position.c
@@ -134,7 +134,7 @@ void calculateEstimatedAltitude(void) {
 #ifdef USE_RANGEFINDER
     static SensorMeasurement rangefinderMeasurement = { .value = 0.0f, .variance = 1.0f };  // todo: rangefinder init variance
     rangefinderMeasurement.value = (float)rangefinderGetLatestAltitude();
-    haveRangefinderAlt = rangefinderIsHealthy() && rangefinderMeasurement.value > 0; // altitude 0 from the rangefinder is invalid
+    haveRangefinderAlt = rangefinderIsHealthy() && rangefinderMeasurement.value >= 0;
     static float rangefinderOffsetCm = 0.0f;
 #endif
 
