6930d97104198280f3141680338d913c834df7f4
ctzsnooze <chris@th.id.au>
2024-11-30 09:39:35 +1100

    fix wrapping when 180 lon meridian is crossed

--- src/main/io/gps.c
+++ src/main/io/gps.c
@@ -2597,11 +2597,14 @@ void GPS_calculateDistanceAndDirectionToHome(void)
 // note that parameter order is from, to
 void GPS_distance2d(const gpsLocation_t *from, const gpsLocation_t *to, vector2_t *distance)
 {
-    float deltaLon = (float)(to->lon - from->lon); // In case we crossed the 180° meridian
-    if (deltaLon > 180.0f) {
-        deltaLon -= 360.0f;
-    } else if (deltaLon < -180.0f) {
-        deltaLon += 360.0f;
+    int32_t deltaLon = to->lon - from->lon; // In case we crossed the 180° meridian
+    const int32_t deg180 = 180 * GPS_DEGREES_DIVIDER; // number of integer longitude steps in 180 degrees
+    if (deltaLon > deg180) {
+        deltaLon -= deg180;  // 360 * GPS_DEGREES_DIVIDER overflows int32_t, so use 180 twice
+        deltaLon -= deg180;
+    } else if (deltaLon <= -deg180) {
+        deltaLon += deg180;
+        deltaLon += deg180;
     }
     distance->x = deltaLon * GPS_cosLat * EARTH_ANGLE_TO_CM; // East-West distance, positive East
     distance->y = (float)(to->lat - from->lat) * EARTH_ANGLE_TO_CM;  // North-South distance, positive North
