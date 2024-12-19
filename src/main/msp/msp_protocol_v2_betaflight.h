01d4a424791ffcb74d32d051bfce6506e6b2b1e1
Petr Ledvina <ledvinap@gmail.com>
2024-12-19 18:54:03 +0100

    MSP - fix comment

--- src/main/msp/msp_protocol_v2_betaflight.h
+++ src/main/msp/msp_protocol_v2_betaflight.h
@@ -39,4 +39,4 @@
 #define MSP2TEXT_RELEASENAME                     6
 #define MSP2TEXT_CUSTOM_MSG_0                    7  // CUSTOM_MSG_MAX_NUM  entries are allocated
 #define CUSTOM_MSG_MAX_NUM                       4
-// next new variable type must be >= MSP2TEXT_CUSTOM_MSG_0 + CUSTOM_MSG_MAX_NUM 
+// next new variable type must be >= MSP2TEXT_CUSTOM_MSG_0 + CUSTOM_MSG_MAX_NUM (11)
