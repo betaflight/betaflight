38928085f26d7e0d1d5825100b69b2fe68bff004
Mark Haslinghuis <mark@numloq.nl>
2024-12-31 20:06:39 +0100

    Adds missing ACCGYRO defines and remove legacy drivers (#14087)

--- src/main/cli/settings.c
+++ src/main/cli/settings.c
@@ -143,16 +143,47 @@
 // Sensor names (used in lookup tables for *_hardware settings and in status command output)
 // sync with accelerationSensor_e
 const char * const lookupTableAccHardware[] = {
-    "AUTO", "NONE", "ADXL345", "MPU6050", "MMA8452", "BMA280", "LSM303DLHC",
-    "MPU6000", "MPU6500", "MPU9250", "ICM20601", "ICM20602", "ICM20608G", "ICM20649", "ICM20689", "ICM42605", "ICM42688P",
-    "BMI160", "BMI270", "LSM6DSO", "LSM6DSV16X", "VIRTUAL"
+    "AUTO",
+    "NONE",
+    "MPU6050",
+    "MPU6000",
+    "MPU6500",
+    "MPU9250",
+    "ICM20601",
+    "ICM20602",
+    "ICM20608G",
+    "ICM20649",
+    "ICM20689",
+    "ICM42605",
+    "ICM42688P",
+    "BMI160",
+    "BMI270",
+    "LSM6DSO",
+    "LSM6DSV16X",
+    "VIRTUAL"
 };
 
 // sync with gyroHardware_e
 const char * const lookupTableGyroHardware[] = {
-    "AUTO", "NONE", "MPU6050", "L3G4200D", "MPU3050", "L3GD20",
-    "MPU6000", "MPU6500", "MPU9250", "ICM20601", "ICM20602", "ICM20608G", "ICM20649", "ICM20689", "ICM42605", "ICM42688P",
-    "BMI160", "BMI270", "LSM6DSO", "LSM6DSV16X", "VIRTUAL"
+    "AUTO",
+    "NONE",
+    "MPU6050",
+    "L3GD20",
+    "MPU6000",
+    "MPU6500",
+    "MPU9250",
+    "ICM20601",
+    "ICM20602",
+    "ICM20608G",
+    "ICM20649",
+    "ICM20689",
+    "ICM42605",
+    "ICM42688P",
+    "BMI160",
+    "BMI270",
+    "LSM6DSO",
+    "LSM6DSV16X",
+    "VIRTUAL"
 };
 
 #if defined(USE_SENSOR_NAMES) || defined(USE_BARO)
