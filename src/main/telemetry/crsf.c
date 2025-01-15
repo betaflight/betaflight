668895b50038bb88a73281c21c0c98bec7c716db
Mark Haslinghuis <mark@numloq.nl>
2025-01-15 23:07:25 +0100

    Flip FM priority for ALTH / POSH

--- src/main/telemetry/crsf.c
+++ src/main/telemetry/crsf.c
@@ -457,10 +457,10 @@ void crsfFrameFlightMode(sbuf_t *dst)
         flightMode = "PASS";
     } else if (FLIGHT_MODE(ANGLE_MODE)) {
         flightMode = "ANGL";
-    } else if (FLIGHT_MODE(ALT_HOLD_MODE)) {
-        flightMode = "ALTH";
     } else if (FLIGHT_MODE(POS_HOLD_MODE)) {
         flightMode = "POSH";
+    } else if (FLIGHT_MODE(ALT_HOLD_MODE)) {
+        flightMode = "ALTH";
     } else if (FLIGHT_MODE(HORIZON_MODE)) {
         flightMode = "HOR";
     } else if (isAirmodeEnabled()) {
