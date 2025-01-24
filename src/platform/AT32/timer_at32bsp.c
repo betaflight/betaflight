6dd0ae79f7de09aa428ac72836cb5357cd0c1536
ctzsnooze <chris@th.id.au>
2025-01-24 21:28:28 +0100

    fix copy and paste errors

--- src/platform/AT32/timer_at32bsp.c
+++ src/platform/AT32/timer_at32bsp.c
@@ -677,7 +677,7 @@ void timerInit(void)
     memset(timerConfig, 0, sizeof(timerConfig));
 
 #if defined(PARTIAL_REMAP_TIM3)
-    GPIO_PinRemautopilotConfig(GPIO_PartialRemap_TIM3, ENABLE);
+    GPIO_PinRemapConfig(GPIO_PartialRemap_TIM3, ENABLE);
 #endif
 
     #ifdef USE_TIMER_MGMT
