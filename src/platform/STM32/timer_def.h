9ab3aa1048d1f9baad28843c7924559ae8c3ba4c
Jacob Dahl <37091262+dakejahl@users.noreply.github.com>
2025-01-27 06:34:29 -0900

    h7: add timer pins (#14153)

--- src/platform/STM32/timer_def.h
+++ src/platform/STM32/timer_def.h
@@ -747,6 +747,11 @@
 #define DEF_TIM_AF__PB14__TCH_TIM12_CH1   D(2, 12)
 #define DEF_TIM_AF__PB15__TCH_TIM12_CH2   D(2, 12)
 
+#define DEF_TIM_AF__PB0__TCH_TIM8_CH2N    D(3, 8)
+#define DEF_TIM_AF__PB1__TCH_TIM8_CH3N    D(3, 8)
+#define DEF_TIM_AF__PB14__TCH_TIM8_CH2N   D(3, 8)
+#define DEF_TIM_AF__PB15__TCH_TIM8_CH3N   D(3, 8)
+
 //PORTC
 #define DEF_TIM_AF__PC6__TCH_TIM3_CH1     D(2, 3)
 #define DEF_TIM_AF__PC7__TCH_TIM3_CH2     D(2, 3)
@@ -1142,7 +1147,7 @@
 
 #elif defined(STM32H7)
 
-#define FULL_TIMER_CHANNEL_COUNT 87
+#define FULL_TIMER_CHANNEL_COUNT 91
 #define USED_TIMERS ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(6) | TIM_N(7) | TIM_N(8) | TIM_N(12) | TIM_N(13) | TIM_N(14) | TIM_N(15) | TIM_N(16) | TIM_N(17) )
 #define HARDWARE_TIMER_DEFINITION_COUNT 17
 #define TIMUP_TIMERS ( BIT(1) | BIT(2) | BIT(3) | BIT(4) | BIT(5) | BIT(6) | BIT(7) | BIT(8) | BIT(15) | BIT(16) | BIT(17) )
