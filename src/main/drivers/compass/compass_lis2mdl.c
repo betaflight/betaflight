75945a65c6803c5cc4a7e1f85d5edd8095a5b1b4
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-01-31 12:37:10 -0900

    fix typo

--- src/main/drivers/compass/compass_lis2mdl.c
+++ src/main/drivers/compass/compass_lis2mdl.c
@@ -84,7 +84,6 @@ static bool lis2mdlRead(magDev_t *mag, int16_t *magData)
 
     if (pendingRead && busReadRegisterBufferStart(dev, LIS2MDL_ADDR_OUTX_L_REG, (uint8_t *)buf, sizeof(buf))) {
         pendingRead = false;
-    }
         return false;
     }
 
