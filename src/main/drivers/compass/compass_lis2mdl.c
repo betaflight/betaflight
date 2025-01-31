7b80d194b43aa6ec7cd8bcf9de4c4f95ae84c95a
Jacob Dahl <37091262+dakejahl@users.noreply.github.com>
2025-01-31 12:36:11 -0900

    Update src/main/drivers/compass/compass_lis2mdl.c
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- src/main/drivers/compass/compass_lis2mdl.c
+++ src/main/drivers/compass/compass_lis2mdl.c
@@ -82,10 +82,9 @@ static bool lis2mdlRead(magDev_t *mag, int16_t *magData)
 
     extDevice_t *dev = &mag->dev;
 
-    if (pendingRead) {
-        if (busReadRegisterBufferStart(dev, LIS2MDL_ADDR_OUTX_L_REG, (uint8_t *)buf, sizeof(buf))) {
-            pendingRead = false;
-        }
+    if (pendingRead && busReadRegisterBufferStart(dev, LIS2MDL_ADDR_OUTX_L_REG, (uint8_t *)buf, sizeof(buf))) {
+        pendingRead = false;
+    }
         return false;
     }
 
