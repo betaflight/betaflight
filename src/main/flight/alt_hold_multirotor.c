70b2145822313c13aa2fdb0739613f2e387ab033
ctzsnooze <chris@th.id.au>
2025-02-11 12:26:21 +0400

    whitespace and return
    Co-Authored-By: MikeNomatter <15355893+mituritsyn@users.noreply.github.com>

--- src/main/flight/alt_hold_multirotor.c
+++ src/main/flight/alt_hold_multirotor.c
@@ -93,8 +93,8 @@ static void altHoldProcessTransitions(void) {
 static void altHoldUpdateTargetAltitude(void)
 {
     // User can adjust the target altitude with throttle, but only when
-    // - throttle is outside deadband, and 
-    // - throttle is not low (zero), and 
+    // - throttle is outside deadband, and
+    // - throttle is not low (zero), and
     // - deadband is not configured to zero
 
     float stickFactor = 0.0f;
