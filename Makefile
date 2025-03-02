f4c9c125d00d4075644f81563dda7d3921d299b3
Thisas <thisas@protonmail.com>
2025-03-02 11:31:47 +0400

    move macos m1 SITL fix into SITL.mk and extract warnings flags from cflags into WARNING_FLAGS varible

--- Makefile
+++ Makefile
@@ -173,14 +173,6 @@ OPTIMISE_DEFAULT      := -O2
 OPTIMISE_SPEED        := -Ofast
 OPTIMISE_SIZE         := -Os
 
-# MacOS arm64 does not support -fuse-linker-plugin
-ifeq ($(shell uname -s),Darwin)
-  ifeq ($(shell uname -m),arm64)
-    # Remove -fuse-linker-plugin for macOS arm64
-    OPTIMISATION_BASE := -flto=auto -ffast-math -fmerge-all-constants
-  endif
-endif
-
 LTO_FLAGS             := $(OPTIMISATION_BASE) $(OPTIMISE_SPEED)
 endif
 
@@ -294,6 +286,7 @@ CC_NO_OPTIMISATION      :=
 #
 TEMPORARY_FLAGS :=
 
+WARNING_FLAGS := -Wall -Wextra -Werror -Wunsafe-loop-optimizations -Wdouble-promotion
 EXTRA_WARNING_FLAGS := -Wold-style-definition
 
 CFLAGS     += $(ARCH_FLAGS) \
@@ -301,7 +294,7 @@ CFLAGS     += $(ARCH_FLAGS) \
               $(addprefix -I,$(INCLUDE_DIRS)) \
               $(DEBUG_FLAGS) \
               -std=gnu17 \
-              -Wall -Wextra -Werror -Wunsafe-loop-optimizations -Wdouble-promotion \
+              $(WARNING_FLAGS) \
               $(EXTRA_WARNING_FLAGS) \
               -ffunction-sections \
               -fdata-sections \
@@ -318,34 +311,6 @@ CFLAGS     += $(ARCH_FLAGS) \
               -pipe \
               -MMD -MP \
               $(EXTRA_FLAGS)
-
-# MacOS arm64 specific flags
-ifeq ($(shell uname -s),Darwin)
-	ifeq ($(shell uname -m),arm64)
-	    CFLAGS := $(ARCH_FLAGS) \
-	            $(addprefix -D,$(OPTIONS)) \
-	            $(addprefix -I,$(INCLUDE_DIRS)) \
-	            $(DEBUG_FLAGS) \
-	            -std=gnu17 \
-	            -Wall -Wextra -Werror -Wdouble-promotion -Wno-error=double-promotion -Wno-error=parentheses-equality -Wno-error=unknown-pragmas -Wno-error=unneeded-internal-declaration \
-	            $(EXTRA_WARNING_FLAGS) \
-	            -ffunction-sections \
-	            -fdata-sections \
-	            -fno-common \
-	            $(TEMPORARY_FLAGS) \
-	            $(DEVICE_FLAGS) \
-	            -D_GNU_SOURCE \
-	            -D$(TARGET) \
-	            $(TARGET_FLAGS) \
-	            -D'__FORKNAME__="$(FORKNAME)"' \
-	            -D'__TARGET__="$(TARGET)"' \
-	            -D'__REVISION__="$(REVISION)"' \
-	            $(CONFIG_REVISION_DEFINE) \
-	            -pipe \
-	            -MMD -MP \
-	            $(EXTRA_FLAGS)
-	endif
-endif
 
 ASFLAGS     = $(ARCH_FLAGS) \
               $(DEBUG_FLAGS) \
--- src/platform/SIMULATOR/mk/SITL.mk
+++ src/platform/SIMULATOR/mk/SITL.mk
@@ -1,3 +1,4 @@
+
 INCLUDE_DIRS := \
         $(INCLUDE_DIRS) \
         $(TARGET_PLATFORM_DIR) \
@@ -9,7 +10,7 @@ MCU_COMMON_SRC  := \
         SIMULATOR/sitl.c \
         SIMULATOR/udplink.c
 
-# Flags
+#Flags
 ARCH_FLAGS      =
 DEVICE_FLAGS    =
 LD_SCRIPT       = $(LINKER_DIR)/sitl.ld
@@ -29,11 +30,38 @@ MCU_EXCLUDES = \
 
 TARGET_MAP  = $(OBJECT_DIR)/$(FORKNAME)_$(TARGET).map
 
-# Choose LD_FLAGS based on the operating system and architecture.
-ifeq ($(shell uname -s),Darwin)
-  ifeq ($(shell uname -m),arm64)
-    # Apple Silicon Mac
-    LD_FLAGS := \
+LD_FLAGS    := \
+            -lm \
+            -lpthread \
+            -lc \
+            -lrt \
+            $(ARCH_FLAGS) \
+            $(LTO_FLAGS) \
+            $(DEBUG_FLAGS) \
+            -Wl,-gc-sections,-Map,$(TARGET_MAP) \
+            -Wl,-L$(LINKER_DIR) \
+            -Wl,--cref \
+            -T$(LD_SCRIPT)
+
+ifneq ($(filter SITL_STATIC,$(OPTIONS)),)
+LD_FLAGS     += \
+              -static \
+              -static-libgcc
+endif
+
+ifneq ($(DEBUG),GDB)
+OPTIMISE_DEFAULT    := -Ofast
+OPTIMISE_SPEED      := -Ofast
+OPTIMISE_SIZE       := -Os
+
+LTO_FLAGS           := $(OPTIMISATION_BASE) $(OPTIMISE_SPEED)
+endif
+
+ifeq ($(OSFAMILY),macosx)
+  ifneq ($(findstring arm,$(ARCHFAMILY)),)
+    override OPTIMISATION_BASE := $(filter-out -fuse-linker-plugin,$(OPTIMISATION_BASE))
+    override WARNING_FLAGS := $(filter-out -Werror -Wunsafe-loop-optimizations,$(WARNING_FLAGS))
+    override LD_FLAGS := \
             -lm \
             -lpthread \
             -lc \
@@ -41,47 +69,14 @@ ifeq ($(shell uname -s),Darwin)
             $(LTO_FLAGS) \
             $(DEBUG_FLAGS) \
             -Wl,-map,$(TARGET_MAP)
-  else
-    # Intel Mac
-    LD_FLAGS := \
-            -lm \
-            -lpthread \
-            -lc \
-            -lrt \
-            $(ARCH_FLAGS) \
-            $(LTO_FLAGS) \
-            $(DEBUG_FLAGS) \
-            -Wl,-gc-sections,-Map,$(TARGET_MAP) \
-            -Wl,-L$(LINKER_DIR) \
-            -Wl,--cref \
-            -T$(LD_SCRIPT)
+    OBJCOPY_EXISTS := $(shell command -v $(OBJCOPY) 2>/dev/null)
+    ifeq ($(OBJCOPY_EXISTS),)
+      BREW_LLVM_PREFIX := $(shell brew --prefix llvm 2>/dev/null)
+      ifneq ($(BREW_LLVM_PREFIX),)
+        override OBJCOPY := $(BREW_LLVM_PREFIX)/bin/llvm-objcopy
+      else
+        $(error OBJCOPY is not found, please install llvm and set PATH or install llvm through homebrew)
+      endif
+    endif
   endif
-else
-  # Linux
-  LD_FLAGS := \
-            -lm \
-            -lpthread \
-            -lc \
-            -lrt \
-            $(ARCH_FLAGS) \
-            $(LTO_FLAGS) \
-            $(DEBUG_FLAGS) \
-            -Wl,-gc-sections,-Map,$(TARGET_MAP) \
-            -Wl,-L$(LINKER_DIR) \
-            -Wl,--cref \
-            -T$(LD_SCRIPT)
-endif
-
-ifneq ($(filter SITL_STATIC,$(OPTIONS)),)
-LD_FLAGS += \
-          -static \
-          -static-libgcc
-endif
-
-ifneq ($(DEBUG),GDB)
-OPTIMISE_DEFAULT := -Ofast
-OPTIMISE_SPEED   := -Ofast
-OPTIMISE_SIZE    := -Os
-
-LTO_FLAGS        := $(OPTIMISATION_BASE) $(OPTIMISE_SPEED)
 endif
