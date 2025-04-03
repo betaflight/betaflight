c0092013c99525c4ddcae3779a5578feea2b8fa0
RubberQuads <128827603+RubberQuads@users.noreply.github.com>
2025-04-03 08:44:33 +0200

    Update src/main/pg/alt_limit.c
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- src/main/pg/alt_limit.c
+++ src/main/pg/alt_limit.c
@@ -19,25 +19,22 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
- #include "platform.h"
+#include "platform.h"
 
- 
- #ifdef USE_ALTITUDE_LIMIT
- 
- #include "flight/alt_limit.h"
- 
- #include "pg/pg.h"
- #include "pg/pg_ids.h"
- 
- #include "alt_limit.h"
+#ifdef USE_ALTITUDE_LIMIT
 
+#include "flight/alt_limit.h"
 
- PG_REGISTER_WITH_RESET_TEMPLATE(altLimitConfig_t, altLimitConfig, PG_ALTLIMIT_CONFIG, 0);
- 
- PG_RESET_TEMPLATE(altLimitConfig_t, altLimitConfig,
-     .ceiling = 120, // maximum allowed altitude in meters (above starting altitude)
-     .buffer = 15, // hight under ceiling where throttle will be scaled down 
-     .active = false, // set to true to enforce altitude limit
- );
- #endif
- 
+#include "pg/pg.h"
+#include "pg/pg_ids.h"
+
+#include "alt_limit.h"
+
+PG_REGISTER_WITH_RESET_TEMPLATE(altLimitConfig_t, altLimitConfig, PG_ALTLIMIT_CONFIG, 0);
+
+PG_RESET_TEMPLATE(altLimitConfig_t, altLimitConfig,
+    .ceiling = 120, // maximum allowed altitude in meters (above starting altitude)
+    .buffer = 15, // hight under ceiling where throttle will be scaled down 
+    .active = false, // set to true to enforce altitude limit
+);
+#endif
