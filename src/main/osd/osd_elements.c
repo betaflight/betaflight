eb7814d1970dc791a5d821cb40c9e07c7e00acc0
Mark Haslinghuis <mark@numloq.nl>
2025-04-23 12:04:19 +0200

    Add variant for RTC OSD element (#14344)
    * Add variant for RTC OSD element
    
    * Update src/main/osd/osd_elements.c
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    * Be more explicit :)
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    ---------
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>

--- src/main/osd/osd_elements.c
+++ src/main/osd/osd_elements.c
@@ -473,7 +473,15 @@ bool osdFormatRtcDateTime(char *buffer)
         return false;
     }
 
-    dateTimeFormatLocalShort(buffer, &dateTime);
+    switch (activeElement.type) {
+    case OSD_ELEMENT_TYPE_2:
+        tfp_sprintf(buffer, "%02d.%02d %02d:%02d", dateTime.month, dateTime.day, dateTime.hours, dateTime.minutes);
+        break;
+    case OSD_ELEMENT_TYPE_1:
+    default:
+        dateTimeFormatLocalShort(buffer, &dateTime);
+        break;
+    }
 
     return true;
 }
