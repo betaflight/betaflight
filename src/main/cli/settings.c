b389c2000dfc1eca4e56b365a5fafa91218ed26b
cvetaevvitaliy <vitaliy.nimych@gmail.com>
2025-05-26 13:00:41 +0300

    Implementation of the ICM456xx gyroscope driver (#14348)
    * add support ICM-45686, ICM45605 gyros
    
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>
    
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>
    
    * update PR issue
    
    * change IMU order
    
    * fix register AAF for Accel, update comments, configure LPF from config
    
    * change IMU order
    
    * fix access to IPREG_TOP1 registers, update define INT1_CONFIG2
    
    * add read Gyro&Acc via SPI DMA
    
    * coderabbitai bot suggestion
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix buf len
    
    * codestyle
    
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>
    
    * coderabbitai bot suggestion: SPI transfer, Inconsistent accelerometer scale setting
    
    * refactor read spi
    
    * update PR issue
    
    * revert "attempts remaining" for gyro detect
    
    * resolve issue
    
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>
    
    ---------
    
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>

--- src/main/cli/settings.c
+++ src/main/cli/settings.c
@@ -161,6 +161,8 @@ const char * const lookupTableAccHardware[] = {
     "LSM6DSO",
     "LSM6DSV16X",
     "IIM42653",
+    "ICM45605",
+    "ICM45686",
     "VIRTUAL"
 };
 
@@ -185,6 +187,8 @@ const char * const lookupTableGyroHardware[] = {
     "LSM6DSO",
     "LSM6DSV16X",
     "IIM42653",
+    "ICM45605",
+    "ICM45686",
     "VIRTUAL"
 };
 
