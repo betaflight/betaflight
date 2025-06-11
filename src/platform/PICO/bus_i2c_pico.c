c7d061041dc6cd6f550b04569711af5a8993c9ed
blckmn <blckmn@betaflight.com>
2025-06-12 04:56:04 +1000

    PICO: i2cHardwareConfigure renamed to i2cPinConfigure
    - possibly merge conflict introduced

--- src/platform/PICO/bus_i2c_pico.c
+++ src/platform/PICO/bus_i2c_pico.c
@@ -59,7 +59,7 @@ const i2cHardware_t i2cHardware[I2CDEV_COUNT] = {
 #endif
 };
 
-void i2cHardwareConfigure(const i2cConfig_t *i2cConfig)
+void i2cPinConfigure(const i2cConfig_t *i2cConfig)
 {
     for (int index = 0 ; index < I2CDEV_COUNT ; index++) {
         const i2cHardware_t *hardware = &i2cHardware[index];
