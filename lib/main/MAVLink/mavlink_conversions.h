c79cf6bcbf4e99f47f6a7f7f7ec5090f81c9d3b2
blckmn <blckmn@betaflight.com>
2025-06-13 07:22:16 +1000

    Moving to a named constant

--- lib/main/MAVLink/mavlink_conversions.h
+++ lib/main/MAVLink/mavlink_conversions.h
@@ -9,9 +9,9 @@
 #endif
 #include <math.h>
 
-#ifndef M_PI_2
-    #define M_PI_2 ((float)asin(1))
-#endif
+#define MAVLINK_PI_2 (1.570796f)
+#define MAVLINK_PI (3.14159265358979323846f)
+#define MAVLINK_ONE_THOUSANDTH (0.001f)
 
 /**
  * @file mavlink_conversions.h
@@ -70,12 +70,12 @@ MAVLINK_HELPER void mavlink_dcm_to_euler(const float dcm[3][3], float* roll, flo
     float phi, theta, psi;
     theta = asinf(-dcm[2][0]);
 
-    if (fabsf(theta - 1.570796f) < 1.0e-3f) {
+    if (fabsf(theta - MAVLINK_PI_2) < MAVLINK_ONE_THOUSANDTH) {
         phi = 0.0f;
         psi = (atan2f(dcm[1][2] - dcm[0][1],
                 dcm[0][2] + dcm[1][1]) + phi);
 
-    } else if (fabsf(theta + 1.570796f) < 1.0e-3f) {
+    } else if (fabsf(theta + MAVLINK_PI_2) < MAVLINK_ONE_THOUSANDTH) {
         phi = 0.0f;
         psi = atan2f(dcm[1][2] - dcm[0][1],
                   dcm[0][2] + dcm[1][1] - phi);
