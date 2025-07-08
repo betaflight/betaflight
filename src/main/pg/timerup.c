5bc7b097b30712953705d0541e1fe1fa512d3133
kedeng <degkxp@hotmail.com>
2025-07-08 12:46:28 +0800

    Improve timer macro consistency

--- src/main/pg/timerup.c
+++ src/main/pg/timerup.c
@@ -41,40 +41,40 @@ void pgResetFn_timerUpConfig(timerUpConfig_t *config)
         config[timno].dmaopt = DMA_OPT_UNUSED;
     }
 
-#if defined(TIMUP1_DMA_OPT) && (TIMUP_TIMERS & BIT(1))
+#if defined(TIMUP1_DMA_OPT) && (TIMUP_TIMERS & TIM_N(1))
     config[TIMER_INDEX(1)].dmaopt = TIMUP1_DMA_OPT;
 #endif
-#if defined(TIMUP2_DMA_OPT) && (TIMUP_TIMERS & BIT(2))
+#if defined(TIMUP2_DMA_OPT) && (TIMUP_TIMERS & TIM_N(2))
     config[TIMER_INDEX(2)].dmaopt = TIMUP2_DMA_OPT;
 #endif
-#if defined(TIMUP3_DMA_OPT) && (TIMUP_TIMERS & BIT(3))
+#if defined(TIMUP3_DMA_OPT) && (TIMUP_TIMERS & TIM_N(3))
     config[TIMER_INDEX(3)].dmaopt = TIMUP3_DMA_OPT;
 #endif
-#if defined(TIMUP4_DMA_OPT) && (TIMUP_TIMERS & BIT(4))
+#if defined(TIMUP4_DMA_OPT) && (TIMUP_TIMERS & TIM_N(4))
     config[TIMER_INDEX(4)].dmaopt = TIMUP4_DMA_OPT;
 #endif
-#if defined(TIMUP5_DMA_OPT) && (TIMUP_TIMERS & BIT(5))
+#if defined(TIMUP5_DMA_OPT) && (TIMUP_TIMERS & TIM_N(5))
     config[TIMER_INDEX(5)].dmaopt = TIMUP5_DMA_OPT;
 #endif
-#if defined(TIMUP6_DMA_OPT) && (TIMUP_TIMERS & BIT(6))
+#if defined(TIMUP6_DMA_OPT) && (TIMUP_TIMERS & TIM_N(6))
     config[TIMER_INDEX(6)].dmaopt = TIMUP6_DMA_OPT;
 #endif
-#if defined(TIMUP7_DMA_OPT) && (TIMUP_TIMERS & BIT(7))
+#if defined(TIMUP7_DMA_OPT) && (TIMUP_TIMERS & TIM_N(7))
     config[TIMER_INDEX(7)].dmaopt = TIMUP7_DMA_OPT;
 #endif
-#if defined(TIMUP8_DMA_OPT) && (TIMUP_TIMERS & BIT(8))
+#if defined(TIMUP8_DMA_OPT) && (TIMUP_TIMERS & TIM_N(8))
     config[TIMER_INDEX(8)].dmaopt = TIMUP8_DMA_OPT;
 #endif
-#if defined(TIMUP15_DMA_OPT) && (TIMUP_TIMERS & BIT(15))
+#if defined(TIMUP15_DMA_OPT) && (TIMUP_TIMERS & TIM_N(15))
     config[TIMER_INDEX(15)].dmaopt = TIMUP15_DMA_OPT;
 #endif
-#if defined(TIMUP16_DMA_OPT) && (TIMUP_TIMERS & BIT(16))
+#if defined(TIMUP16_DMA_OPT) && (TIMUP_TIMERS & TIM_N(16))
     config[TIMER_INDEX(16)].dmaopt = TIMUP16_DMA_OPT;
 #endif
-#if defined(TIMUP17_DMA_OPT) && (TIMUP_TIMERS & BIT(17))
+#if defined(TIMUP17_DMA_OPT) && (TIMUP_TIMERS & TIM_N(17))
     config[TIMER_INDEX(17)].dmaopt = TIMUP17_DMA_OPT;
 #endif
-#if defined(TIMUP20_DMA_OPT) && (TIMUP_TIMERS & BIT(20))
+#if defined(TIMUP20_DMA_OPT) && (TIMUP_TIMERS & TIM_N(20))
     config[TIMER_INDEX(20)].dmaopt = TIMUP20_DMA_OPT;
 #endif
 }
