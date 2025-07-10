81a1deddfc170d80e16b3d247118964511250646
blckmn <blckmn@betaflight.com>
2025-07-10 11:08:35 +1000

    Renamed files in common location (to avoid issues with naming similiarity)
    - reverted array structure as pio pulls data by bit, not whole words. may consider changing this to full size frame with logic for skipping byte depending on format inside PIO

--- src/platform/APM32/mk/APM32F4.mk
+++ src/platform/APM32/mk/APM32F4.mk
@@ -190,7 +190,7 @@ MCU_COMMON_SRC = \
         drivers/serial_escserial.c \
         drivers/serial_pinconfig.c \
         APM32/system_apm32f4xx.c \
-        common/stm32/ledstrip_ws2811.c
+        common/stm32/ledstrip_ws2811_stm32.c
 
 VCP_SRC = \
         APM32/usb/vcp/usbd_cdc_descriptor.c \
