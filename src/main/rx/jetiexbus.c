18463163c6697f27e3942901b86cdd7372b34a4d
Mark Haslinghuis <mark@numloq.nl>
2025-08-10 17:26:08 +0200

    Update src/main/rx/jetiexbus.c
    Co-authored-by: Petr Ledvina <ledvinap@gmail.com>

--- src/main/rx/jetiexbus.c
+++ src/main/rx/jetiexbus.c
@@ -112,7 +112,7 @@ static void jetiExBusDecodeChannelFrame(uint8_t *exBusFrame)
     uint16_t value;
     uint8_t frameAddr;
     uint8_t channelDataLen = exBusFrame[EXBUS_HEADER_SUBLEN];
-    uint8_t receivedChannelCount = MIN((channelDataLen) / 2, JETIEXBUS_CHANNEL_COUNT);
+    uint8_t receivedChannelCount = MIN(channelDataLen / 2, JETIEXBUS_CHANNEL_COUNT);
 
     // Decode header
     switch (((uint16_t)exBusFrame[EXBUS_HEADER_SYNC] << 8) | ((uint16_t)exBusFrame[EXBUS_HEADER_REQ])) {
