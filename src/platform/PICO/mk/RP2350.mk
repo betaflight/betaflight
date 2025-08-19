edeb5fac00747cd35042dd9ea5b5331c94594dfd
Matthew Selby <matthew_selby@yahoo.co.uk>
2025-08-19 19:40:50 +0100

    PICO add -fno-builtin-memcpy to guard against misalignment.
    Guard against misalignments in code that gcc generates for memcpy.

--- src/platform/PICO/mk/RP2350.mk
+++ src/platform/PICO/mk/RP2350.mk
@@ -238,7 +238,7 @@ SYS_INCLUDE_DIRS += \
             $(SDK_DIR)/rp2350/boot_stage2/include
 
 #Flags
-ARCH_FLAGS      = -mthumb -mcpu=cortex-m33 -march=armv8-m.main+fp+dsp -mcmse -mfloat-abi=softfp
+ARCH_FLAGS      = -fno-builtin-memcpy -mthumb -mcpu=cortex-m33 -march=armv8-m.main+fp+dsp -mcmse -mfloat-abi=softfp
 ARCH_FLAGS      += -DPICO_COPY_TO_RAM=$(RUN_FROM_RAM)
 
 # Automatically treating constants as single-precision breaks pico-sdk (-Werror=double-promotion)
