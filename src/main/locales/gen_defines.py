ae3d3974a4a8c67a0d075722c321df2973a91d54
HThuren <thuren.henrik@gmail.com>
2025-09-08 18:26:23 +0200

    On to rebase

--- src/main/locales/gen_defines.py
+++ src/main/locales/gen_defines.py
@@ -39,25 +39,22 @@ import xml.etree.ElementTree as ET
 hdKey = "_HD"
 
 def wr_bf_header(outFile, filename):
-    with open(filename, "r") as fr:
+    with open(filename, "r", encoding="utf-8") as fr:
         outFile.write(fr.read())
 
 def wr_bf_notice_bf(outFile, locale):
     outFile.write("/*\n")
-    outFile.write("\tNOTICE !\n\tNOTICE !\n\tNOTICE, this header file for LOCALE '" + locale + "' are generated from " + locale + "/bf_locale.xml\n")
+    outFile.write("\tNOTICE !\n\tNOTICE, this header file for LOCALE '" + locale + "' are generated from " + locale + "/bf_locale.xml\n")
     outFile.write("\tChanges to translation ie. "  + locale + "/bf_locale.h must be done in "  + locale + "/bf_locale.xml\n\n")
-    outFile.write("\tTo generate use 'make xx LOCALE=" + locale + "'")
-    outFile.write("\n\n")
-    outFile.write("\tWant to USE_EXTENDED_HD, ie. have #define TXT TR2( \"short\", \"long\"), x, y), \n")
-    outFile.write("\twrite string section for TXT with 'short' followed by string section TXT_HD with 'long'\n\n")
-    outFile.write("\tTo generate use 'make xx LOCALE=" + locale + " USE_HD_EXTENDED=1'\n")
+    outFile.write("\tTo enable TR2 (HD extended text), write TXT then TXT_HD and build with USE_HD_EXTENDED=1\n")
+    outFile.write("\tTo build use 'make xx LOCALE=" + locale + "'\n")
     outFile.write("*/\n\n")
 
 def wr_bf_notice_ut(outFile):
     outFile.write("/*\n")
-    outFile.write("\tNOTICE !\n\tNOTICE !\n\tNOTICE, this header file are generated from en/bf_locale.xml\n")
+    outFile.write("\tNOTICE !\n\tNOTICE, this header file are generated from en/bf_locale.xml\n")
     outFile.write("\tChanges to translation ie. en/bf_locale.h must be done in en/bf_locale.xml\n\n")
-    outFile.write("\tThis files handle defines not yet translated in some LOCALE, makeing possible to compile target\n")
+    outFile.write("\tThis files handle defines not yet translated in some LOCALE, making it possible to compile target\n")
     outFile.write("*/\n\n")
 
 def wr_define_bf(outFile, id, mes, desc, error):
@@ -65,6 +62,7 @@ def wr_define_bf(outFile, id, mes, desc, error):
         newline = "#define " + id + " " * max(1, 35 - len(id)) + mes + " " * max(1, 43 - len(mes)) + "\t// " + desc
     else:
         newline = id + ": " + error + " " * max(1, 35 - len(id)) + mes + " " * max(1, 43 - len(mes)) + "\t// " + desc + '\n'
+        sys.exit(1)
     print(newline, file=outFile)
 
 def wr_define_ut(outFile, id, mes, error):
@@ -77,9 +75,17 @@ if os.getenv('V1') == '@':
 else:
     verbose = True
 
+if len(sys.argv) < 4:
+    print("Usage: gen_defines.py TASK(BF|UT) <source_dir> <locale>", file=sys.stderr)
+    sys.exit(2)
 task   = sys.argv[1]
 source = sys.argv[2]
 locale = sys.argv[3]
+
+if task not in ("BF", "UT"):
+    print(f"Unknown TASK '{task}'; expected BF or UT", file=sys.stderr)
+    sys.exit(2)
+
 from_filename    = source + '/' + locale + '/bf_locale.xml'
 to_filename      = source + '/' + locale + '/bf_locale.h'
 untrans_filename = source + '/untranslated.h' 
@@ -115,7 +121,7 @@ for string_element in root.findall('string'):
 
     if len(message) > maxLen and maxLen > 0:
         errorMess = "ERROR - define " + currentKey + " with maxLength of " + str(maxLen) + " exceed with text: " + message + " / " + str(len(message))
-        print(errorMess)
+        print(errorMess, file=sys.stderr)
     else:
         errorMess = ""
 
