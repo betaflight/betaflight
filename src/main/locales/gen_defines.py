03f63e561b448ba8b73309ce88866d151f4d1c41
HThuren <thuren.henrik@gmail.com>
2025-09-08 20:02:22 +0200

    On to rebase

--- src/main/locales/gen_defines.py
+++ src/main/locales/gen_defines.py
@@ -45,9 +45,11 @@ def wr_bf_header(outFile, filename):
 def wr_bf_notice_bf(outFile, locale):
     outFile.write("/*\n")
     outFile.write("\tNOTICE!\n\tNOTICE: this header file for LOCALE '" + locale + "' is generated from " + locale + "/bf_locale.xml\n")
-    outFile.write("\tChanges to translations (i.e., "  + locale + "/bf_locale.h) must be made in "  + locale + "/bf_locale.xml\n\n")   outFile.write("\tTo enable TR2 (HD extended text), write TXT then TXT_HD and build with USE_HD_EXTENDED=1\n")
+    outFile.write("\tChanges to translations (i.e., "  + locale + "/bf_locale.h) must be made in "  + locale + "/bf_locale.xml\n\n")   
+    outFile.write("\tTo enable TR2 (HD extended text), write TXT then TXT_HD and build with USE_HD_EXTENDED=1\n")
     outFile.write("\tTo build use 'make <target> LOCALE=" + locale + "'\n")
     outFile.write("*/\n\n")
+    outFile.write("#pragma once\n\n")
 
 def wr_bf_notice_ut(outFile):
     outFile.write("/*\n")
@@ -55,6 +57,7 @@ def wr_bf_notice_ut(outFile):
     outFile.write("\tChanges to translation ie. en/bf_locale.h must be made in en/bf_locale.xml\n\n")
     outFile.write("\tThis file provides fallback defines for strings not yet translated in a locale, allowing targets to compile.\n")
     outFile.write("*/\n\n")
+    outFile.write("#pragma once\n\n")
 
 def wr_define_bf(outFile, id, mes, desc, error):
     if error == "":
