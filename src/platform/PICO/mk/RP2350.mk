e2ce96c09579e2039d3bdc87d45b27e4dff7228b
Matthew Selby <matthew_selby@yahoo.co.uk>
2025-09-15 00:17:16 +0100

    pico -fno-builtin-memcpy add comment

--- src/platform/PICO/mk/RP2350.mk
+++ src/platform/PICO/mk/RP2350.mk
@@ -238,8 +238,10 @@ SYS_INCLUDE_DIRS += \
             $(SDK_DIR)/rp2350/boot_stage2/include
 
 #Flags
-ARCH_FLAGS      = -fno-builtin-memcpy -mthumb -mcpu=cortex-m33 -march=armv8-m.main+fp+dsp -mcmse -mfloat-abi=softfp
+ARCH_FLAGS      = -mthumb -mcpu=cortex-m33 -march=armv8-m.main+fp+dsp -mcmse -mfloat-abi=softfp
 ARCH_FLAGS      += -DPICO_COPY_TO_RAM=$(RUN_FROM_RAM)
+# work around memcpy alignment issue
+ARCH_FLAGS      += -fno-builtin-memcpy
 
 # Automatically treating constants as single-precision breaks pico-sdk (-Werror=double-promotion)
 # We should go through BF code and explicitly declare constants as single rather than double as required,
