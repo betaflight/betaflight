ea8dff9892ee9b8acd575038b6e8adbc2585a883
Jay Blackman <blckmn@users.noreply.github.com>
2025-09-22 20:58:32 +1000

    Update config.mk

--- mk/config.mk
+++ mk/config.mk
@@ -62,16 +62,16 @@ endif #config
 
 .PHONY: configs
 configs:
-ifeq ($(shell realpath $(CONFIG_DIR)),$(shell realpath $(CONFIGS_SUBMODULE_DIR)))
+ifeq ($(shell realpath "$(CONFIG_DIR)"),$(shell realpath $(CONFIGS_SUBMODULE_DIR)))
 	@echo "Updating config submodule: $(CONFIGS_SUBMODULE_DIR)"
 	$(V1) git submodule update --init -- $(CONFIGS_SUBMODULE_DIR) || { echo "Config submodule update failed. Please check your git configuration."; exit 1; }
 	@echo "Submodule update succeeded."
 else
 ifeq ($(wildcard $(CONFIG_DIR)),)
 	@echo "Hydrating clone for configs: $(CONFIG_DIR)"
-	$(V1) git clone $(CONFIGS_REPO_URL) $(CONFIG_DIR)
+	$(V1) git clone $(CONFIGS_REPO_URL) "$(CONFIG_DIR)"
 else
-	$(V1) git -C $(CONFIG_DIR) pull origin
+	$(V1) git -C "$(CONFIG_DIR)" pull --ff-only origin
 endif
 endif
 
