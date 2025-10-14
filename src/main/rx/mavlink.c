5cb6b7c309645ee1c6e6adcfbf56b1edbf05a908
Vladimir Demidov <demvlad2@yandex.ru>
2025-10-14 21:17:59 +0300

    MAVLink serial RX providers improvement (#14702)
    * MAVLink serial rx reads data by callback function
    
    * Removed extra semicoma
    
    * Added missed break in the switch
    
    * The incomming data recv is removed from mavlink.c telemetry
    
    * The MAVLINK serial RX providers and the telemetry use shared serial port
    
    * Added telemetries send in serial RX MAVLink provider mode
    
    * Resolved Coderabbitai notices
    
    * Resolved Coderabbitai notices
    
    * Restored telemetry timing gate in Serial RX Mavlink mode
    
    * Telemetries send code refactoring
    
    * Added settings of mavlink telemetry baud rate
    
    * Added ATOMIC operation for txbuff_free variable
    
    * Added default mavlink_min_txbuff for builds without telemetry
    
    * Improved atomic operation by Coderabbitai
    
    * Added checking of mavlink telemetry to use castom baud rate settings
    
    * Added checking USE_TELEMETRY_MAVLINK in Serial RX MAVLink code
    
    * Flexible MAVLink telemetry flow control setup: the mavlink_txbuff_margin parameter is added. The mavlink_min_txbuff param is renamed to mavlink_txbuff_change
    
    * The mavlink_min_txbuff cli parameters is used to tune mavlink telemetry flow control
    
    * The actual heartbeat packet transmit rate is added into DEBUG
    
    * Resolved unused mavlinkSendBatteryStatus()  function issue
    
    * FIXME comment is added

--- src/main/rx/mavlink.c
+++ src/main/rx/mavlink.c
@@ -24,13 +24,41 @@
 #ifdef USE_SERIALRX_MAVLINK
 
 #include "common/utils.h"
+#include "common/maths.h"
+
+#include "io/serial.h"
 
 #include "rx/rx.h"
 #include "rx/mavlink.h"
 
+#ifdef USE_TELEMETRY
+#include "telemetry/telemetry.h"
+#endif
+
+#include "drivers/time.h"
+#include "drivers/nvic.h"
+
+#include "build/debug.h"
+
+// mavlink library uses unnames unions that's causes GCC to complain if -Wpedantic is used
+// until this is resolved in mavlink library - ignore -Wpedantic for mavlink code
+// FIXME
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#include "common/mavlink.h"
+#pragma GCC diagnostic pop
+
 #define MAVLINK_CHANNEL_COUNT 18
+#define MAVLINK_BAUD_RATE_INDEX BAUD_460800
 static uint16_t mavlinkChannelData[MAVLINK_CHANNEL_COUNT];
 static bool frameReceived;
+
+static serialPort_t *serialPort = NULL;
+
+static mavlink_message_t mavRecvMsg;
+static mavlink_status_t mavRecvStatus;
+static volatile uint8_t txbuff_free = 100;  // tx buffer space in %, start with empty buffer
+static volatile bool txbuff_valid = false;
 
 void mavlinkRxHandleMessage(const mavlink_rc_channels_override_t *msg) {
     const uint16_t *channelsPtr = (uint16_t*)&msg->chan1_raw;
@@ -58,20 +86,95 @@ static float mavlinkReadRawRC(const rxRuntimeState_t *rxRuntimeState, uint8_t ch
     return mavlinkChannelData[channel];
 }
 
+static bool handleIncoming_RC_CHANNELS_OVERRIDE(void) {
+    mavlink_rc_channels_override_t msg;
+    mavlink_msg_rc_channels_override_decode(&mavRecvMsg, &msg);
+    mavlinkRxHandleMessage(&msg);
+    return true;
+}
+
+// Get RADIO_STATUS data
+static void handleIncoming_RADIO_STATUS(void)
+{
+    mavlink_radio_status_t msg;
+    mavlink_msg_radio_status_decode(&mavRecvMsg, &msg);
+    txbuff_valid = true;
+    txbuff_free = msg.txbuf;
+    DEBUG_SET(DEBUG_MAVLINK_TELEMETRY, 1, txbuff_free); // Last known TX buffer free space
+}
+
+STATIC_UNIT_TESTED void mavlinkDataReceive(uint16_t c, void *data)
+{
+    rxRuntimeState_t *const rxRuntimeState = (rxRuntimeState_t *const)data;
+    uint8_t result = mavlink_parse_char(0, c, &mavRecvMsg, &mavRecvStatus);
+    if (result == MAVLINK_FRAMING_OK) {
+        switch (mavRecvMsg.msgid) {
+        case MAVLINK_MSG_ID_RC_CHANNELS_OVERRIDE:
+            handleIncoming_RC_CHANNELS_OVERRIDE();
+            rxRuntimeState->lastRcFrameTimeUs = micros();
+            break;
+        case MAVLINK_MSG_ID_RADIO_STATUS:
+            handleIncoming_RADIO_STATUS();
+            break;
+        }
+    }
+}
+
 bool mavlinkRxInit(const rxConfig_t *rxConfig, rxRuntimeState_t *rxRuntimeState)
 {
     frameReceived = false;
+    for (int i = 0; i < MAVLINK_CHANNEL_COUNT; ++i) {
+        mavlinkChannelData[i] = rxConfig->midrc;
+    }
 
     rxRuntimeState->channelData = mavlinkChannelData;
     rxRuntimeState->channelCount = MAVLINK_CHANNEL_COUNT;
     rxRuntimeState->rcReadRawFn = mavlinkReadRawRC;
     rxRuntimeState->rcFrameStatusFn = mavlinkFrameStatus;
 
-    for (int i = 0; i < MAVLINK_CHANNEL_COUNT; ++i) {
-        mavlinkChannelData[i] = rxConfig->midrc;;
+    const serialPortConfig_t *portConfig = findSerialPortConfig(FUNCTION_RX_SERIAL);
+    if (!portConfig) {
+        return false;
     }
 
-    return true;
+    baudRate_e baudRateIndex = portConfig->telemetry_baudrateIndex;
+    if (baudRateIndex == BAUD_AUTO || (portConfig->functionMask & FUNCTION_TELEMETRY_MAVLINK) == 0) {
+        // default rate for ELRS TX module
+        baudRateIndex = MAVLINK_BAUD_RATE_INDEX;
+    }
+
+    serialPort = openSerialPort(portConfig->identifier,
+        FUNCTION_RX_SERIAL,
+        mavlinkDataReceive,
+        rxRuntimeState,
+        baudRates[baudRateIndex],
+        MODE_RXTX,
+        (rxConfig->serialrx_inverted ? SERIAL_INVERTED : 0)
+    );
+#ifdef USE_TELEMETRY_MAVLINK
+    telemetrySharedPort = serialPort;
+#endif
+
+    return serialPort != NULL;
 }
 
+#ifdef USE_TELEMETRY_MAVLINK
+bool isValidMavlinkTxBuffer (void) {
+    return txbuff_valid;
+}
+
+bool shouldSendMavlinkTelemetry(void) {
+    const uint8_t mavlink_min_txbuff =  telemetryConfig()->mavlink_min_txbuff;
+    bool shouldSendTelemetry = false;
+
+    if (txbuff_valid) {
+        shouldSendTelemetry = txbuff_free > mavlink_min_txbuff;
+    }
+    DEBUG_SET(DEBUG_MAVLINK_TELEMETRY, 0, shouldSendTelemetry ? 1 : 0);
+
+    return shouldSendTelemetry;
+}
 #endif
+
+#endif
+
