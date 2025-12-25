fc61abf97f963f66e7d84cee07b9a422ba611f77
blckmn <blckmn@betaflight.com>
2025-12-26 08:46:06 +1100

    POST RELEASE: Setting up for 4.5.4 (if required)

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -26,7 +26,7 @@
 #define FC_FIRMWARE_IDENTIFIER      "BTFL"
 #define FC_VERSION_MAJOR            4  // increment when a major release is made (big new feature, etc)
 #define FC_VERSION_MINOR            5  // increment when a minor release is made (small new feature, change etc)
-#define FC_VERSION_PATCH_LEVEL      3  // increment when a bug is fixed
+#define FC_VERSION_PATCH_LEVEL      4  // increment when a bug is fixed
 
 #define FC_VERSION_STRING STR(FC_VERSION_MAJOR) "." STR(FC_VERSION_MINOR) "." STR(FC_VERSION_PATCH_LEVEL)
 
