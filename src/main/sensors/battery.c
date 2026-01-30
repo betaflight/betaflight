f824605b384b64f88a11967e177eb54ea28d0579
Michael De Backer <github-michaeldebacker@viditis.be>
2026-01-30 04:28:30 +0100

    Prevent division by zero
    Prevent division by zero if batteryVoltageMax < batteryVoltageMin

--- src/main/sensors/battery.c
+++ src/main/sensors/battery.c
@@ -501,10 +501,18 @@ uint8_t calculateBatteryPercentageRemaining(void)
         if (batteryCapacity > 0) {
             batteryPercentage = constrain(((float)batteryCapacity - currentMeter.mAhDrawn) * 100 / batteryCapacity, 0, 100);
         } else {
-            uint32_t batteryVoltage = (uint32_t)voltageMeter.displayFiltered;
+            uint32_t batteryVoltage = voltageMeter.displayFiltered;
             uint32_t batteryVoltageMin = batteryConfig()->vbatmincellvoltage * batteryCellCount;
             uint32_t batteryVoltageMax = batteryConfig()->vbatmaxcellvoltage * batteryCellCount;
-            batteryPercentage = (batteryVoltage <= batteryVoltageMin) ? 0 : (batteryVoltage >= batteryVoltageMax) ? 100 : (uint8_t)(((batteryVoltage - batteryVoltageMin) * 100) / (batteryVoltageMax - batteryVoltageMin));
+            if (batteryVoltage <= batteryVoltageMin) {
+                batteryPercentage = 0;
+            } else if (batteryVoltage >= batteryVoltageMax) {
+                batteryPercentage = 100;
+            } else if (batteryVoltageMax > batteryVoltageMin) {
+                batteryPercentage = (uint8_t)(((batteryVoltage - batteryVoltageMin) * 100) / (batteryVoltageMax - batteryVoltageMin));
+            } else {
+                batteryPercentage = 0;
+            }
         }
     }
 
