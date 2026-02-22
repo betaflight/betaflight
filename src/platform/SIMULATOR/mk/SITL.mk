522978e5431ca583a6fdaae87ca4096d779e576c
blckmn <blckmn@betaflight.com>
2026-02-22 17:05:11 +1100

    SITL: add -z,noexecstack linker flag to suppress RWX segment warning

--- src/platform/SIMULATOR/mk/SITL.mk
+++ src/platform/SIMULATOR/mk/SITL.mk
@@ -45,6 +45,7 @@ LD_FLAGS    := \
             -Wl,-gc-sections,-Map,$(TARGET_MAP) \
             -Wl,-L$(LINKER_DIR) \
             -Wl,--cref \
+            -Wl,-z,noexecstack \
             -T$(LD_SCRIPT)
 
 ifneq ($(filter SITL_STATIC,$(OPTIONS)),)
