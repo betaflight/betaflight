7d1a21b44bb6f10705b5e09af2e362ee0a0bde5a
blckmn <blckmn@betaflight.com>
2026-03-16 06:36:42 +1100

    Replace F7 platform guard for SERIAL_CHECK_TX with ENABLE_SERIAL_SKIP_CHECK_TX macro

--- src/main/target/common_post.h
+++ src/main/target/common_post.h
@@ -714,3 +714,7 @@ extern struct linker_symbol __config_end;
  #endif
 
 ******************************************************/
+
+#if !defined(ENABLE_SERIAL_SKIP_CHECK_TX)
+#define ENABLE_SERIAL_SKIP_CHECK_TX 0
+#endif
