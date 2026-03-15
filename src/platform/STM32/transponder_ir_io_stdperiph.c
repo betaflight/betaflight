58f722ad2c9713655e4b3f2a7c91008543411361
blckmn <blckmn@betaflight.com>
2026-03-15 19:46:52 +1100

    Refactor transponder IR DMA buffer type to uint32_t

--- src/platform/STM32/transponder_ir_io_stdperiph.c
+++ src/platform/STM32/transponder_ir_io_stdperiph.c
@@ -201,7 +201,7 @@ bool transponderIrInit(const ioTag_t ioTag, const transponderProvider_e provider
     return true;
 }
 
-bool isTransponderIrReady(void)
+bool transponderIsIrReady(void)
 {
     return !transponderIrDataTransferInProgress;
 }
