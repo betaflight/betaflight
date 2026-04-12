012cfb7509ae2a490a1f9d8bac69ee5efad82698
druckgott <forum@coolpixx.de>
2026-04-12 13:14:53 +0200

    Update gps_rescue_multirotor.c
    add position hold to rescue
    
    Update gps_rescue_multirotor.c
    
    Update gps_rescue_multirotor.c
    
    Update gps_rescue_multirotor.c

--- src/main/flight/gps_rescue_multirotor.c
+++ src/main/flight/gps_rescue_multirotor.c
@@ -784,6 +784,9 @@ void gpsRescueUpdate(void)
                 // otherwise behave as for a normal rescue
                 initialiseRescueValues ();
                 returnAltitudeLow = getAltitudeCmControl() < rescueState.intent.returnAltitudeCm;
+                if (positionEstimatorIsValidXY())
+                    resetPositionControl(TASK_GPS_RESCUE_RATE_HZ);
+                }
                 rescueState.phase = RESCUE_ATTAIN_ALT;
             }
         }
@@ -811,6 +814,14 @@ void gpsRescueUpdate(void)
 
         // give velocity P and I no error that otherwise could be present due to velocity drift at the start of the rescue
         rescueState.intent.targetVelocityCmS = rescueState.sensor.velocityToHomeCmS;
+        if (positionEstimatorIsValidXY())
+            positionControl();
+            gpsRescueAngle[AI_PITCH] = autopilotAngle[AI_PITCH];
+            gpsRescueAngle[AI_ROLL]  = autopilotAngle[AI_ROLL];
+        } else {
+            // Work like bevor without position hold
+            resetPositionControl(POSHOLD_TASK_RATE_HZ);
+        }
         break;
 
     case RESCUE_ROTATE:
