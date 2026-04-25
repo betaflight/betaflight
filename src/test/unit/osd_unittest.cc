196cff1b005428baef427755289aefa94944fa59
Michael De Backer <github-michaeldebacker@viditis.be>
2026-04-26 00:24:33 +0200

    Change SYM_PB_EMPTY

--- src/test/unit/osd_unittest.cc
+++ src/test/unit/osd_unittest.cc
@@ -1378,7 +1378,7 @@ TEST_F(OsdTest, TestBatteryUsage_CapacityZero)
         SYM_PB_FULL,
         SYM_PB_FULL,
         SYM_PB_FULL,
-        SYM_PB_EMPTY,
+        SYM_PB_FULL,
         SYM_PB_EMPTY,
         SYM_PB_EMPTY,
         SYM_PB_EMPTY,
