2437318e1e4570bf3d578ff6dec71e21a92a0159
Michael De Backer <github-michaeldebacker@viditis.be>
2026-04-25 02:22:50 +0200

    Reorder osdAnalyzeActiveElements and displayClearScreen calls

--- src/test/unit/osd_unittest.cc
+++ src/test/unit/osd_unittest.cc
@@ -1303,15 +1303,14 @@ TEST_F(OsdTest, TestBatteryUsage_Percentage_Fallback)
     batteryProfilesMutable(0)->batteryCapacity = 0;
     currentBatteryProfile = batteryProfiles(0);
 
-    osdAnalyzeActiveElements();
-
     // TYPE 4
     osdElementConfigMutable()->item_pos[OSD_MAIN_BATT_USAGE] =
         OSD_POS(2, 1) | OSD_PROFILE_1_FLAG | (OSD_ELEMENT_TYPE_4 << 14);
 
-    displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
+    osdAnalyzeActiveElements();
 
     simulationBatteryPercentage = 0;
+    displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
     osdRefresh();
     displayPortTestBufferSubstring(2, 1, "%c0%%", SYM_MAH);
 
@@ -1328,6 +1327,8 @@ TEST_F(OsdTest, TestBatteryUsage_Percentage_Fallback)
     // TYPE 3
     osdElementConfigMutable()->item_pos[OSD_MAIN_BATT_USAGE] =
         OSD_POS(2, 1) | OSD_PROFILE_1_FLAG | (OSD_ELEMENT_TYPE_3 << 14);
+
+    osdAnalyzeActiveElements();
 
     simulationBatteryPercentage = 0;
     displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
