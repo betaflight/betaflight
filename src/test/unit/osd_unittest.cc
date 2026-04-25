e2c5ae62927d45ac9213795eeed582ad26b0760b
Michael De Backer <github-michaeldebacker@viditis.be>
2026-04-25 04:03:10 +0200

    Adjust battery usage display positions in unit tests
    Updated battery usage display positions for test cases.

--- src/test/unit/osd_unittest.cc
+++ src/test/unit/osd_unittest.cc
@@ -1303,28 +1303,28 @@ TEST_F(OsdTest, TestBatteryUsage_Percentage_Fallback)
     batteryProfilesMutable(0)->batteryCapacity = 0;
     currentBatteryProfile = batteryProfiles(0);
 
+    // TYPE 4 (used %)
     osdElementConfigMutable()->item_pos[OSD_MAIN_BATT_USAGE] =
         OSD_POS(2, 1) | OSD_PROFILE_1_FLAG | (OSD_ELEMENT_TYPE_4 << 14);
 
     osdAnalyzeActiveElements();
 
-    // TYPE 4
     simulationBatteryPercentage = 0;
     displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
     osdRefresh();
-    displayPortTestBufferSubstring(3, 1, "%c0%%", SYM_MAH);
+    displayPortTestBufferSubstring(2, 1, "%c0%%", SYM_MAH);
 
     simulationBatteryPercentage = 50;
     displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
     osdRefresh();
-    displayPortTestBufferSubstring(3, 1, "%c50%%", SYM_MAH);
+    displayPortTestBufferSubstring(2, 1, "%c50%%", SYM_MAH);
 
     simulationBatteryPercentage = 100;
     displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
     osdRefresh();
-    displayPortTestBufferSubstring(3, 1, "%c100%%", SYM_MAH);
+    displayPortTestBufferSubstring(2, 1, "%c100%%", SYM_MAH);
 
-    // TYPE 3
+    // TYPE 3 (remaining %)
     osdElementConfigMutable()->item_pos[OSD_MAIN_BATT_USAGE] =
         OSD_POS(2, 1) | OSD_PROFILE_1_FLAG | (OSD_ELEMENT_TYPE_3 << 14);
 
@@ -1333,17 +1333,17 @@ TEST_F(OsdTest, TestBatteryUsage_Percentage_Fallback)
     simulationBatteryPercentage = 0;
     displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
     osdRefresh();
-    displayPortTestBufferSubstring(3, 1, "%c100%%", SYM_MAH);
+    displayPortTestBufferSubstring(2, 1, "%c100%%", SYM_MAH);
 
     simulationBatteryPercentage = 50;
     displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
     osdRefresh();
-    displayPortTestBufferSubstring(3, 1, "%c50%%", SYM_MAH);
+    displayPortTestBufferSubstring(2, 1, "%c50%%", SYM_MAH);
 
     simulationBatteryPercentage = 100;
     displayClearScreen(&testDisplayPort, DISPLAY_CLEAR_WAIT);
     osdRefresh();
-    displayPortTestBufferSubstring(3, 1, "%c0%%", SYM_MAH);
+    displayPortTestBufferSubstring(2, 1, "%c0%%", SYM_MAH);
 }
 
 // STUBS
