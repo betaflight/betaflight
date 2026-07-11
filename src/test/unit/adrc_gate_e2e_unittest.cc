799ff89e606b934bda2659ef9f41c404eb420cd4
danusha2345 <danusha2345@users.noreply.github.com>
2026-07-11 16:26:31 +0300

    test(adrc): cover gate feedback epochs end to end

---
+++ src/test/unit/adrc_gate_e2e_unittest.cc
@@ -0,0 +1,144 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software and/or
+ * modify this software under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Betaflight is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Betaflight. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cmath>
+
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "common/axis.h"
+
+#include "fc/runtime_config.h"
+
+#include "flight/adrc.h"
+#include "flight/pid.h"
+#include "flight/pid_init.h"
+
+#include "sensors/gyro.h"
+}
+
+// The focused target links pid_unittest.cc to reuse its established Betaflight platform stubs.
+extern float simulatedThrottle;
+extern float simulatedSetpointRate[XYZ_AXIS_COUNT];
+extern pidProfile_t *pidProfile;
+
+void resetTest(void);
+timeUs_t currentTestTime(void);
+
+namespace {
+
+constexpr float TEST_AUTHORITY_SCALE = 1.0f;
+constexpr float TEST_GROUND_SETPOINT_DPS = 300.0f;
+
+void prepareAdrcFeedbackLoop(const uint16_t rearmHoldMs)
+{
+    resetTest();
+
+    pidProfile->pid_type = PID_TYPE_ADRC;
+    pidProfile->adrc.liftoffIdleHoldMs = rearmHoldMs;
+    pidInitConfig(pidProfile);
+
+    ENABLE_ARMING_FLAG(ARMED);
+    pidStabilisationState(PID_STABILISATION_ON);
+
+    simulatedThrottle = 0.0f;
+    for (int axis = FD_ROLL; axis <= FD_YAW; ++axis) {
+        simulatedSetpointRate[axis] = 0.0f;
+        gyro.gyroADCf[axis] = 0.0f;
+        adrcResetState(&pidRuntime.adrc, axis);
+    }
+    adrcResetGate(&pidRuntime.adrc);
+}
+
+float publishGroundConstrainedCommand(void)
+{
+    simulatedSetpointRate[FD_ROLL] = TEST_GROUND_SETPOINT_DPS;
+    pidController(pidProfile, currentTestTime());
+    EXPECT_FALSE(pidRuntime.adrc.liftoff);
+    EXPECT_GT(std::fabs(pidData[FD_ROLL].Sum), 1.0f);
+
+    pidUpdateAdrcAppliedOutput(pidProfile, TEST_AUTHORITY_SCALE, pidProfile->pidSumLimitYaw);
+    const float staleOutput = pidRuntime.adrc.lastOutput[FD_ROLL];
+    EXPECT_GT(std::fabs(staleOutput), 1.0f);
+    return staleOutput;
+}
+
+void isolateGroundEpochFeedback(const float staleOutput)
+{
+    simulatedSetpointRate[FD_ROLL] = 0.0f;
+    for (int axis = FD_ROLL; axis <= FD_YAW; ++axis) {
+        adrcResetState(&pidRuntime.adrc, axis);
+    }
+    pidRuntime.adrc.lastOutput[FD_ROLL] = staleOutput;
+}
+
+void expectNeutralOpenAndNextObserverLoop(void)
+{
+    simulatedThrottle = 0.6f;
+    pidController(pidProfile, currentTestTime());
+
+    ASSERT_TRUE(pidRuntime.adrc.liftoff);
+    EXPECT_FLOAT_EQ(0.0f, pidRuntime.adrc.lastOutput[FD_ROLL]);
+    EXPECT_FLOAT_EQ(0.0f, pidRuntime.adrc.z2[FD_ROLL]);
+    EXPECT_FLOAT_EQ(0.0f, pidData[FD_ROLL].P);
+    EXPECT_FLOAT_EQ(0.0f, pidData[FD_ROLL].I);
+    EXPECT_FLOAT_EQ(0.0f, pidData[FD_ROLL].D);
+    EXPECT_FLOAT_EQ(0.0f, pidData[FD_ROLL].Sum);
+
+    // This is the real mixer-to-observer handoff. mixTable() computes the authority scale and
+    // calls this function after applying its constraints; the observer consumes it one loop later.
+    pidUpdateAdrcAppliedOutput(pidProfile, TEST_AUTHORITY_SCALE, pidProfile->pidSumLimitYaw);
+    EXPECT_FLOAT_EQ(0.0f, pidRuntime.adrc.lastOutput[FD_ROLL]);
+
+    pidController(pidProfile, currentTestTime());
+    EXPECT_TRUE(pidRuntime.adrc.liftoff);
+    EXPECT_FLOAT_EQ(0.0f, pidRuntime.adrc.z2[FD_ROLL]);
+    EXPECT_FLOAT_EQ(0.0f, pidData[FD_ROLL].D);
+    EXPECT_FLOAT_EQ(0.0f, pidData[FD_ROLL].Sum);
+}
+
+TEST(AdrcGateE2eTest, FirstOpenDropsGroundMixerFeedbackBeforeObserverUsesIt)
+{
+    prepareAdrcFeedbackLoop(0);
+
+    const float staleOutput = publishGroundConstrainedCommand();
+    isolateGroundEpochFeedback(staleOutput);
+    expectNeutralOpenAndNextObserverLoop();
+}
+
+TEST(AdrcGateE2eTest, OptInRearmStartsAnotherCleanFeedbackEpoch)
+{
+    prepareAdrcFeedbackLoop(100);
+
+    simulatedThrottle = 0.6f;
+    pidController(pidProfile, currentTestTime());
+    ASSERT_TRUE(pidRuntime.adrc.liftoff);
+    pidUpdateAdrcAppliedOutput(pidProfile, TEST_AUTHORITY_SCALE, pidProfile->pidSumLimitYaw);
+
+    simulatedThrottle = 0.0f;
+    for (int loop = 0; loop < 20 && pidRuntime.adrc.liftoff; ++loop) {
+        pidController(pidProfile, currentTestTime());
+        pidUpdateAdrcAppliedOutput(pidProfile, TEST_AUTHORITY_SCALE, pidProfile->pidSumLimitYaw);
+    }
+    ASSERT_FALSE(pidRuntime.adrc.liftoff);
+
+    const float staleOutput = publishGroundConstrainedCommand();
+    isolateGroundEpochFeedback(staleOutput);
+    expectNeutralOpenAndNextObserverLoop();
+}
+
+} // namespace
