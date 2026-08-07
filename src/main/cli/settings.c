ee4848c14b4059958fecfff4cef94ef82743a8e1
Vladimir Demidov <demvlad2@yandex.ru>
2026-08-07 11:58:30 +0300

    Added alternative hyperbolic curves for WINGs in advanced TPA mode. The curve goes through given reference speed, what has optimal plane settings

--- src/main/cli/settings.c
+++ src/main/cli/settings.c
@@ -561,7 +561,7 @@ static const char* const lookupTableFreqDomain[] = {
 
 #ifdef USE_ADVANCED_TPA
 const char* const lookupTableTpaCurveType[] = {
-    "CLASSIC", "HYPERBOLIC",
+    "CLASSIC", "HYPERBOLIC", "VREF",
 };
 #endif
 
@@ -1396,6 +1396,10 @@ const clivalue_t valueTable[] = {
     { PARAM_NAME_TPA_CURVE_PID_THR0, VAR_UINT16  | PROFILE_VALUE, .config.minmaxUnsigned = { 0, TPA_CURVE_PID_MAX}, PG_PID_PROFILE, offsetof(pidProfile_t, tpa_curve_pid_thr0) },
     { PARAM_NAME_TPA_CURVE_PID_THR100, VAR_UINT16  | PROFILE_VALUE, .config.minmaxUnsigned = { 0, TPA_CURVE_PID_MAX}, PG_PID_PROFILE, offsetof(pidProfile_t, tpa_curve_pid_thr100) },
     { PARAM_NAME_TPA_CURVE_EXPO, VAR_INT8  | PROFILE_VALUE, .config.minmaxUnsigned = { TPA_CURVE_EXPO_MIN, TPA_CURVE_EXPO_MAX}, PG_PID_PROFILE, offsetof(pidProfile_t, tpa_curve_expo) },
+    { PARAM_NAME_SPEED_CURVE_VREF, VAR_UINT8 | PROFILE_VALUE, .config.minmaxUnsigned = { 0, UINT8_MAX }, PG_PID_PROFILE, offsetof(pidProfile_t, speed_curve_vref) },
+    { PARAM_NAME_SPEED_CURVE_POWER, VAR_UINT8 | PROFILE_VALUE, .config.minmaxUnsigned = { 5, 30 }, PG_PID_PROFILE, offsetof(pidProfile_t, speed_curve_power) },
+    { PARAM_NAME_SPEED_CURVE_MIN, VAR_UINT16 | PROFILE_VALUE, .config.minmaxUnsigned = { 1, 100 }, PG_PID_PROFILE, offsetof(pidProfile_t, speed_curve_min) },
+    { PARAM_NAME_SPEED_CURVE_MAX, VAR_UINT16 | PROFILE_VALUE, .config.minmaxUnsigned = { 100, 500 }, PG_PID_PROFILE, offsetof(pidProfile_t, speed_curve_max) },
 #endif // USE_ADVANCED_TPA
 
     { PARAM_NAME_EZ_LANDING_THRESHOLD,      VAR_UINT8  | PROFILE_VALUE, .config.minmaxUnsigned = { 0, 200 }, PG_PID_PROFILE, offsetof(pidProfile_t, ez_landing_threshold) },
--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -239,6 +239,10 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .tpa_curve_pid_thr0 = 200,
         .tpa_curve_pid_thr100 = 70,
         .tpa_curve_expo = 20,
+        .speed_curve_vref = 20,      // Reference speed value which has optimal plane settins m/s
+        .speed_curve_power = 20,     // Hyperbolic curves power *0.1
+        .speed_curve_min = 20,       // Hyperbolic curves minimum *0.01
+        .speed_curve_max = 200,      // Hyperbolic curves maximum *0.01
         .tpa_speed_type = TPA_SPEED_BASIC,
         .tpa_speed_basic_delay = 1000,
         .tpa_speed_basic_gravity = 50,
@@ -436,6 +440,9 @@ void pidUpdateTpaFactor(float throttle)
     switch (pidRuntime.tpaCurveType) {
     case TPA_CURVE_HYPERBOLIC:
         tpaFactor = pwlInterpolate(&pidRuntime.tpaCurvePwl, tpaArgument);
+        break;
+    case TPA_CURVE_VREF:
+        tpaFactor = pwlInterpolate(&pidRuntime.tpaCurvePwl, pidRuntime.tpaSpeed.refSpeed / MIN(pidRuntime.tpaSpeed.speed, 1.0f));
         break;
     case TPA_CURVE_CLASSIC:
     default:
