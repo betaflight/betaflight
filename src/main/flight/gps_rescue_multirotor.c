48396171127197a5f0ae21207ba2ec6be2bd61bf
ctzsnooze <chris@th.id.au>
2026-08-15 10:38:29 +1000

    initPositionHold, not reset, at the start of a rescue

--- src/main/flight/gps_rescue_multirotor.c
+++ src/main/flight/gps_rescue_multirotor.c
@@ -554,8 +554,7 @@ void initRescueValues(void)
     rescueState.intent.xyAttenuator = 0.0f;        // For a slower start to gaining velocity
 
     resetAltitudeControl(); // Initialise altitude in autopilot multirotor
-    resetPositionControl(TASK_GPS_RESCUE_RATE_HZ); // Initialise position control in autopilot multirotor
-
+initPositionHold(); // Initialise position hold at current location 
 }
 #endif // !ENABLE_RESCUE_PLAN
 
