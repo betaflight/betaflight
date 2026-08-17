59dabd1560f1c1a9817c5ae57bf30e5ee001b698
Sergey Tsypanov <sergei.tsypanov@yandex.ru>
2026-08-17 15:32:59 +0300

    Don't include NMEA if USE_GPS_NMEA is not defined

--- src/main/fc/parameter_names.h
+++ src/main/fc/parameter_names.h
@@ -263,7 +263,9 @@
 #define PARAM_NAME_GPS_UBLOX_ENABLE_ANA "gps_ublox_enable_ana"
 #define PARAM_NAME_GPS_SET_HOME_POINT_ONCE "gps_set_home_point_once"
 #define PARAM_NAME_GPS_USE_3D_SPEED "gps_use_3d_speed"
+#ifdef USE_GPS_NMEA
 #define PARAM_NAME_GPS_NMEA_CUSTOM_COMMANDS "gps_nmea_custom_commands"
+#endif
 #define PARAM_NAME_GPS_UPDATE_RATE_HZ "gps_update_rate_hz"
 
 #ifdef USE_GPS_RESCUE
