2dc9392b6da79d3c31eb8ee599fb7d5508db1c45
kensherman <ken.b.sherman@gmail.com>
2026-08-31 19:55:09 -0500

    setting velD

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -3964,7 +3964,7 @@ RAM_CODE static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t
         gpsSol.velned.velN = ned_vel_north;
         gpsSol.velned.velE = ned_vel_east;
         gpsSol.groundSpeed = (uint16_t)sqrtf((ned_vel_north * ned_vel_north) + (ned_vel_east * ned_vel_east));
-        (void)sbufReadU32(src);             // ned_vel_down
+        gpsSol.velned.velD = (int32_t)sbufReadU32(src);             // ned_vel_down
         gpsSol.groundCourse = ((uint16_t)sbufReadU16(src) % 36000) / 10; // incoming value expected to be in centidegrees, output value in decidegrees
         (void)sbufReadU16(src);             // true_yaw
         (void)sbufReadU16(src);             // year
