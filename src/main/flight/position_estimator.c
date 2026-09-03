a184f2031ce901b13338a03583259e9741b97917
ctzsnooze <chris@th.id.au>
2026-09-03 18:54:57 +1000

    set baro R to 70  down from 100

--- src/main/flight/position_estimator.c
+++ src/main/flight/position_estimator.c
@@ -91,9 +91,9 @@
 
 #define R_OPTICALFLOW_VEL 200.0f   // (cm/s)^2 at max quality
 
-#define Q_JERK_Z          3000.0f
-#define R_ACCEL_Z          500.0f
-#define R_BARO_ALT        100.0f   // cm^2 lower value favours baro data vs others, reduced by higher user prefer baro values
+#define Q_JERK_Z         3000.0f
+#define R_ACCEL_Z         500.0f
+#define R_BARO_ALT         70.0f   // cm^2 lower value favours baro data vs others, increased by lower user prefer baro values
 #define R_GPS_VEL_Z_BASE  100.0f   // (cm/s)^2, increases as sAcc increases to allow more baro / accelerometer influence
 #define R_GPS_ALT_BASE    200.0f   // cm^2 , increases as vAcc increases to allow more baro / accel influence
 #define R_RANGEFINDER_ALT 100.0f   // cm^2
