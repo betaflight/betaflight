1793d993eaa8305c164eaf038d9ba6b47096ca01
ctzsnooze <chris@th.id.au>
2026-09-05 23:11:54 +1000

    fix whitespace

--- src/main/sensors/barometer.c
+++ src/main/sensors/barometer.c
@@ -543,10 +543,9 @@ uint32_t baroUpdate(timeUs_t currentTimeUs)
                     baro.altitude = 0.0f;
                 }
             }
-                DEBUG_SET(DEBUG_BARO, 1, lrintf(baro.pressure / 100.0f)); // hPa
-                DEBUG_SET(DEBUG_BARO, 2, baro.temperature);               // c°C
-                DEBUG_SET(DEBUG_BARO, 3, lrintf(baro.altitude));          // cm, with temp compensation if not virtual
-
+            DEBUG_SET(DEBUG_BARO, 1, lrintf(baro.pressure / 100.0f)); // hPa
+            DEBUG_SET(DEBUG_BARO, 2, baro.temperature);               // c°C
+            DEBUG_SET(DEBUG_BARO, 3, lrintf(baro.altitude));          // cm, with temp compensation if not virtual
 
             if (baro.dev.combined_read) {
                 state = BARO_STATE_PRESSURE_START;
